79447bb759875ab5cb87d991c8ea9216cd3dca73
Melis Akman <melisakman@airbears2-10-142-119-84.airbears2.1918.berkeley.edu>
2016-06-03 09:53:10 -0700

    XRQ database

---
+++ shells/SNPeff_database_XRQ.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/vector/instrumentData/blackmanlab/snpEff
+#SBATCH -J snpEff
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000
+#SBATCH --time=4:00:00
+#SBATCH -o /global/home/users/makman/snpEff/outs/snpEff_database_XRQ.out
+#SBATCH -e /global/home/users/makman/snpEff/outs/snpEff_database_XRQ.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load java
+srun java -jar /clusterfs/vector/instrumentData/blackmanlab/snpEff/snpEff.jar build -gff3 -v XRQ
