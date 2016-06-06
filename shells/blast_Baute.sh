ddd97b2ba4546a3f70292efe62b6a3e52e5d518d
Melis Akman <melisakman@airbears2-10-142-117-144.airbears2.1918.berkeley.edu>
2016-06-06 16:16:19 -0700

    blast

---
+++ shells/blast_Baute.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+#SBATCH -D /global/users/makman/blast/
+#SBATCH -J blast
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000
+#SBATCH --time=48:00:00
+#SBATCH -o /global/home/users/makman/blast/outs/Baute_SNPs.out
+#SBATCH -e /global/home/users/makman/blast/outs/BAute_SNPs.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --array=1-3
+module load blast/2.2.30 
+srun blastn -query SNPs_Baute.fa -db /clusterfs/vector/instrumentData/blackmanlab/blast_databases/Hulke -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 -template_type coding -template_length 18 -window_size 25 -outfmt "7 qseqid qlen sseqid slen evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/users/makman/blast/Baute2Hulke.csv
+srun blastn -query SNPs_Baute.fa -db /clusterfs/vector/instrumentData/blackmanlab/blast_databases/HA412 -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 -template_type coding -template_length 18 -window_size 25 -outfmt "7 qseqid qlen sseqid slen evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/users/makman/blast/Baute2HA412.csv
+srun blastn -query SNPs_Baute.fa -db /clusterfs/vector/instrumentData/blackmanlab/blast_databases/XRQ -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 -template_type coding -template_length 18 -window_size 25 -outfmt "7 qseqid qlen sseqid slen evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/users/makman/blast/Baute2XRQ.csv
+
+
