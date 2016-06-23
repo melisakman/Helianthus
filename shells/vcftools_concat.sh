f7e01bcd6523c69f187f4c49df5dfe09d5d9e7e5
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2016-06-22 18:48:50 -0700

    gzip

--- shells/vcftools_concat.sh
+++ shells/vcftools_concat.sh
@@ -15,21 +15,21 @@
 module load perl
 module load vcftools/0.1.13
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
-srun /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat chrom1_cultivated_filtered_ordered.vcf \
-chrom2_cultivated_filtered_ordered.vcf \
-chrom3_cultivated_filtered_ordered.vcf \
-chrom4_cultivated_filtered_ordered.vcf \
-chrom5_cultivated_filtered_ordered.vcf \
-chrom6_cultivated_filtered_ordered.vcf \
-chrom7_cultivated_filtered_ordered.vcf \
-chrom8_cultivated_filtered_ordered.vcf \
-chrom9_cultivated_filtered_ordered.vcf \
-chrom10_cultivated_filtered_ordered.vcf \
-chrom11_cultivated_filtered_ordered.vcf \
-chrom12_cultivated_filtered_ordered.vcf \
-chrom13_cultivated_filtered_ordered.vcf \
-chrom14_cultivated_filtered_ordered.vcf \
-chrom15_cultivated_filtered_ordered.vcf \
-chrom16_cultivated_filtered_ordered.vcf \
-chrom17_cultivated_filtered_ordered.vcf \
+srun /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat chrom1_cultivated_filtered_ordered.vcf.gz \
+chrom2_cultivated_filtered_ordered.vcf.gz \
+chrom3_cultivated_filtered_ordered.vcf.gz \
+chrom4_cultivated_filtered_ordered.vcf.gz \
+chrom5_cultivated_filtered_ordered.vcf.gz \
+chrom6_cultivated_filtered_ordered.vcf.gz \
+chrom7_cultivated_filtered_ordered.vcf.gz \
+chrom8_cultivated_filtered_ordered.vcf.gz \
+chrom9_cultivated_filtered_ordered.vcf.gz \
+chrom10_cultivated_filtered_ordered.vcf.gz \
+chrom11_cultivated_filtered_ordered.vcf.gz \
+chrom12_cultivated_filtered_ordered.vcf.gz \
+chrom13_cultivated_filtered_ordered.vcf.gz \
+chrom14_cultivated_filtered_ordered.vcf.gz \
+chrom15_cultivated_filtered_ordered.vcf.gz \
+chrom16_cultivated_filtered_ordered.vcf.gz \
+chrom17_cultivated_filtered_ordered.vcf.gz \
 | gzip -c > cultivated_filtered_ordered_combined.vcf.gz
