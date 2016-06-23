e7c9b489a620d5cd1a733124ffa4692f115f2f93
Melis Akman <melisakman@airbears2-10-142-119-141.airbears2.1918.berkeley.edu>
2016-06-23 09:30:51 -0700

    gzip

--- shells/gzip.sh
+++ shells/gzip.sh
@@ -9,7 +9,7 @@
 #SBATCH --mem=16000
 #SBATCH --time=48:00:00
 #SBATCH -o /global/home/users/makman/snpEff/outs/gzip.out
-#SBATCH -e /global/home/users/makman/snpEff/outs/vgzip.err
+#SBATCH -e /global/home/users/makman/snpEff/outs/gzip.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-srun gzip *
+srun gzip *.vcf
