e56ff2e922fc557af19eac54059a5938379526d4
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2016-07-13 09:37:11 -0700

    cat

---
+++ shells/trans_cat.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/vector/instrumentData/blackmanlab/
+#SBATCH -J gzip
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000
+#SBATCH --time=60:00:00
+#SBATCH -o /global/home/users/makman/trinity/outs/cat.out
+#SBATCH -e /global/home/users/makman/trinity/outs/cat.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+srun cat Ari_copy/Ari-leaf-2-clean_1.fq.gz Ari_copy/Ari-meristem-2-clean_1.fq.gz Ari_copy/Ari-node-2-clean_1.fq.gz > Ari_copy/Ari_2_NLM_1.fq.gz
+srun cat Ari_copy/Ari-leaf-2-clean_2.fq.gz Ari_copy/Ari-meristem-2-clean_2.fq.gz Ari_copy/Ari-node-2-clean_2.fq.gz > Ari_copy/Ari_2_NLM_2.fq.gz
+srun cat Ari_copy/Ari-root-2-clean_1.fq.gz Ari_copy/Ari-shoot-2-clean_1.fq.gz > Ari_copy/Ari_2_RS_1.fq.gz
+srun cat Ari_copy/Ari-root-2-clean_2.fq.gz Ari_copy/Ari-shoot-2-clean_2.fq.gz > Ari_copy/Ari_2_RS_2.fq.gz
+
+srun cat NB_copy/NB-leaf-3-clean_1.fq.gz NB_copy/NB-meristem-3-clean_1.fq.gz NB_copy/NB-node-3-clean_1.fq.gz > NB_copy/NB_3_NLM_1.fq.gz
+srun cat NB_copy/NB-leaf-3-clean_2.fq.gz NB_copy/NB-meristem-3-clean_2.fq.gz NB_copy/NB-node-3-clean_2.fq.gz > NB_copy/NB_3_NLM_2.fq.gz
+srun cat NB_copy/NB-root-2-clean_1.fq.gz NB_copy/NB-shoot-2-clean_1.fq.gz > NB_copy/NB_2_RS_1.fq.gz
+srun cat NB_copy/NB-root-2-clean_2.fq.gz NB_copy/NB-shoot-2-clean_2.fq.gz > NB_copy/NB_2_RS_2.fq.gz
