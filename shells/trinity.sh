665875ff0d16fec4997654ec1b1fa5a89c0dbd59
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2016-07-18 13:04:04 -0700

    trinity

--- shells/trinity.sh
+++ shells/trinity.sh
@@ -3,11 +3,11 @@
 #SBATCH -J Trinity
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
-#SBATCH --mem=256000
+#SBATCH --mem=128000
 #SBATCH --time=120:00:00
 #SBATCH -o /global/home/users/makman/trinity/outs/trinity.out
 #SBATCH -e /global/home/users/makman/trinity/outs/trinity.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load trinity
-srun Trinity --seqType fq --max_memory 256G --left Ari_2_NLM_1.fq --right Ari_2_NLM_1.fq --CPU 64 --normalize_reads --output trinity_Ari2_NLM --full_cleanup
+srun Trinity --seqType fq --max_memory 128G --left Ari_2_NLM_1.fq --right Ari_2_NLM_1.fq --CPU 64 --normalize_reads --output trinity_Ari2_NLM --full_cleanup
