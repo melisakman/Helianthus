2c15a8118a2ecebf5cccf118a445ab1f9a8a6f88
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2016-07-18 13:17:17 -0700

    trinity

--- shells/trinity.sh
+++ shells/trinity.sh
@@ -10,4 +10,5 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load trinity
+module load bowtie
 srun Trinity --seqType fq --max_memory 128G --left Ari_2_NLM_1.fq --right Ari_2_NLM_1.fq --CPU 64 --normalize_reads --output trinity_Ari2_NLM --full_cleanup
