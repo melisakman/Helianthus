0302e477d1a1f66477163219fef3ca321d8d4ce2
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2016-09-22 15:37:58 -0700

    blast

--- shells/blast_transcriptomes.sh
+++ shells/blast_transcriptomes.sh
@@ -13,7 +13,7 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 #SBATCH --array=1-6
-module load bs
+module load blast/2.2.30
 srun blastn -query trinity_Ari_all_Trinity_TPM170_filtered.fasta -db /clusterfs/vector/instrumentData/blackmanlab/blast_databases/NB -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 -template_type coding -template_length 18 -window_size 25 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/Ari2NB.txt
 srun blastn -query trinity_Ari_all_Trinity_TPM170_filtered.fasta -db /clusterfs/vector/instrumentData/blackmanlab/blast_databases/XRQ -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 -template_type coding -template_length 18 -window_size 25 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/Ari2XRQ.txt
 srun blastn -query trinity_NB_all_Trinity_TPM140_filtered.fasta -db /clusterfs/vector/instrumentData/blackmanlab/blast_databases/XRQ -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 -template_type coding -template_length 18 -window_size 25 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/NB2XRQ.txt
