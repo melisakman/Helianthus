2059f3c0d55e3241f07588dbf6ad3dffeaa17476
Melis Akman <melisakman@airbears2-10-142-116-28.airbears2.1918.berkeley.edu>
2016-09-23 14:40:49 -0700

    mem

--- shells/subsample_reads_Ari.sh
+++ shells/subsample_reads_Ari.sh
@@ -13,39 +13,39 @@
 #SBATCH --mail-user=makman@berkeley.edu 
 #SBATCH --mail-type=All 
 #SBATCH --array=1-36
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-16-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-16-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-16-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-16-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-50-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-50-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-50-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-50-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-55-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-55-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-55-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-55-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-1-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-1-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-1-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-1-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-2-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-2-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-2-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-2-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-3-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-3-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-3-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-3-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-1-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-1-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-1-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-1-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-2-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-2-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-2-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-2-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-3-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-3-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-3-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-3-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-1-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-1-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-1-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-1-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-2-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-2-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-2-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-2-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-3-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-3-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-3-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-3-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-1-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-1-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-1-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-1-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-2-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-2-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-2-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-2-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-3-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-3-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-3-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-3-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-1-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-1-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-1-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-1-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-2-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-2-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-2-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-2-sub_2.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-3-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-3-sub_1.fq
-srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-3-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-3-sub_2.fq
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-16-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-16-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-16-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-16-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-50-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-50-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-50-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-50-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-55-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-55-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-R1-55-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-R1-55-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-1-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-1-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-1-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-1-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-2-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-2-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-2-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-2-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-3-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-3-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-leaf-3-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-leaf-3-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-1-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-1-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-1-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-1-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-2-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-2-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-2-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-2-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-3-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-3-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-meristem-3-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-meristem-3-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-1-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-1-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-1-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-1-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-2-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-2-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-2-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-2-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-3-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-3-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-node-3-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-node-3-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-1-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-1-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-1-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-1-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-2-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-2-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-2-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-2-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-3-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-3-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-root-3-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-root-3-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-1-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-1-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-1-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-1-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-2-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-2-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-2-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-2-sub_2.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-3-clean_1.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-3-sub_1.fq | gzip
+srun /global/home/users/makman/seqtk/seqtk sample -s100 Ari_copy/Ari-shoot-3-clean_2.fq.gz 10000000 > /global/home/users/makman/data/Ari-shoot-3-sub_2.fq | gzip
