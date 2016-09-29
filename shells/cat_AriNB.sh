4abca868e164308ce60c836fc152edc967b2d451
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2016-09-29 13:51:02 -0700

    count lanes

--- shells/cat_AriNB.sh
+++ shells/cat_AriNB.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH -D /clusterfs/vector/instrumentData/blackmanlab/Helianthus/subsample/data/
+#SBATCH -D /global/scratch/makman/data/
 #SBATCH -J cat
 #SBATCH -o /global/home/users/makman/trinity/outs/cat.out
 #SBATCH -e /global/home/users/makman/trinity/outs/cat.err
@@ -11,5 +11,5 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 #SBATCH --array=1-2
-srun cat *_1.fq.gz > /global/scratch/makman/data/AriNB10M_all_1.fq.gz
-srun cat *_2.fq.gz > /global/scratch/makman/data/AriNB10M_all_2.fq.gz
+srun cat /clusterfs/vector/instrumentData/blackmanlab/Helianthus/subsample/data/*_1.fq.gz > AriNB10M_all_1.fq.gz
+srun cat /clusterfs/vector/instrumentData/blackmanlab/Helianthus/subsample/data/*_2.fq.gz > AriNB10M_all_2.fq.gz
