f5ae1c8c143ec0782dd52e67b56b48f473abeb99
Melis Akman <melisakman@airbears2-10-142-117-207.airbears2.1918.berkeley.edu>
2016-09-30 13:27:37 -0700

    count lanes combined

---
+++ shells/gunzip_reads_combined.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/vector/scratch/makman/
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=8:00:00
+#SBATCH -o /global/home/users/makman/trinity/outs/gunzipcombined_reads.out
+#SBATCH -e /global/home/users/makman/trinity/outs/gunzipcombined_reads.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --array=1-2
+#SBATCH --job-name=count
+srun gunzip AriNB10M_all_1.fq.gz
+srun gunzip AriNB10M_all_2.fq.gz
