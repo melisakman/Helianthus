8a4d7dad6860c54623f4ed81fa072786232159f9
Melis Akman <melisakman@airbears2-10-142-119-65.airbears2.1918.berkeley.edu>
2016-10-04 13:47:32 -0700

    trinity

--- shells/trinity_UCB_AriNB.sh
+++ shells/trinity_UCB_AriNB.sh
@@ -1,17 +1,19 @@
-#!/bin/bash -l
-#!/usr/bin/env perl
-#SBATCH -D /home/makman/Helianthus/AriNBcombined/
+#!/bin/bash
+#SBATCH -D /global/scratch/makman
 #SBATCH -J Trinity
-#SBATCH --partition=bigmemh
-#SBATCH --mem=312000
-#SBATCH -o /home/makman/Helianthus/outs/trinityAriNBcombined.out
-#SBATCH -e /home/makman/Helianthus/outs/trinityAriNBcombined.err
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --max_nodes=4000
+#SBATCH --cmds_per_node=20
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=128000
+#SBATCH --time=140:00:00
+#SBATCH -o ~/trinity/outs/trinityAriNBcombined.out
+#SBATCH -e ~/trinity/outs/trinityAriNBcombined.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-#SBATCH --ntasks=64
-module load perlnew/5.18.4
-module load gmap/2015-06-23
-module load rsem/1.2.23
-module load trinity/2.0.4
-module load java
-Trinity --seqType fq --max_memory 312G --left ../AriNB10M_all_1.fq.gz --right ../AriNB10M_all_2.fq.gz --CPU 64 --normalize_reads --output trinity_AriNB_combined --full_cleanup
+export MODULEPATH=$MODULEPATH:~/modules
+
+module load trinity/2.2.0
+module load bowtie/1.1.1
+Trinity --seqType fq --max_memory 128G --left ./AriNB10M_all_1.fq.gz --right ./AriNB10M_all_2.fq.gz --CPU 20 --normalize_reads --output trinity_AriNB_combined --full_cleanup
