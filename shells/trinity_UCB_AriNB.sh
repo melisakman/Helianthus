8f8bb1c13bf22de6c946cf3dc151c50bc37c7d08
Melis Akman <melisakman@airbears2-10-142-119-65.airbears2.1918.berkeley.edu>
2016-10-04 13:22:32 -0700

    tirnity install

--- shells/trinity_UCB_AriNB.sh
+++ shells/trinity_UCB_AriNB.sh
@@ -12,6 +12,6 @@
 module load perlnew/5.18.4
 module load gmap/2015-06-23
 module load rsem/1.2.23
-module load trinity/2.0.6
+module load trinity/2.0.4
 module load java
 Trinity --seqType fq --max_memory 312G --left ../AriNB10M_all_1.fq.gz --right ../AriNB10M_all_2.fq.gz --CPU 64 --normalize_reads --output trinity_AriNB_combined --full_cleanup
