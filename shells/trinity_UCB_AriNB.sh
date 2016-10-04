10c3e28adcbacd6b553a60d5c6002089aca3441e
Melis Akman <melisakman@airbears2-10-142-119-65.airbears2.1918.berkeley.edu>
2016-10-04 13:58:58 -0700

    trinity

--- shells/trinity_UCB_AriNB.sh
+++ shells/trinity_UCB_AriNB.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH -D /global/scratch/makman
+#SBATCH -D /global/home/users/makman/
 #SBATCH -J Trinity
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
-#SBATCH --max_nodes=4000
-#SBATCH --cmds_per_node=20
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=20
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=128000
 #SBATCH --time=140:00:00
-#SBATCH -o ~/trinity/outs/trinityAriNBcombined.out
-#SBATCH -e ~/trinity/outs/trinityAriNBcombined.err
+#SBATCH -o /global/home/users/makman//trinity/outs/trinityAriNBcombined.out
+#SBATCH -e /global/home/users/makman//trinity/outs/trinityAriNBcombined.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-export MODULEPATH=$MODULEPATH:~/modules
+export MODULEPATH=$MODULEPATH:/global/home/users/makman/modules
 
 module load trinity/2.2.0
 module load bowtie/1.1.1
-Trinity --seqType fq --max_memory 128G --left ./AriNB10M_all_1.fq.gz --right ./AriNB10M_all_2.fq.gz --CPU 20 --normalize_reads --output trinity_AriNB_combined --full_cleanup
+Trinity --seqType fq --max_memory 128G --left /global/scratch/makman/AriNB10M_all_1.fq.gz --right /global/scratch/makman/AriNB10M_all_2.fq.gz --CPU 20 --normalize_reads --output trinity_AriNB_combined --full_cleanup
