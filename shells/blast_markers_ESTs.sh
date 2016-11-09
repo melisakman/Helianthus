fcea4605690626debd0398eeb5ce9fbcbae915e5
Melis Akman <melisakman@airbears2-10-142-110-17.airbears2.1918.berkeley.edu>
2016-11-09 14:09:08 -0800

    blast

--- shells/blast_markers_ESTs.sh
+++ shells/blast_markers_ESTs.sh
@@ -8,13 +8,12 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=16000
 #SBATCH --time=48:00:00
-#SBATCH -o /global/home/users/makman/blast/outs/marker_primers_blast.out
-#SBATCH -e /global/home/users/makman/blast/outs/marker_primers_blast.err
+#SBATCH -o /global/home/users/makman/blast/outs/marker_primers_blast_ESTs.out
+#SBATCH -e /global/home/users/makman/blast/outs/marker_primers_blast_ESTs.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-#SBATCH --array=1-4
+#SBATCH --array=1-3
 module load blast/2.2.30
-srun blastn -task "blastn-short" -query marker_primers.fa -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/HA412_mRNA -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/markerPrimers2HA412mRNA.txt
-srun blastn -task "blastn-short" -query marker_primers.fa -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/HA412 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/markerPrimers2HA412.txt
-srun blastn -task "blastn-short" -query marker_primers.fa -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/XRQ_mRNA -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/markerPrimers2XRQmRNA.txt
-srun blastn -task "blastn-short" -query marker_primers.fa -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/XRQ -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/markerPrimers2XRQ.txt
+srun blastn -task "blastn-short" -query marker_primers.fa -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/ESTs_assembly -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/markerPrimers2ESTs.txt
+srun blastn -query /global/home/users/makman/blast/ESTs_assembly.fasta -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/XRQ -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 -template_type coding -template_length 18 -window_size 25 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/ESTs2XRQ.txt
+srun blastn -query /global/home/users/makman/blast/ESTs_assembly.fasta -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/XRQ_mRNA -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 -template_type coding -template_length 18 -window_size 25 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/ESTs2XRQmRNA.txt
