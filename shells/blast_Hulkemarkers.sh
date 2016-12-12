a9049bced28f80787dc20da905f97d526dad2135
Melis Akman <melisakman@airbears2-10-142-109-165.airbears2.1918.berkeley.edu>
2016-12-12 12:48:14 -0800

    more blast

---
+++ shells/blast_Hulkemarkers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -D /global/home/users/makman/blast/
+#SBATCH -J blast
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000
+#SBATCH --time=48:00:00
+#SBATCH -o /global/home/users/makman/blast/outs/Hulkemarkers_blast.out
+#SBATCH -e /global/home/users/makman/blast/outs/Hulkemarkers_blast.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load blast/2.2.30
+srun blastn -query /clusterfs/vector/instrumentData/blackmanlab/Helianthus/Hulke/all_markers.fa -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/XRQ -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 -template_length 18 -window_size 25 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/Hulkemarkers2XRQ.txt
