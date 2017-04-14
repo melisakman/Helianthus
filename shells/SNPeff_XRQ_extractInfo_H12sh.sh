045c289e409c3870b7c308d783080ad34feee5e7
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-04-14 10:28:39 -0700

    SNPEff H12

---
+++ shells/SNPeff_XRQ_extractInfo_H12sh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/vector/scratch/makman/snpEff/results/
+#SBATCH -J snpEff
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=64000
+#SBATCH --time=16:00:00
+#SBATCH -o /global/home/users/makman/snpEff/outs/snpEff_XRQ_extractCandidates_H12.out
+#SBATCH -e /global/home/users/makman/snpEff/outs/snpEff_XRQ_extractCandidates_H12.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load python
+python ./extract_info_candidates.py XRQ_fil_ordered_combined_snpeff_noHeader.vcf ./H12_genes.txt
