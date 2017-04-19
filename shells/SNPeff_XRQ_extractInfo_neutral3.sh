2fdca9b2b62af80f7c8475722f1488ff05143430
Melis Akman <melisakman@airbears2-10-142-108-187.airbears2.1918.berkeley.edu>
2017-04-19 10:32:05 -0700

    SNPEff neutral genes

--- shells/SNPeff_XRQ_extractInfo_neutral3.sh
+++ shells/SNPeff_XRQ_extractInfo_neutral3.sh
@@ -8,11 +8,9 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=16000
 #SBATCH --time=16:00:00
-#SBATCH -o /global/home/users/makman/snpEff/outs/snpEff_XRQ_extractCandidates_neutral.out
-#SBATCH -e /global/home/users/makman/snpEff/outs/snpEff_XRQ_extractCandidates_neutral.err
+#SBATCH -o /global/home/users/makman/snpEff/outs/snpEff_XRQ_extractCandidates_neutral3.out
+#SBATCH -e /global/home/users/makman/snpEff/outs/snpEff_XRQ_extractCandidates_neutral3.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-#SBATCH --array=1-2
 module load python
 python ./extract_info_candidates.py XRQ_fil_ordered_combined_snpeff_noHeader.vcf ./neutralGenes_3.txt
-python ./extract_info_candidates.py XRQ_fil_ordered_combined_snpeff_noHeader.vcf ./neutralGenes_4.txt
