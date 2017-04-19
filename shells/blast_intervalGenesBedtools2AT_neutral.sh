95e71ca1ea707ddea99c59151d515757768650d3
Melis Akman <melisakman@airbears2-10-142-108-187.airbears2.1918.berkeley.edu>
2017-04-19 09:41:20 -0700

    blast neutral genes

--- shells/blast_intervalGenesBedtools2AT_neutral.sh
+++ shells/blast_intervalGenesBedtools2AT_neutral.sh
@@ -12,7 +12,7 @@
 #SBATCH -e /global/home/users/makman/blast/outs/intervalGenes_blast_AT_neutral.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-#SBATCH --array = 1-2
+#SBATCH --array=1-2
 module load blast/2.2.30
 srun blastx -query /global/home/users/makman/blast/bedtoolsOverlaps/CDs_neutral_3.fasta -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/TAIR10_pep_20101214 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/bedtoolsOverlaps/intervalGenes2AT_neutral_3.txt
 srun blastx -query /global/home/users/makman/blast/bedtoolsOverlaps/CDs_neutral_4.fasta -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/TAIR10_pep_20101214 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/bedtoolsOverlaps/intervalGenes2AT_neutral_4.txt
