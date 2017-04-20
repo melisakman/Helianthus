0cca4c6e1f12f13c43cf9f1a154f250dd6030a3f
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-04-19 22:24:39 -0700

    neutral genes snpeff new

---
+++ shells/SNPeff_XRQ_extractInfo_neutral_new.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/vector/scratch/makman/snpEff/results/
+#SBATCH -J snpEff
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000
+#SBATCH --time=16:00:00
+#SBATCH -o /global/home/users/makman/snpEff/outs/snpEff_XRQ_extractCandidates_neutral_new.out
+#SBATCH -e /global/home/users/makman/snpEff/outs/snpEff_XRQ_extractCandidates_neutral_new.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load python
+python ./extract_info_candidates.py XRQ_fil_ordered_combined_snpeff_noHeader.vcf ./neutralGenes_new.txt
