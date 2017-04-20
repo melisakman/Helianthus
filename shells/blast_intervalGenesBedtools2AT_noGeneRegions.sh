780e57b6877f8c5afbf55efd3158a202c4efa58f
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-04-20 15:44:07 -0700

    no gene regions blast

---
+++ shells/blast_intervalGenesBedtools2AT_noGeneRegions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -D /global/home/users/makman/blast/bedtoolsOverlaps
+#SBATCH -J blast
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000
+#SBATCH --time=48:00:00
+#SBATCH -o /global/home/users/makman/blast/outs/intervalGenes_blast_AT_noGeneRegions.out
+#SBATCH -e /global/home/users/makman/blast/outs/intervalGenes_blast_AT_noGeneRegions.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --array=1-2
+module load blast/2.2.30
+srun blastx -query /global/home/users/makman/blast/bedtoolsOverlaps/noGeneRegions_7Kflank_CDs.fasta -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/TAIR10_pep_20101214 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/bedtoolsOverlaps/intervalGenes2AT_noGeneRegions.txt
