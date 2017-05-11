bb7f2d7eab665ea03222031c4d6691c9e9866efc
Melis Akman <melisakman@airbears2-10-142-108-188.airbears2.1918.berkeley.edu>
2017-05-11 10:52:13 -0700

    candidates blast 2 AT

--- shells/blast_ChapmanBauteBlackmanMandel.sh
+++ shells/blast_ChapmanBauteBlackmanMandel.sh
@@ -9,9 +9,11 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=16000
 #SBATCH --time=48:00:00
-#SBATCH -o /global/home/users/makman/blast/outs/combinedGenes_2AT.out
-#SBATCH -e /global/home/users/makman/blast/outs/combinedGenes_2AT.err
+#SBATCH -o /global/home/users/makman/blast/outs/combinedGenes_2XRQ.out
+#SBATCH -e /global/home/users/makman/blast/outs/combinedGenes_2XRQ.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
+-#SBATCH --array=1-2
 module load blast/2.2.30
-srun blastx -query genes_combined.txt -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/TAIR10_pep_20101214 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/candidateGenes2AT.txt
+-srun blastn -query genes_combined.txt -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/XRQ -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 -template_type coding -template_length 18 -window_size 25 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/combinedGenes2XRQ.txt
+-srun blastn -query genes_combined.txt -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/XRQ_mRNA -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 -template_type coding -template_length 18 -window_size 25 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/combinedGenes2XRQmRNA.txt
