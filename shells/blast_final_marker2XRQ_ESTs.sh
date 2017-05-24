d78c0ec7eaa108ede23bfed85b6500522b908723
Melis Akman <melisakman@airbears2-10-142-109-179.airbears2.1918.berkeley.edu>
2017-05-24 13:26:50 -0700

    final markers 2 ESTs

--- shells/blast_final_marker2XRQ_ESTs.sh
+++ shells/blast_final_marker2XRQ_ESTs.sh
@@ -12,9 +12,8 @@
 #SBATCH -e /global/home/users/makman/blast/outs/finalmarkers2ESTsXRQ_blast.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-#SBATCH --array=1-3
 
 module load blast/2.2.30
-blastn -task "blastn-short" -query ./final/ORS_HT_ZVG_CRT_F_R_markers.txt -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/ESTs_assembly -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/final/primer_markers_2ESTs.txt
-blastn -task "blastn-short" -query ./final/ORS_HT_ZVG_CRT_F_R_markers.txt -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/XRQ -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/final/primer_markers_2XRQ.txt
-blastx -query ./final/NSA_SFW_markers.txt -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/XRQ -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/final/long_markers_2XRQ.txt
+#blastn -task "blastn-short" -query ./final/ORS_HT_ZVG_CRT_F_R_markers.txt -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/ESTs_assembly -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/final/primer_markers_2ESTs.txt
+#blastn -task "blastn-short" -query ./final/ORS_HT_ZVG_CRT_F_R_markers.txt -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/XRQ -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/final/primer_markers_2XRQ.txt
+blastn -query ./final/NSA_SFW_markers.txt -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/XRQ -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out /global/home/users/makman/blast/final/long_markers_2XRQ.txt
