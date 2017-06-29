168da60996a75aacb37ef35da57adf69d198b7b9
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-06-29 08:45:42 -0700

    chr10

--- shells/vcftools_filter_all.sh
+++ shells/vcftools_filter_all.sh
@@ -6,14 +6,13 @@
 #SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=2
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=54000
-#SBATCH --time=72:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64000
+#SBATCH --time=10:00:00
 #SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_filter_all_10.out
 #SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_filter_all_10.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-#SBATCH --array=1-1
 module load vcftools/0.1.13
 module load gcc/4.8.5 
 #zcat ./HanXRQChr02_complete_ordered_norm.vcf.gz | /global/scratch/makman/bcftools/bcftools filter -g 5 -e 'REF="N"'| /global/scratch/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > HanXRQChr02_complete_ordered_norm_maf_filt_indel_in.vcf.gz
