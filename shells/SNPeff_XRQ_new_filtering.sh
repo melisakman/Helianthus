4272cb7e3c4e6966249a52a96cc9b2520cba9219
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-07-06 20:54:20 +0300

    snpeff new filtering

--- shells/SNPeff_XRQ_new_filtering.sh
+++ shells/SNPeff_XRQ_new_filtering.sh
@@ -13,4 +13,4 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load java
-java -Xmx8g -jar /clusterfs/vector/scratch/makman/snpEff/snpEff.jar -stats /clusterfs/vector/scratch/makman/snpEff/results/combined_XRQ_new_filtering_stats.html XRQ /global/scratch/makman/HubnerData/vcf/new_filtering/vcf/XRQ_combined_new_filtering.vcf.gz > /clusterfs/vector/scratch/makman/snpEff/XRQ_combined_new_filtering_snpeff.vcf.gz
+java -Xmx8g -jar /clusterfs/vector/scratch/makman/snpEff/snpEff.jar -stats /clusterfs/vector/scratch/makman/snpEff/results/combined_XRQ_new_filtering_stats.html XRQ /clusterfs/vector/scratch/makman/snpEff/XRQ_combined_new_filtering.vcf.gz > /clusterfs/vector/scratch/makman/snpEff/XRQ_combined_new_filtering_snpeff.vcf.gz
