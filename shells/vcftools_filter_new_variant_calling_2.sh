8ed900143781cf5a6aae158c1b71ad06c48c1308
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-06 16:50:12 -0700

    vcf filter

--- shells/vcftools_filter_new_variant_calling_2.sh
+++ shells/vcftools_filter_new_variant_calling_2.sh
@@ -6,8 +6,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=64000
-#SBATCH --time=80:00:00
+#SBATCH --mem=12000
+#SBATCH --time=12:00:00
 #SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_new_variant_calling_filter01.out
 #SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_new_variant_calling_filter01.err
 #SBATCH --mail-user=makman@berkeley.edu
