8c3d660d828f1eb821ea5fea7468aa3f4346675e
Melis Akman <melisakman@airbears2-10-142-111-45.airbears2.1918.berkeley.edu>
2017-09-11 10:15:42 -0700

    vcf filter

--- shells/vcftools/vcftools_concat_XRQ_new_variant calling.sh
+++ shells/vcftools/vcftools_concat_XRQ_new_variant calling.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/vector/scratch/makman/Hubnerdata/newFiltering/
-#SBATCH -J vcfMerge
+#SBATCH -D /clusterfs/vector/scratch/makman/Hubnerdata/Sariel_new_variant_calling/filtered/ordered/
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
 #SBATCH --nodes=1
@@ -8,11 +7,27 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=64000
 #SBATCH --time=48:00:00
-#SBATCH -o /global/home/users/makman/snpEff/outs/vcftools_merge_XRQ_new_filtering.out
-#SBATCH -e /global/home/users/makman/snpEff/outs/vcftools_merge_XRQ_new_filtering.err
+#SBATCH -o /global/home/users/makman/snpEff/outs/vcftools_merge_XRQ_new_variantCalling.out
+#SBATCH -e /global/home/users/makman/snpEff/outs/vcftools_merge_XRQ_new_variantCalling.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load perl
 module load vcftools/0.1.13
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat HanXRQChr01_complete_ordered_norm_maf_filt_indel_in.vcf.gz HanXRQChr02_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr03_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr04_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr05_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr06_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr07_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr08_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr09_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr10_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr11_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr12_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr13_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr14_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr15_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr16_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz HanXRQChr17_complete_ordered_norm_maf_filt_indel_in_ordered.vcf.gz > XRQ_combined_new_filtering.vcf
+/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat ../HanXRQChr01_filter01.vcf \
+HanXRQChr02_filter01_ordered.vcf \
+HanXRQChr03_filter01_ordered.vcf \
+HanXRQChr04_filter01_ordered.vcf \
+HanXRQChr05_filter01_ordered.vcf \
+HanXRQChr06_filter01_ordered.vcf \
+HanXRQChr07_filter01_ordered.vcf \
+HanXRQChr08_filter01_ordered.vcf \
+HanXRQChr09_filter01_ordered.vcf \
+HanXRQChr10_filter01_ordered.vcf \
+HanXRQChr11_filter01_ordered.vcf \
+HanXRQChr12_filter01_ordered.vcf \
+HanXRQChr13_filter01_ordered.vcf \
+HanXRQChr14_filter01_ordered.vcf \
+HanXRQChr15_filter01_ordered.vcf \
+HanXRQChr16_filter01_ordered.vcf \
+HanXRQChr17_filter01_ordered.vcf > HanXRQ_filtered_ordered_newVC.vcf
