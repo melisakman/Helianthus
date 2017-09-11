fb2d00553c0675f0e2e9c32121c62ba89ef4feb0
Melis Akman <melisakman@airbears2-10-142-111-45.airbears2.1918.berkeley.edu>
2017-09-11 10:51:12 -0700

    vcf filter

--- shells/SNPEff/SNPeff_XRQ_new_variant calling.sh
+++ shells/SNPEff/SNPeff_XRQ_new_variant calling.sh
@@ -6,11 +6,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
-#SBATCH --mem=16000
-#SBATCH --time=8:00:00
-#SBATCH -o /global/home/users/makman/snpEff/outs/snpEff_XRQ_new_filtering.out
-#SBATCH -e /global/home/users/makman/snpEff/outs/snpEff_XRQ_new_filtering.err
+#SBATCH --mem=64000
+#SBATCH --time=48:00:00
+#SBATCH -o /global/home/users/makman/snpEff/outs/snpEff_XRQ_new_VC.out
+#SBATCH -e /global/home/users/makman/snpEff/outs/snpEff_XRQ_new_VC.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load java
-java -Xmx8g -jar /clusterfs/vector/scratch/makman/snpEff/snpEff.jar -stats /clusterfs/vector/scratch/makman/snpEff/results/combined_XRQ_new_filtering_stats.html XRQ /clusterfs/vector/scratch/makman/snpEff/vcf/XRQ_combined_new_filtering.vcf > /clusterfs/vector/scratch/makman/snpEff/vcf/XRQ_combined_new_filtering_snpeff.vcf
+java -Xmx8g -jar /clusterfs/vector/scratch/makman/snpEff/snpEff.jar -stats /clusterfs/vector/scratch/makman/snpEff/results/HanXRQ_new_VC_stats.html XRQ /clusterfs/vector/scratch/makman/Hubnerdata/Sariel_new_variant_calling/filtered/ordered/HanXRQ_filtered_ordered_newVC.vcf > /clusterfs/vector/scratch/makman/snpEff/vcf/HanXRQ_filtered_ordered_newVC_snpeff.vcf
