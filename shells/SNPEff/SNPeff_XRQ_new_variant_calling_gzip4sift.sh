89d6d0ef88eab92ccb34ac63c43597d09de22dc7
Melis Akman <melisakman@airbears2-10-142-111-45.airbears2.1918.berkeley.edu>
2017-09-11 12:29:39 -0700

    snpeff new ariant calling

---
+++ shells/SNPEff/SNPeff_XRQ_new_variant_calling_gzip4sift.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/vector/scratch/makman/snpEff/
+#SBATCH -J snpEff
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=84000
+#SBATCH --time=48:00:00
+#SBATCH -o /global/home/users/makman/snpEff/outs/snpEff_XRQ_new_VC_gzip.out
+#SBATCH -e /global/home/users/makman/snpEff/outs/snpEff_XRQ_new_VC_gzip.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+cat /clusterfs/vector/scratch/makman/snpEff/vcf/HanXRQ_filtered_ordered_newVC_snpeff.vcf | gzip > ../
