9848de21b145e5b15dede6f51c42c02d24ca648d
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-18 16:31:42 -0700

    filter bams and dedup

--- shells/exclude_chr00_dedup_new_variant_Calling_wilds.sh
+++ shells/exclude_chr00_dedup_new_variant_Calling_wilds.sh
@@ -13,7 +13,7 @@
 
 module load samtools/1.3.1
 
-srun samtools view HT013_SD2W-18.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > HT013_SD2W-18.sort.dup.realign_nochr00_dedup.bam
+srun samtools view HT013_SD2W-18.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S HT013_SD2W-18.sort.dup.realign_nochr00_dedup.bam
 # srun samtools view HT172_SK1W-Q.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > HT172_SK1W-Q.sort.dup.realign_nochr00_dedup.bam
 # srun samtools view HT211_MO1W-39.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > HT211_MO1W-39.sort.dup.realign_nochr00_dedup.bam
 # srun samtools view HT254_KS1W-27.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > HT254_KS1W-27.sort.dup.realign_nochr00_dedup.bam
