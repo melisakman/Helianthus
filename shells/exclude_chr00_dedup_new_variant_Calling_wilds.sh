98e465748f8eb116b30e89dff4e6d21143847d81
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-18 16:06:31 -0700

    filter bams and dedup

---
+++ shells/exclude_chr00_dedup_new_variant_Calling_wilds.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=filter-dedup
+#SBATCH --account=fc_blackman
+#SBATCH --partition=vector
+#SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/bams/
+#SBATCH --nodes=1
+#SBATCH --time=16:00:00
+#SBATCH -o ../outs/filter_dedup_new_VC_bams.out
+#SBATCH -e ../outs/filter_dedup_new_VC_bams.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load samtools/1.3.1
+
+srun samtools view HT013_SD2W-18.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > HT013_SD2W-18.sort.dup.realign_nochr00_dedup.bam
+# srun samtools view HT172_SK1W-Q.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > HT172_SK1W-Q.sort.dup.realign_nochr00_dedup.bam
+# srun samtools view HT211_MO1W-39.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > HT211_MO1W-39.sort.dup.realign_nochr00_dedup.bam
+# srun samtools view HT254_KS1W-27.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > HT254_KS1W-27.sort.dup.realign_nochr00_dedup.bam
+# srun samtools view HT330_ND1W-6.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > HT330_ND1W-6.sort.dup.realign_nochr00_dedup.bam
+# srun samtools view HT360_SD1W-35.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > HT360_SD1W-35.sort.dup.realign_nochr00_dedup.bam
+# srun samtools view HT55_IA2W-17.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > HT55_IA2W-17.sort.dup.realign_nochr00_dedup.bam 
+# srun samtools view Q075_IA1W-1.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > Q075_IA1W-1.sort.dup.realign_nochr00_dedup.bam
+# srun samtools view Q086_MB1W-40.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > Q086_MB1W-40.sort.dup.realign_nochr00_dedup.bam
+# srun samtools view QPG4_KS2W-35.sort.dup.realign.bam -R chr_regions.txt -b | samtools rmdup -S > QPG4_KS2W-35.sort.dup.realign_nochr00_dedup.bam
