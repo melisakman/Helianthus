94a28d1c17ac45fc290ccf6c351a14d52e727d2a
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-20 16:40:58 -0700

    index

--- shells/haplotype/haplotype_sam_index_2.sh
+++ shells/haplotype/haplotype_sam_index_2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 #SBATCH --job-name=index
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --exclude=n0007.vector0
 #SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/bams/
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
