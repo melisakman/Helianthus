0c9a0a95d93ac4e9d5cbf86f7e34f1ce2f7a032b
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-19 22:50:46 -0700

    filter chr00

--- shells/haplotype/filter_chr00_1.sh
+++ shells/haplotype/filter_chr00_1.sh
@@ -7,11 +7,11 @@
 #SBATCH --nodes=1
 #SBATCH --time=16:00:00
 #SBATCH --mem=32000
-#SBATCH -o ../outs/filter_chr00_new_VC_1.out
-#SBATCH -e ../outs/filter_chr00_new_VC_1.err
+#SBATCH -o /outs/filter_chr00_new_VC_1.out
+#SBATCH -e /outs/filter_chr00_new_VC_1.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
 module load samtools/1.3.1
 
-samtools view -b ./bams/raw/HT013_SD2W-18.sort.dup.realign.bam -L ./bams/raw/chr_group.txt > ./bams/raw/HT013_SD2W-18.sort.dup.realign_nochr00.bam
+samtools view -b bams/raw/HT013_SD2W-18.sort.dup.realign.bam -L bams/raw/chr_group.txt > bams/raw/HT013_SD2W-18.sort.dup.realign_nochr00.bam
