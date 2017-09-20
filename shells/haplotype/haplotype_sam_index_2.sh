6fb3ced268b83f0ea24ec1192e4ff345df066d6a
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-20 16:42:08 -0700

    index

--- shells/haplotype/haplotype_sam_index_2.sh
+++ shells/haplotype/haplotype_sam_index_2.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=index
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
-#SBATCH --exclude=n0007.vector0
+#SBATCH --exclude=n0000.savio2,n0007.vector0,n0014.savio1,n0015.savio1,n0016.savio1,n0021.savio1,n0028.savio1,n0039.savio1,n0050.savio1,n0071.savio2,n0096.savio2,n0107.savio2,n0111.savio1,n0114.savio2,n0117.savio2,n0130.savio2,n0181.savio2,n0218.savio2,n0234.savio2,n0280.savio2
 #SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/bams/
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
