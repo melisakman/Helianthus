f961e21d418f08944cffc263fcb653e663ed1b82
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-20 14:32:43 -0700

    read group

--- shells/haplotype/haplotype_sam_index_2.sh
+++ shells/haplotype/haplotype_sam_index_2.sh
@@ -14,13 +14,13 @@
 
 module load samtools
 
-# samtools index HT013_SD2W-18.sort.dup.realign_nochr00_dedup.bam HT013_SD2W-18.sort.dup.realign_nochr00_dedup.bai
-# samtools index HT360_SD1W-35.sort.dup.realign_nochr00_dedup.bam HT360_SD1W-35.sort.dup.realign_nochr00_dedup.bai
-# samtools index HT172_SK1W-Q.sort.dup.realign_nochr00_dedup.bam HT172_SK1W-Q.sort.dup.realign_nochr00_dedup.bai
-# samtools index HT55_IA2W-17.sort.dup.realign_nochr00_dedup.bam HT55_IA2W-17.sort.dup.realign_nochr00_dedup.bai
-# samtools index HT211_MO1W-39.sort.dup.realign_nochr00_dedup.bam HT211_MO1W-39.sort.dup.realign_nochr00_dedup.bai
-samtools index Q075_IA1W-1.sort.dup.realign_nochr00_dedup.bam Q075_IA1W-1.sort.dup.realign_nochr00_dedup.bai
-samtools index HT254_KS1W-27.sort.dup.realign_nochr00_dedup.bam HT254_KS1W-27.sort.dup.realign_nochr00_dedup.bai
-samtools index Q086_MB1W-40.sort.dup.realign_nochr00_dedup.bam Q086_MB1W-40.sort.dup.realign_nochr00_dedup.bai
-samtools index HT330_ND1W-6.sort.dup.realign_nochr00_dedup.bam HT330_ND1W-6.sort.dup.realign_nochr00_dedup.bai 
-samtools index QPG4_KS2W-35.sort.dup.realign_nochr00_dedup.bam QPG4_KS2W-35.sort.dup.realign_nochr00_dedup.bai
+# samtools index HT013_SD2W-18.sort.dup.realign_nochr00_dedeup_reGroups.bam HT013_SD2W-18.sort.dup.realign_nochr00_dedeup_reGroups.bai
+# samtools index HT360_SD1W-35.sort.dup.realign_nochr00_dedeup_reGroups.bam HT360_SD1W-35.sort.dup.realign_nochr00_dedeup_reGroups.bai
+# samtools index HT172_SK1W-Q.sort.dup.realign_nochr00_dedeup_reGroups.bam HT172_SK1W-Q.sort.dup.realign_nochr00_dedeup_reGroups.bai
+# samtools index HT55_IA2W-17.sort.dup.realign_nochr00_dedeup_reGroups.bam HT55_IA2W-17.sort.dup.realign_nochr00_dedeup_reGroups.bai
+# samtools index HT211_MO1W-39.sort.dup.realign_nochr00_dedeup_reGroups.bam HT211_MO1W-39.sort.dup.realign_nochr00_dedeup_reGroups.bai
+samtools index Q075_IA1W-1.sort.dup.realign_nochr00_dedeup_reGroups.bam Q075_IA1W-1.sort.dup.realign_nochr00_dedeup_reGroups.bai
+samtools index HT254_KS1W-27.sort.dup.realign_nochr00_dedeup_reGroups.bam HT254_KS1W-27.sort.dup.realign_nochr00_dedeup_reGroups.bai
+samtools index Q086_MB1W-40.sort.dup.realign_nochr00_dedeup_reGroups.bam Q086_MB1W-40.sort.dup.realign_nochr00_dedeup_reGroups.bai
+samtools index HT330_ND1W-6.sort.dup.realign_nochr00_dedeup_reGroups.bam HT330_ND1W-6.sort.dup.realign_nochr00_dedeup_reGroups.bai 
+samtools index QPG4_KS2W-35.sort.dup.realign_nochr00_dedeup_reGroups.bam QPG4_KS2W-35.sort.dup.realign_nochr00_dedeup_reGroups.bai
---
+++ shells/haplotype/haplotype_sam_readGroups4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#SBATCH --job-name=base_calib
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/bams/
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=32000
+#SBATCH -o /clusterfs/vector/scratch/makman/haplotype_networks/outs/base_calibrate_readGroup4.out
+#SBATCH -e /clusterfs/vector/scratch/makman/haplotype_networks/outs/base_calibrate_readGroup4.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load picard
+
+# java -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=HT013_SD2W-18.sort.dup.realign_nochr00_dedup.bam O=HT013_SD2W-18.sort.dup.realign_nochr00_dedup_reGroups.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=1
+# java -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=HT360_SD1W-35.sort.dup.realign_nochr00_dedup.bam O=HT360_SD1W-35.sort.dup.realign_nochr00_dedup_reGroups.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=1
+# java -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=HT172_SK1W-Q.sort.dup.realign_nochr00_dedup.bam O=HT172_SK1W-Q.sort.dup.realign_nochr00_dedup_reGroups.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=1
+# java -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=HT55_IA2W-17.sort.dup.realign_nochr00_dedup.bam O=HT55_IA2W-17.sort.dup.realign_nochr00_dedup_reGroups.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=1
+# java -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=HT211_MO1W-39.sort.dup.realign_nochr00_dedup.bam O=HT211_MO1W-39.sort.dup.realign_nochr00_dedup_reGroups.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=1
+# java -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=Q075_IA1W-1.sort.dup.realign_nochr00_dedup.bam O=Q075_IA1W-1.sort.dup.realign_nochr00_dedup_reGroups.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=1
+java -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=HT254_KS1W-27.sort.dup.realign_nochr00_dedup.bam O=HT254_KS1W-27.sort.dup.realign_nochr00_dedup_reGroups.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=1
+java -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=Q086_MB1W-40.sort.dup.realign_nochr00_dedup.bam O=Q086_MB1W-40.sort.dup.realign_nochr00_dedup_reGroups.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=1
+# java -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=HT330_ND1W-6.sort.dup.realign_nochr00_dedup.bam O=HT330_ND1W-6.sort.dup.realign_nochr00_dedup_reGroups.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=1
+# java -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=QPG4_KS2W-35.sort.dup.realign_nochr00_dedup.bam O=QPG4_KS2W-35.sort.dup.realign_nochr00_dedup_reGroups.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=1
