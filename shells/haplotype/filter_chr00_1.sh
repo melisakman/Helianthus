d1cee39f9ea01e0605fa3b61a11ee5549a1005d5
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-19 23:31:45 -0700

    filter chr00

--- shells/haplotype/filter_chr00_1.sh
+++ shells/haplotype/filter_chr00_1.sh
@@ -3,15 +3,15 @@
 #SBATCH --job-name=filter-dedup
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
-#SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/
+#SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/bams
 #SBATCH --nodes=1
 #SBATCH --time=16:00:00
 #SBATCH --mem=32000
-#SBATCH -o /outs/filter_chr00_new_VC_1.out
-#SBATCH -e /outs/filter_chr00_new_VC_1.err
+#SBATCH -o /clusterfs/vector/scratch/makman/haplotype_networks/outs/filter_chr00_new_VC_1.out
+#SBATCH -e /clusterfs/vector/scratch/makman/haplotype_networks/outs/filter_chr00_new_VC_1.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
 module load samtools/1.3.1
 
-samtools view -b ./bams/HT013_SD2W-18.sort.dup.realign.bam -L ./bams/chr_group.txt > ./bams/HT013_SD2W-18.sort.dup.realign_nochr00.bam
+srun samtools view -b HT013_SD2W-18.sort.dup.realign.bam -L chr_group.txt > HT013_SD2W-18.sort.dup.realign_nochr00.bam
