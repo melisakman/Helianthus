f285a69330b9680fc8143bc2ea34abe503d15f2e
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-20 16:54:49 -0700

    index

--- shells/haplotype/haplotype_sam_index_2.sh
+++ shells/haplotype/haplotype_sam_index_2.sh
@@ -3,7 +3,6 @@
 #SBATCH --job-name=index
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
-#SBATCH --NodeName=n0001.vector0
 #SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/bams/
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
@@ -15,8 +14,8 @@
 
 module load samtools
 
-samtools index Q075_IA1W-1.sort.dup.realign_nochr00_dedup_reGroups.bam Q075_IA1W-1.sort.dup.realign_nochr00_dedup_reGroups.bai
-samtools index HT254_KS1W-27.sort.dup.realign_nochr00_dedup_reGroups.bam HT254_KS1W-27.sort.dup.realign_nochr00_dedup_reGroups.bai
-samtools index Q086_MB1W-40.sort.dup.realign_nochr00_dedup_reGroups.bam Q086_MB1W-40.sort.dup.realign_nochr00_dedup_reGroups.bai
-samtools index HT330_ND1W-6.sort.dup.realign_nochr00_dedup_reGroups.bam HT330_ND1W-6.sort.dup.realign_nochr00_dedup_reGroups.bai 
-samtools index QPG4_KS2W-35.sort.dup.realign_nochr00_dedup_reGroups.bam QPG4_KS2W-35.sort.dup.realign_nochr00_dedup_reGroups.bai
+srun samtools index Q075_IA1W-1.sort.dup.realign_nochr00_dedup_reGroups.bam Q075_IA1W-1.sort.dup.realign_nochr00_dedup_reGroups.bai
+# samtools index HT254_KS1W-27.sort.dup.realign_nochr00_dedup_reGroups.bam HT254_KS1W-27.sort.dup.realign_nochr00_dedup_reGroups.bai
+# samtools index Q086_MB1W-40.sort.dup.realign_nochr00_dedup_reGroups.bam Q086_MB1W-40.sort.dup.realign_nochr00_dedup_reGroups.bai
+# samtools index HT330_ND1W-6.sort.dup.realign_nochr00_dedup_reGroups.bam HT330_ND1W-6.sort.dup.realign_nochr00_dedup_reGroups.bai 
+# samtools index QPG4_KS2W-35.sort.dup.realign_nochr00_dedup_reGroups.bam QPG4_KS2W-35.sort.dup.realign_nochr00_dedup_reGroups.bai
