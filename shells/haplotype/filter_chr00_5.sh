9460a03f63452ad33c86aa8d7328da095b694037
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-19 23:35:44 -0700

    filter chr00

--- shells/haplotype/filter_chr00_5.sh
+++ shells/haplotype/filter_chr00_5.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name=filter-dedup
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
-#SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/bams/raw/
+#SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/bams
 #SBATCH --nodes=1
 #SBATCH --time=16:00:00
 #SBATCH --mem=48000
-#SBATCH -o ../outs/filter_chr00_new_VC_5.out
-#SBATCH -e ../outs/filter_chr00_new_VC_5.err
+#SBATCH -o /clusterfs/vector/scratch/makman/haplotype_networks/outs/filter_chr00_new_VC_5.out
+#SBATCH -e /clusterfs/vector/scratch/makman/haplotype_networks/outs/filter_chr00_new_VC_5.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
--- shells/haplotype/filter_chr00_9.sh
+++ shells/haplotype/filter_chr00_9.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name=filter-dedup
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
-#SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/bams/raw/
+#SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/bams
 #SBATCH --nodes=1
 #SBATCH --time=16:00:00
 #SBATCH --mem=48000
-#SBATCH -o ../outs/filter_chr00_new_VC_9.out
-#SBATCH -e ../outs/filter_chr00_new_VC_9.err
+#SBATCH -o /clusterfs/vector/scratch/makman/haplotype_networks/outs/filter_chr00_new_VC_9.out
+#SBATCH -e /clusterfs/vector/scratch/makman/haplotype_networks/outs/filter_chr00_new_VC_9.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
