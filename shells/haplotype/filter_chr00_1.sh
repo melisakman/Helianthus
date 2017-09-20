8c985a0dbf3fe98dd2d7e999165f868ab543c8a1
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-19 22:24:43 -0700

    filter chr00

--- shells/haplotype/filter_chr00_1.sh
+++ shells/haplotype/filter_chr00_1.sh
@@ -14,12 +14,3 @@
 module load samtools/1.3.1
 
 srun samtools view -b ./HT013_SD2W-18.sort.dup.realign.bam -L ./chr_group.txt > HT013_SD2W-18.sort.dup.realign_nochr00.bam
-# srun samtools view -b HT172_SK1W-Q.sort.dup.realign.bam -L ./chr_group.txt > HT172_SK1W-Q.sort.dup.realign_nochr00.bam
-# srun samtools view -b HT211_MO1W-39.sort.dup.realign.bam -L ./chr_group.txt > HT211_MO1W-39.sort.dup.realign_nochr00.bam
-# srun samtools view -b HT254_KS1W-27.sort.dup.realign.bam -L ./chr_group.txt > HT254_KS1W-27.sort.dup.realign_nochr00.bam
-# srun samtools view -b HT330_ND1W-6.sort.dup.realign.bam -L ./chr_group.txt > HT330_ND1W-6.sort.dup.realign_nochr00.bam
-# srun samtools view -b HT360_SD1W-35.sort.dup.realign.bam -L ./chr_group.txt > HT360_SD1W-35.sort.dup.realign_nochr00.bam
-# srun samtools view -b HT55_IA2W-17.sort.dup.realign.bam -L ./chr_group.txt > HT55_IA2W-17.sort.dup.realign_nochr00.bam 
-# srun samtools view -b Q075_IA1W-1.sort.dup.realign.bam -L ./chr_group.txt > Q075_IA1W-1.sort.dup.realign_nochr00.bam
-# srun samtools view -b Q086_MB1W-40.sort.dup.realign.bam -L ./chr_group.txt > Q086_MB1W-40.sort.dup.realign_nochr00.bam
-# srun samtools view -b QPG4_KS2W-35.sort.dup.realign.bam -L ./chr_group.txt > QPG4_KS2W-35.sort.dup.realign_nochr00.bam
