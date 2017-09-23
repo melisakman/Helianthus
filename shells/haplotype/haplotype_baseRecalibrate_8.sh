b1620a76808fd1c84974645ef77c1a7cb6e1e6cd
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-22 17:14:11 -0700

    base recal

--- shells/haplotype/haplotype_baseRecalibrate_8.sh
+++ shells/haplotype/haplotype_baseRecalibrate_8.sh
@@ -5,8 +5,8 @@
 #SBATCH --qos=vector_batch
 #SBATCH -D /clusterfs/vector/scratch/makman/haplotype_networks/bams/
 #SBATCH --nodes=1
-#SBATCH --time=5:00:00
-#SBATCH --mem=32000
+#SBATCH --time=8:00:00
+#SBATCH --mem=48000
 #SBATCH -o ../outs/base_calibrate_chr00_new_VC8.out
 #SBATCH -e ../outs/base_calibrate_chr00_new_VC8.err
 #SBATCH --mail-user=makman@berkeley.edu
