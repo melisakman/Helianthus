c3cbbcc7a36ea66133773e3569159f4652408148
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-27 12:09:18 -0700

    older samples

--- shells/haplotype/merge/merge_wild1.sh
+++ shells/haplotype/merge/merge_wild1.sh
@@ -6,8 +6,8 @@
 #SBATCH --nodes=1
 #SBATCH --time=10:00:00
 #SBATCH --mem=48000
-#SBATCH -o /clusterfs/vector/scratch/makman/haplotype_networks/outs/merge_new_VC_1.out
-#SBATCH -e /clusterfs/vector/scratch/makman/haplotype_networks/outs/merge_new_VC_1.err
+#SBATCH -o /clusterfs/vector/scratch/makman/haplotype_networks/outs/merge_new_VC_wd1.out
+#SBATCH -e /clusterfs/vector/scratch/makman/haplotype_networks/outs/merge_new_VC_wd1.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
@@ -15,7 +15,7 @@ module load samtools/1.3.1
 # srun samtools merge ann01-cwIA_nochr00_combined.bam ann01-cwIA_3e2_nochr00.bam ann01-cwIA_3e3_nochr00.bam 
 # srun samtools merge ann04-nwAR_nochr00_combined.bam ann04-nwAR_3e4_nochr00.bam ann04-nwAR_3e5_nochr00.bam
 srun samtools merge ann05-ccNM_nochr00_combined.bam ann05-ccNM_3e6_nochr00.bam ann05-ccNM_3e7_nochr00.bam
-srun samtools merge ann10-ccIL_nochr00_combined.bam ann10-ccIL_3ea_nochr00.bam ann10-ccIL_3eb_nochr00.bam
+# srun samtools merge ann10-ccIL_nochr00_combined.bam ann10-ccIL_3ea_nochr00.bam ann10-ccIL_3eb_nochr00.bam
 # srun samtools merge ann20-seAZ_nochr00_combined.bam ann20-seAZ_3ec_nochr00.bam ann20-seAZ_3ed_nochr00.bam
 # srun samtools merge ann28-swSK_nochr00_combined.bam ann28-swSK_3ef_nochr00.bam ann28-swSK_3f0_nochr00.bam
 # srun samtools merge ann29-neSD_nochr00_combined.bam ann29-neSD_3f1_nochr00.bam ann29-neSD_3f2_nochr00.bam
