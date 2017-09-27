f7dff33e35af16ddcf7794018fc8c38be057f2de
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-09-27 15:37:06 -0700

    older samples

--- shells/haplotype/merge/merge_landrace1.sh
+++ shells/haplotype/merge/merge_landrace1.sh
@@ -12,20 +12,20 @@
 #SBATCH --mail-type=All
 
 module load samtools/1.3.1
-srun samtools merge Anzac_Pueblo_combined.bam Anzac_Pueblo*.bam 
-srun samtools merge Arikara_combined.bam Arikara*.bam
-# srun samtools merge Havasupai_combined.bam Havasupai*.bam
-# srun samtools merge Hidatsa1_combined.bam Hidatsa1*.bam
-# srun samtools merge Hopi_combined.bam Hopi*.bam
-# srun samtools merge Laguna_Pueblo_combined.bam Laguna_Pueblo*.bam
-# srun samtools merge Mandan2_combined.bam Mandan2*.bam
-# srun samtools merge MexCult14_combined.bam MexCult14*.bam
-# srun samtools merge MexCult15_combined.bam MexCult15*.bam 
-# srun samtools merge MexCult3_combined.bam MexCult3*.bam
-# srun samtools merge MexCult6_combined.bam MexCult6*.bam
-# srun samtools merge MexCult7_combined.bam MexCult7*.bam
-# srun samtools merge MexCult9_combined.bam MexCult9*.bam
-# srun samtools merge Pueblo_combined.bam Pueblo*.bam
-# srun samtools merge Seneca_combined.bam Seneca*.bam
-# srun samtools merge Zuni_combined.bam Zuni*.bam
+srun samtools merge Anzac_Pueblo_combined.bam Anzac_Pueblo*_nochr00.bam 
+srun samtools merge Arikara_combined.bam Arikara*_nochr00.bam
+# srun samtools merge Havasupai_combined.bam Havasupai*_nochr00.bam
+# srun samtools merge Hidatsa1_combined.bam Hidatsa1*_nochr00.bam
+# srun samtools merge Hopi_combined.bam Hopi*_nochr00.bam
+# srun samtools merge Laguna_Pueblo_combined.bam Laguna_Pueblo*_nochr00.bam
+# srun samtools merge Mandan2_combined.bam Mandan2*_nochr00.bam
+# srun samtools merge MexCult14_combined.bam MexCult14*_nochr00.bam
+# srun samtools merge MexCult15_combined.bam MexCult15*_nochr00.bam 
+# srun samtools merge MexCult3_combined.bam MexCult3*_nochr00.bam
+# srun samtools merge MexCult6_combined.bam MexCult6*_nochr00.bam
+# srun samtools merge MexCult7_combined.bam MexCult7*_nochr00.bam
+# srun samtools merge MexCult9_combined.bam MexCult9*_nochr00.bam
+# srun samtools merge Pueblo_combined.bam Pueblo*_nochr00.bam
+# srun samtools merge Seneca_combined.bam Seneca*_nochr00.bam
+# srun samtools merge Zuni_combined.bam Zuni*_nochr00.bam
 
