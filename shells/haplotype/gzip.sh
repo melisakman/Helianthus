b3c87d567d11c88453f85093b806b9afd6490e47
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-10-05 16:23:45 -0700

    older samples calibrate

--- shells/haplotype/gzip.sh
+++ shells/haplotype/gzip.sh
@@ -5,8 +5,8 @@
 #SBATCH --qos=vector_batch
 #SBATCH -D /clusterfs/vector/scratch/makman/Hubnerdata/Sariel_new_variant_calling/filtered/ordered/
 #SBATCH --time=8:00:00
-#SBATCH -o ../outs/zip.out
-#SBATCH -e ../outs/zip.err
+#SBATCH -o /clusterfs/vector/scratch/makman/haplotype_networks/outs/zip.out
+#SBATCH -e /clusterfs/vector/scratch/makman/haplotype_networks/outs/zip.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
