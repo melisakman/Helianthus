478b97e331e0a0f778d4d9935a29b45b1a23677f
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-10-09 14:08:17 -0700

    basecal

--- shells/haplotype/Networks_for_genes.sh
+++ shells/haplotype/Networks_for_genes.sh
@@ -55,6 +55,7 @@ do
 	Var_current_path=${i}
 	Var_current_file=`basename $Var_current_path`
 	Var_current_sample=${Var_current_file/_dedup_recal.bam/}
+	echo Var_current_sample
 	
 	# srun samtools view -hb -@ 19 $Var_current_path ${var_current_gene_chr}:${var_lower_flanking}-${var_upper_flanking} -o ../Haplo_by_gene/BAMs_for_gene/Reads_${var_current_gene_name}_${Var_current_sample}.bam
 # 	srun samtools index ../Haplo_by_gene/BAMs_for_gene/Reads_${var_current_gene_name}_${Var_current_sample}.bam
@@ -152,32 +153,32 @@ do
 # # 
 # # doing read backed phasing sample by sample
 # # Make a loop to process each BAM file individually
- 	for i in $( ls /clusterfs/vector/scratch/makman/haplotype_networks/bams/bam_recal/*.bam ); do
-	 	Var_current_path=${i}
- 		Var_current_file=`basename $Var_current_path`
- 		Var_current_sample=${Var_current_file/_dedup_recal.bam/}
-
-/clusterfs/vector/scratch/makman/haplotype_networks/jre1.7.0_80/bin/java -Djava.io.tmpdir=/clusterfs/vector/scratch/makman/temp3 -server -Xmx2g -jar /clusterfs/vector/scratch/makman/GenomeAnalysisTK-2.8-1-g932cd3a/GenomeAnalysisTK.jar \
-      -T ReadBackedPhasing \
-      -R /clusterfs/vector/scratch/makman/haplotype_networks/HanXRQr1.0-20151230_no_Chr00.fasta \
-      -I /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/BAMs_for_gene/Reads_${var_current_gene_name}_${Var_current_sample}.bam \
-      --variant /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_per_gene/${var_current_gene_name}.recode.vcf \
-      -o /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_readback/phased_${var_current_gene_name}_${Var_current_sample}.vcf \
-      -L Current_gene_tested.bed \
-       --min_base_quality_score 17 \
-       --min_mapping_quality_score 20 \
-       --phaseQualityThresh 20 \
-      --sampleToPhase ${Var_current_sample} \
-      -et NO_ET \
-      -K /clusterfs/vector/scratch/makman/GenomeAnalysisTK-2.8-1-g932cd3a/makman.key
-# 	
-# 		# Compress vcf for merging with other files
+#  	for i in $( ls /clusterfs/vector/scratch/makman/haplotype_networks/bams/bam_recal/*.bam ); do
+# 	 	Var_current_path=${i}
+#  		Var_current_file=`basename $Var_current_path`
+#  		Var_current_sample=${Var_current_file/_dedup_recal.bam/}
 # 
-# 		srun /clusterfs/vector/scratch/makman/tabix-0.2.6/bgzip -c /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_readback/phased_${var_current_gene_name}_${Var_current_sample}.vcf > /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_readback/phased_${var_current_gene_name}_${Var_current_sample}.vcf.gz 
-# 		srun /clusterfs/vector/scratch/makman/tabix-0.2.6/tabix -p vcf /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_readback/phased_${var_current_gene_name}_${Var_current_sample}.vcf.gz  
-# 
-	done
-# 
+# /clusterfs/vector/scratch/makman/haplotype_networks/jre1.7.0_80/bin/java -Djava.io.tmpdir=/clusterfs/vector/scratch/makman/temp3 -server -Xmx2g -jar /clusterfs/vector/scratch/makman/GenomeAnalysisTK-2.8-1-g932cd3a/GenomeAnalysisTK.jar \
+#       -T ReadBackedPhasing \
+#       -R /clusterfs/vector/scratch/makman/haplotype_networks/HanXRQr1.0-20151230_no_Chr00.fasta \
+#       -I /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/BAMs_for_gene/Reads_${var_current_gene_name}_${Var_current_sample}.bam \
+#       --variant /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_per_gene/${var_current_gene_name}.recode.vcf \
+#       -o /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_readback/phased_${var_current_gene_name}_${Var_current_sample}.vcf \
+#       -L Current_gene_tested.bed \
+#        --min_base_quality_score 17 \
+#        --min_mapping_quality_score 20 \
+#        --phaseQualityThresh 20 \
+#       --sampleToPhase ${Var_current_sample} \
+#       -et NO_ET \
+#       -K /clusterfs/vector/scratch/makman/GenomeAnalysisTK-2.8-1-g932cd3a/makman.key
+# # 	
+# # 		# Compress vcf for merging with other files
+# # 
+# # 		srun /clusterfs/vector/scratch/makman/tabix-0.2.6/bgzip -c /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_readback/phased_${var_current_gene_name}_${Var_current_sample}.vcf > /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_readback/phased_${var_current_gene_name}_${Var_current_sample}.vcf.gz 
+# # 		srun /clusterfs/vector/scratch/makman/tabix-0.2.6/tabix -p vcf /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_readback/phased_${var_current_gene_name}_${Var_current_sample}.vcf.gz  
+# # 
+# 	done
+# # 
 #  	srun vcf-merge /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_readback/phased_${var_current_gene_name}_*.vcf.gz  | ~/programs/tabix-0.2.6/bgzip -c > /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_final/merged_${var_current_gene_name}_phased.vcf.gz
 # 	srun /clusterfs/vector/scratch/makman/bcftools/bcftools reheader -o /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_final/renamed_${var_current_gene_name}_phased.vcf.gz -s /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/Scripts/Names_for_pegas.txt /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_final/merged_${var_current_gene_name}_phased.vcf.gz
  
@@ -194,4 +195,6 @@ do
 	#rm /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/gVCF_samples/raw.snps.indels_${var_current_gene_name}*
  
 done
-
+# 
+# #CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  1       
+# Anzac_Pueblo    Arikara Havasupai       Hidatsa#1       Hopi    Laguna_Pueblo   Mandan#2        MexCult1        MexCult14       MexCult15       MexCult3        MexCult6        MexCult7        MexCult9        PI_531071       PPN020  PPN026  PPN035  PPN046  PPN069  PPN072  PPN083  PPN107  PPN153  PPN173  PPN179  PPN185  PPN191  PPN203  PPN237  PPN240  PPN255  PPN262  Pueblo  SF_33   Seneca  Zuni    ann01-cwIA      ann04-nwAR      ann05-ccNM      ann06-seWY      ann08-ceTN      ann10-ccIL      ann20-seAZ      ann22-nwTX      ann28-swSK      ann29-neSD      ann34-cwKS      ann39-ncNE      ann44-ccCA      annIA   annKS   annWY
