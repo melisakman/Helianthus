88b1db744dffdc1b1c4ee0dfa6b06a414ff7557e
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-10-18 10:40:40 -0700

    network

--- shells/other/gzip.sh
+++ shells/other/gzip.sh
@@ -1,15 +1,12 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/vector/scratch/makman/Hubnerdata/ordered/
 #SBATCH -J gunzip
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=16000
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
 #SBATCH --time=48:00:00
 #SBATCH -o /global/home/users/makman/snpEff/outs/gzip.out
 #SBATCH -e /global/home/users/makman/snpEff/outs/gzip.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-srun gunzip XRQ_fil_ordered_combined.vcf.gz
+srun gzip /clusterfs/vector/scratch/makman/Hubnerdata/Sariel_new_variant_calling/filtered/ordered/HanXRQ_filtered_ordered_newVC.vcf
