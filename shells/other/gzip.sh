aacaddd500e76d5a6ecd60fa1ff54ab5aa1879a9
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-10-18 10:41:40 -0700

    network

--- shells/other/gzip.sh
+++ shells/other/gzip.sh
@@ -1,9 +1,12 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/vector/scratch/makman/Hubnerdata/ordered/
 #SBATCH -J gunzip
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000
 #SBATCH --time=48:00:00
 #SBATCH -o /global/home/users/makman/snpEff/outs/gzip.out
 #SBATCH -e /global/home/users/makman/snpEff/outs/gzip.err
