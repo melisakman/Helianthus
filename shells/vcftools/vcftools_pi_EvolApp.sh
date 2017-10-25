5e3e1e7b918de8e0cb74fe23cf27db290f15564a
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-10-24 21:28:57 -0700

    pi

--- shells/vcftools/vcftools_pi_EvolApp.sh
+++ shells/vcftools/vcftools_pi_EvolApp.sh
@@ -11,13 +11,13 @@
 #SBATCH --mail-type=All
 #SBATCH --array=1-10
 module load vcftools/0.1.13
-vcftools --vcf Plastome_for_Pegas_Q30.vcf --keep wd_list.txt --window-pi 10000 --out wd_pi_cp
-vcftools --vcf Plastome_for_Pegas_Q30.vcf --keep lr_list.txt --window-pi 10000 --out lr_pi_cp
-vcftools --vcf Plastome_for_Pegas_Q30.vcf --keep ethno_list.txt --window-pi 10000 --out ethno_pi_cp
-vcftools --vcf Plastome_for_Pegas_Q30.vcf --keep archeo_list.txt --window-pi 10000 --out archeo_pi_cp
-vcftools --vcf Plastome_for_Pegas_Q30.vcf --keep cv_list.txt --window-pi 10000 --out cv_pi_cp
-vcftools --vcf Mito_for_Pegas_Q30.vcf --keep wd_list.txt --window-pi 10000 --out wd_pi_mt
-vcftools --vcf Mito_for_Pegas_Q30.vcf --keep lr_list.txt --window-pi 10000 --out lr_pi_mt
-vcftools --vcf Mito_for_Pegas_Q30.vcf --keep ethno_list.txt --window-pi 10000 --out ethno_pi_mt
-vcftools --vcf Mito_for_Pegas_Q30.vcf --keep archeo_list.txt --window-pi 10000 --out archeo_pi_mt
-vcftools --vcf Mito_for_Pegas_Q30.vcf --keep cv_list.txt --window-pi 10000 --out cv_pi_mt
+vcftools --vcf Plastome_for_Pegas_Q30.vcf --keep wd_list.txt --site-pi --out wd_pi_cp
+vcftools --vcf Plastome_for_Pegas_Q30.vcf --keep lr_list.txt --site-pi --out lr_pi_cp
+vcftools --vcf Plastome_for_Pegas_Q30.vcf --keep ethno_list.txt --site-pi --out ethno_pi_cp
+vcftools --vcf Plastome_for_Pegas_Q30.vcf --keep archeo_list.txt --site-pi --out archeo_pi_cp
+vcftools --vcf Plastome_for_Pegas_Q30.vcf --keep cv_list.txt --site-pi --out cv_pi_cp
+vcftools --vcf Mito_for_Pegas_Q30.vcf --keep wd_list.txt --site-pi --out wd_pi_mt
+vcftools --vcf Mito_for_Pegas_Q30.vcf --keep lr_list.txt --site-pi --out lr_pi_mt
+vcftools --vcf Mito_for_Pegas_Q30.vcf --keep ethno_list.txt --site-pi --out ethno_pi_mt
+vcftools --vcf Mito_for_Pegas_Q30.vcf --keep archeo_list.txt --site-pi --out archeo_pi_mt
+vcftools --vcf Mito_for_Pegas_Q30.vcf --keep cv_list.txt --site-pi --out cv_pi_mt
