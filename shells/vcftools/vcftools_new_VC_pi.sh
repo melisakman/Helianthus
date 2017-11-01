ef7e857c7a64d1cf3ed7535d48e3077d19274eea
Melis Akman <melisakman@airbears2-10-142-108-149.airbears2.1918.berkeley.edu>
2017-11-01 13:01:08 -0700

    removing Mexcult 7 and 14 from pi, fst, tajD

--- shells/vcftools/vcftools_new_VC_pi.sh
+++ shells/vcftools/vcftools_new_VC_pi.sh
@@ -9,26 +9,26 @@
 #SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_new_VC_pi15_update.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-#SBATCH --array=1-34
+#SBATCH --array=1-17
 
 module load vcftools/0.1.13
-vcftools --gzvcf HanXRQChr01_filter01.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr01_10kb
-vcftools --gzvcf HanXRQChr02_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr02_10kb
-vcftools --gzvcf HanXRQChr03_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr03_10kb
-vcftools --gzvcf HanXRQChr04_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr04_10kb
-vcftools --gzvcf HanXRQChr05_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr05_10kb
-vcftools --gzvcf HanXRQChr06_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr06_10kb
-vcftools --gzvcf HanXRQChr07_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr07_10kb
-vcftools --gzvcf HanXRQChr08_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr08_10kb
-vcftools --gzvcf HanXRQChr09_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr09_10kb
-vcftools --gzvcf HanXRQChr10_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr10_10kb
-vcftools --gzvcf HanXRQChr11_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr11_10kb
-vcftools --gzvcf HanXRQChr12_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr12_10kb
-vcftools --gzvcf HanXRQChr13_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr13_10kb
-vcftools --gzvcf HanXRQChr14_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr14_10kb
-vcftools --gzvcf HanXRQChr15_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr15_10kb
-vcftools --gzvcf HanXRQChr16_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr16_10kb
-vcftools --gzvcf HanXRQChr17_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr17_10kb
+# vcftools --gzvcf HanXRQChr01_filter01.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr01_10kb
+# vcftools --gzvcf HanXRQChr02_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr02_10kb
+# vcftools --gzvcf HanXRQChr03_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr03_10kb
+# vcftools --gzvcf HanXRQChr04_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr04_10kb
+# vcftools --gzvcf HanXRQChr05_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr05_10kb
+# vcftools --gzvcf HanXRQChr06_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr06_10kb
+# vcftools --gzvcf HanXRQChr07_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr07_10kb
+# vcftools --gzvcf HanXRQChr08_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr08_10kb
+# vcftools --gzvcf HanXRQChr09_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr09_10kb
+# vcftools --gzvcf HanXRQChr10_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr10_10kb
+# vcftools --gzvcf HanXRQChr11_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr11_10kb
+# vcftools --gzvcf HanXRQChr12_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr12_10kb
+# vcftools --gzvcf HanXRQChr13_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr13_10kb
+# vcftools --gzvcf HanXRQChr14_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr14_10kb
+# vcftools --gzvcf HanXRQChr15_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr15_10kb
+# vcftools --gzvcf HanXRQChr16_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr16_10kb
+# vcftools --gzvcf HanXRQChr17_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/wd_pi_chr17_10kb
 vcftools --gzvcf HanXRQChr01_filter01.vcf --minQ 50 --minDP 3 --maxDP 25 --keep lr_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/lr_pi_chr01_10kb
 vcftools --gzvcf HanXRQChr02_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep lr_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/lr_pi_chr02_10kb
 vcftools --gzvcf HanXRQChr03_filter01_ordered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep lr_list.txt --max-missing 0.8 --window-pi 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/pi/lr_pi_chr03_10kb
