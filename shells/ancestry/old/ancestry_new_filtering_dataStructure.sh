013a5eb8d2c3dad9ad216049f3c8bd68c3c571c9
Melis Akman <melisakman@airbears2-10-142-108-149.airbears2.1918.berkeley.edu>
2017-11-01 14:39:18 -0700

    ancestry

---
+++ shells/ancestry/old/ancestry_new_filtering_dataStructure.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/vector/scratch/makman/Hubnerdata/newFiltering/
+#SBATCH -J ancestry
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=18000
+#SBATCH --time=48:00:00
+#SBATCH -o /clusterfs/vector/scratch/makman/ancestry/outs/ancestry_dataStructure.out
+#SBATCH -e /clusterfs/vector/scratch/makman/ancestry/outs/ancestry_dataStructure.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --array=1-17
+module load python
+# gunzip *_ordered.vcf.gz
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr01_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr01_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr02_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr02_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr03_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr03_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr04_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr04_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr05_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr05_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr06_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr06_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr07_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr07_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr08_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr08_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr09_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr09_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr10_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr10_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr11_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr11_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr12_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr12_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr13_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr13_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr14_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr14_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr15_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr15_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr16_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr16_ancestry_dataStructure.txt
+python /clusterfs/vector/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py HanXRQChr17_complete_ordered_norm_maf_filt_indel_in_ordered.vcf /clusterfs/vector/scratch/makman/ancestry/results/dataStructure/HanXRQChr17_ancestry_dataStructure.txt
+
