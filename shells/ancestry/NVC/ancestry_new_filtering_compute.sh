a5b482c3bf49886927fc0c9ecc609da360578635
Melis Akman <melisakman@airbears2-10-142-108-149.airbears2.1918.berkeley.edu>
2017-11-01 16:34:47 -0700

    ancestry

---
+++ shells/ancestry/NVC/ancestry_new_filtering_compute.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/vector/scratch/makman/ancestry/
+#SBATCH -J vcfFilter
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=18000
+#SBATCH --time=48:00:00
+#SBATCH -o /clusterfs/vector/scratch/makman/ancestry/outs/ancestry_compute.out
+#SBATCH -e /clusterfs/vector/scratch/makman/ancestry/outs/ancestry_compute.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --array=1-17
+module load python
+
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr01_ancestry_dataStructure.txt -output ./results/compute/chr01_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr02_ancestry_dataStructure.txt -output ./results/compute/chr02_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr03_ancestry_dataStructure.txt -output ./results/compute/chr03_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr04_ancestry_dataStructure.txt -output ./results/compute/chr04_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr05_ancestry_dataStructure.txt -output ./results/compute/chr05_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr06_ancestry_dataStructure.txt -output ./results/compute/chr06_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr07_ancestry_dataStructure.txt -output ./results/compute/chr07_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr08_ancestry_dataStructure.txt -output ./results/compute/chr08_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr09_ancestry_dataStructure.txt -output ./results/compute/chr09_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr10_ancestry_dataStructure.txt -output ./results/compute/chr10_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr11_ancestry_dataStructure.txt -output ./results/compute/chr11_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr12_ancestry_dataStructure.txt -output ./results/compute/chr12_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr13_ancestry_dataStructure.txt -output ./results/compute/chr13_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr14_ancestry_dataStructure.txt -output ./results/compute/chr14_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr15_ancestry_dataStructure.txt -output ./results/compute/chr15_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr16_ancestry_dataStructure.txt -output ./results/compute/chr16_ancestry.txt
+./infocalc -column 2 -input ./results/dataStructure/HanXRQChr17_ancestry_dataStructure.txt -output ./results/compute/chr17_ancestry.txt
