df7fce81bdeedc3631cb15971d0c4ead4d0b0068
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-10 17:12:53 -0800

    copy from vector to global

--- shells/GATK/copy.sh
+++ shells/GATK/copy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/vector/scratch/makman/GATK_variant_calling/fastq/
 #SBATCH -J copy
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
 #SBATCH --mem=36000
 #SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/copy.out
