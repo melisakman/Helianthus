8165f6620f411d3645a13b8894551d9ae7599788
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-10 22:14:05 -0800

    zcat

--- shells/GATK/cat_fastq_3.sh
+++ shells/GATK/cat_fastq_3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/
 #SBATCH -J catfastq3
-#SBATCH --partition=vector
+#SBATCH --partition=savio2
 #SBATCH --mem=36000
-#SBATCH --qos=vector_batch
+#SBATCH --qos=savio_normal
 #SBATCH --time=48:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/cat_fastq3.out
 #SBATCH -e /global/home/users/makman/GATK/outs/cat_fastq3.err
--- shells/GATK/cat_fastq_4.sh
+++ shells/GATK/cat_fastq_4.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/
 #SBATCH -J catfastq4
-#SBATCH --partition=vector
+#SBATCH --partition=savio2
 #SBATCH --mem=36000
-#SBATCH --qos=vector_batch
+#SBATCH --qos=savio_normal
 #SBATCH --time=48:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/cat_fastq4.out
 #SBATCH -e /global/home/users/makman/GATK/outs/cat_fastq4.err
