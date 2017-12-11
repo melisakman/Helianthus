8357c4502ed993769294926b5ec318d289fa15ad
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-10 17:08:27 -0800

    copy from vector to global

--- shells/GATK/copy.sh
+++ shells/GATK/copy.sh
@@ -10,7 +10,6 @@
 #SBATCH -e /global/home/users/makman/GATK/outs/copy.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-ssh dtn
-cp /clusterfs/vector/scratch/makman/GATK_variant_calling/fastq/* /global/scratch/makman/GATK/fastq/
+cp /clusterfs/vector/scratch/makman/GATK_variant_calling/fastq/* /clusterfs/rosalind/users/makman/GATK/fastq/
 
 
