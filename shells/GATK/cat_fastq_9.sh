347e164eff59790e1a9af03f3b3460f36fb6ea0b
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-11 13:53:04 -0800

    cat landrace fastqs

--- shells/GATK/cat_fastq_9.sh
+++ shells/GATK/cat_fastq_9.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/
 #SBATCH -J catfastq9
 #SBATCH --account=fc_blackman
 #SBATCH --partition=savio2
 #SBATCH --mem=36000
 #SBATCH --qos=savio_normal
 #SBATCH --time=48:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/cat_fastq8.out
-#SBATCH -e /global/home/users/makman/GATK/outs/cat_fastq8.err
+#SBATCH -o /global/home/users/makman/GATK/outs/cat_fastq9.out
+#SBATCH -e /global/home/users/makman/GATK/outs/cat_fastq9.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
