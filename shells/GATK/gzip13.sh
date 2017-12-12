688eeea2ab324d615e6258bf416aa739804f1d84
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-12 12:28:22 -0800

    gzip modern cultivars

--- shells/GATK/gzip13.sh
+++ shells/GATK/gzip13.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/
 #SBATCH -J gzip13
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
 #SBATCH --mem=48000
+#SBATCH --qos=savio_normal
 #SBATCH --time=48:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/gzip13.out
 #SBATCH -e /global/home/users/makman/GATK/outs/gzip13.err
