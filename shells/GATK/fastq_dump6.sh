b6adf263e5028dda8f52e6654ee79c7ca38b51b7
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-12 12:29:32 -0800

    fq dump

--- shells/GATK/fastq_dump6.sh
+++ shells/GATK/fastq_dump6.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/sra/
 #SBATCH -J fqdump6
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
 #SBATCH --mem=48000
+#SBATCH --qos=savio_normal
 #SBATCH --time=36:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/fastqdump6.out
 #SBATCH -e /global/home/users/makman/GATK/outs/fastqdump6.err
