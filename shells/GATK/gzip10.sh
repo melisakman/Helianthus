9983593c6c2dd6babeb105f3e94e711428181095
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-12 10:25:46 -0800

    gzip

--- shells/GATK/gzip10.sh
+++ shells/GATK/gzip10.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/
-#SBATCH -J gzip9
+#SBATCH -J gzip10
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio
 #SBATCH --qos=rosalind_savio_normal
 #SBATCH --mem=48000
 #SBATCH --time=48:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/gzip9.out
-#SBATCH -e /global/home/users/makman/GATK/outs/gzip9.err
+#SBATCH -o /global/home/users/makman/GATK/outs/gzip10.out
+#SBATCH -e /global/home/users/makman/GATK/outs/gzip10.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 # gzip MexCult6_R2.fastq
@@ -31,6 +31,9 @@
 # gzip annMO1W-39_R2.fastq
 # gzip annND1W-6_R1.fastq
 # gzip annND1W-6_R2.fastq
-gzip annSK1W-Q_R1.fastq
-gzip annSK1W-Q_R2.fastq
-gzip PPN262_R1.fastq
+# gzip annSK1W-Q_R1.fastq
+# gzip annSK1W-Q_R2.fastq
+# gzip PPN262_R1.fastq
+gzip PPN262_R2.fastq
+gzip SF33_R1.fastq
+gzip SF33_R2.fastq
