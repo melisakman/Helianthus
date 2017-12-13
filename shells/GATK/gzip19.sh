9fe9b7a53222e7b2beef19511bd0a721930a9ae1
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-12 21:19:07 -0800

    gzip

---
+++ shells/GATK/gzip19.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/
+#SBATCH -J gzip19
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --mem=48000
+#SBATCH --qos=savio_normal
+#SBATCH --time=48:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/gzip19.out
+#SBATCH -e /global/home/users/makman/GATK/outs/gzip19.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+gzip HA124_R1.fastq
+# gzip HA124_R2.fastq
+
