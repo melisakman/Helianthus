90594fa2b9f4a3aa78d7d2bc48489f35c60cd4c5
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-13 16:28:22 -0800

    trimm

--- shells/GATK/trim/trimmomatic17.sh
+++ shells/GATK/trim/trimmomatic17.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/
 #SBATCH -J trim17
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
 #SBATCH --mem=48000
+#SBATCH --qos=savio_normal
 #SBATCH --time=48:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/trim17.out
 #SBATCH -e /global/home/users/makman/GATK/outs/trim17.err
