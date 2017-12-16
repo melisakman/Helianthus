fc923f4be9489996c9b279bc54778600d30739d6
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-16 02:13:56 -0800

    trim time out

--- shells/GATK/trim/trimmomatic14_2.sh
+++ shells/GATK/trim/trimmomatic14_2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/
 #SBATCH -J trim14
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
 #SBATCH --mem=54000
+#SBATCH --qos=savio_normal
 #SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/trim14a2.out
 #SBATCH -e /global/home/users/makman/GATK/outs/trim14a2.err
