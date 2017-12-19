1dc5d4c5649619df8fdde396e32e9ae914d96fcb
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-19 14:48:14 -0800

    XPCLR both maps

--- shells/XPCLR/XPCLR_old_VR_Sarielmap.sh
+++ shells/XPCLR/XPCLR_old_VR_Sarielmap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/XPCLR/XPCLR/helianthus
+#SBATCH -D /clusterfs/rosalind/users/makman/XPCLR/XPCLR/bin
 #SBATCH -J XPCLR_Sa
 #SBATCH --account=fc_blackman
 #SBATCH --partition=savio2
@@ -11,20 +11,20 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 #SBATCH --array=1-17
-../bin/XPCLR -xpclr wd_genotype_Chr01.txt lr_genotype_Chr01.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr01_xpclr.txt -w1 0.005 200 2000 1 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr02.txt lr_genotype_Chr02.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr02_xpclr.txt -w1 0.005 200 2000 2 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr03.txt lr_genotype_Chr03.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr03_xpclr.txt -w1 0.005 200 2000 3 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr04.txt lr_genotype_Chr04.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr04_xpclr.txt -w1 0.005 200 2000 4 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr05.txt lr_genotype_Chr05.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr05_xpclr.txt -w1 0.005 200 2000 5 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr06.txt lr_genotype_Chr06.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr06_xpclr.txt -w1 0.005 200 2000 6 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr07.txt lr_genotype_Chr07.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr07_xpclr.txt -w1 0.005 200 2000 7 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr08.txt lr_genotype_Chr08.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr08_xpclr.txt -w1 0.005 200 2000 8 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr09.txt lr_genotype_Chr09.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr09_xpclr.txt -w1 0.005 200 2000 9 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr10.txt lr_genotype_Chr10.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr10_xpclr.txt -w1 0.005 200 2000 10 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr11.txt lr_genotype_Chr11.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr11_xpclr.txt -w1 0.005 200 2000 11 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr12.txt lr_genotype_Chr12.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr12_xpclr.txt -w1 0.005 200 2000 12 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr13.txt lr_genotype_Chr13.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr13_xpclr.txt -w1 0.005 200 2000 13 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr14.txt lr_genotype_Chr14.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr14_xpclr.txt -w1 0.005 200 2000 14 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr15.txt lr_genotype_Chr15.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr15_xpclr.txt -w1 0.005 200 2000 15 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr16.txt lr_genotype_Chr16.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr16_xpclr.txt -w1 0.005 200 2000 16 -p0 0.95
-../bin/XPCLR -xpclr wd_genotype_Chr17.txt lr_genotype_Chr17.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr17_xpclr.txt -w1 0.005 200 2000 17 -p0 0.95
+.XPCLR -xpclr ../helianthus/wd_genotype_Chr01.txt ../helianthus/lr_genotype_Chr01.txt ../helianthus/HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt ../helianthus/Sariel_map/wd_lr_Chr01_xpclr.txt -w1 0.005 200 2000 1 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr02.txt lr_genotype_Chr02.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr02_xpclr.txt -w1 0.005 200 2000 2 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr03.txt lr_genotype_Chr03.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr03_xpclr.txt -w1 0.005 200 2000 3 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr04.txt lr_genotype_Chr04.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr04_xpclr.txt -w1 0.005 200 2000 4 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr05.txt lr_genotype_Chr05.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr05_xpclr.txt -w1 0.005 200 2000 5 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr06.txt lr_genotype_Chr06.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr06_xpclr.txt -w1 0.005 200 2000 6 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr07.txt lr_genotype_Chr07.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr07_xpclr.txt -w1 0.005 200 2000 7 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr08.txt lr_genotype_Chr08.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr08_xpclr.txt -w1 0.005 200 2000 8 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr09.txt lr_genotype_Chr09.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr09_xpclr.txt -w1 0.005 200 2000 9 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr10.txt lr_genotype_Chr10.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr10_xpclr.txt -w1 0.005 200 2000 10 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr11.txt lr_genotype_Chr11.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr11_xpclr.txt -w1 0.005 200 2000 11 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr12.txt lr_genotype_Chr12.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr12_xpclr.txt -w1 0.005 200 2000 12 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr13.txt lr_genotype_Chr13.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr13_xpclr.txt -w1 0.005 200 2000 13 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr14.txt lr_genotype_Chr14.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr14_xpclr.txt -w1 0.005 200 2000 14 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr15.txt lr_genotype_Chr15.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr15_xpclr.txt -w1 0.005 200 2000 15 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr16.txt lr_genotype_Chr16.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr16_xpclr.txt -w1 0.005 200 2000 16 -p0 0.95
+# ../bin/XPCLR -xpclr wd_genotype_Chr17.txt lr_genotype_Chr17.txt HanXRQr1.0-20151230.bp_to_cM.280x801_modified_XPCLR.txt Sariel_map/wd_lr_Chr17_xpclr.txt -w1 0.005 200 2000 17 -p0 0.95
