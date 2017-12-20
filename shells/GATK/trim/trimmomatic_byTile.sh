62e6e4ee3df050eb6cd36df8b311fa7f0adac336
Melis Akman <melisakman@airbears2-10-142-109-93.airbears2.1918.berkeley.edu>
2017-12-20 13:07:04 -0800

    trim by tile

--- shells/GATK/trim/trimmomatic_byTile.sh
+++ shells/GATK/trim/trimmomatic_byTile.sh
@@ -11,6 +11,12 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load java
+module load fastqc/0.11.5 
 
-./filterbytile.sh in1=../annIA1W-1_R1_trimmed_fastq.gz in2=../annIA1W-1_R2_trimmed_fastq.gz out1=../annIA1W-1_R1_trimmed_byTile.fastq.gz out2=../annIA1W-1_R2_trimmed_byTile.fastq.gz
+# ./filterbytile.sh in1=../annIA1W-1_R1_trimmed_fastq.gz in2=../annIA1W-1_R2_trimmed_fastq.gz out1=../annIA1W-1_R1_trimmed_byTile.fastq.gz out2=../annIA1W-1_R2_trimmed_byTile.fastq.gz
+
+./filterbytile.sh in1=../annIA1W-1_R1_trimmed_fastq.gz in2=../annIA1W-1_R2_trimmed_fastq.gz out1=../annIA1W-1_R1_trimmed_byTile_agg.fastq.gz out2=../annIA1W-1_R2_trimmed_byTile_agg.fastq.gz ud=0.75 qd=1 ed=1 ua=.5 qa=.5 ea=.5
+fastqc ../annIA1W-1_R1_trimmed_byTile_agg.fastq.gz
+fastqc ../annIA1W-1_R2_trimmed_byTile_agg.fastq.gz
+
 
