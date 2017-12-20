7f98a76333109cd041fee2f31feb8d0970560390
Melis Akman <melisakman@airbears2-10-142-109-93.airbears2.1918.berkeley.edu>
2017-12-20 12:34:52 -0800

    trim by tile

---
+++ shells/GATK/trim/trimmomatic_byTile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/
+#SBATCH -J trimTile
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --mem=48000
+#SBATCH --qos=savio_normal
+#SBATCH --time=48:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/trim_byTile.out
+#SBATCH -e /global/home/users/makman/GATK/outs/trim_byTile.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load java
+
+./filterbytile.sh in1=annIA1W-1_R1_trimmed.fastq in2=annIA1W-1_R2_trimmed.fastq out1=annIA1W-1_R1_trimmed_byTile.fastq out2=annIA1W-1_R2_trimmed_byTile.fastq
+
