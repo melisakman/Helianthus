3b5395357256d52f332b0be7bfee1ae5150cb7c1
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-21 00:06:29 -0800

    bwa

--- shells/GATK/bwa/bwa25.sh
+++ shells/GATK/bwa/bwa25.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/
 #SBATCH -J bwa25
-#SBATCH --account=fc_blackman
-#SBATCH --partition=savio2
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
 #SBATCH --mem=48000
-#SBATCH --qos=savio_normal
 #SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/bwa25.out
 #SBATCH -e /global/home/users/makman/GATK/outs/bwa25.err
