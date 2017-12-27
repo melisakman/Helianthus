14f7fde42afdf5a015afad5c8d05a6b9aed943fe
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-27 14:49:34 -0800

    remove pair infor from fastq

--- shells/GATK/fix_fastq_custom/fix_fastq6.sh
+++ shells/GATK/fix_fastq_custom/fix_fastq6.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/trimmed/
 #SBATCH -J fixfastq6
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --mem=54000
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --mem=48000
+#SBATCH --qos=savio_normal
 #SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/fix_fastq6.out
 #SBATCH -e /global/home/users/makman/GATK/outs/fix_fastq6.err
