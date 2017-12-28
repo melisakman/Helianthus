3ffc0797dcb18494557a7ba3461854413cced828
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-28 12:45:57 -0800

    gzip

--- shells/GATK/gzip/gzip_all.sh
+++ shells/GATK/gzip/gzip_all.sh
@@ -11,37 +11,37 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
-HA124_R1_trimmed.fastq
-HA124_R2_trimmed.fastq
-HA316_R1_trimmed.fastq
-HA316_R2_trimmed.fastq
-HA433_R1_trimmed.fastq
-HA433_R2_trimmed.fastq
-HA442_R1_trimmed.fastq
-HA442_R2_trimmed.fastq
-HA821_R1_trimmed.fastq
-HA821_R2_trimmed.fastq
-HA89_R1_trimmed.fastq 
-HA89_R2_trimmed.fastq 
-PPN255_Manchurian_R1_trimmed.fastq
-PPN255_Manchurian_R2_trimmed.fastq
-RHA271_R1_trimmed.fastq
-RHA271_R2_trimmed.fastq
-RHA309_R1_trimmed.fastq
-RHA309_R2_trimmed.fastq
-RHA311_R1_trimmed.fastq
-RHA311_R2_trimmed.fastq
-RHA328_R1_trimmed.fastq
-RHA328_R2_trimmed.fastq
-RHA355_R1_trimmed.fastq
-RHA355_R2_trimmed.fastq
-RHA358_R1_trimmed.fastq
-RHA358_R2_trimmed.fastq
-RHA408_R1_trimmed.fastq
-RHA408_R2_trimmed.fastq
-RHA426_R1_trimmed.fastq
-RHA426_R2_trimmed.fastq
-RHA857_R1_trimmed.fastq
-RHA857_R2_trimmed.fastq
-VIR847_R1_trimmed.fastq
-VIR847_R2_trimmed.fastq
+gzip HA124_R1_trimmed.fastq
+gzip HA124_R2_trimmed.fastq
+gzip HA316_R1_trimmed.fastq
+gzip HA316_R2_trimmed.fastq
+gzip HA433_R1_trimmed.fastq
+gzip HA433_R2_trimmed.fastq
+gzip HA442_R1_trimmed.fastq
+gzip HA442_R2_trimmed.fastq
+gzip HA821_R1_trimmed.fastq
+gzip HA821_R2_trimmed.fastq
+gzip HA89_R1_trimmed.fastq 
+gzip HA89_R2_trimmed.fastq 
+gzip PPN255_Manchurian_R1_trimmed.fastq
+gzip PPN255_Manchurian_R2_trimmed.fastq
+gzip RHA271_R1_trimmed.fastq
+gzip RHA271_R2_trimmed.fastq
+gzip RHA309_R1_trimmed.fastq
+gzip RHA309_R2_trimmed.fastq
+gzip RHA311_R1_trimmed.fastq
+gzip RHA311_R2_trimmed.fastq
+gzip RHA328_R1_trimmed.fastq
+gzip RHA328_R2_trimmed.fastq
+gzip RHA355_R1_trimmed.fastq
+gzip RHA355_R2_trimmed.fastq
+gzip RHA358_R1_trimmed.fastq
+gzip RHA358_R2_trimmed.fastq
+gzip RHA408_R1_trimmed.fastq
+gzip RHA408_R2_trimmed.fastq
+gzip RHA426_R1_trimmed.fastq
+gzip RHA426_R2_trimmed.fastq
+gzip RHA857_R1_trimmed.fastq
+gzip RHA857_R2_trimmed.fastq
+gzip VIR847_R1_trimmed.fastq
+gzip VIR847_R2_trimmed.fastq
