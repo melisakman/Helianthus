f717b0f643f9a464bef2a27d8a749c0b5b748dd6
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-29 22:24:53 -0800

    gzip

---
+++ shells/GATK/fix_fastq_custom/gzip10.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/trimmed
+#SBATCH -J gzip10
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --mem=48000
+#SBATCH --qos=savio_normal
+#SBATCH --time=72:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/gzip10.out
+#SBATCH -e /global/home/users/makman/GATK/outs/gzip10.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+gzip HA404_R1_trimmed.fastq
+gzip HA404_R2_trimmed.fastq
+
+
