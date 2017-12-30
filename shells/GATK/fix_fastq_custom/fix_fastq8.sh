5700eded23b76bf125275a800150a9e1e3ac7921
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2017-12-29 18:56:36 -0800

    fix fastq

---
+++ shells/GATK/fix_fastq_custom/fix_fastq8.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/trimmed_fixed/
+#SBATCH -J fixfastq8
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --mem=48000
+#SBATCH --qos=savio_normal
+#SBATCH --time=72:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/fix_fastq8.out
+#SBATCH -e /global/home/users/makman/GATK/outs/fix_fastq8.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+gunzip HA404_R1_trimmed.fastq.gz
+# gunzip HA404_R2_trimmed.fastq.gz
+
+python ../trimmed/remove_pair_info.py HA404_R1_trimmed.fastq HA404_R1_trimmed_fixed.fastq
+# python ../trimmed/remove_pair_info.py HA404_R2_trimmed.fastq HA404_R2_trimmed_fixed.fastq
+
+gzip HA404_R1_trimmed_fixed.fastq
+# gzip HA404_R2_trimmed_fixed.fastq
+
