7569b295cf61ed8455014938d4f740944e266a76
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-01-13 12:03:18 -0800

    concat vcf

--- shells/GATK/8_baseRecalibrator/knownsites/concat_vcf.sh
+++ shells/GATK/8_baseRecalibrator/knownsites/concat_vcf.sh
@@ -1,10 +1,9 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/vector/scratch/makman/Hubnerdata/Sariel_new_variant_calling/merged_Nov2017
 #SBATCH -J concatVCF
-#SBATCH --account=fc_blackman
-#SBATCH --partition=savio2
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
 #SBATCH --mem=64000
-#SBATCH --qos=savio_normal
 #SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/concat_vcf.out
 #SBATCH -e /global/home/users/makman/GATK/outs/concat_vcf.err
