2e1f0e1799261421c3f8d2a108795b73626929af
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-01-16 22:37:06 -0800

    variant calling all

--- shells/GATK/11_variantCalling/variant_calling3.sh
+++ shells/GATK/11_variantCalling/variant_calling3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/
 #SBATCH -J varcal3
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
 #SBATCH --mem=64000
+#SBATCH --qos=savio_normal
 #SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/variant_cal3.out
 #SBATCH -e /global/home/users/makman/GATK/outs/variant_cal3.err
