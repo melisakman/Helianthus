127d56e67abb5d947af61aa404d9c0fb5e34b379
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-02 16:32:52 -0800

    re-run slow jobs

--- shells/GATK/11_variantCalling/variant_calling64.sh
+++ shells/GATK/11_variantCalling/variant_calling64.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/
 #SBATCH -J varcal64
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1 
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=64000
-#SBATCH --time=200:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=300:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/variant_cal64.out
 #SBATCH -e /global/home/users/makman/GATK/outs/variant_cal64.err
 #SBATCH --mail-user=makman@berkeley.edu
--- shells/GATK/11_variantCalling/variant_calling7.sh
+++ shells/GATK/11_variantCalling/variant_calling7.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/
 #SBATCH -J varcal7
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
-#SBATCH --ntasks-per-node=1 
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=64000
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=1
-#SBATCH --time=200:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=300:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/variant_cal7.out
 #SBATCH -e /global/home/users/makman/GATK/outs/variant_cal7.err
 #SBATCH --mail-user=makman@berkeley.edu
