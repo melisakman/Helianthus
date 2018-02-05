98a53d0caa9d6592f1b3a54d70dac39e4c716ff5
Melis Akman <melisakman@airbears2-10-142-109-141.airbears2.1918.berkeley.edu>
2018-02-05 12:57:22 -0800

    mexcult bwa

--- shells/GATK/mex_cults/bwa_mexcult3.sh
+++ shells/GATK/mex_cults/bwa_mexcult3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/trimmed
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/
 #SBATCH -J bwaM3
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
@@ -13,6 +13,12 @@
 #SBATCH --mail-type=All
 
 module load bwa/0.7.17-r1188
+
+# java -jar /clusterfs/rosalind/users/makman/Trimmomatic-0.36/trimmomatic-0.36.jar PE HI.4520.005.NEBNext_Index_30.Mex_A1516_R1.fastq.gz HI.4520.005.NEBNext_Index_30.Mex_A1516_R2.fastq.gz HI.4520.005.NEBNext_Index_30.Mex_A1516_R1_trimmed.fastq.gz HI.4520.005.NEBNext_Index_30.Mex_A1516_R1_unpaired.fastq.gz HI.4520.005.NEBNext_Index_30.Mex_A1516_R2_trimmed.fastq.gz HI.4520.005.NEBNext_Index_30.Mex_A1516_R2_unpaired.fastq.gz ILLUMINACLIP:adaptersRay.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+# java -jar /clusterfs/rosalind/users/makman/Trimmomatic-0.36/trimmomatic-0.36.jar PE HI.4520.005.NEBNext_Index_37.Mex_A1517_R1.fastq.gz HI.4520.005.NEBNext_Index_37.Mex_A1517_R2.fastq.gz HI.4520.005.NEBNext_Index_37.Mex_A1517_R1_trimmed.fastq.gz HI.4520.005.NEBNext_Index_37.Mex_A1517_R1_unpaired.fastq.gz HI.4520.005.NEBNext_Index_37.Mex_A1517_R2_trimmed.fastq.gz HI.4520.005.NEBNext_Index_37.Mex_A1517_R2_unpaired.fastq.gz ILLUMINACLIP:adaptersRay.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+java -jar /clusterfs/rosalind/users/makman/Trimmomatic-0.36/trimmomatic-0.36.jar PE HI.4520.005.NEBNext_Index_38.Mex_A1572_R1.fastq.gz HI.4520.005.NEBNext_Index_38.Mex_A1572_R2.fastq.gz HI.4520.005.NEBNext_Index_38.Mex_A1572_R1_trimmed.fastq.gz HI.4520.005.NEBNext_Index_38.Mex_A1572_R1_unpaired.fastq.gz HI.4520.005.NEBNext_Index_38.Mex_A1572_R2_trimmed.fastq.gz HI.4520.005.NEBNext_Index_38.Mex_A1572_R2_unpaired.fastq.gz ILLUMINACLIP:adaptersRay.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+# java -jar /clusterfs/rosalind/users/makman/Trimmomatic-0.36/trimmomatic-0.36.jar PE HI.4520.005.NEBNext_Index_39.Mex_A1574_R1.fastq.gz HI.4520.005.NEBNext_Index_39.Mex_A1574_R2.fastq.gz HI.4520.005.NEBNext_Index_39.Mex_A1574_R1_trimmed.fastq.gz HI.4520.005.NEBNext_Index_39.Mex_A1574_R1_unpaired.fastq.gz HI.4520.005.NEBNext_Index_39.Mex_A1574_R2_trimmed.fastq.gz HI.4520.005.NEBNext_Index_39.Mex_A1574_R2_unpaired.fastq.gz ILLUMINACLIP:adaptersRay.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+# java -jar /clusterfs/rosalind/users/makman/Trimmomatic-0.36/trimmomatic-0.36.jar PE HI.4520.005.NEBNext_Index_40.Mex_Ann261_R1.fastq.gz HI.4520.005.NEBNext_Index_40.Mex_Ann261_R2.fastq.gz HI.4520.005.NEBNext_Index_40.Mex_Ann261_R1_trimmed.fastq.gz HI.4520.005.NEBNext_Index_40.Mex_Ann261_R1_unpaired.fastq.gz HI.4520.005.NEBNext_Index_40.Mex_Ann261_R2_trimmed.fastq.gz HI.4520.005.NEBNext_Index_40.Mex_Ann261_R2_unpaired.fastq.gz ILLUMINACLIP:adaptersRay.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 
 # bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_mem/HanXRQr1.0-20151230.fa HI.4520.005.NEBNext_Index_30.Mex_A1516_R1_trimmed.fastq.gz HI.4520.005.NEBNext_Index_30.Mex_A1516_R2_trimmed.fastq.gz > Mex_A1516.sam
 # bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_mem/HanXRQr1.0-20151230.fa HI.4520.005.NEBNext_Index_37.Mex_A1517_R1_trimmed.fastq.gz HI.4520.005.NEBNext_Index_37.Mex_A1517_R2_trimmed.fastq.gz > Mex_A1517.sam
