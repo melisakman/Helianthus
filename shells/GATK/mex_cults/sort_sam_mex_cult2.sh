4f5337b2ba5087c33647fffcb0e7c4f968ea0dda
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-06 22:37:24 -0800

    mexcult sort markdups

--- shells/GATK/mex_cults/sort_sam_mex_cult2.sh
+++ shells/GATK/mex_cults/sort_sam_mex_cult2.sh
@@ -3,10 +3,10 @@
 #SBATCH -J sortmex2
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
-#SBATCH --mem=64000
+#SBATCH --mem=128000
 #SBATCH --time=360:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/sam_sort_mexcult2.out
 #SBATCH -e /global/home/users/makman/GATK/outs/sam_sort_mexcult2.err
@@ -16,7 +16,7 @@
 module load bio/picard/2.9.0 
 
 # java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx48G -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar SortSam INPUT=Mex_A1516.sam OUTPUT=Mex_A1516_sorted.bam SORT_ORDER=coordinate 
-java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx48G -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar SortSam INPUT=Mex_A1517.sam OUTPUT=Mex_A1517_sorted.bam SORT_ORDER=coordinate 
+# java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx48G -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar SortSam INPUT=Mex_A1517.sam OUTPUT=Mex_A1517_sorted.bam SORT_ORDER=coordinate 
 # java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx72G -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar SortSam INPUT=Mex_A1572.sam OUTPUT=Mex_A1572_sorted.bam SORT_ORDER=coordinate 
 # java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx72G -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar SortSam INPUT=Mex_A1574.sam OUTPUT=Mex_A1574_sorted.bam SORT_ORDER=coordinate 
 # java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx72G -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar SortSam INPUT=Mex_Ann261.sam OUTPUT=Mex_Ann261_sorted.bam SORT_ORDER=coordinate 
