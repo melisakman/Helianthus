6eb4bbe90fde9e3357dd9b8ca6c9954a60f2f563
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-11 12:39:53 -0800

    combine gvcf

--- shells/GATK/12_combine_gvcfs/combine_gvcf.sh
+++ shells/GATK/12_combine_gvcfs/combine_gvcf.sh
@@ -3,10 +3,11 @@
 #SBATCH -J combine_gvcfs
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --qos=rosalind_htc2_norma
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=128000
 #SBATCH --time=300:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/combine_gvcfs.out
 #SBATCH -e /global/home/users/makman/GATK/outs/combine_gvcfs.err
