079f59ebfc83767fc6a0d44a4b2264ff44ec1064
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-11 16:28:20 -0800

    combine gvcf

--- shells/GATK/12_combine_gvcfs/combine_gvcf.sh
+++ shells/GATK/12_combine_gvcfs/combine_gvcf.sh
@@ -1,15 +1,14 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/
 #SBATCH -J combine_gvcfs
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --partition=vector_batch
+#SBATCH --qos=vector
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
 #SBATCH --time=300:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/combine_gvcfs_savionormal.out
-#SBATCH -e /global/home/users/makman/GATK/outs/combine_gvcfs_savionormal.err
+#SBATCH -o /global/home/users/makman/GATK/outs/combine_gvcfs_vector.out
+#SBATCH -e /global/home/users/makman/GATK/outs/combine_gvcfs_vector.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
@@ -83,4 +82,4 @@ module load java
 --variant annSD2W-18.g.vcf.gz \
 --variant annSK1W-Q.g.vcf.gz \
 --variant annWY.g.vcf.gz \
--O combined_savio_normal.g.vcf.gz
+-O combined_vector.g.vcf.gz
