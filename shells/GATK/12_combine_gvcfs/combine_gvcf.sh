1ea8ea3f8a8d2ab7146422321c41dd1e76e3880a
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-12 13:58:32 -0800

    mex wd haplotypecaller

--- shells/GATK/12_combine_gvcfs/combine_gvcf.sh
+++ shells/GATK/12_combine_gvcfs/combine_gvcf.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/
 #SBATCH -J combine_gvcfs
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
+#SBATCH --partition=rosalind_co
+#SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=1
-#SBATCH --mem=256000
+#SBATCH --mem=64000
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=48
+#SBATCH --cpus-per-task=20
 #SBATCH --time=300:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/combine_gvcfs_vector2.out
-#SBATCH -e /global/home/users/makman/GATK/outs/combine_gvcfs_vector2.err
+#SBATCH -o /global/home/users/makman/GATK/outs/combine_gvcfs_savio.out
+#SBATCH -e /global/home/users/makman/GATK/outs/combine_gvcfs_savio.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 zcat combined_vector.g.vcf.gz | tail
@@ -84,4 +84,4 @@ module load java
 --variant annSD2W-18.g.vcf.gz \
 --variant annSK1W-Q.g.vcf.gz \
 --variant annWY.g.vcf.gz \
--O combined_vector.g.vcf.gz
+-O combined_savio.g.vcf.gz
---
+++ shells/GATK/mex_cults/variant_calling_mex5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/
+#SBATCH -J varcal5
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=64000
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/variant_cal_mex5.out
+#SBATCH -e /global/home/users/makman/GATK/outs/variant_cal_mex5.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load java
+
+# /clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" HaplotypeCaller -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Mex_A1516_sorted_markdup_readGroups_recal.bam -O Mex_A1516.g.vcf.gz -ERC GVCF
+# /clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" HaplotypeCaller -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Mex_A1517_sorted_markdup_readGroups_recal.bam -O Mex_A1517.g.vcf.gz -ERC GVCF
+# /clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" HaplotypeCaller -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Mex_A1572_sorted_markdup_readGroups_recal.bam -O Mex_A1572.g.vcf.gz -ERC GVCF
+# /clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" HaplotypeCaller -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Mex_A1574_sorted_markdup_readGroups_recal.bam -O Mex_A1574.g.vcf.gz -ERC GVCF
+/clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" HaplotypeCaller -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Mex_Ann261_sorted_markdup_readGroups_recal.bam -O Mex_Ann261.g.vcf.gz -ERC GVCF
