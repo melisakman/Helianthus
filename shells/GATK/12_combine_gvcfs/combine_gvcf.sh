0309472794ae57e625583d570f050bcc59d7da02
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-13 10:10:53 -0800

    combine gvcfs savio normal

--- shells/GATK/12_combine_gvcfs/combine_gvcf.sh
+++ shells/GATK/12_combine_gvcfs/combine_gvcf.sh
@@ -1,13 +1,14 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/
 #SBATCH -J comg_nsavio
-#SBATCH --partition=co_rosalind
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
 #SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=1
 #SBATCH --mem=64000
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
-#SBATCH --time=300:00:00
+#SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/combine_gvcfs_savionormal.out
 #SBATCH -e /global/home/users/makman/GATK/outs/combine_gvcfs_savionormal.err
 #SBATCH --mail-user=makman@berkeley.edu
