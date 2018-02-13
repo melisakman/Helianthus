e2f34c7673132bd3e6098289f557c270927de146
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-13 11:50:23 -0800

    haplotypecaller mex wd

--- shells/GATK/mex_cults/variant_calling_mex3.sh
+++ shells/GATK/mex_cults/variant_calling_mex3.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=32000
+#SBATCH --mem=40000
 #SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/variant_cal_mex3.out
 #SBATCH -e /global/home/users/makman/GATK/outs/variant_cal_mex3.err
@@ -16,6 +16,6 @@ module load java
 
 # /clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" HaplotypeCaller -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Mex_A1516_sorted_markdup_readGroups_recal.bam -O Mex_A1516.g.vcf.gz -ERC GVCF
 # /clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" HaplotypeCaller -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Mex_A1517_sorted_markdup_readGroups_recal.bam -O Mex_A1517.g.vcf.gz -ERC GVCF
-/clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx32G" HaplotypeCaller -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Mex_A1572_sorted_markdup_readGroups_recal.bam -O Mex_A1572.g.vcf.gz -ERC GVCF
+/clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx40G" HaplotypeCaller -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Mex_A1572_sorted_markdup_readGroups_recal.bam -O Mex_A1572.g.vcf.gz -ERC GVCF
 # /clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" HaplotypeCaller -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Mex_A1574_sorted_markdup_readGroups_recal.bam -O Mex_A1574.g.vcf.gz -ERC GVCF
 # /clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" HaplotypeCaller -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Mex_Ann261_sorted_markdup_readGroups_recal.bam -O Mex_Ann261.g.vcf.gz -ERC GVCF
