93c920343c9a643b7d176d40ac68373cca0ab3a0
Melis Akman <melisakman@airbears2-10-142-108-30.airbears2.1918.berkeley.edu>
2018-02-14 10:30:25 -0800

    genotype

--- shells/GATK/13_genotyping/genotype_chr16.sh
+++ shells/GATK/13_genotyping/genotype_chr16.sh
@@ -1,13 +1,11 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
 #SBATCH -J gntype16
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
-#SBATCH --mem=64000
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --mem=48000
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=8
 #SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/genotype_chr16.out
 #SBATCH -e /global/home/users/makman/GATK/outs/genotype_chr16.err
@@ -16,7 +14,7 @@
 
 module load java
 
-/clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G" GenotypeGVCFs \
+/clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx48G" GenotypeGVCFs \
 -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
 -V combined_chr16.g.vcf.gz \
 -O VC_MA_chr16.vcf.gz
