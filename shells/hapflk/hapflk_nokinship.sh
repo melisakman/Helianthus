c48eb79dfbb79f35ed5aab0b414bd8b938aceeab
Melis Akman <melisakman@airbears2-10-142-109-228.airbears2.1918.berkeley.edu>
2018-02-15 10:37:33 -0800

    hapflk

---
+++ shells/hapflk/hapflk_nokinship.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=hapflk
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH -D /clusterfs/rosalind/users/makman/hapflk-1.4/hapflk-tutorial
+#SBATCH --nodes=1
+#SBATCH --time=5:00:00
+#SBATCH --mem=32000
+#SBATCH -o hapflk_nokinship.out
+#SBATCH -e hapflk_nokinship.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load hapflk/1.4
+
+
+hapflk --file hapmap3-lct -K 15 --nfit=1 --ncpu=2
