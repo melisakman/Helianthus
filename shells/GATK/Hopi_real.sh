515bca0b424955815fffba25aaa9a46139767699
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-19 13:13:22 -0800

    hopi index

--- shells/GATK/Hopi_real.sh
+++ shells/GATK/Hopi_real.sh
@@ -1,10 +1,14 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/
 #SBATCH -J run
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
-#SBATCH --mem=96000
-#SBATCH --time=572:00:00
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/Hopi_real_2.out
 #SBATCH -e /global/home/users/makman/GATK/outs/Hopi_real_2.err
 #SBATCH --mail-user=makman@berkeley.edu
