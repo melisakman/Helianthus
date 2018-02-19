9ef8ff0085baa76a8bb3797e73d54b703251d876
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-19 13:07:57 -0800

    hopi index

--- shells/GATK/Hopi_real.sh
+++ shells/GATK/Hopi_real.sh
@@ -13,7 +13,7 @@
 module load java
 module load bwa/0.7.17-r1188
 module load picard/2.9.0 
-samtools/1.6
+samtools
 
 
 # 
@@ -26,8 +26,8 @@ samtools/1.6
 # gzip Hopi_real_R2_trimmed_fixed.fastq
 # bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_mem/HanXRQr1.0-20151230.fa Hopi_real_R1_trimmed_fixed.fastq.gz Hopi_real_R2_trimmed_fixed.fastq.gz > Hopi_real.sam
 # java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar SortSam INPUT=Hopi_real.sam OUTPUT=Hopi_real_sorted.bam SORT_ORDER=coordinate 
-java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx92G -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar MarkDuplicates INPUT=Hopi_real_sorted.bam OUTPUT=Hopi_real_sorted_markdup.bam METRICS_FILE=metrics_Hopi_real.txt
-java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=Hopi_real_sorted_markdup.bam O=Hopi_real_sorted_markdup_readGroup.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=Hopi_real
+# java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx92G -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar MarkDuplicates INPUT=Hopi_real_sorted.bam OUTPUT=Hopi_real_sorted_markdup.bam METRICS_FILE=metrics_Hopi_real.txt
+# java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G -jar /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/picard/2.4.1/picard.jar AddOrReplaceReadGroups I=Hopi_real_sorted_markdup.bam O=Hopi_real_sorted_markdup_readGroup.bam RGPL=Illumina RGPU=1 RGLB=1 RGSM=Hopi_real
 samtools index Hopi_real_sorted_markdup_readGroup.bam Hopi_real_sorted_markdup_readGroup.bai
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T BaseRecalibrator -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Hopi_real_sorted_markdup_readGroup.bam -L HanXRQChr01 -knownSites /clusterfs/rosalind/users/makman/merged_NVC.vcf -o Hopi_real_dedup.table 
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T PrintReads -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Hopi_real_sorted_markdup_readGroup.bam -BQSR Hopi_real_dedup.table -o Hopi_real_sorted_markdup_recal.bam
