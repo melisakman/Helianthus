9134d53733569986a1c8f55779cc4a6b501bdd2d
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-20 15:12:47 -0800

    Hopi haplo

---
+++ shells/GATK/Hopi/Hopi_chr01.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/
+#SBATCH -J hapcal1
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/Hopi_real_hapcal1.out
+#SBATCH -e /global/home/users/makman/GATK/outs/Hopi_real_hapcal1.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load java
+module load bwa/0.7.17-r1188
+module load picard/2.9.0 
+module load samtools/1.6
+
+/clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" HaplotypeCaller -L HanXRQChr01 -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -I Hopi_real_sorted_markdup_recal.bam -O Hopi_real_chr01.g.vcf.gz -ERC GVCF
+
+
+
+
+
+
+
+
+
