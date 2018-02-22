bb46057ce49daa2193dd151fedc0e5cb5eea312a
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-21 22:21:26 -0800

    WGCNA

---
+++ shells/WGCNA/WGCNA_1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/WGCNA_Ara/
+#SBATCH -J WGCNA
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/WGCNA/WGCNA_power.out
+#SBATCH -e /global/home/users/makman/WGCNA/WGCNA_power.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load r/3.4.2
+R
+
+install.packages("WGCNA")
+
+library(WGCNA)
+enableWGCNAThreads()
+lnames = load(file = "Arab_dataInput.RData")
+lnames
+# Choose a set of soft-thresholding powers
+powers = c(c(1:10), seq(from = 12, to=20, by=2))
+# Call the network topology analysis function
+sft = pickSoftThreshold(datArab, powerVector = powers, verbose = 5)
+sft 
