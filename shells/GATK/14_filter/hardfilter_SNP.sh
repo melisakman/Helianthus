2ad5ea43b9bdd80eb8c6de117a9446e2ffc23257
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-23 16:58:44 -0800

    hard filter test chr01

--- shells/GATK/14_filter/hardfilter_SNP.sh
+++ shells/GATK/14_filter/hardfilter_SNP.sh
@@ -16,17 +16,17 @@ module load gcc/4.8.5
 module load java
 
 java -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -Xmx60G -T SelectVariants \
--R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \ 
--V VC_MA_chr01.vcf \ 
--selectType SNP \ 
--o VC_MA_chr01_snps.vcf 
+	-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \ 
+	-V VC_MA_chr01.vcf \ 
+	-selectType SNP \ 
+	-o VC_MA_chr01_snps.vcf 
 
 java -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -Xmx60G -T VariantFiltration \ 
--R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \ 
--V VC_MA_chr01_snps.vcf \ 
---filterExpression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \ 
---filterName "my_snp_filter" \ 
--o VC_MA_chr01_snps_filtered.vcf 
+	-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \ 
+	-V VC_MA_chr01_snps.vcf \ 
+	--filterExpression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \ 
+	--filterName "my_snp_filter" \ 
+	-o VC_MA_chr01_snps_filtered.vcf 
 
 
 
