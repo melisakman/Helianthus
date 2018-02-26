01556d98fb18dc07c9d51394e0b5722bd4957a3e
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-26 09:20:11 -0800

    hard filter

--- shells/GATK/14_filter/sed.sh
+++ shells/GATK/14_filter/sed.sh
@@ -1,11 +1,13 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
 #SBATCH -J sed
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
 #SBATCH --mem=64000
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=20
 #SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/sed_SNP.out
 #SBATCH -e /global/home/users/makman/GATK/outs/sed_SNP.err
