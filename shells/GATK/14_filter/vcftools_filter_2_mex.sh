c230f75291bfd31aaa8957f54fa40694a6220043
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-26 15:49:28 -0800

    filter 2

---
+++ shells/GATK/14_filter/vcftools_filter_2_mex.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
+#SBATCH -J filt2M
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/filter_VC_MA_mex2.out
+#SBATCH -e /global/home/users/makman/GATK/outs/filter_VC_MA_mex2.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load bio/vcftools/0.1.15
+module load gcc/4.8.5 
+
+zcat VC_MA_combined_snps_mex_filtered.vcf.gz | /clusterfs/rosalind/users/makman/bcftools/bcftools filter -g 5 -e 'REF="N"'| vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | gzip -c > VC_MA_combined_snps_mex_hardfiltered_filtered.vcf.gz
