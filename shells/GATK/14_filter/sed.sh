e701b3443263528ed3579ddb5e7df328420dc55c
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-26 08:34:58 -0800

    hard filter

---
+++ shells/GATK/14_filter/sed.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
+#SBATCH -J sed
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/sed_SNP.out
+#SBATCH -e /global/home/users/makman/GATK/outs/sed_SNP.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+zcat VC_MA_combined_snps_filtered.vcf.gz | sed 's/HanXRQChr//' | gzip > VC_MA_combined_snps_filtered_hanxrq_removed.vcf.gz
--- shells/SNPRelate/hardfilter_SNP.sh
+++ shells/SNPRelate/hardfilter_SNP.sh
@@ -17,16 +17,22 @@ R
 library(gdsfmt)
 library(SNPRelate)
 setwd("/clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs")
+
 vcf.fn = "VC_MA_combined_snps_filtered.vcf.gz"
 snpgdsVCF2GDS(vcf.fn, "test.gds", method="biallelic.only")
 snpgdsSummary("test.gds")
-genofile <- snpgdsOpen(test.gds())
+(genofile <- snpgdsOpen("test.gds"))
+
+
 set.seed(1000)
 # Try different LD thresholds for sensitivity analysis
-snpset <- snpgdsLDpruning(genofile, ld.threshold=0.2)
+snpset <- snpgdsLDpruning(genofile, ld.threshold=0.8)
 names(snpset)
 snpset.id <- unlist(snpset)
-pca <- snpgdsPCA(genofile, snp.id=snpset.id, num.thread=2)
+
+
+
+pca <- snpgdsPCA(genofile, num.thread=2)
 pc.percent <- pca$varprop*100
 head(round(pc.percent, 2))
 tab <- data.frame(sample.id = pca$sample.id, EV1 = pca$eigenvect[,1],    # the first eigenvector
