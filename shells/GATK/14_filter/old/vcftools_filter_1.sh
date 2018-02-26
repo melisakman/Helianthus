90bcd217569bedab55ba539552e1f94f88925815
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-26 15:26:36 -0800

    filter 2

--- shells/GATK/14_filter/old/vcftools_filter_1.sh
+++ shells/GATK/14_filter/old/vcftools_filter_1.sh
@@ -2,12 +2,12 @@
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
 #SBATCH -J filt1
 #SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
 #SBATCH --nodes=1
 #SBATCH --mem=64000
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=12
 #SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/filter_VC_MA2.out
 #SBATCH -e /global/home/users/makman/GATK/outs/filter_VC_MA2.err
