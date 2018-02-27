114f8e6210f38dde6b723bf8465031167ec3a2f0
Melis Akman <melisakman@airbears2-10-142-108-109.airbears2.1918.berkeley.edu>
2018-02-27 09:58:38 -0800

    sed for SNPRelate

--- shells/GATK/14_filter/sed.sh
+++ shells/GATK/14_filter/sed.sh
@@ -1,9 +1,12 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
 #SBATCH -J sed
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
 #SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
 #SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/sed_SNP.out
@@ -12,5 +15,5 @@
 #SBATCH --mail-type=All
 # zcat VC_MA_combined_snps_hardfiltered_filtered.vcf.gz | sed 's/HanXRQChr//' | gzip > VC_MA_combined_snps_hardfiltered_filtered_hanxrq_removed.vcf.gz
 # zcat VC_MA_combined_snps_mex_hardfiltered_filtered.vcf.gz | sed 's/HanXRQChr//' | gzip > VC_MA_combined_snps_mex_hardfiltered_filtered_hanxrq_removed.vcf.gz
-zcat /clusterfs/vector/scratch/makman/Hubnerdata/Sariel_new_variant_calling/merged_Nov2017/merged_NVC.vcf.gz | sed 's/HanXRQChr//' | gzip > /clusterfs/vector/scratch/makman/Hubnerdata/Sariel_new_variant_calling/merged_Nov2017/merged_NVC_hanxrq_removed.vcf.gz
+zcat merged_NVC.vcf.gz | sed 's/HanXRQChr//' | gzip > merged_NVC_hanxrq_removed.vcf.gz
 
