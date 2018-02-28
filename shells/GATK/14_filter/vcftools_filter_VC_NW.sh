2a941116b8e0b4b8db2260373b26539a2cdb48c6
Melis Akman <melisakman@airbears2-10-142-108-109.airbears2.1918.berkeley.edu>
2018-02-27 16:00:16 -0800

    filter vcf NW

--- shells/GATK/14_filter/vcftools_filter_VC_NW.sh
+++ shells/GATK/14_filter/vcftools_filter_VC_NW.sh
@@ -1,13 +1,9 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
 #SBATCH -J filt2
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
 #SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
 #SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/filter_VC_NW.out
 #SBATCH -e /global/home/users/makman/GATK/outs/filter_VC_NW.err
