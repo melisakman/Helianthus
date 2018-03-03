b91c62590960489ad984f2a588794012eda9297a
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-03-03 00:21:41 -0800

    compare GATK and merged

--- shells/GATK/compare_Sariel_GATK/compare_merged_VCMA.sh
+++ shells/GATK/compare_Sariel_GATK/compare_merged_VCMA.sh
@@ -1,9 +1,13 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
 #SBATCH -J compare
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
 #SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
 #SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/compare_Sariel_VCMA.out
 #SBATCH -e /global/home/users/makman/GATK/outs/compare_Sariel_VCMA.err
