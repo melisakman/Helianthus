521904b15157ba6cbd5ba8a3bbd45bfd5c2431e5
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-03-03 00:14:09 -0800

    compare GATK and merged

--- shells/GATK/compare_Sariel_GATK/compare_merged_VCMA.sh
+++ shells/GATK/compare_Sariel_GATK/compare_merged_VCMA.sh
@@ -10,7 +10,8 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat VC_MA_combined_snps_hardfiltered_filtered.vcf.gz VC_MA_combined_indel_hardfiltered_filtered.vcf.gz > VC_MA_combined_all_hardfiltered_filtered.vcf
+# /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat VC_MA_combined_snps_hardfiltered_filtered.vcf.gz VC_MA_combined_indel_hardfiltered_filtered.vcf.gz > VC_MA_combined_all_hardfiltered_filtered.vcf
 # zcat merged_NVC.vcf.gz | /clusterfs/rosalind/users/makman/bcftools/bcftools filter -g 5 -e 'REF="N"'| vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout > merged_NVC_filtered.vcf
-python overlap_vcfs1.py VC_MA_combined_all_hardfiltered_filtered.vcf merged_NVC_filtered.vcf
-python overlap_vcfs2.py merged_NVC_filtered.vcf VC_MA_combined_all_hardfiltered_filtered.vcf
+python overlap_vcfs.py VC_MA_combined_all_hardfiltered_filtered.vcf merged_NVC_filtered.vcf
+# python overlap_vcfs2.py merged_NVC_filtered.vcf VC_MA_combined_all_hardfiltered_filtered.vcf
+
--- shells/GATK/compare_Sariel_GATK/overlap_vcfs.py
+++ shells/GATK/compare_Sariel_GATK/overlap_vcfs.py
@@ -13,6 +13,10 @@ file3 = open(OutputFile1, 'w')
 file4 = open(OutputFile2, 'w')
 file5 = open(OutputFile3, 'w')
 
+count = 0
+
+file2_lines = file2.readlines()
+length =len(file2_lines)
 
 for line1 in file1:
 	found = 0
@@ -22,26 +26,45 @@ for line1 in file1:
 	elif split1[0] == "#CHROM":
 		file3.write(line1)
 	else:
-		chr1 = split1[0]
-		pos1 = split1[1]
-		print "processing SNP on " + pos1 
+		chr1 = str(split1[0])
+		pos1 = int(split1[1])
+		print "processing SNP on " + str(chr1) + " " + str(pos1) 
 	file2.seek(0)
-	for line2 in file2:
+	for line2 in file2_lines[count:]:
 		split2 = line2.split("\t")
 		if split2[0][0] == "#":
-			continue
-		elif split2[0] == "#CHROM":
-			continue
-		else:
-			chr2 = split2[0]
-			pos2 = split2[1]
-			if chr1 == chr2 and pos1 == pos2:
-				file3.write(line1)
-				file4.write(line2)
-				found = 1
-				break				
+			if count < 1636:
+				count += 1
+				continue
 			else:
-				found = 0
+			 	continue
+		elif split2[0] == "#CHROM":
+			if count < 1636:
+				count += 1
+				continue
+			else:
+			 	continue
+		else:
+			chr2 = str(split2[0])
+			pos2 = int(split2[1])
+			if chr1 ==chr2:
+				if pos1 > pos2:
+					count += 1
+					continue
+				elif pos1 == pos2:
+					file3.write(line1)
+					file4.write(line2)
+					count += 1
+					found = 1
+					break
+				else:
+					break		
+					found = 0
+			else:
+				if int(chr1[10:]) < int(chr2[10:]):
+					break
+				else:
+					count +=1
 	if found == 0:
 		file5.write(line1)	
 			
