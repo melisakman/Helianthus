8bf90e33f9675b597a7a17adff41c3e914dc776d
Melis Akman <melisakman@airbears2-10-142-108-132.airbears2.1918.berkeley.edu>
2018-03-06 14:55:55 -0800

    hardfilter apply

--- shells/GATK/14_filter/hardfilter_apply.sh
+++ shells/GATK/14_filter/hardfilter_apply.sh
@@ -16,6 +16,7 @@
 module load gcc/4.8.5 
 module load java
 module load perl
+export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
 /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat VC_MA_combined_snps_info_hardfilter.vcf.gz VC_MA_combined_indel_info_hardfilter.vcf.gz | /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-sort | gzip -c > VC_MA_combined_all_info_hardfilter.vcf.gz
 
