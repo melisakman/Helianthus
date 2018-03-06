eb609749664390403aac811c71113fa541c0c4ff
Melis Akman <melisakman@airbears2-10-142-108-132.airbears2.1918.berkeley.edu>
2018-03-06 15:15:32 -0800

    hardfilter apply

--- shells/GATK/14_filter/hardfilter_apply.sh
+++ shells/GATK/14_filter/hardfilter_apply.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
 #SBATCH -J hfilt
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=1
 #SBATCH --mem=64000
 #SBATCH --ntasks-per-node=1
@@ -12,12 +13,11 @@
 #SBATCH -e /global/home/users/makman/GATK/outs/hardfilterapply_VC_MA.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-module load gcc/4.8.5 
-module load java
-module load perl
-export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat VC_MA_combined_snps_info_hardfilter.vcf.gz VC_MA_combined_indel_info_hardfilter.vcf.gz | /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-sort | gzip -c > VC_MA_combined_all_info_hardfilter.vcf.gz
+module load java
+module load bio/vcftools/0.1.15
+
+vcf-concat VC_MA_combined_snps_info_hardfilter.vcf.gz VC_MA_combined_indel_info_hardfilter.vcf.gz | vcf-sort | gzip -c > VC_MA_combined_all_info_hardfilter.vcf.gz
 
 
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
