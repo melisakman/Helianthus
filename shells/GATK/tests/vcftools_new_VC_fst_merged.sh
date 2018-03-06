d84de4c799534b40e022534c0c02279e9e0bf098
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-03-05 22:33:45 -0800

    compare GATK and merged

--- shells/GATK/tests/vcftools_new_VC_fst_merged.sh
+++ shells/GATK/tests/vcftools_new_VC_fst_merged.sh
@@ -1,30 +1,29 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/vector/scratch/makman/Hubnerdata/Sariel_new_variant_calling/filtered/ordered
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
 #SBATCH -J vcffst
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
 #SBATCH --time=4:00:00
-#SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_new_VC_fst_merged.out
-#SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_new_VC_fst_merged.err
+#SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_VCMA_fst.out
+#SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_VCMA_fst.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-#SBATCH --array=1-17
 
-module load vcftools/0.1.13
-vcftools --gzvcf ../../merged_Nov2017/chr01.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr01_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr02.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr02_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr03.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr03_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr04.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr04_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr05.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr05_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr06.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr06_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr07.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr07_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr08.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr08_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr09.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr09_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr10.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr10_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr11.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr11_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr12.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr12_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr13.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr13_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr14.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr14_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr15.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr15_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr16.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr16_10kb
-vcftools --gzvcf ../../merged_Nov2017/chr17.vcf.gz --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8  --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out /clusterfs/vector/scratch/makman/new_variant_calling/merged/Fst/lr_wd_chr17_10kb
+module load bio/vcftools/0.1.15
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr01 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr01_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr02 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr02_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr03 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr03_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr04 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr04_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr05 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr05_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr06 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr06_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr07 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr07_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr08 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr08_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr09 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr09_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr10 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr10_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr11 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr11_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr12 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr12_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr13 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr13_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr14 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr14_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr15 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr15_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr16 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr16_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr17 --minQ 50 --minDP 5 --maxDP 50 --max-missing 0.8 --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out lr_wd_chr17_10kb
