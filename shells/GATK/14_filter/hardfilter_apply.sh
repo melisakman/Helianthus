425bf9ace499587f50c3ee893e86a76650c6a204
Melis Akman <melisakman@airbears2-10-142-108-132.airbears2.1918.berkeley.edu>
2018-03-06 14:54:35 -0800

    hardfilter apply

---
+++ shells/GATK/14_filter/hardfilter_apply.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
+#SBATCH -J hfilt
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=100:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/hardfilterapply_VC_MA.out
+#SBATCH -e /global/home/users/makman/GATK/outs/hardfilterapply_VC_MA.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load gcc/4.8.5 
+module load java
+module load perl
+
+/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat VC_MA_combined_snps_info_hardfilter.vcf.gz VC_MA_combined_indel_info_hardfilter.vcf.gz | /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-sort | gzip -c > VC_MA_combined_all_info_hardfilter.vcf.gz
+
+
+java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
+	-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+	-V VC_MA_combined_all_info_hardfilter.vcf.gz \
+	--excludeFiltered \
+	-o VC_MA_combined_all_hardfiltered.vcf.gz 
+	
--- shells/SNPRelate/snprelate_SNP.sh
+++ shells/SNPRelate/snprelate_SNP.sh
@@ -12,7 +12,7 @@ setwd("/clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs")
 vcf.fn = "VC_MA_combined_snps_mex_hardfiltered_filtered_hanxrq_removed.vcf.gz"
 snpgdsVCF2GDS(vcf.fn, "test_mex.gds", method="biallelic.only")
 snpgdsSummary("test_mex.gds")
-genofile <- snpgdsOpen("test.gds")
+genofile <- snpgdsOpen("test_mex.gds")
 
 
 set.seed(1000)
@@ -123,4 +123,45 @@ head(tab)
 
 write.csv(tab, file = "PCA_VC_NW.csv")
 
+
+## filtered all VCMA (SNP+indel)
+
+srun --account co_rosalind -p savio2_htc --qos rosalind_htc2_normal --mem=64000 --time=400:00:00 --pty bash
+
+module load r/3.4.3
+
+R
+library(gdsfmt)
+library(SNPRelate)
+setwd("/clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs")
+
+vcf.fn = "VC_MA_combined_all_hardfiltered_filtered.vcf"
+snpgdsVCF2GDS(vcf.fn, "all_filtered.gds", method="biallelic.only")
+snpgdsSummary("all_filtered.gds")
+genofile <- snpgdsOpen("all_filtered.gds")
+
+
+set.seed(1000)
+# Try different LD thresholds for sensitivity analysis
+snpset <- snpgdsLDpruning(genofile, ld.threshold=0.2)
+names(snpset)
+head(snpset$chr1)
+snpset.id <- unlist(snpset)
+
+
+
+pca <- snpgdsPCA(genofile, snp.id=snpset.id, num.thread=2)
+pc.percent <- pca$varprop*100
+head(round(pc.percent, 2))
+tab <- data.frame(sample.id = pca$sample.id, EV1 = pca$eigenvect[,1],    # the first eigenvector
+	EV2 = pca$eigenvect[,2],    # the second eigenvector
+	EV3 = pca$eigenvect[,3],
+	EV4 = pca$eigenvect[,4],
+	EV5 = pca$eigenvect[,5],
+	EV6 = pca$eigenvect[,6],
+	EV7 = pca$eigenvect[,7],
+	stringsAsFactors = FALSE)
+head(tab)
+
+write.table(tab, file = "PCA_SNP_mex.csv")
 
