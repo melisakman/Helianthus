96679d1bf376c07e5cbecb99e2d499e8cb2a472e
Melis Akman <melisakman@airbears2-10-142-108-132.airbears2.1918.berkeley.edu>
2018-03-06 17:13:39 -0800

    hardfilter apply

--- shells/GATK/14_filter/14_1_hardfilter/hardfilter_apply_mex.sh
+++ shells/GATK/14_filter/14_1_hardfilter/hardfilter_apply_mex.sh
@@ -9,8 +9,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
 #SBATCH --time=100:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/hardfilterapply_VC_MA.out
-#SBATCH -e /global/home/users/makman/GATK/outs/hardfilterapply_VC_MA.err
+#SBATCH -o /global/home/users/makman/GATK/outs/hardfilterapply_VC_MA_mex.out
+#SBATCH -e /global/home/users/makman/GATK/outs/hardfilterapply_VC_MA_mex.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
@@ -18,13 +18,13 @@ module load java
 module load bio/vcftools/0.1.15
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
-# vcf-concat VC_MA_combined_snps_info_hardfilter.vcf.gz VC_MA_combined_indel_info_hardfilter.vcf.gz > VC_MA_combined_all_info_hardfilter.vcf
-vcf-sort VC_MA_combined_all_info_hardfilter.vcf > VC_MA_combined_all_info_hardfilter_sorted.vcf
-gzip VC_MA_combined_all_info_hardfilter_sorted.vcf
+vcf-concat VC_MA_combined_snps_mex_info_hardfilter.vcf.gz.tbi VC_MA_combined_indel_mex_info_hardfilter.vcf.gz > VC_MA_combined_mex_all_info_hardfilter.vcf
+vcf-sort VC_MA_combined_mex_all_info_hardfilter.vcf | gzip -c > VC_MA_combined_all_mex_info_hardfilter_sorted.vcf.gz
+
 
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
 	-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
-	-V VC_MA_combined_all_info_hardfilter_sorted.vcf.gz \
+	-V VC_MA_combined_all_mex_info_hardfilter_sorted.vcf.gz \
 	--excludeFiltered \
-	-o VC_MA_combined_all_hardfiltered.vcf.gz 
+	-o VC_MA_combined_mex_all_hardfiltered.vcf.gz 
 	
