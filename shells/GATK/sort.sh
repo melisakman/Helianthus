a146e761764d9ed7adc281915c9b531b7d4e7e06
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-03-07 15:33:00 -0800

    hardfilter apply

--- shells/GATK/sort.sh
+++ shells/GATK/sort.sh
@@ -16,22 +16,22 @@
 module load bio/vcftools/0.1.15
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr01 > VC_MA_combined_all_info_hardfilter_Chr01_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr02 > VC_MA_combined_all_info_hardfilter_Chr02_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr03 > VC_MA_combined_all_info_hardfilter_Chr03_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr04 > VC_MA_combined_all_info_hardfilter_Chr04_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr05 > VC_MA_combined_all_info_hardfilter_Chr05_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr06 > VC_MA_combined_all_info_hardfilter_Chr06_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr07 > VC_MA_combined_all_info_hardfilter_Chr07_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr08 > VC_MA_combined_all_info_hardfilter_Chr08_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr09 > VC_MA_combined_all_info_hardfilter_Chr09_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr10 > VC_MA_combined_all_info_hardfilter_Chr10_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr11 > VC_MA_combined_all_info_hardfilter_Chr11_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr12 > VC_MA_combined_all_info_hardfilter_Chr12_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr13 > VC_MA_combined_all_info_hardfilter_Chr13_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr14 > VC_MA_combined_all_info_hardfilter_Chr14_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr15 > VC_MA_combined_all_info_hardfilter_Chr15_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr16 > VC_MA_combined_all_info_hardfilter_Chr16_sorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr17 > VC_MA_combined_all_info_hardfilter_Chr17_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr01 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr01_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr02 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr02_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr03 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr03_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr04 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr04_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr05 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr05_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr06 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr06_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr07 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr07_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr08 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr08_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr09 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr09_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr10 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr10_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr11 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr11_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr12 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr12_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr13 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr13_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr14 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr14_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr15 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr15_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr16 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr16_sorted.vcf
+vcf-annotate -a VC_MA_combined_all_info_hardfilter.vcf -c HanXRQChr17 | vcf-sort -t /clusterfs/rosalind/users/makman/temp > VC_MA_combined_all_info_hardfilter_Chr17_sorted.vcf
 
 gzip VC_MA_combined_all_info_hardfilter_Chr*_sorted.vcf
