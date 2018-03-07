8f9cb335e9caf11d0657dd39cd8c3dd38d0c380c
Melis Akman <melisakman@airbears2-10-142-108-132.airbears2.1918.berkeley.edu>
2018-03-06 17:26:44 -0800

    hardfilter apply

--- shells/GATK/14_filter/14_1_hardfilter/hardfilter_apply.sh
+++ shells/GATK/14_filter/14_1_hardfilter/hardfilter_apply.sh
@@ -19,7 +19,7 @@ module load bio/vcftools/0.1.15
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
 # vcf-concat VC_MA_combined_snps_info_hardfilter.vcf.gz VC_MA_combined_indel_info_hardfilter.vcf.gz > VC_MA_combined_all_info_hardfilter.vcf
-vcf-sort --temp /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf > VC_MA_combined_all_info_hardfilter_sorted.vcf
+vcf-sort -t /clusterfs/rosalind/users/makman/temp VC_MA_combined_all_info_hardfilter.vcf > VC_MA_combined_all_info_hardfilter_sorted.vcf
 gzip VC_MA_combined_all_info_hardfilter_sorted.vcf
 
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
