0bf7e3f7aede79de2a7541b59e336d8b7a1f374e
Melis Akman <melisakman@airbears2-10-142-108-164.airbears2.1918.berkeley.edu>
2018-03-08 10:12:31 -0800

    hardfilter apply

--- shells/GATK/14_filter/14_2_filter/vcftools_filter_2.sh
+++ shells/GATK/14_filter/14_2_filter/vcftools_filter_2.sh
@@ -2,8 +2,8 @@
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
 #SBATCH -J filt2
 #SBATCH --account=co_rosalind
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=1
 #SBATCH --mem=64000
 #SBATCH --ntasks-per-node=1
@@ -17,4 +17,4 @@ module load bio/vcftools/0.1.15
 module load gcc/4.8.5 
 
 zcat VC_MA_combined_all_hardfiltered.vcf.gz | /clusterfs/rosalind/users/makman/bcftools/bcftools filter -g 5 -e 'REF="N"'| vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | gzip -c > VC_MA_combined_all_hardfiltered_filtered.vcf.gz
-zcat VC_MA_combined_mex_all_hardfiltered.vcf.gz | /clusterfs/rosalind/users/makman/bcftools/bcftools filter -g 5 -e 'REF="N"'| vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | gzip -c > VC_MA_combined_mex_all_hardfiltered_filtered.vcf.gz
+# zcat VC_MA_combined_mex_all_hardfiltered.vcf.gz | /clusterfs/rosalind/users/makman/bcftools/bcftools filter -g 5 -e 'REF="N"'| vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | gzip -c > VC_MA_combined_mex_all_hardfiltered_filtered.vcf.gz
