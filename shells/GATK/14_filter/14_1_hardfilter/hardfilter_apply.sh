79c2b8eef393d1a516687529ee22c1d503ef2531
Melis Akman <melisakman@airbears2-10-142-108-164.airbears2.1918.berkeley.edu>
2018-03-08 10:14:11 -0800

    hardfilter apply

--- shells/GATK/14_filter/14_1_hardfilter/hardfilter_apply.sh
+++ shells/GATK/14_filter/14_1_hardfilter/hardfilter_apply.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=64000
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
-#SBATCH --time=100:00:00
+#SBATCH --time=48:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/hardfilterapply_VC_MA.out
 #SBATCH -e /global/home/users/makman/GATK/outs/hardfilterapply_VC_MA.err
 #SBATCH --mail-user=makman@berkeley.edu
--- shells/GATK/14_filter/14_2_filter/vcftools_filter_2.sh
+++ shells/GATK/14_filter/14_2_filter/vcftools_filter_2.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=64000
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
-#SBATCH --time=400:00:00
+#SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/filter_VC_MA2.out
 #SBATCH -e /global/home/users/makman/GATK/outs/filter_VC_MA2.err
 #SBATCH --mail-user=makman@berkeley.edu
