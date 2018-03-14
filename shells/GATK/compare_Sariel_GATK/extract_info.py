e1f142360491d08e6b78628a0406f7b39b9ca1f5
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-03-13 23:03:50 -0700

    allele_freq

--- shells/GATK/compare_Sariel_GATK/extract_info.py
+++ shells/GATK/compare_Sariel_GATK/extract_info.py
@@ -2,7 +2,7 @@ import sys
 
 InputFile1 = sys.argv[1] # vcf file
 
-OutputFile1 = "allele_freq_overlap_VC_MA.txt" 
+OutputFile1 = "allele_freq_nonoverlap_VCMA_properlyFiletered.txt" 
 
 file1 = open(InputFile1, "r")
 file2 = open(OutputFile1, 'w')
@@ -24,38 +24,41 @@ for line1 in file1:
 	cv_alt = 0
 
 	splt1 = line1.split("\t")
-	ref = splt1[3]
-	alt = splt1[4]
-	pos = splt1[1]
-	chr = splt1[0]	
-	allele_list = []
-	allele_list= splt1[9:20] + splt1[21:26] + splt1[27:30] + splt1[31:53] + splt1[55:61] + splt1[62:70] + splt1[71:76]
-	for j in range_wd:
-		print j
-		if str(allele_list[j][0:3]) == "0/0":
-			wild_ref += 1
-		elif str(allele_list[j][0:3]) == "0/1":
-			wild_hetero += 1
-		elif str(allele_list[j][0:3]) == "1/1":
-			wild_alt += 1
-	for k in range_lr:
-		if str(allele_list[k][0:3]) == "0/0":
-			lr_ref += 1
-		elif str(allele_list[k][0:3]) == "0/1":
-			lr_hetero += 1
-		elif str(allele_list[k][0:3]) == "1/1":
-			lr_alt += 1
-	for m in range_cv:
-		if str(allele_list[m][0:3]) == "0/0":
-			cv_ref += 1
-		elif str(allele_list[m][0:3]) == "0/1":
-			cv_hetero += 1
-		elif str(allele_list[m][0:3]) == "1/1":
-			cv_alt += 1
-	file2.write(str(chr) + ";" + str(pos) + ";" + str(ref) + "/" + str(alt) +  "\t" + 
-	str(wild_ref) + "/" + str(wild_hetero) + "/" + str(wild_alt) + "\t" + str(lr_ref) + 
-	"/" + str(lr_hetero) + "/" + str(lr_alt) + "\t" + str(cv_ref) + "/" + str(cv_hetero) + 
-	"/" + str(cv_alt) + "\n")
+	if splt1[0][0] == "#":
+		continue
+	else:
+		ref = splt1[3]
+		alt = splt1[4]
+		pos = splt1[1]
+		chr = splt1[0]	
+		allele_list = []
+		allele_list= splt1[9:20] + splt1[21:26] + splt1[27:30] + splt1[31:53] + splt1[55:61] + splt1[62:70] + splt1[71:76]
+		for j in range_wd:	
+			print j
+			if str(allele_list[j][0:3]) == "0/0":
+				wild_ref += 1
+			elif str(allele_list[j][0:3]) == "0/1":
+				wild_hetero += 1
+			elif str(allele_list[j][0:3]) == "1/1":
+				wild_alt += 1
+		for k in range_lr:
+			if str(allele_list[k][0:3]) == "0/0":
+				lr_ref += 1
+			elif str(allele_list[k][0:3]) == "0/1":
+				lr_hetero += 1
+			elif str(allele_list[k][0:3]) == "1/1":
+				lr_alt += 1
+		for m in range_cv:
+			if str(allele_list[m][0:3]) == "0/0":
+				cv_ref += 1
+			elif str(allele_list[m][0:3]) == "0/1":
+				cv_hetero += 1
+			elif str(allele_list[m][0:3]) == "1/1":
+				cv_alt += 1
+		file2.write(str(chr) + ";" + str(pos) + ";" + str(ref) + "/" + str(alt) +  "\t" + 
+		str(wild_ref) + "/" + str(wild_hetero) + "/" + str(wild_alt) + "\t" + str(lr_ref) + 
+		"/" + str(lr_hetero) + "/" + str(lr_alt) + "\t" + str(cv_ref) + "/" + str(cv_hetero) + 
+		"/" + str(cv_alt) + "\n")
 	
 
 
