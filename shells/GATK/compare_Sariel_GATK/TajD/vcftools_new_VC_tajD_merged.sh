faf20f742d1bf5be1534953ef98879d7c8cd33c5
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-03-15 12:52:50 -0700

    pi,tajD

---
+++ shells/GATK/compare_Sariel_GATK/TajD/vcftools_new_VC_tajD_merged.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/vector/scratch/makman/Hubnerdata/Sariel_new_variant_calling/filtered/ordered
+#SBATCH -J tajD
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=200:00:00
+#SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_VCMA_merged_tajD.out
+#SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_VCMA_merged_tajD.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --array=1-34
+module load vcftools/0.1.13
+
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_list.txt --max-missing 0.8 --TajimaD 10000 --out tajD/VCMA_wd_tajD_10kb
+vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --minQ 50 --minDP 3 --maxDP 25 --keep lr_list.txt --max-missing 0.8 --TajimaD 10000 --out tajD/VCMA_lr_tajD_10kb
+
+vcftools --vcf merged_NVC_filtered_dups_removed.vcf --minQ 50 --minDP 3 --maxDP 25 --keep wd_merged_list.txt --max-missing 0.8 --TajimaD 10000 --out tajD/merged_wd_tajD_10kb
+vcftools --vcf merged_NVC_filtered_dups_removed.vcf --minQ 50 --minDP 3 --maxDP 25 --keep lr_merged_list.txt --max-missing 0.8 --TajimaD 10000 --out tajD/merged_lr_tajD_10kb
