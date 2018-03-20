328f910d6ebff58e435925103978c0d17c70c761
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-03-19 22:39:50 -0700

    G12 merged

--- shells/H12/vcf2genotype_merged.sh
+++ shells/H12/vcf2genotype_merged.sh
@@ -1,12 +1,14 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/ready/sams/gvcfs
 #SBATCH -J vcf2genotype
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem=8000
-#SBATCH --time=48:00:00
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/H12/outs/vcf2genotype_merged_filtered.out
 #SBATCH -e /global/home/users/makman/H12/outs/vcf2genotype_merged_filtered.err
 #SBATCH --mail-user=makman@berkeley.edu
