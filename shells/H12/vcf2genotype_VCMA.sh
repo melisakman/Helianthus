c340a8bcaf14f71cc9c8e13b8e4f90a90b20ab00
Melis Akman <melisakman@airbears2-10-142-109-89.airbears2.1918.berkeley.edu>
2018-03-20 09:47:41 -0700

    G12 VCMA

--- shells/H12/vcf2genotype_VCMA.sh
+++ shells/H12/vcf2genotype_VCMA.sh
@@ -16,23 +16,23 @@ module load bio/vcftools/0.1.15
 
 
 
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr01 --recode --stdout > VCMA_filtered_chr01.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr02 --recode --stdout > VCMA_filtered_chr02.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr03 --recode --stdout > VCMA_filtered_chr03.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr04 --recode --stdout > VCMA_filtered_chr04.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr05 --recode --stdout > VCMA_filtered_chr05.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr06 --recode --stdout > VCMA_filtered_chr06.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr07 --recode --stdout > VCMA_filtered_chr07.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr08 --recode --stdout > VCMA_filtered_chr08.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr09 --recode --stdout > VCMA_filtered_chr09.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr10 --recode --stdout > VCMA_filtered_chr10.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr11 --recode --stdout > VCMA_filtered_chr11.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr12 --recode --stdout > VCMA_filtered_chr12.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr13 --recode --stdout > VCMA_filtered_chr13.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr14 --recode --stdout > VCMA_filtered_chr14.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr15 --recode --stdout > VCMA_filtered_chr15.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr16 --recode --stdout > VCMA_filtered_chr16.vcf
-vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr17 --recode --stdout > VCMA_filtered_chr17.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr01 --recode --stdout > VCMA_filtered_chr01.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr02 --recode --stdout > VCMA_filtered_chr02.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr03 --recode --stdout > VCMA_filtered_chr03.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr04 --recode --stdout > VCMA_filtered_chr04.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr05 --recode --stdout > VCMA_filtered_chr05.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr06 --recode --stdout > VCMA_filtered_chr06.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr07 --recode --stdout > VCMA_filtered_chr07.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr08 --recode --stdout > VCMA_filtered_chr08.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr09 --recode --stdout > VCMA_filtered_chr09.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr10 --recode --stdout > VCMA_filtered_chr10.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr11 --recode --stdout > VCMA_filtered_chr11.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr12 --recode --stdout > VCMA_filtered_chr12.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr13 --recode --stdout > VCMA_filtered_chr13.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr14 --recode --stdout > VCMA_filtered_chr14.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr15 --recode --stdout > VCMA_filtered_chr15.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr16 --recode --stdout > VCMA_filtered_chr16.vcf
+# vcftools --vcf VC_MA_combined_all_hardfiltered_filtered.vcf --chr HanXRQChr17 --recode --stdout > VCMA_filtered_chr17.vcf
 
 
 python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py VCMA_filtered_chr01.vcf /clusterfs/rosalind/users/makman/G12/lr_cv_G12_VCMA_samples.txt /clusterfs/rosalind/users/makman/G12/VCMA_filtered_chr01_G12_input.txt
