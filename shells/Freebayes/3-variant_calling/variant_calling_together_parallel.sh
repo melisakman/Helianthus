5725ab42aaad09ae2cdbc31f2a65be9a4f286d86
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-04-09 13:58:34 -0700

    freebayes

--- shells/Freebayes/3-variant_calling/variant_calling_together_parallel.sh
+++ shells/Freebayes/3-variant_calling/variant_calling_together_parallel.sh
@@ -15,8 +15,8 @@
 #SBATCH --mail-type=All
 # module load freebayes/v1.1.0-56-ga180635
 module load parallel/20180322
-./fasta_generate_regions.py /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa.fai 100000 > /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa.100kbp.regions
-./freebayes-parallel /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa.100kbp.regions 20 -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -r HanXRQChr01 /clusterfs/rosalind/users/makman/GATK/bams/Anzac_Pueblo_sorted_markdup_recal_RG.bam \
+# ./fasta_generate_regions.py /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa.fai 100000 > /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa.100kbp.regions
+./freebayes-parallel 20 -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -r HanXRQChr01 /clusterfs/rosalind/users/makman/GATK/bams/Anzac_Pueblo_sorted_markdup_recal_RG.bam \
 /clusterfs/rosalind/users/makman/GATK/bams/Arikara_sorted_markdup_recal_RG.bam \
 /clusterfs/rosalind/users/makman/GATK/bams/HA124_sorted_markdup_recal_RG.bam \
 /clusterfs/rosalind/users/makman/GATK/bams/HA316_sorted_markdup_recal_RG.bam \
