ae817037a6a39d54bd92bdd8d1a33891602f64c4
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-04-12 20:48:32 -0700

    freebayes failed

--- shells/Freebayes/3-variant_calling/variant_calling_together13.sh
+++ shells/Freebayes/3-variant_calling/variant_calling_together13.sh
@@ -1,17 +1,20 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bams
 #SBATCH -J fb13
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
 #SBATCH --nodes=1
-#SBATCH --mem=64000
+#SBATCH --mem=128000
 #SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
 #SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/freebayes_combined_chr13.out
 #SBATCH -e /global/home/users/makman/GATK/outs/freebayes_combined_chr13.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load freebayes/v1.1.0-56-ga180635
+TMPDIR=/clusterfs/rosalind/users/makman/temp
 
 freebayes -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -r HanXRQChr13 Anzac_Pueblo_sorted_markdup_recal_RG.bam \
 Arikara_sorted_markdup_recal_RG.bam \
