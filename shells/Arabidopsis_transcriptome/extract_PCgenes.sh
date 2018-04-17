d6ce85c601d4a62ce3bb2421db8f16602df14e26
Melis Akman <melisakman@airbears2-10-142-109-164.airbears2.1918.berkeley.edu>
2018-04-17 12:36:22 -0700

    Arabidopsis extract snps

---
+++ shells/Arabidopsis_transcriptome/extract_PCgenes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/Arabidopsis
+#SBATCH -J snpID
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --mem=16000
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/Arabidopsis/extractPC1_genes.out
+#SBATCH -e /global/home/users/makman/Arabidopsis/extractPC1_genes.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+python extract_variant_snpID.py testy_recode_snpid_fixed.vcf V1_ordered_10K.csv V1_top10K.vcf V1_top10K_pos.txt
