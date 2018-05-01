520c78e656e08c923872ccca81f42b4067f27613
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-01 11:40:50 -0700

    freebayes chr6 invariant

--- shells/Freebayes/3-variant_calling/invariant/variant_calling_together6_invariant12.sh
+++ shells/Freebayes/3-variant_calling/invariant/variant_calling_together6_invariant12.sh
@@ -1,9 +1,8 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bams
 #SBATCH -J fb6-in12
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=800:00:00
