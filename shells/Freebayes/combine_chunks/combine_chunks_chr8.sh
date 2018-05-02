32305b3d85c2bdd27583e205e8becc89d8631666
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-02 10:32:41 -0700

    concat chr08

--- shells/Freebayes/combine_chunks/combine_chunks_chr8.sh
+++ shells/Freebayes/combine_chunks/combine_chunks_chr8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/VCMA_ethno
-#SBATCH -J cat-sort
+#SBATCH -J chr8_cat
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio
 #SBATCH --qos=rosalind_savio_normal
@@ -9,10 +9,10 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
 #SBATCH --time=100:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/VCMA_ethno_combine_snp_indel.out
-#SBATCH -e /global/home/users/makman/GATK/outs/VCMA_ethno_combine_snp_indel.err
+#SBATCH -o /global/home/users/makman/GATK/outs/chunks_combine_chr8.out
+#SBATCH -e /global/home/users/makman/GATK/outs/chunks_combine_chr8.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
+module load bio/vcftools/0.1.15
 TMPDIR=/clusterfs/rosalind/users/makman/temp
-# /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat VC_MA_combined_snps_ethno_hardfiltered_filtered.vcf.gz VC_MA_combined_indel_ethno_hardfiltered_filtered.vcf.gz > VC_MA_combined_all_ethno_hardfiltered_filtered.vcf.gz
+vcf-concat freebayes_combined_chr08.vcf freebayes_combined_chr08_chunk1.vcf freebayes_combined_chr08_chunk2.vcf freebayes_combined_chr08_chunk3.vcf > freebayes_combined_chr08_chunksTogether.vcf
