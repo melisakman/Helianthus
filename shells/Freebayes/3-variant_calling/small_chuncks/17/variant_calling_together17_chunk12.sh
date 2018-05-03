7dfa36cbcfe33df8416724b5e9a1d64406d63d77
Melis Akman <melisakman@airbears2-10-142-109-194.airbears2.1918.berkeley.edu>
2018-05-03 10:12:02 -0700

    chr02

--- shells/Freebayes/3-variant_calling/small_chuncks/17/variant_calling_together17_chunk12.sh
+++ shells/Freebayes/3-variant_calling/small_chuncks/17/variant_calling_together17_chunk12.sh
@@ -6,14 +6,14 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=800:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/freebayes_combined_chr17_chunk12.out
-#SBATCH -e /global/home/users/makman/GATK/outs/freebayes_combined_chr17_chunk12.err
+#SBATCH -o /global/home/users/makman/GATK/outs/freebayes_combined_chr17_chunk12_1.out
+#SBATCH -e /global/home/users/makman/GATK/outs/freebayes_combined_chr17_chunk12_1.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load freebayes/v1.1.0-56-ga180635
 TMPDIR=/clusterfs/rosalind/users/makman/temp
 
-freebayes -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -r HanXRQChr17:185606340-195606339 Anzac_Pueblo_sorted_markdup_recal_RG.bam \
+freebayes -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa -r HanXRQChr17:195500138-195606339 Anzac_Pueblo_sorted_markdup_recal_RG.bam \
 Arikara_sorted_markdup_recal_RG.bam \
 HA124_sorted_markdup_recal_RG.bam \
 HA316_sorted_markdup_recal_RG.bam \
@@ -89,4 +89,4 @@ Mex_A1516_sorted_markdup_readGroups_recal_RG.bam \
 Mex_A1517_sorted_markdup_readGroups_recal_RG.bam \
 Mex_A1572_sorted_markdup_readGroups_recal_RG.bam \
 Mex_A1574_sorted_markdup_readGroups_recal_RG.bam \
-Mex_Ann261_sorted_markdup_readGroups_recal_RG.bam > ../freebayes/freebayes_combined_chr17_chunk12.vcf
+Mex_Ann261_sorted_markdup_readGroups_recal_RG.bam > ../freebayes/freebayes_combined_chr17_chunk12-1.vcf
