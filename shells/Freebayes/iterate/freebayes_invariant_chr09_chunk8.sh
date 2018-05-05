6a0f01b907aef6282899692a83be549273366afa
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-04 22:17:24 -0700

    Freebayes invariants

--- shells/Freebayes/iterate/freebayes_invariant_chr09_chunk8.sh
+++ shells/Freebayes/iterate/freebayes_invariant_chr09_chunk8.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bams
 #SBATCH -J fb09-8
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=2
 #SBATCH --time=800:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/freebayes_invariant_chr09_chunk8.out
 #SBATCH -e /global/home/users/makman/GATK/outs/freebayes_invariant_chr09_chunk8.err
