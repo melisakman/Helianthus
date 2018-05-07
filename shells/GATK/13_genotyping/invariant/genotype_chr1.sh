ffb3babcd93ecdd357bee76203d6fd55c348f413
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-07 16:02:28 -0700

    GATK invariants

--- shells/GATK/13_genotyping/invariant/genotype_chr1.sh
+++ shells/GATK/13_genotyping/invariant/genotype_chr1.sh
@@ -16,7 +16,8 @@
 
 module load java
 module load gatk/3.8.1
-java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk/3.8.0/GenomeAnalysisTK.jar GenotypeGVCFs --includeNonVariantSites \
+java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk/3.8.0/GenomeAnalysisTK.jar GenotypeGVCFs \
 -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+--includeNonVariantSites \
 -V combined_chr01_ethno.g.vcf.gz \
 -O ../../invariants/VCMA_chr01.vcf.gz
