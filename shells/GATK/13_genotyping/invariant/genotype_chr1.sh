5c592fa8fe0ade3aeae921170f00c804c4c7e217
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-07 15:20:49 -0700

    GATK invariants

---
+++ shells/GATK/13_genotyping/invariant/genotype_chr1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/gvcf/combined
+#SBATCH -J gntyp1M
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/genotype_chr1_invariant.out
+#SBATCH -e /global/home/users/makman/GATK/outs/genotype_chr1_invariant.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load java
+
+/clusterfs/rosalind/users/makman/gatk-4.0.0.0/gatk --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G" GenotypeGVCFs --includeNonVariantSites \
+-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+-V combined_chr01_ethno.g.vcf.gz \
+-O ../../invariant/VCMA_chr01.vcf.gz
