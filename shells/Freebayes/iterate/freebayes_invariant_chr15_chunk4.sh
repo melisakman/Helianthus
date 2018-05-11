d54a55af310667b6cd6d3bb21d1ab6defbf85f3e
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-10 23:49:59 -0700

    freebayes chr15

--- shells/Freebayes/iterate/freebayes_invariant_chr15_chunk4.sh
+++ shells/Freebayes/iterate/freebayes_invariant_chr15_chunk4.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bams
 #SBATCH -J fb15-4
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=800:00:00
@@ -14,7 +13,7 @@
 module load freebayes/v1.1.0-56-ga180635
 TMPDIR=/clusterfs/rosalind/users/makman/temp
 
-freebayes -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa --report-monomorphic -r HanXRQChr15:24000001-32000000\
+freebayes -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa --report-monomorphic -r HanXRQChr15:24000001-32000000 \
 Anzac_Pueblo_sorted_markdup_recal_RG.bam \
 Arikara_sorted_markdup_recal_RG.bam \
 HA124_sorted_markdup_recal_RG.bam \
