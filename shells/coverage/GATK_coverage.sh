3b33fdffdccfa2f6d918b536194405f0f8d82be2
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-16 12:48:14 -1000

    coverage

--- shells/coverage/GATK_coverage.sh
+++ shells/coverage/GATK_coverage.sh
@@ -10,7 +10,7 @@
 #SBATCH -e /global/home/users/makman/GATK/outs/coverage.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-
+module load java 
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
 -T DepthOfCoverage -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
 -o ../../coverage_haplotype/coverage -nct 5 -I Anzac_Pueblo_sorted_markdup_recal_RG.bam \
