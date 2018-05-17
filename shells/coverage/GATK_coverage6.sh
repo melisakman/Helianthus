706b8680ec3fe7221f9b6745199038ca273e281c
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-16 21:46:38 -1000

    coverage

---
+++ shells/coverage/GATK_coverage6.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bams
+#SBATCH -J coverage6
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=800:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/coverage6.out
+#SBATCH -e /global/home/users/makman/GATK/outs/coverage6.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load java 
+java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
+-T DepthOfCoverage -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+-L HanXRQChr06 -o ../../coverage_haplotype/coverage_chr06 -I ./bams.list
---
+++ shells/coverage/GATK_coverage7.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bams
+#SBATCH -J coverage7
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=800:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/coverage7.out
+#SBATCH -e /global/home/users/makman/GATK/outs/coverage7.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load java 
+java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
+-T DepthOfCoverage -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+-L HanXRQChr07 -o ../../coverage_haplotype/coverage_chr07 -I ./bams.list
