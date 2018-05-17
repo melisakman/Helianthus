dcdb4f6a2428e970e7ccca1fda1057857a1f0e81
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-16 22:02:14 -1000

    coverage

---
+++ shells/coverage/GATK_coverage12.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bams
+#SBATCH -J coverage12
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --time=800:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/coverage12.out
+#SBATCH -e /global/home/users/makman/GATK/outs/coverage12.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load java 
+java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
+-T DepthOfCoverage -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+-L HanXRQChr12 -o ../../coverage_haplotype/coverage_chr12 -I ./bams.list
---
+++ shells/coverage/GATK_coverage17.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bams
+#SBATCH -J coverage17
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --time=800:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/coverage17.out
+#SBATCH -e /global/home/users/makman/GATK/outs/coverage17.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load java 
+java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
+-T DepthOfCoverage -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+-L HanXRQChr17 -o ../../coverage_haplotype/coverage_chr17 -I ./bams.list
