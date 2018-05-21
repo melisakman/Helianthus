d5c53b5534f578042cf06fe075798e7ad77d260c
Melis Akman <melisakman@airbears2-10-142-109-29.airbears2.1918.berkeley.edu>
2018-05-21 15:06:14 -0700

    freebayes gzip

---
+++ shells/Freebayes/gzip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/freebayes/combined/
+#SBATCH -J gzip
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --time=800:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/freebayes_gzip.out
+#SBATCH -e /global/home/users/makman/GATK/outs/freebayes_gzip.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+gzip ./*.vcf
