77ddb591fbcbd6b09a5dda2c912dc96875249864
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-22 14:52:10 -0700

    freebayes Anzac Pueblo

---
+++ shells/Freebayes/3-variant_calling/invariant/Anzac_Pueblo/freebayes_invariant_Anzac_Pueblo_chr09.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bams
+#SBATCH -J APchr09
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=800:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/freebayes_invariant_chr09_Anzac.out
+#SBATCH -e /global/home/users/makman/GATK/outs/freebayes_invariant_chr09_Anzac.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load freebayes/v1.1.0-56-ga180635
+TMPDIR=/clusterfs/rosalind/users/makman/temp
+
+freebayes -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa --report-monomorphic -r HanXRQChr09 \
+Anzac_Pueblo_sorted_markdup_recal_RG.bam > ../freebayes/freebayes_invariant_chr09_AnzacPueblo.vcf
