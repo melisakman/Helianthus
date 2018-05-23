909f087f2b52b33d8f81a8c1201ecf8339ee157f
Melis Akman <melisakman@airbears2-10-142-109-45.airbears2.1918.berkeley.edu>
2018-05-23 15:14:14 -0700

    freebayes Anzac canceled

--- shells/Freebayes/3-variant_calling/invariant/Anzac_Pueblo/freebayes_invariant_Anzac_Pueblo_chr04.sh
+++ shells/Freebayes/3-variant_calling/invariant/Anzac_Pueblo/freebayes_invariant_Anzac_Pueblo_chr04.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bams
-#SBATCH -J APchr04
-#SBATCH --account=fc_blackman
-#SBATCH --partition=savio2
-#SBATCH --time=72:00:00
-#SBATCH --qos=savio_normal
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH -o /global/home/users/makman/GATK/outs/freebayes_invariant_chr04_Anzac.out
-#SBATCH -e /global/home/users/makman/GATK/outs/freebayes_invariant_chr04_Anzac.err
+#SBATCH -J APchr04b
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/freebayes_invariant_chr04_Anzac_b.out
+#SBATCH -e /global/home/users/makman/GATK/outs/freebayes_invariant_chr04_Anzac_b.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load freebayes/v1.1.0-56-ga180635
 TMPDIR=/clusterfs/rosalind/users/makman/temp
 
-freebayes -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa --report-monomorphic -r HanXRQChr04 \
-Anzac_Pueblo_sorted_markdup_recal_RG.bam > ../freebayes/freebayes_invariant_chr04_AnzacPueblo.vcf
+freebayes -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa --report-monomorphic -r HanXRQChr04:142687842- \
+Anzac_Pueblo_sorted_markdup_recal_RG.bam > ../freebayes/freebayes_invariant_chr04_AnzacPueblo_b.vcf
