6777fa90bd0bf282ee2e14f2acd87536e0fa7c52
Melis Akman <melisakman@airbears2-10-142-109-45.airbears2.1918.berkeley.edu>
2018-05-23 15:46:51 -0700

    freebayes ends canceled all

--- shells/Freebayes/3-variant_calling/invariant/freebayes_invariant_chr13_chunk25.sh
+++ shells/Freebayes/3-variant_calling/invariant/freebayes_invariant_chr13_chunk25.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bams
 #SBATCH -J fb13-25
-#SBATCH --account=fc_blackman
-#SBATCH --partition=savio2
-#SBATCH --time=72:00:00
-#SBATCH --qos=savio_normal
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
+#SBATCH --time=800:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/freebayes_invariant_chr13_chunk25.out
 #SBATCH -e /global/home/users/makman/GATK/outs/freebayes_invariant_chr13_chunk25.err
 #SBATCH --mail-user=makman@berkeley.edu
@@ -14,7 +14,7 @@
 module load freebayes/v1.1.0-56-ga180635
 TMPDIR=/clusterfs/rosalind/users/makman/temp
 
-freebayes -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa --report-monomorphic -r HanXRQChr13:192000001- \
+freebayes -f /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa --report-monomorphic -r HanXRQChr13:193344322- \
 Arikara_sorted_markdup_recal_RG.bam \
 HA124_sorted_markdup_recal_RG.bam \
 HA316_sorted_markdup_recal_RG.bam \
@@ -90,4 +90,4 @@ Mex_A1516_sorted_markdup_readGroups_recal_RG.bam \
 Mex_A1517_sorted_markdup_readGroups_recal_RG.bam \
 Mex_A1572_sorted_markdup_readGroups_recal_RG.bam \
 Mex_A1574_sorted_markdup_readGroups_recal_RG.bam \
-Mex_Ann261_sorted_markdup_readGroups_recal_RG.bam > ../freebayes/freebayes_invariant_chr13_chunk25.vcf
+Mex_Ann261_sorted_markdup_readGroups_recal_RG.bam > ../freebayes/freebayes_invariant_chr13_chunk25b.vcf
