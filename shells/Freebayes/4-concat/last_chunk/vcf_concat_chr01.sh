af48eb764c56953e2c90fe64b2e685d484b89272
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-24 10:46:59 -0700

    freebayes concat 01

--- shells/Freebayes/4-concat/last_chunk/vcf_concat_chr01.sh
+++ shells/Freebayes/4-concat/last_chunk/vcf_concat_chr01.sh
@@ -10,15 +10,19 @@
 #SBATCH -e /global/home/users/makman/GATK/outs/freebayes_vcf_concat_chr01_all.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-# export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
-# TMPDIR=/clusterfs/rosalind/users/makman/temp
+export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
+TMPDIR=/clusterfs/rosalind/users/makman/temp
 # /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
 # freebayes_invariant_chr01_combined.vcf freebayes_invariant_chr01_chunk20.vcf > freebayes_invariant_chr01_combined_end.vcf
 # 
 # /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
 # freebayes_invariant_chr01_AnzacPueblo.vcf freebayes_invariant_chr01_AnzacPueblo_b.vcf > freebayes_invariant_chr01_AnzacPueblo_all.vcf
 
-module load bio/vcftools/0.1.15
-export PERL5LIB=/global/software/sl-7.x86_64/modules/apps/bio/vcftools/0.1.15/share/perl5/
-export PATH = $PATH:/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix
-vcf-merge freebayes_invariant_chr01_combined_end.vcf freebayes_invariant_chr01_AnzacPueblo_all.vcf > freebayes_invariant_chr01_all.vcf
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes_invariant_chr01_combined_end.vcf > freebayes_invariant_chr01_combined_end.vcf.gz 
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes_invariant_chr01_AnzacPueblo_all.vcf > freebayes_invariant_chr01_AnzacPueblo_all.vcf.gz 
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf freebayes_invariant_chr01_combined_end.vcf.gz  
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf freebayes_invariant_chr01_AnzacPueblo_all.vcf.gz  
+
+
+/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-merge freebayes_invariant_chr01_combined_end.vcf.gz freebayes_invariant_chr01_AnzacPueblo_all.vcf.gz > freebayes_invariant_chr01_all.vcf.gz
