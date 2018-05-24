dd9caddfe7cbfb89349b47b1dd277ae0ae91f207
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-24 10:26:46 -0700

    freebayes concat 01

--- shells/Freebayes/4-concat/last_chunk/vcf_concat_chr01.sh
+++ shells/Freebayes/4-concat/last_chunk/vcf_concat_chr01.sh
@@ -10,13 +10,14 @@
 #SBATCH -e /global/home/users/makman/GATK/outs/freebayes_vcf_concat_chr01_all.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
-TMPDIR=/clusterfs/rosalind/users/makman/temp
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
-freebayes_invariant_chr01_combined.vcf freebayes_invariant_chr01_chunk20.vcf > freebayes_invariant_chr01_combined_end.vcf
+# export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
+# TMPDIR=/clusterfs/rosalind/users/makman/temp
+# /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
+# freebayes_invariant_chr01_combined.vcf freebayes_invariant_chr01_chunk20.vcf > freebayes_invariant_chr01_combined_end.vcf
+# 
+# /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
+# freebayes_invariant_chr01_AnzacPueblo.vcf freebayes_invariant_chr01_AnzacPueblo_b.vcf > freebayes_invariant_chr01_AnzacPueblo_all.vcf
 
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
-freebayes_invariant_chr01_AnzacPueblo.vcf freebayes_invariant_chr01_AnzacPueblo_b.vcf > freebayes_invariant_chr01_AnzacPueblo_all.vcf
+bio/vcftools/0.1.15
 
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-merge \
-freebayes_invariant_chr01_combined_end.vcf freebayes_invariant_chr01_AnzacPueblo_all.vcf > freebayes_invariant_chr01_all.vcf
+vcf-merge freebayes_invariant_chr01_combined_end.vcf freebayes_invariant_chr01_AnzacPueblo_all.vcf > freebayes_invariant_chr01_all.vcf
