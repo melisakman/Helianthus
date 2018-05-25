06cb2b0c976b3f8dccd501841a53107d34e332c4
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-25 14:29:52 -0700

    gatk filter test

--- shells/Freebayes/5-filter/hardfilter_indel_GATK_test.sh
+++ shells/Freebayes/5-filter/hardfilter_indel_GATK_test.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=6
 #SBATCH --time=800:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/hardfilter_freebayes_GATK_test.out
 #SBATCH -e /global/home/users/makman/GATK/outs/hardfilter_freebayes_GATK_test.err
@@ -15,8 +15,8 @@ module load java
 module load gatk/4.0.1.2
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c ../freebayes_invariant_chr01_chunk1.vcf > freebayes_invariant_chr01_chunk1.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf freebayes_invariant_chr01_chunk1.vcf.gz  
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c ../freebayes_invariant_chr01_chunk1.vcf > freebayes_invariant_chr01_chunk1.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf freebayes_invariant_chr01_chunk1.vcf.gz  
 
 
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
