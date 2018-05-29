c69ec4ea9f851c46bb9e59432c8d770fa91ae25b
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-29 14:50:26 -0700

    concat chr07

--- shells/Freebayes/4-concat/final_concat_merge/vcf_concat_chr07.sh
+++ shells/Freebayes/4-concat/final_concat_merge/vcf_concat_chr07.sh
@@ -17,35 +17,35 @@ export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcfto
 TMPDIR=/clusterfs/rosalind/users/makman/temp
 module load bcftools/1.6
 
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
-freebayes_invariant_chr07_chunk1.vcf \
-freebayes_invariant_chr07_chunk1b.vcf \
-freebayes_invariant_chr07_chunk1c.vcf \
-freebayes_invariant_chr07_chunk1d.vcf \
-freebayes_invariant_chr07_chunk2.vcf \
-freebayes_invariant_chr07_chunk2b.vcf \
-freebayes_invariant_chr07_chunk3.vcf \
-freebayes_invariant_chr07_chunk3b.vcf \
-freebayes_invariant_chr07_chunk4.vcf \
-freebayes_invariant_chr07_chunk5.vcf \
-freebayes_invariant_chr07_chunk6.vcf \
-freebayes_invariant_chr07_chunk7.vcf \
-freebayes_invariant_chr07_chunk8.vcf \
-freebayes_invariant_chr07_chunk9.vcf \
-freebayes_invariant_chr07_chunk10.vcf \
-freebayes_invariant_chr07_chunk10b.vcf \
-freebayes_invariant_chr07_chunk11.vcf \
-freebayes_invariant_chr07_chunk11b.vcf \
-freebayes_invariant_chr07_chunk12.vcf \
-freebayes_invariant_chr07_chunk13.vcf \
-freebayes_invariant_chr07_chunk13b.vcf \
-freebayes_invariant_chr07_chunk13c.vcf \
-freebayes_invariant_chr07_chunk13d.vcf \
-freebayes_invariant_chr07_chunk13e.vcf > freebayes_invariant_chr07_combined_end.vcf
+# /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
+# freebayes_invariant_chr07_chunk1.vcf \
+# freebayes_invariant_chr07_chunk1b.vcf \
+# freebayes_invariant_chr07_chunk1c.vcf \
+# freebayes_invariant_chr07_chunk1d.vcf \
+# freebayes_invariant_chr07_chunk2.vcf \
+# freebayes_invariant_chr07_chunk2b.vcf \
+# freebayes_invariant_chr07_chunk3.vcf \
+# freebayes_invariant_chr07_chunk3b.vcf \
+# freebayes_invariant_chr07_chunk4.vcf \
+# freebayes_invariant_chr07_chunk5.vcf \
+# freebayes_invariant_chr07_chunk6.vcf \
+# freebayes_invariant_chr07_chunk7.vcf \
+# freebayes_invariant_chr07_chunk8.vcf \
+# freebayes_invariant_chr07_chunk9.vcf \
+# freebayes_invariant_chr07_chunk10.vcf \
+# freebayes_invariant_chr07_chunk10b.vcf \
+# freebayes_invariant_chr07_chunk11.vcf \
+# freebayes_invariant_chr07_chunk11b.vcf \
+# freebayes_invariant_chr07_chunk12.vcf \
+# freebayes_invariant_chr07_chunk13.vcf \
+# freebayes_invariant_chr07_chunk13b.vcf \
+# freebayes_invariant_chr07_chunk13c.vcf \
+# freebayes_invariant_chr07_chunk13d.vcf \
+# freebayes_invariant_chr07_chunk13e.vcf > freebayes_invariant_chr07_combined.vcf
 
 /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes_invariant_chr07_combined.vcf > freebayes_invariant_chr07_combined.vcf.gz 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes_invariant_chr07_AnzacPueblo.vcf > freebayes_invariant_chr07_AnzacPueblo.vcf.gz 
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes_invariant_chr07_AnzacPueblo.vcf > freebayes_invariant_chr07_AnzacPueblo.vcf.gz 
 /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf freebayes_invariant_chr07_combined.vcf.gz  
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf freebayes_invariant_chr07_AnzacPueblo.vcf.gz  
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf freebayes_invariant_chr07_AnzacPueblo.vcf.gz  
 
 bcftools merge -m all -O z freebayes_invariant_chr07_combined.vcf.gz freebayes_invariant_chr07_AnzacPueblo.vcf.gz > final_combined/freebayes_invariant_chr07_all.vcf.gz
