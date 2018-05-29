897191b421e7d350e4ef7e278a8f40760e8fdd32
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-29 12:26:50 -0700

    concat chr07

--- shells/Freebayes/4-concat/final_concat_merge/vcf_concat_chr07.sh
+++ shells/Freebayes/4-concat/final_concat_merge/vcf_concat_chr07.sh
@@ -1,11 +1,14 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/freebayes/
 #SBATCH -J catchr07
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --time=800:00:00
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=100:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/freebayes_vcf_concat_chr07.out
 #SBATCH -e /global/home/users/makman/GATK/outs/freebayes_vcf_concat_chr07.err
 #SBATCH --mail-user=makman@berkeley.edu
@@ -14,27 +17,26 @@ export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcfto
 TMPDIR=/clusterfs/rosalind/users/makman/temp
 module load bcftools/1.6
 
-# /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
-# freebayes_invariant_chr07_chunk1.vcf \
-# freebayes_invariant_chr07_chunk2.vcf \
-# freebayes_invariant_chr07_chunk2b.vcf \
-# freebayes_invariant_chr07_chunk3.vcf \
-# freebayes_invariant_chr07_chunk3b.vcf \
-# freebayes_invariant_chr07_chunk4.vcf \
-# freebayes_invariant_chr07_chunk5.vcf \
-# freebayes_invariant_chr07_chunk6.vcf \
-# freebayes_invariant_chr07_chunk7.vcf \
-# freebayes_invariant_chr07_chunk8.vcf \
-# freebayes_invariant_chr07_chunk9.vcf \
-# freebayes_invariant_chr07_chunk10.vcf \
-# freebayes_invariant_chr07_chunk11.vcf \
-# freebayes_invariant_chr07_chunk11b.vcf \
-# freebayes_invariant_chr07_chunk12.vcf \
-# freebayes_invariant_chr07_chunk13.vcf \
-# freebayes_invariant_chr07_chunk13b.vcf > freebayes_invariant_chr07_combined.vcf
-
 /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
-freebayes_invariant_chr07_combined.vcf \
+freebayes_invariant_chr07_chunk1.vcf \
+freebayes_invariant_chr07_chunk1b.vcf \
+freebayes_invariant_chr07_chunk1c.vcf \
+freebayes_invariant_chr07_chunk1d.vcf \
+freebayes_invariant_chr07_chunk2.vcf \
+freebayes_invariant_chr07_chunk2b.vcf \
+freebayes_invariant_chr07_chunk3.vcf \
+freebayes_invariant_chr07_chunk3b.vcf \
+freebayes_invariant_chr07_chunk4.vcf \
+freebayes_invariant_chr07_chunk5.vcf \
+freebayes_invariant_chr07_chunk6.vcf \
+freebayes_invariant_chr07_chunk7.vcf \
+freebayes_invariant_chr07_chunk8.vcf \
+freebayes_invariant_chr07_chunk9.vcf \
+freebayes_invariant_chr07_chunk10.vcf \
+freebayes_invariant_chr07_chunk10b.vcf \
+freebayes_invariant_chr07_chunk11.vcf \
+freebayes_invariant_chr07_chunk11b.vcf \
+freebayes_invariant_chr07_chunk12.vcf \
 freebayes_invariant_chr07_chunk13.vcf \
 freebayes_invariant_chr07_chunk13b.vcf \
 freebayes_invariant_chr07_chunk13c.vcf \
--- shells/Freebayes/5-filter/hardfilter_freebayes_invariant_chr01test.sh
+++ shells/Freebayes/5-filter/hardfilter_freebayes_invariant_chr01test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/freebayes/
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/freebayes/final_combined
 #SBATCH -J filchr01S
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
@@ -14,18 +14,22 @@
 module load bcftools/1.6
 module load bio/vcftools
 module load java
-# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes_invariant_chr01_chunk1.vcf > freebayes_invariant_chr01_chunk1.vcf.gz
-# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf freebayes_invariant_chr01_chunk1.vcf.gz  
 
-
-# java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
-# 	-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
-# 	-V freebayes_invariant_chr01_chunk1.vcf.gz \
-# 	-selectType INDEL \
-# 	-o freebayes_invariant_chr01_chunk1_test.vcf.gz 
-# 
-
-
-zcat freebayes_invariant_chr01_chunk1_test.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr01_chunk1_test_filtered.vcf.gz
-
+zcat freebayes_invariant_chr01_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr01_allFiltered.vcf.gz
+zcat freebayes_invariant_chr02_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr02_allFiltered.vcf.gz
+zcat freebayes_invariant_chr03_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr03_allFiltered.vcf.gz
+zcat freebayes_invariant_chr04_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr04_allFiltered.vcf.gz
+zcat freebayes_invariant_chr05_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr05_allFiltered.vcf.gz
+zcat freebayes_invariant_chr06_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr06_allFiltered.vcf.gz
+zcat freebayes_invariant_chr07_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr07_allFiltered.vcf.gz
+zcat freebayes_invariant_chr08_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr08_allFiltered.vcf.gz
+zcat freebayes_invariant_chr09_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr09_allFiltered.vcf.gz
+zcat freebayes_invariant_chr10_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr10_allFiltered.vcf.gz
+zcat freebayes_invariant_chr11_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr11_allFiltered.vcf.gz
+zcat freebayes_invariant_chr12_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr12_allFiltered.vcf.gz
+zcat freebayes_invariant_chr13_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr13_allFiltered.vcf.gz
+zcat freebayes_invariant_chr14_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr14_allFiltered.vcf.gz
+zcat freebayes_invariant_chr15_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr15_allFiltered.vcf.gz
+zcat freebayes_invariant_chr16_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr16_allFiltered.vcf.gz
+zcat freebayes_invariant_chr17_all.vcf.gz | bcftools filter -g 5 -e 'REF="N" || QUAL/DP < 2.0'| /clusterfs/rosalind/users/makman/vcflib/bin/vcffilter -f "QUAL > 50 & SAF > 1 & SAR > 1 & RPR > 1 & RPL > 1" | vcftools --gzvcf - --maf 0.01 --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | vcf-sort | gzip -c > freebayes_invariant_chr17_allFiltered.vcf.gz
 
