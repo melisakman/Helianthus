227d2e47a6e13a587e91c351ac74e71cb276765d
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-29 10:02:20 -0700

    freebayes filter

--- shells/Freebayes/5-filter/hardfilter_SNP_invariant_chr01.sh
+++ shells/Freebayes/5-filter/hardfilter_SNP_invariant_chr01.sh
@@ -1,14 +1,11 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/freebayes/
 #SBATCH -J filchr01S
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=400:00:00
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=800:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/bcftools_filter_freebayes_chr01.out
 #SBATCH -e /global/home/users/makman/GATK/outs/bcftools_filter_freebayes_chr01.err
 #SBATCH --mail-user=makman@berkeley.edu
