c865a6e577138048defd2de017427ec5167d1f3d
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-29 10:26:54 -0700

    variant meta caller]

---
+++ shells/Variant_meta_caller/VMC_test01.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/
+#SBATCH -J VMC
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=800:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/VMC_chr01.out
+#SBATCH -e /global/home/users/makman/GATK/outs/VMC_chr01.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+/global/home/users/makman/VariantMetaCaller_v1.0/VariantMetaCaller -t prioritize --allInfo \
+-c /global/home/users/makman/VariantMetaCaller_v1.0/definitions.config \
+-v GATK HaplotypeCaller fastq/invariants/VCMA_chr01.vcf.gz \
+-v samtools samtools samtools_VC/samtools_invariant_chr01.vcf.gz \
+-v freebayes freebayes freebayes/final_combined/freebayes_invariant_chr01_all.vcf.gz \
+-o VMC_chr01_test.vcf --allInfo 
