3886eb2fd40cb24cdfe47be2f63fec48974b3425
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-30 00:52:24 -0700

    gatk sort

--- shells/GATK/15-combine_sort/invariant/GATK_combine_sort_chr04.sh
+++ shells/GATK/15-combine_sort/invariant/GATK_combine_sort_chr04.sh
@@ -2,11 +2,13 @@
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/invariants/
 #SBATCH -J sort04
 #SBATCH --account=co_rosalind
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=rosalind_htc2_normal
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --time=800:00:00
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/GATK_filtered_combineSort_chr04.out
 #SBATCH -e /global/home/users/makman/GATK/outs/GATK_filtered_combineSort_chr04.err
 #SBATCH --mail-user=makman@berkeley.edu
