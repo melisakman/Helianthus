15cfb5a26085eec648a260c618828ba06e4a68dc
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-30 17:11:32 -0700

    gatk sort

--- shells/GATK/15-combine_sort/invariant/GATK_combine_sort_chr14.sh
+++ shells/GATK/15-combine_sort/invariant/GATK_combine_sort_chr14.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=savio2_htc
 #SBATCH --qos=rosalind_htc2_normal
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=6
 #SBATCH --time=800:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/GATK_filtered_combineSort_chr14.out
 #SBATCH -e /global/home/users/makman/GATK/outs/GATK_filtered_combineSort_chr14.err
@@ -17,5 +17,5 @@ module load bio/vcftools/0.1.15
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
 vcf-concat VCMA_chr14_NO_VARIATION_hardfiltered.vcf.gz VCMA_chr14_SNP_hardfiltered.vcf.gz VCMA_chr14_indel_hardfiltered.vcf.gz > VCMA_GATK_chr14_unsorted.vcf
-vcf-sort -t /clusterfs/rosalind/users/makman/temp VCMA_GATK_chr14_unsorted.vcf > filtered_combined/VCMA_GATK_chr14_sorted.vcf
+vcf-sort -t /global/home/users/makman/temp VCMA_GATK_chr14_unsorted.vcf > filtered_combined/VCMA_GATK_chr14_sorted.vcf
 /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c filtered_combined/VCMA_GATK_chr14_sorted.vcf > filtered_combined/VCMA_GATK_chr14_sorted.vcf.gz 
