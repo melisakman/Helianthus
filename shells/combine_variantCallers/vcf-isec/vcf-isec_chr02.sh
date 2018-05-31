1c241d438a821f005d7060ab455a7dbd3a7fca6b
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-31 12:01:50 -0700

    vcftools isec

--- shells/combine_variantCallers/vcf-isec/vcf-isec_chr02.sh
+++ shells/combine_variantCallers/vcf-isec/vcf-isec_chr02.sh
@@ -20,9 +20,9 @@ module load bcftools/1.6
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf > freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf.gz
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c samtools_VC/samtools_invariant_chr02_Filtered.vcf > samtools_VC/samtools_invariant_chr02_Filtered.vcf.gz
 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p ./fastq/invariants/filtered_combined/VCMA_GATK_chr02_secondFilter.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p ./freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p ./samtools_VC/samtools_invariant_chr02_Filtered.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./fastq/invariants/filtered_combined/VCMA_GATK_chr02_secondFilter.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./samtools_VC/samtools_invariant_chr02_Filtered.vcf.gz
 
 bcftools isec -n 2+ -O z fastq/invariants/filtered_combined/VCMA_GATK_chr02_secondFilter.vcf.gz \
 freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf.gz \
