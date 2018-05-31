aad5ddfa0e8b7d4be0f041b745c6b5a87c143a26
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-31 13:03:48 -0700

    samtools filter

--- shells/samtools_VC/2-filter/noMAF/hardfilter_samtools_invariant9.sh
+++ shells/samtools_VC/2-filter/noMAF/hardfilter_samtools_invariant9.sh
@@ -2,13 +2,11 @@
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/samtools_VC/
 #SBATCH -J filST9
 #SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=400:00:00
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=800:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/bcftools_filter_samtools9.out
 #SBATCH -e /global/home/users/makman/GATK/outs/bcftools_filter_samtools9.err
 #SBATCH --mail-user=makman@berkeley.edu
