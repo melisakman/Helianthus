1afeafd4e05e5f6295a07cd85b7de8d3f633bb4a
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-31 12:27:40 -0700

    VMC

--- shells/combine_variantCallers/Variant_meta_caller/VMC_test01.sh
+++ shells/combine_variantCallers/Variant_meta_caller/VMC_test01.sh
@@ -14,15 +14,12 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
-gunzip samtools_VC/samtools_invariant_chr01_Filtered.vcf.gz > samtools_VC/samtools_invariant_chr01_Filtered.vcf
-gunzip freebayes/final_combined/freebayes_invariant_chr01_allFiltered.vcf.gz > freebayes/final_combined/freebayes_invariant_chr01_allFiltered.vcf
-# gunzip samtools_VC/samtools_invariant_chr01.vcf.gz
-
 /global/home/users/makman/VariantMetaCaller_v1.0/VariantMetaCaller -t prioritize --allInfo \
 -c /global/home/users/makman/VariantMetaCaller_v1.0/definitions.config \
--v samtools samtools samtools_VC/samtools_invariant_chr01_Filtered.vcf \
--v freebayes freebayes freebayes/final_combined/freebayes_invariant_chr01_allFiltered.vcf \
+-v GATK HaplotypeCaller fastq/invariants/filtered_combined/VCMA_GATK_chr02_secondFilter.vcf.gz \
+-v freebayes freebayes freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf.gz \
 -o VMC_chr01_test.vcf --allInfo
 
 
 # -v GATK HaplotypeCaller fastq/invariants/filtered_combined/VCMA_GATK_chr01_sorted.vcf.gz \
+# -v samtools samtools samtools_VC/samtools_invariant_chr01_Filtered.vcf \
