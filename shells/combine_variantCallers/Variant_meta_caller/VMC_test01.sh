d522e39989f772b06d26168d228440744ad95f56
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-31 16:03:31 -0700

    VMC

--- shells/combine_variantCallers/Variant_meta_caller/VMC_test01.sh
+++ shells/combine_variantCallers/Variant_meta_caller/VMC_test01.sh
@@ -1,20 +1,16 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/
 #SBATCH -J VMC
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=100:00:00
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --ntasks=1
+#SBATCH --mem=128000
+#SBATCH --time=800:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/VMC_chr01.out
 #SBATCH -e /global/home/users/makman/GATK/outs/VMC_chr01.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
-gunzip fastq/invariants/filtered_combined/VCMA_GATK_chr02_secondFilter.vcf.gz > VCMA_GATK_chr02_secondFilter.vcf
 
 /global/home/users/makman/VariantMetaCaller_v1.0/VariantMetaCaller -t prioritize --allInfo \
 -c /global/home/users/makman/VariantMetaCaller_v1.0/definitions.config \
