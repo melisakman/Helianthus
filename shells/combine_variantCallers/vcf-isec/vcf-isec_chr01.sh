19dcc1b30083b8eb3294ef55f3129a648584c137
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-31 10:50:20 -0700

    vcftools isec

--- shells/combine_variantCallers/vcf-isec/vcf-isec_chr01.sh
+++ shells/combine_variantCallers/vcf-isec/vcf-isec_chr01.sh
@@ -7,17 +7,17 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
 #SBATCH --time=800:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/vcf-isec_chr01.out
-#SBATCH -e /global/home/users/makman/GATK/outs/vcf-isec_chr01.err
+#SBATCH -o /global/home/users/makman/GATK/outs/vcf-isec_chr02.out
+#SBATCH -e /global/home/users/makman/GATK/outs/vcf-isec_chr02.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
 module load bcftools/1.6
 
-gunzip fastq/invariants/filtered_combined/VCMA_GATK_chr01_secondFilter.vcf.gz
-gunzip freebayes/final_combined/freebayes_invariant_chr01_allFiltered.vcf
-# gunzip samtools_VC/samtools_invariant_chr01_Filtered.vcf.gz
+gunzip fastq/invariants/filtered_combined/VCMA_GATK_chr02_secondFilter.vcf.gz
+gunzip freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf
+gunzip samtools_VC/samtools_invariant_chr02_Filtered.vcf.gz
 # 
-# bcftools isec -n 2+ -O z fastq/invariants/filtered_combined/VCMA_GATK_chr01_sorted_filtered.vcf \
-# freebayes/final_combined/freebayes_invariant_chr01_allFiltered.vcf \
-# samtools_VC/samtools_invariant_chr01_Filtered.vcf > GATK_FB_ST_2plus_intersect_chr01.vcf
+bcftools isec -n 2+ -O z fastq/invariants/filtered_combined/VCMA_GATK_chr02_sorted_filtered.vcf \
+freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf \
+samtools_VC/samtools_invariant_chr02_Filtered.vcf > GATK_FB_ST_2plus_intersect_chr02.vcf
