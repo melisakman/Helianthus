a7b1c228bd5f0a86e882d81f2bc82dc53e385013
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-05-31 23:18:52 -0700

    vcf-isec

--- shells/combine_variantCallers/vcf-isec/GATK_hardfilter/vcf-isec_chr07.sh
+++ shells/combine_variantCallers/vcf-isec/GATK_hardfilter/vcf-isec_chr07.sh
@@ -13,13 +13,13 @@
 
 module load bcftools/1.6
 
-# gunzip fastq/invariants/filtered_combined/VCMA_GATK_chr07_sorted.vcf.gz
-# gunzip freebayes/final_combined/freebayes_invariant_chr07_allFiltered.vcf.gz
-# gunzip samtools_VC/samtools_invariant_chr07_Filtered_sorted.vcf.gz
-# 
-# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c fastq/invariants/filtered_combined/VCMA_GATK_chr07_sorted.vcf > fastq/invariants/filtered_combined/VCMA_GATK_chr07_sorted.vcf.gz
-# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes/final_combined/freebayes_invariant_chr07_allFiltered.vcf > freebayes/final_combined/freebayes_invariant_chr07_allFiltered.vcf.gz
-# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c samtools_VC/samtools_invariant_chr07_Filtered_sorted.vcf > samtools_VC/samtools_invariant_chr07_Filtered_sorted.vcf.gz
+gunzip fastq/invariants/filtered_combined/VCMA_GATK_chr07_sorted.vcf.gz
+gunzip freebayes/final_combined/freebayes_invariant_chr07_allFiltered.vcf.gz
+gunzip samtools_VC/samtools_invariant_chr07_Filtered_sorted.vcf.gz
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c fastq/invariants/filtered_combined/VCMA_GATK_chr07_sorted.vcf > fastq/invariants/filtered_combined/VCMA_GATK_chr07_sorted.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes/final_combined/freebayes_invariant_chr07_allFiltered.vcf > freebayes/final_combined/freebayes_invariant_chr07_allFiltered.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c samtools_VC/samtools_invariant_chr07_Filtered_sorted.vcf > samtools_VC/samtools_invariant_chr07_Filtered_sorted.vcf.gz
 
 /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./fastq/invariants/filtered_combined/VCMA_GATK_chr07_sorted.vcf.gz
 /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./freebayes/final_combined/freebayes_invariant_chr07_allFiltered.vcf.gz
