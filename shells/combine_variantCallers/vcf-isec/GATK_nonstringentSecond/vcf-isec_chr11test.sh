4afa136726528f6c306fb35c67a5bee6b23ac5c8
Melis Akman <melisakman@airbears2-10-142-63-167.airbears2.1918.berkeley.edu>
2018-06-01 15:21:18 -0700

    chr11 stringent vs nonstringent

---
+++ shells/combine_variantCallers/vcf-isec/GATK_nonstringentSecond/vcf-isec_chr11test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/
+#SBATCH -J isecchr11
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=800:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/vcf-isec_chr11stringentVSnon.out
+#SBATCH -e /global/home/users/makman/GATK/outs/vcf-isec_chr11stringentVSnon.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load bcftools/1.6
+
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./fastq/invariants/filtered_combined/VCMA_GATK_chr11_nonstringentFilter.vcf.gz
+
+bcftools isec -n +2 -O z -p vcf-isec_chr11_strvsnon fastq/invariants/filtered_combined/VCMA_GATK_chr11_nonstringentFilter.vcf.gz \
+fastq/invariants/filtered_combined/filtered_combined/VCMA_GATK_chr11_secondFilter.vcf.gz
