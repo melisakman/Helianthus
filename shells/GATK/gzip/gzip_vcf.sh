d538cf64982eb6328002667352735c6a534c9d8c
Melis Akman <melisakman@airbears2-10-142-63-167.airbears2.1918.berkeley.edu>
2018-06-01 15:01:09 -0700

    GK chr01 gzip

--- shells/GATK/gzip/gzip_vcf.sh
+++ shells/GATK/gzip/gzip_vcf.sh
@@ -1,14 +1,18 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/invariants/filtered_combined/
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/invariants/
 #SBATCH -J zip
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --time=800:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/gzip.out
 #SBATCH -e /global/home/users/makman/GATK/outs/gzip.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
-gzip VCMA_GATK_chr07_sorted.vcf
+gunzip VCMA_GATK_chr01.vcf.gz
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c VCMA_GATK_chr01.vcf > VCMA_GATK_chr01.vcf.gz
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p VCMA_GATK_chr01.vcf.gz
