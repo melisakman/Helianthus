6bce78e2e91a0866682f2541e208031754dd575b
Melis Akman <melisakman@airbears2-10-142-63-167.airbears2.1918.berkeley.edu>
2018-06-01 13:24:58 -0700

    GATK nonstringent filter

---
+++ shells/combine_variantCallers/vcf-isec/GATK_nonstringentSecond/vcf-isec_chr11.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/
+#SBATCH -J isecchr11
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=800:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/vcf-isec_chr11.out
+#SBATCH -e /global/home/users/makman/GATK/outs/vcf-isec_chr11.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load bcftools/1.6
+
+gunzip fastq/invariants/filtered_combined/VCMA_GATK_chr11_nonstringentFilter.vcf.gz
+gunzip freebayes/final_combined/freebayes_invariant_chr11_allFiltered.vcf.gz
+gunzip samtools_VC/samtools_invariant_chr11_Filtered_sorted.vcf.gz
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c fastq/invariants/filtered_combined/VCMA_GATK_chr11_nonstringentFilter.vcf > fastq/invariants/filtered_combined/VCMA_GATK_chr11_nonstringentFilter.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes/final_combined/freebayes_invariant_chr11_allFiltered.vcf > freebayes/final_combined/freebayes_invariant_chr11_allFiltered.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c samtools_VC/samtools_invariant_chr11_Filtered_sorted.vcf > samtools_VC/samtools_invariant_chr11_Filtered_sorted.vcf.gz
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./fastq/invariants/filtered_combined/VCMA_GATK_chr11_nonstringentFilter.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./freebayes/final_combined/freebayes_invariant_chr11_allFiltered.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./samtools_VC/samtools_invariant_chr11_Filtered_sorted.vcf.gz
+
+bcftools isec -n +2 -O z -p vcf-isec_chr11 fastq/invariants/filtered_combined/VCMA_GATK_chr11_nonstringentFilter.vcf.gz \
+freebayes/final_combined/freebayes_invariant_chr11_allFiltered.vcf.gz \
+samtools_VC/samtools_invariant_chr11_Filtered_sorted.vcf.gz
+
