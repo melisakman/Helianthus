074eaff582e0bad6611efe3b7ff74993310fc942
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-01 23:36:48 -0700

    chr11 stringent vs nonstringent

--- shells/combine_variantCallers/vcf-isec/GATK_nonstringentSecond/vcf-isec_chr11test.sh
+++ shells/combine_variantCallers/vcf-isec/GATK_nonstringentSecond/vcf-isec_chr11test.sh
@@ -17,4 +17,4 @@ module load bcftools/1.6
 /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./fastq/invariants/filtered_combined/VCMA_GATK_chr11_nonstringentFilter.vcf.gz
 
 bcftools isec -n +2 -O z -p vcf-isec_chr11_strvsnon fastq/invariants/filtered_combined/VCMA_GATK_chr11_nonstringentFilter.vcf.gz \
-fastq/invariants/filtered_combined/filtered_combined/VCMA_GATK_chr11_secondFilter.vcf.gz
+fastq/invariants/filtered_combined/VCMA_GATK_chr11_secondFilter.vcf.gz 
