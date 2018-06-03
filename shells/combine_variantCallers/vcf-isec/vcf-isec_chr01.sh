25da3e4a394b04055da19429a632e1c236d918fc
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-03 14:32:23 -0700

    vcf-isec

--- shells/combine_variantCallers/vcf-isec/vcf-isec_chr01.sh
+++ shells/combine_variantCallers/vcf-isec/vcf-isec_chr01.sh
@@ -14,6 +14,9 @@ export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcfto
 TMPDIR=/clusterfs/rosalind/users/makman/temp
 PATH=$PATH:/clusterfs/rosalind/users/makman/tabix-0.2.6/
 
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf 0000_renamed.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf 0001_sorted.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf 0002_sorted.vcf.gz
 /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-isec -n +2 \
 0000_renamed.vcf.gz 0001_sorted.vcf.gz 0002_sorted.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > deneme_intersect.vcf.gz
 
