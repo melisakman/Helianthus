d804152f5b1b4200839f36a43297d8e13afc98e2
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-03 18:58:16 -0700

    vcf-isec

--- shells/combine_variantCallers/vcf-isec/vcf-isec_chr15.sh
+++ shells/combine_variantCallers/vcf-isec/vcf-isec_chr15.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/secondFilter/vcf-isec_chr01
-#SBATCH -J isecchr01
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/secondFilter/vcf-isec_chr15
+#SBATCH -J isecchr15
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
 #SBATCH --time=800:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/chr01_vcf-isec.out
-#SBATCH -e /global/home/users/makman/GATK/outs/chr01_vcf-isec.err
+#SBATCH -o /global/home/users/makman/GATK/outs/chr15_vcf-isec.out
+#SBATCH -e /global/home/users/makman/GATK/outs/chr15_vcf-isec.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
@@ -18,6 +18,6 @@ PATH=$PATH:/clusterfs/rosalind/users/makman/tabix-0.2.6/
 /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf 0001_sorted.vcf.gz
 /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf 0002_sorted.vcf.gz
 /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-isec -n +2 \
-0000_renamed.vcf.gz 0001_sorted.vcf.gz 0002_sorted.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > chr01_intersect.vcf.gz
+0000_renamed.vcf.gz 0001_sorted.vcf.gz 0002_sorted.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > chr15_intersect.vcf.gz
 
 
