9b1311f4cd4b6b3afb0252b11a0a775a3f1b4405
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-03 23:47:21 -0700

    bcftools-isec

--- shells/combine_variantCallers/bcftools-isec/GATK_secondFilter/vcf-isec_chr02.sh
+++ shells/combine_variantCallers/bcftools-isec/GATK_secondFilter/vcf-isec_chr02.sh
@@ -26,11 +26,18 @@ export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcfto
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf > freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf.gz
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c samtools_VC/samtools_invariant_chr02_Filtered_sorted.vcf > samtools_VC/samtools_invariant_chr02_Filtered_sorted.vcf.gz
 # 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./fastq/invariants/filtered_combined/VCMA_GATK_chr02_secondFilter.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./fastq/invariants/filtered_combined/VCMA_GATK_chr02_secondFilter.vcf.gz
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf.gz
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./samtools_VC/samtools_invariant_chr02_Filtered_sorted.vcf.gz
 
 bcftools isec -n +2 -O z -p vcf-isec_chr02 fastq/invariants/filtered_combined/VCMA_GATK_chr02_secondFilter.vcf.gz \
 freebayes/final_combined/freebayes_invariant_chr02_allFiltered.vcf.gz \
 samtools_VC/samtools_invariant_chr02_Filtered_sorted.vcf.gz
+
+bcftools view -S bcftools_isec/secondFilter/sample_names.txt 0000.vcf.gz | bcftools reheader -s bcftools_isec/secondFilter/sample_rename.txt -o bcftools_isec/secondFilter/vcf-isec_chr02/0000_renamed.vcf
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c bcftools_isec/secondFilter/vcf-isec_chr02/0000_renamed.vcf > bcftools_isec/secondFilter/vcf-isec_chr02/0000_renamed.vcf.gz
+
+/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-shuffle-cols -t bcftools_isec/secondFilter/vcf-isec_chr02/0000_renamed.vcf.gz bcftools_isec/secondFilter/vcf-isec_chr02/0001.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > bcftools_isec/secondFilter/vcf-isec_chr02/0001_sorted.vcf.gz
+/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-shuffle-cols -t bcftools_isec/secondFilter/vcf-isec_chr02/0000_renamed.vcf.gz bcftools_isec/secondFilter/vcf-isec_chr02/0002.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > bcftools_isec/secondFilter/vcf-isec_chr02/0002_sorted.vcf.gz
 
