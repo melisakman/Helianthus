388b359b1ab1e561850b7c16b912b23fd76cfc9b
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-04 00:55:19 -0700

    snpeff

---
+++ shells/SNPEff/VCMA/SNPeff_VCMA_chr15.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/SNPEff/VCMA
+#SBATCH -J snpEff15
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000
+#SBATCH --time=8:00:00
+#SBATCH -o /global/home/users/makman/snpEff/outs/snpEff_XRQ_VCMA_chr15.out
+#SBATCH -e /global/home/users/makman/snpEff/outs/snpEff_XRQ_VCMA_chr15.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load java
+zcat ../../bcftools_isec/secondFilter/vcf-isec_chr15/chr15_intersect.vcf.gz | java -Xmx16g -jar /clusterfs/vector/scratch/makman/snpEff/snpEff.jar -noLof -stats VCMA_SNPEff_stats_chr15.html XRQ | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > chr15_intersect_snpeff.vcf.gz
---
+++ shells/SNPEff/VCMA/SNPeff_VCMA_chr16.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/SNPEff/VCMA
+#SBATCH -J snpEff16
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000
+#SBATCH --time=8:00:00
+#SBATCH -o /global/home/users/makman/snpEff/outs/snpEff_XRQ_VCMA_chr16.out
+#SBATCH -e /global/home/users/makman/snpEff/outs/snpEff_XRQ_VCMA_chr16.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load java
+zcat ../../bcftools_isec/secondFilter/vcf-isec_chr16/chr16_intersect.vcf.gz | java -Xmx16g -jar /clusterfs/vector/scratch/makman/snpEff/snpEff.jar -noLof -stats VCMA_SNPEff_stats_chr16.html XRQ | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > chr16_intersect_snpeff.vcf.gz
