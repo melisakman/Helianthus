bc6718c2a1a3b0de18540145bab7aca8a2643ce9
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-06 11:55:31 -0700

    blast

---
+++ shells/blast/blast_VCMA_fst_tajD.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/secondFilter/
+#SBATCH -J blast
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000
+#SBATCH --time=48:00:00
+#SBATCH -o /global/home/users/makman/blast/outs/VCMA_fst_tajD_genes2AT.out
+#SBATCH -e /global/home/users/makman/blast/outs/VCMA_fst_tajD_genes2AT.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load blast/2.6.0
+blastx -query fst_tajD_intersect_gene_CDs.txt -db /clusterfs/vector/instrumentData/blackmanlab/Helianthus/blast_databases/TAIR10_pep_20101214 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out VCMA_fst_tajD_Genes2AT.txt
