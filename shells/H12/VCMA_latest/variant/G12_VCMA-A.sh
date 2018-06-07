2ded68e152d9cc5da130bdba57ea0660912f657a
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-07 16:30:49 -0700

    G12 variants

---
+++ shells/H12/VCMA_latest/variant/G12_VCMA-A.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/secondFilter/G12/
+#SBATCH -J G12_A
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000
+#SBATCH --time=48:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/G12_VCMA-A2.out
+#SBATCH -e /global/home/users/makman/H12/outs/G12_VCMA-A2.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module unload python/3.5
+module unload python/3.5-consultsw
+module unload python/3.6
+module load python/2.7.14
+module load numpy/1.13.3
+
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr01_G12_input.txt 46 -o chr01_G12_VCMA_output.txt
+python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr02_G12_input.txt 46 -o chr02_G12_VCMA_output.txt
+python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr03_G12_input.txt 46 -o chr03_G12_VCMA_output.txt
+python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr04_G12_input.txt 46 -o chr04_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr05_G12_input.txt 46 -o chr05_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr06_G12_input.txt 46 -o chr06_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr07_G12_input.txt 46 -o chr07_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr08_G12_input.txt 46 -o chr08_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr09_G12_input.txt 46 -o chr09_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr10_G12_input.txt 46 -o chr10_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr11_G12_input.txt 46 -o chr11_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr12_G12_input.txt 46 -o chr12_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr13_G12_input.txt 46 -o chr13_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr14_G12_input.txt 46 -o chr14_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr15_G12_input.txt 46 -o chr15_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr16_G12_input.txt 46 -o chr16_G12_VCMA_output.txt
+# python /clusterfs/vector/scratch/makman/H12/SelectionHapStats/scripts/H12_H2H1.py VCMA_chr17_G12_input.txt 46 -o chr17_G12_VCMA_output.txt
+# 
---
+++ shells/H12/VCMA_latest/variant/vcf2genotype_VCMA-C.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/secondFilter/
+#SBATCH -J G12-C
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/vcf2genotype_VCMA_C.out
+#SBATCH -e /global/home/users/makman/H12/outs/vcf2genotype_VCMA_C.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+
+module load python/2.7
+
+
+
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr01_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr01_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr02_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr02_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr03_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr03_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr04_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr04_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr05_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr05_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr06_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr06_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr07_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr07_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr08_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr08_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr09_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr09_variantsOnly_G12_input.txt
+python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr10_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr10_variantsOnly_G12_input.txt
+python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr11_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr11_variantsOnly_G12_input.txt
+python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr12_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr12_variantsOnly_G12_input.txt
+python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr13_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr13_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr14_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr14_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr15_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr15_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr16_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr16_variantsOnly_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype.py G12/chr17_intersect_variantsOnly.vcf G12/lr_cv_samples.txt G12/VCMA_chr17_variantsOnly_G12_input.txt
