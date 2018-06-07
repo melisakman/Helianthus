c0809db9f5f694778b8d94f516e9f7b25a12e07c
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-07 11:10:52 -0700

    snp indel gzip

--- shells/GATK/17-extractVariant/SNP_indel_extract_all3.sh
+++ shells/GATK/17-extractVariant/SNP_indel_extract_all3.sh
@@ -89,6 +89,25 @@ export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcfto
 /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c vcf-isec_chr16_3/chr16_intersect_3.vcf > vcf-isec_chr16_3/chr16_intersect_3.vcf.gz
 /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c vcf-isec_chr17_3/chr17_intersect_3.vcf > vcf-isec_chr17_3/chr17_intersect_3.vcf.gz
 
+rm vcf-isec_chr01_3/chr01_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr02_3/chr02_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr03_3/chr03_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr04_3/chr04_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr05_3/chr05_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr06_3/chr06_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr07_3/chr07_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr08_3/chr08_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr09_3/chr09_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr10_3/chr10_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr11_3/chr11_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr12_3/chr12_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr13_3/chr13_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr14_3/chr14_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr15_3/chr15_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr16_3/chr16_intersect_3_variantsOnly.vcf
+rm vcf-isec_chr17_3/chr17_intersect_3_variantsOnly.vcf
+
+
 # java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
 # 	-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
 # 	-V vcf-isec_chr01_3/chr01_intersect_3.vcf.gz \
