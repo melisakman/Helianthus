b2a2a53821b6e1f46e1dce502a9c727ed7deb6fa
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-12 16:23:54 -0700

    sift info extract

--- shells/sift/VCMA/latest/extract_info_candidates_VCMA_wholeLine.py
+++ shells/sift/VCMA/latest/extract_info_candidates_VCMA_wholeLine.py
@@ -56,11 +56,11 @@ for line1 in file1:
 			ref = splt1[3]
 			alt = splt1[4]
 			pos = splt1[1]
-			
-			if splt1[7].find("SIFTINFO") != -1:
-				type = "SNPEff: " + ann[1] + "; " + ann[2] + "; " + ann[10] + "| SIFT: " + ann[-8] + "; " + ann[-2] + "; " + ann[-1] 
-			else:
-				type = "SNPEff: " + ann[1] + "; " + ann[2] + "; " + ann[10] 
+			type = splt1[7]
+			# if splt1[7].find("SIFTINFO") != -1:
+# 				type = "SNPEff: " + ann[1] + "; " + ann[2] + "; " + ann[10] + "| SIFT: " + ann[20] + "; " + ann[26] + "; " + ann[27] 
+# 			else:
+# 				type = "SNPEff: " + ann[1] + "; " + ann[2] + "; " + ann[10] 
 			allele_list = []
 			allele_list= [splt1[9]] + [splt1[12]] + splt1[20:26] + splt1[38:40] + [splt1[43]] + [splt1[57]] + [splt1[59]] + [splt1[26]]+ splt1[28:31] + [splt1[32]] + splt1[10:12] + splt1[54:57] + [splt1[42]] + splt1[60:63] + splt1[65:71] + splt1[72:80] + splt1[81:86] + [splt1[33]] + [splt1[35]] + [splt1[37]]
 			if gene in gene_list:
