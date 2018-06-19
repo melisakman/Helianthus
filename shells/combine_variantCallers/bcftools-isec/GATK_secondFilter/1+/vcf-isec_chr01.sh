10069ee782528856a15392ebaf20fc29cb140358
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-19 10:26:02 -0700

    stats for 3 variant calls

--- shells/combine_variantCallers/bcftools-isec/GATK_secondFilter/1+/vcf-isec_chr01.sh
+++ shells/combine_variantCallers/bcftools-isec/GATK_secondFilter/1+/vcf-isec_chr01.sh
@@ -12,8 +12,20 @@
 #SBATCH --mail-type=All
 
 module load bcftools/1.6
+module unload python
+module load python/2.7.14
+matplotlib/2.1.0
 
-bcftools isec -n +1 -O z -p vcf-isec_chr01_1 fastq/invariants/filtered_combined/VCMA_GATK_chr01_secondFilter.vcf.gz \
-freebayes/final_combined/freebayes_invariant_chr01_allFiltered.vcf.gz \
-samtools_VC/samtools_invariant_chr01_Filtered_sorted.vcf.gz
 
+
+# bcftools isec -n +1 -O z -p vcf-isec_chr01_1 fastq/invariants/filtered_combined/VCMA_GATK_chr01_secondFilter.vcf.gz \
+# freebayes/final_combined/freebayes_invariant_chr01_allFiltered.vcf.gz \
+# samtools_VC/samtools_invariant_chr01_Filtered_sorted.vcf.gz
+
+bcftools stats fastq/invariants/filtered_combined/VCMA_GATK_chr01_secondFilter.vcf.gz > bcftools_isec/secondFilter/stats/VCMA_GATK_chr01_secondFilter.vchk
+bcftools stats freebayes/final_combined/freebayes_invariant_chr01_allFiltered.vcf.gz > bcftools_isec/secondFilter/stats/freebayes_invariant_chr01_allFiltered.vchk
+bcftools stats samtools_VC/samtools_invariant_chr01_Filtered_sorted.vcf.gz > bcftools_isec/secondFilter/stats/samtools_invariant_chr01_Filtered_sorted.vchk
+
+plot-vcfstats bcftools_isec/secondFilter/stats/VCMA_GATK_chr01_secondFilter.vchk -p plot_VCMA_GATK_chr01
+plot-vcfstats bcftools_isec/secondFilter/stats/freebayes_invariant_chr01_allFiltered.vchk -p plot_freebayes_invariant_chr01
+plot-vcfstats bcftools_isec/secondFilter/stats/samtools_invariant_chr01_Filtered_sorted.vchk -p plot_samtools_invariant_chr01
