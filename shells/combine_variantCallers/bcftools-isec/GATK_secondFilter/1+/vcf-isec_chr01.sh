7e7150d02e1f6c641fd5191efdf1cc2bfbedd8c9
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-19 00:31:16 -0700

    1+ bcftools isec

---
+++ shells/combine_variantCallers/bcftools-isec/GATK_secondFilter/1+/vcf-isec_chr01.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/
+#SBATCH -J isecchr01
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=800:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/vcf-isec_chr01_1+.out
+#SBATCH -e /global/home/users/makman/GATK/outs/vcf-isec_chr01_1+.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load bcftools/1.6
+
+bcftools isec -n +1 -O z -p vcf-isec_chr01_1 fastq/invariants/filtered_combined/VCMA_GATK_chr01_secondFilter.vcf.gz \
+freebayes/final_combined/freebayes_invariant_chr01_allFiltered.vcf.gz \
+samtools_VC/samtools_invariant_chr01_Filtered_sorted.vcf.gz
+
