eb39eeff99e102acdaada5cd5d21a8d02c4de52d
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-19 18:42:16 -0700

    Greg truth concordance

--- shells/GATK/tests/Greg_truth_concordance.py
+++ shells/GATK/tests/Greg_truth_concordance.py
@@ -16,14 +16,15 @@ for line1 in file1:
 	pos1 = split1[1]
 	alt1 = split[3]
 	truth[pos1] = alt1
+
 all3_concord = 0
 all3_nonconcord = 0
-12_concord = 0
-12_nonconcord = 0
-13_concord = 0
-13_nonconcord = 0
-23_concord = 0
-23_nonconcord = 0
+all12_concord = 0
+all12_nonconcord = 0
+all13_concord = 0
+all13_nonconcord = 0
+all23_concord = 0
+all23_nonconcord = 0
 for line2 in file2:
 	split2 = line2.split["\t"]:
 	pos2 = split2[1]
@@ -37,21 +38,21 @@ for line2 in file2:
 				all3_nonconcord +=1
 		elif type == "110":
 			if truth(pos2) = alt2:
-				12_concord +=1
+				all12_concord +=1
 			else:
-				12_nonconcord +=1
+				all12_nonconcord +=1
 		elif type == "101":
 			if truth(pos2) = alt2:
-				13_concord +=1
+				all13_concord +=1
 			else:
-				13_nonconcord +=1
+				all13_nonconcord +=1
 		elif type == "011":
 			if truth(pos2) = alt2:
-				23_concord +=1
+				all23_concord +=1
 			else:
-				23_nonconcord +=1
+				all23_nonconcord +=1
 
 print "all 3 concordant ref and alt = " + str(all3_concord) + " all 3 non-concordant = " + str(all3_nonconcord)				
-print "GATK FB concordant ref and alt = " + str(12_concord) + " GATK FB non-concordant = " + str(12_nonconcord)				
-print "GATK ST concordant ref and alt = " + str(13_concord) + " GATK ST non-concordant = " + str(13_nonconcord)				
-print "FB ST concordant ref and alt = " + str(23_concord) + " FB ST non-concordant = " + str(23_nonconcord)				
+print "GATK FB concordant ref and alt = " + str(all12_concord) + " GATK FB non-concordant = " + str(all12_nonconcord)				
+print "GATK ST concordant ref and alt = " + str(all13_concord) + " GATK ST non-concordant = " + str(all13_nonconcord)				
+print "FB ST concordant ref and alt = " + str(all23_concord) + " FB ST non-concordant = " + str(all23_nonconcord)				
