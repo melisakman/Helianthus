9886982ffc59326965bbaa08622457de3385d8aa
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-19 21:50:56 -0700

    Greg truth concordance

--- shells/GATK/tests/Greg_truth_concordance.py
+++ shells/GATK/tests/Greg_truth_concordance.py
@@ -33,9 +33,11 @@ for line2 in file2:
 		if type == "111":
 			if truth[pos2] == alt2:
 				all3_concord +=1
-				print "all3_concord" + line2
+				print "all3_concord " + line2
 			else:
 				all3_nonconcord +=1
+				print "all3_nonconcord " + line2
+
 		elif type == "110":
 			if truth[pos2] == alt2:
 				all12_concord +=1
