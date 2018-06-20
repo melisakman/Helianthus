2c88c3ce7f1043d96e3f1cee793eb6df2a891594
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-19 22:23:12 -0700

    Greg truth concordance

--- shells/GATK/tests/Greg_truth_concordance.py
+++ shells/GATK/tests/Greg_truth_concordance.py
@@ -16,12 +16,16 @@ for line1 in file1:
 	pos1 = split1[1]
 	alt1 = split1[3][:-1]
 	truth[pos1] = alt1
+all = 0
 all3_concord = 0
 all3_nonconcord = 0
+all12 = 0
 all12_concord = 0
 all12_nonconcord = 0
+all13 = 0
 all13_concord = 0
 all13_nonconcord = 0
+all23 = 0
 all23_concord = 0
 all23_nonconcord = 0
 for line2 in file2:
@@ -31,6 +35,7 @@ for line2 in file2:
 	type = split2[4][:-1]
 	if pos2 in truth:
 		if type == "111":
+			all +=1
 			if truth[pos2] == alt2:
 				all3_concord +=1
 # 				print "all3_concord " + line2
@@ -39,22 +44,25 @@ for line2 in file2:
 # 				print "all3_nonconcord " + line2
 
 		elif type == "110":
+			all12 +=1
 			if truth[pos2] == alt2:
 				all12_concord +=1
 			else:
 				all12_nonconcord +=1
 		elif type == "101":
+			all13 +=1
 			if truth[pos2] == alt2:
 				all13_concord +=1
 			else:
 				all13_nonconcord +=1
 		elif type == "011":
+			all23 +=1
 			if truth[pos2] == alt2:
 				all23_concord +=1
 			else:
 				all23_nonconcord +=1
 
-print "all 3 concordant ref and alt = " + str(all3_concord) + " all 3 non-concordant = " + str(all3_nonconcord)				
-print "GATK FB concordant ref and alt = " + str(all12_concord) + " GATK FB non-concordant = " + str(all12_nonconcord)				
-print "GATK ST concordant ref and alt = " + str(all13_concord) + " GATK ST non-concordant = " + str(all13_nonconcord)				
-print "FB ST concordant ref and alt = " + str(all23_concord) + " FB ST non-concordant = " + str(all23_nonconcord)				
+print "all 3 concordant ref and alt = " + str(all3_concord) + " all 3 non-concordant = " + str(all3_nonconcord)	+ " total = " + str(all)			
+print "GATK FB concordant ref and alt = " + str(all12_concord) + " GATK FB non-concordant = " + str(all12_nonconcord) + " total = " + str(all12)		
+print "GATK ST concordant ref and alt = " + str(all13_concord) + " GATK ST non-concordant = " + str(all13_nonconcord) + " total = " + str(all13)				
+print "FB ST concordant ref and alt = " + str(all23_concord) + " FB ST non-concordant = " + str(all23_nonconcord) + " total = " + str(all23)				
