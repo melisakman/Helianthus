632ae8f2791c1f7ad956847f44c52b851149bf1f
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-22 11:28:35 -0700

    fst compare

--- shells/combine_variantCallers/compare_filtering/vcftools_VCMA_fst_compare_filtering.sh
+++ shells/combine_variantCallers/compare_filtering/vcftools_VCMA_fst_compare_filtering.sh
@@ -4,27 +4,27 @@
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
 #SBATCH --time=4:00:00
-#SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_VCMA_fst_compareFiltering.out
-#SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_VCMA_fst_compareFiltering.err
+#SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_VCMA_fst_compareFiltering_b.out
+#SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_VCMA_fst_compareFiltering_b.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
 module load bio/vcftools/0.1.15
 
-vcftools --gzvcf secondFilter/vcf-isec_chr01/chr01_intersect.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/second_filtering_2
+vcftools --vcf secondFilter/vcf-isec_chr01/chr01_intersect.vcf --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/second_filtering_2
 
-vcftools --gzvcf secondFilter/vcf-isec_chr01_3/chr01_intersect_3.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/second_filtering_3
+# vcftools --gzvcf secondFilter/vcf-isec_chr01_3/chr01_intersect_3.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/second_filtering_3
 
-# vcftools --gzvcf hardFilter/vcf-isec_chr01_hardFilter/chr01_intersect.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/hard_filtering_2
-# 
-# vcftools --gzvcf hardFilter/vcf-isec_chr01_hardFilter_3/chr01_intersect_3.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/hard_filtering_3
+vcftools --gzvcf hardFilter/vcf-isec_chr01_hardFilter/chr01_intersect.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/hard_filtering_2
 
-vcftools --gzvcf ../fastq/invariants/filtered_combined/VCMA_GATK_chr01_sorted.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/GATK_hard
+vcftools --gzvcf hardFilter/vcf-isec_chr01_hardFilter_3/chr01_intersect_3.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/hard_filtering_3
 
-vcftools --gzvcf ../fastq/invariants/filtered_combined/VCMA_GATK_chr01_secondFilter.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/GATK_second
+# vcftools --gzvcf ../fastq/invariants/filtered_combined/VCMA_GATK_chr01_sorted.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/GATK_hard
 
-vcftools --gzvcf ../freebayes/final_combined/freebayes_invariant_chr01_allFiltered.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/FB
+# vcftools --gzvcf ../fastq/invariants/filtered_combined/VCMA_GATK_chr01_secondFilter.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/GATK_second
 
-vcftools --gzvcf ../samtools_VC/samtools_invariant_chr01_Filtered_sorted.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/ST
+# vcftools --gzvcf ../freebayes/final_combined/freebayes_invariant_chr01_allFiltered.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/FB
+
+# vcftools --gzvcf ../samtools_VC/samtools_invariant_chr01_Filtered_sorted.vcf.gz --weir-fst-pop secondFilter/wd_list.txt --weir-fst-pop secondFilter/lr_list.txt --fst-window-size 10000 --out ../compare_filtering/ST
 
 
