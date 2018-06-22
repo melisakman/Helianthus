650a18b57bdbdc97859a9c1ad47334c6370e8278
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-22 12:23:37 -0700

    check false negatives

---
+++ shells/GATK/tests/check_false_negatives.py
@@ -0,0 +1,20 @@
+##This script is for detecting what the false negative sites are.
+## input sites.txt file is from bcftools isec and truth set is from Greg
+
+import sys
+
+InputFile1 = sys.argv[1] # sites file
+InputFile2 = sys.argv[2] # file with sites that do not show in the truth set
+OutputFile1 =sys.argv[3] # output
+file1 = open(InputFile1, "r")
+file2 = open(InputFile2, "r")
+file3 = open(OutputFile1, "w")
+list = []
+for line2 in file2:
+	list.append(line2[:-1])
+
+for line1 in file1:
+	split = line1.split("\t")
+	pos = split[1]
+	if pos in list:
+		file3.write(line1)
