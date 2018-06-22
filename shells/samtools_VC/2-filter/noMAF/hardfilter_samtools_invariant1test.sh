344fdc1380ca36dfde4eb9e2863e423c826a98ee
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-06-22 13:49:07 -0700

    samtools filter test

--- shells/samtools_VC/2-filter/noMAF/hardfilter_samtools_invariant1test.sh
+++ shells/samtools_VC/2-filter/noMAF/hardfilter_samtools_invariant1test.sh
@@ -4,10 +4,7 @@
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio
 #SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
-#SBATCH --mem=64000
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
 #SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/bcftools_filter_samtools1test.out
 #SBATCH -i /global/home/users/makman/GATK/outs/bcftools_filter_samtools1test.err
