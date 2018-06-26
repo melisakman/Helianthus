b514222c1cd7dc343da98641688f4545ec92af53
Melis Akman <melisakman@airbears2-10-142-109-85.airbears2.1918.berkeley.edu>
2018-06-26 12:31:49 -0700

    filter by qual/depth

---
+++ shells/variant_calling/Freebayes/15-filter/only_qual_by_depth/hardfilter_freebayes_invariant4.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/freebayes/final_combined
+#SBATCH -J filFB4
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --mem=16000
+#SBATCH --qos=savio_normal
+#SBATCH --time=72:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/qual_by_depth_freebayes4.out
+#SBATCH -e /global/home/users/makman/GATK/outs/qual_by_depth_freebayes4.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load bcftools/1.6
+module load bio/vcftools
+module load java
+
+# zcat old/freebayes_invariant_chr01_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr01_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr02_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr02_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr03_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr03_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr04_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr04_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr05_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr05_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr06_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr06_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr07_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr07_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr08_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr08_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr09_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr09_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr10_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr10_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr11_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr11_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr12_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr12_qual_by_depth.vcf.gz
+# zcat old/freebayes_invariant_chr13_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr13_qual_by_depth.vcf.gz
+zcat old/freebayes_invariant_chr14_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr14_qual_by_depth.vcf.gz
+zcat old/freebayes_invariant_chr15_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr15_qual_by_depth.vcf.gz
+zcat old/freebayes_invariant_chr16_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr16_qual_by_depth.vcf.gz
+zcat old/freebayes_invariant_chr17_all.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "indel" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "mnp" && QUAL/DP > 2.0 && QUAL > 50 || TYPE = "ref" && QUAL < 0.0000434' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariant_chr17_qual_by_depth.vcf.gz
--- shells/variant_calling/combine_variantCallers/bcftools-isec_before_filtering/bcftools-isec_chr02.sh
+++ shells/variant_calling/combine_variantCallers/bcftools-isec_before_filtering/bcftools-isec_chr02.sh
@@ -1,31 +1,27 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/
-#SBATCH -J 3isehr01
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=400:00:00
-#SBATCH --exclude=n0000.savio2,n0013.cortex0,n0021.savio2,n0030.savio1,n0042.savio2,n0048.savio2,n0050.savio1,n0096.savio2,n0097.savio1,n0107.savio2,n0109.savio2,n0113.savio2,n0114.savio2,n0133.savio1,n0166.savio2,n0182.savio2,n0218.savio2,n0234.savio2,n0281.savio2
-#SBATCH -o /global/home/users/makman/GATK/outs/bcf-isec_chr01_3.out
-#SBATCH -e /global/home/users/makman/GATK/outs/bcf-isec_chr01_3.err
+#SBATCH -J 3isehr02
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --mem=24000
+#SBATCH --qos=savio_normal
+#SBATCH --time=72:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/bcf-isec_chr02_3.out
+#SBATCH -e /global/home/users/makman/GATK/outs/bcf-isec_chr02_3.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
 module load bcftools/1.6
 
-# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c ./fastq/invariants/VCMA_chr01.vcf > ./fastq/invariants/VCMA_chr01.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes/final_combined/old/freebayes_invariant_chr01_all.vcf > freebayes/final_combined/old/freebayes_invariant_chr01_all.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c samtools_VC/old/samtools_invariant_chr01_combined.vcf > samtools_VC/old/samtools_invariant_chr01_combined.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c ./fastq/invariants/VCMA_chr02.vcf > ./fastq/invariants/VCMA_chr02.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes/final_combined/old/freebayes_invariant_chr02_all.vcf > freebayes/final_combined/old/freebayes_invariant_chr02_all.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c samtools_VC/old/samtools_invariant_chr02_combined.vcf > samtools_VC/old/samtools_invariant_chr02_combined.vcf.gz
 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./fastq/invariants/VCMA_chr01.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./freebayes/final_combined/old/freebayes_invariant_chr01_all.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./samtools_VC/old/samtools_invariant_chr01_combined.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./fastq/invariants/VCMA_chr02.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./freebayes/final_combined/old/freebayes_invariant_chr02_all.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ./samtools_VC/old/samtools_invariant_chr02_combined.vcf.gz
 
-bcftools isec -n +2 -O z -p bcftools_isec/chr01_3 ./fastq/invariants/VCMA_chr01.vcf.gz \
-./freebayes/final_combined/old/freebayes_invariant_chr01_all.vcf.gz \
-./samtools_VC/old/samtools_invariant_chr01_combined.vcf.gz
+bcftools isec -n +2 -O z -p bcftools_isec/chr02_3 ./fastq/invariants/VCMA_chr02.vcf.gz \
+./freebayes/final_combined/old/freebayes_invariant_chr02_all.vcf.gz \
+./samtools_VC/old/samtools_invariant_chr02_combined.vcf.gz
 
