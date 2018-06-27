e4768e94878924f091728f7da96e4c8020d41680
Melis Akman <melisakman@airbears2-10-142-109-85.airbears2.1918.berkeley.edu>
2018-06-26 17:23:18 -0700

    samtools dup removal

--- shells/variant_calling/samtools_VC/12-filter/filter_duplicates/filterdubs_samtools_invariant2.sh
+++ shells/variant_calling/samtools_VC/12-filter/filter_duplicates/filterdubs_samtools_invariant2.sh
@@ -1,42 +1,42 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/samtools_VC/
-#SBATCH -J fildup
+#SBATCH -J fildup2
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio2_htc
 #SBATCH --qos=rosalind_htc2_normal
 #SBATCH --cpus-per-task=4
 #SBATCH --time=72:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/dupfilter_samtools1.out
-#SBATCH -e /global/home/users/makman/GATK/outs/dupfilter_samtools1.err
+#SBATCH -o /global/home/users/makman/GATK/outs/dupfilter_samtools2.out
+#SBATCH -e /global/home/users/makman/GATK/outs/dupfilter_samtools2.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
-gunzip samtools_invariant_chr02_Filtered.vcf.gz
-gunzip samtools_invariant_chr03_Filtered.vcf.gz
-gunzip samtools_invariant_chr04_Filtered.vcf.gz
-# gunzip samtools_invariant_chr05_Filtered.vcf.gz
-# gunzip samtools_invariant_chr06_Filtered.vcf.gz
-# gunzip samtools_invariant_chr07_Filtered.vcf.gz
-# gunzip samtools_invariant_chr08_Filtered.vcf.gz
-# gunzip samtools_invariant_chr09_Filtered.vcf.gz
-# gunzip samtools_invariant_chr10_Filtered.vcf.gz
-# gunzip samtools_invariant_chr11_Filtered.vcf.gz
-# gunzip samtools_invariant_chr12_Filtered.vcf.gz
-# gunzip samtools_invariant_chr13_Filtered.vcf.gz
-# gunzip samtools_invariant_chr14_Filtered.vcf.gz
-# gunzip samtools_invariant_chr15_Filtered.vcf.gz
-# gunzip samtools_invariant_chr16_Filtered.vcf.gz
-# gunzip samtools_invariant_chr17_Filtered.vcf.gz
+# gunzip -c samtools_invariant_chr02_Filtered.vcf.gz > samtools_invariant_chr02_Filtered.vcf
+# gunzip -c samtools_invariant_chr03_Filtered.vcf.gz > samtools_invariant_chr03_Filtered.vcf
+# gunzip -c samtools_invariant_chr04_Filtered.vcf.gz > samtools_invariant_chr04_Filtered.vcf
+gunzip -c samtools_invariant_chr05_Filtered.vcf.gz > samtools_invariant_chr05_Filtered.vcf
+gunzip -c samtools_invariant_chr06_Filtered.vcf.gz > samtools_invariant_chr06_Filtered.vcf
+gunzip -c samtools_invariant_chr07_Filtered.vcf.gz > samtools_invariant_chr07_Filtered.vcf
+gunzip -c samtools_invariant_chr08_Filtered.vcf.gz > samtools_invariant_chr08_Filtered.vcf
+gunzip -c samtools_invariant_chr09_Filtered.vcf.gz > samtools_invariant_chr09_Filtered.vcf
+# gunzip -c samtools_invariant_chr10_Filtered.vcf.gz > samtools_invariant_chr10_Filtered.vcf
+# gunzip -c samtools_invariant_chr11_Filtered.vcf.gz > samtools_invariant_chr11_Filtered.vcf
+# gunzip -c samtools_invariant_chr12_Filtered.vcf.gz > samtools_invariant_chr12_Filtered.vcf
+# gunzip -c samtools_invariant_chr13_Filtered.vcf.gz > samtools_invariant_chr13_Filtered.vcf
+# gunzip -c samtools_invariant_chr14_Filtered.vcf.gz > samtools_invariant_chr14_Filtered.vcf
+# gunzip -c samtools_invariant_chr15_Filtered.vcf.gz > samtools_invariant_chr15_Filtered.vcf
+# gunzip -c samtools_invariant_chr16_Filtered.vcf.gz > samtools_invariant_chr16_Filtered.vcf
+# gunzip -c samtools_invariant_chr17_Filtered.vcf.gz > samtools_invariant_chr17_Filtered.vcf
 
-python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr01_Filtered.vcf samtools_invariant_chr01_Filtered_dupsRemoved.vcf
-python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr02_Filtered.vcf samtools_invariant_chr02_Filtered_dupsRemoved.vcf
-python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr03_Filtered.vcf samtools_invariant_chr03_Filtered_dupsRemoved.vcf
-python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr04_Filtered.vcf samtools_invariant_chr04_Filtered_dupsRemoved.vcf
-# python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr05_Filtered.vcf samtools_invariant_chr05_Filtered_dupsRemoved.vcf
-# python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr06_Filtered.vcf samtools_invariant_chr06_Filtered_dupsRemoved.vcf
-# python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr07_Filtered.vcf samtools_invariant_chr07_Filtered_dupsRemoved.vcf
-# python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr08_Filtered.vcf samtools_invariant_chr08_Filtered_dupsRemoved.vcf
-# python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr09_Filtered.vcf samtools_invariant_chr09_Filtered_dupsRemoved.vcf
+# python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr01_Filtered.vcf samtools_invariant_chr01_Filtered_dupsRemoved.vcf
+# python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr02_Filtered.vcf samtools_invariant_chr02_Filtered_dupsRemoved.vcf
+# python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr03_Filtered.vcf samtools_invariant_chr03_Filtered_dupsRemoved.vcf
+# python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr04_Filtered.vcf samtools_invariant_chr04_Filtered_dupsRemoved.vcf
+python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr05_Filtered.vcf samtools_invariant_chr05_Filtered_dupsRemoved.vcf
+python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr06_Filtered.vcf samtools_invariant_chr06_Filtered_dupsRemoved.vcf
+python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr07_Filtered.vcf samtools_invariant_chr07_Filtered_dupsRemoved.vcf
+python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr08_Filtered.vcf samtools_invariant_chr08_Filtered_dupsRemoved.vcf
+python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr09_Filtered.vcf samtools_invariant_chr09_Filtered_dupsRemoved.vcf
 # python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr10_Filtered.vcf samtools_invariant_chr10_Filtered_dupsRemoved.vcf
 # python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr11_Filtered.vcf samtools_invariant_chr11_Filtered_dupsRemoved.vcf
 # python ~/git/Helianthus/shells/variant_calling/samtools_VC12-filter/samtools_filter_by_qual.py samtools_invariant_chr12_Filtered.vcf samtools_invariant_chr12_Filtered_dupsRemoved.vcf
