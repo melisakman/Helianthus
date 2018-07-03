fb659a525cf35ff1d681de31e830b139d0f6012d
Melis Akman <melisakman@airbears2-10-142-109-246.airbears2.1918.berkeley.edu>
2018-07-03 12:20:41 -0700

    bcftools3

---
+++ shells/variant_calling/combine_variantCallers/bcftools-isec/1-bcf-isec/3/bcftools-isec_chr05_3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/
+#SBATCH -J 3chr05
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --time=24:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/bcf-isec_chr05.out
+#SBATCH -e /global/home/users/makman/GATK/outs/bcf-isec_chr05.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load bcftools/1.6
+
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c fastq/invariants/filtered_combined/VCMA_GATK_chr05_secondFilter.vcf > fastq/invariants/filtered_combined/VCMA_GATK_chr05_secondFilter.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes/final_combined/freebayes_invariant_chr05_allFiltered.vcf > freebayes/final_combined/freebayes_invariant_chr05_allFiltered.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c samtools_VC/samtools_invariant_chr05_Filtered_dupsRemoved.vcf > samtools_VC/samtools_invariant_chr05_Filtered_dupsRemoved.vcf.gz
+# 
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix fastq/invariants/filtered_combined/VCMA_GATK_chr05_secondFilter.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/final_combined/freebayes_invariant_chr05_allFiltered.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools_VC/samtools_invariant_chr05_Filtered_dupsRemoved.vcf.gz
+
+bcftools isec -n =3 -O z -p bcftools_isec/chr05_3 fastq/invariants/filtered_combined/VCMA_GATK_chr05_secondFilter.vcf.gz \
+samtools_VC/samtools_invariant_chr05_Filtered_dupsRemoved.vcf.gz \
+freebayes/final_combined/freebayes_invariant_chr05_allFiltered.vcf.gz
---
+++ shells/variant_calling/combine_variantCallers/bcftools-isec/1-bcf-isec/3/bcftools-isec_chr14_3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/
+#SBATCH -J 3chr14
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --cpus-per-task=4
+#SBATCH --time=24:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/bcf-isec_chr14.out
+#SBATCH -e /global/home/users/makman/GATK/outs/bcf-isec_chr14.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load bcftools/1.6
+
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c fastq/invariants/filtered_combined/VCMA_GATK_chr14_secondFilter.vcf > fastq/invariants/filtered_combined/VCMA_GATK_chr14_secondFilter.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes/final_combined/freebayes_invariant_chr14_allFiltered.vcf > freebayes/final_combined/freebayes_invariant_chr14_allFiltered.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c samtools_VC/samtools_invariant_chr14_Filtered_dupsRemoved.vcf > samtools_VC/samtools_invariant_chr14_Filtered_dupsRemoved.vcf.gz
+# 
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix fastq/invariants/filtered_combined/VCMA_GATK_chr14_secondFilter.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/final_combined/freebayes_invariant_chr14_allFiltered.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools_VC/samtools_invariant_chr14_Filtered_dupsRemoved.vcf.gz
+
+bcftools isec -n =3 -O z -p bcftools_isec/chr14_3 fastq/invariants/filtered_combined/VCMA_GATK_chr14_secondFilter.vcf.gz \
+samtools_VC/samtools_invariant_chr14_Filtered_dupsRemoved.vcf.gz \
+freebayes/final_combined/freebayes_invariant_chr14_allFiltered.vcf.gz
