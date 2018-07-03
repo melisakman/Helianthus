91b4d791c87ab89f900449a8ca561ff248e32e67
Melis Akman <melisakman@airbears2-10-142-109-246.airbears2.1918.berkeley.edu>
2018-07-03 12:07:58 -0700

    shuffle cols

---
+++ shells/variant_calling/combine_variantCallers/bcftools-isec/2-shuffle_cols/rename_shuffle_chr02.sh
@@ -0,0 +1,23 @@
+q#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/chr02_2
+#SBATCH -J shfchr02
+#SBATCH --partition=vector
+#SBATCH --qos=vector_batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=800:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/rename_shuffle_chr02.out
+#SBATCH -e /global/home/users/makman/GATK/outs/rename_shuffle_chr02.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load bcftools/1.6
+module load vcftools/0.1.13
+export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
+
+bcftools view -S ../old/secondFilter/sample_names.txt 0000.vcf.gz | bcftools reheader -s ../old/secondFilter/sample_rename.txt -o 0000_renamed.vcf
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c 0000_renamed.vcf > 0000_renamed.vcf.gz
+
+/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-shuffle-cols -t 0000_renamed.vcf.gz 0001.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > 0001_sorted.vcf.gz
+/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-shuffle-cols -t 0000_renamed.vcf.gz 0002.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > 0002_sorted.vcf.gz
