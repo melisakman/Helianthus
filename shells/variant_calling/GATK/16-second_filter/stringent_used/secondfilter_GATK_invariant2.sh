c773ba2104426b47e8f82524c933b83b87afbd8a
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-07-03 22:12:33 -0700

    GATK

--- shells/variant_calling/GATK/16-second_filter/stringent_used/secondfilter_GATK_invariant2.sh
+++ shells/variant_calling/GATK/16-second_filter/stringent_used/secondfilter_GATK_invariant2.sh
@@ -15,7 +15,7 @@ module load bio/vcftools
 module load java
 
 # zcat VCMA_GATK_chr01_sorted.vcf.gz | vcftools --vcf - --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr01_secondFilter.vcf.gz
-zcat VCMA_GATK_chr02_sorted.vcf.gz | vcftools --vcf - --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/b/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr02_secondFilter.vcf.gz
+zcat VCMA_GATK_chr02_sorted.vcf.gz | vcftools --vcf - --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr02_secondFilter.vcf.gz
 # zcat VCMA_GATK_chr03_sorted.vcf.gz | vcftools --vcf - --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr03_secondFilter.vcf.gz
 # zcat VCMA_GATK_chr04_sorted.vcf.gz | vcftools --vcf - --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr04_secondFilter.vcf.gz
 # zcat VCMA_GATK_chr05_sorted.vcf.gz | vcftools --vcf - --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr05_secondFilter.vcf.gz
