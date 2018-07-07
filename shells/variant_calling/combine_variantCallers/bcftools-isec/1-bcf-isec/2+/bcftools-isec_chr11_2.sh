b46701fd5c64788cb8e0502da9449ec0dcb6f94a
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-07-06 18:36:53 -0700

    ST remove dups

--- shells/variant_calling/combine_variantCallers/bcftools-isec/1-bcf-isec/2+/bcftools-isec_chr11_2.sh
+++ shells/variant_calling/combine_variantCallers/bcftools-isec/1-bcf-isec/2+/bcftools-isec_chr11_2.sh
@@ -15,11 +15,11 @@ module load bcftools/1.6
 
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c fastq/invariants/filtered_combined/VCMA_GATK_chr11_secondFilter.vcf > fastq/invariants/filtered_combined/VCMA_GATK_chr11_secondFilter.vcf.gz
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c freebayes/final_combined/freebayes_invariant_chr11_allFiltered.vcf > freebayes/final_combined/freebayes_invariant_chr11_allFiltered.vcf.gz
-# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c samtools_VC/samtools_invariant_chr11_Filtered_dupsRemoved.vcf > samtools_VC/samtools_invariant_chr11_Filtered_dupsRemoved.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c samtools_VC/samtools_invariant_chr11_Filtered_dupsRemoved.vcf > samtools_VC/samtools_invariant_chr11_Filtered_dupsRemoved.vcf.gz
 # 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix fastq/invariants/filtered_combined/VCMA_GATK_chr11_secondFilter.vcf.gz
-# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/final_combined/freebayes_invariant_chr11_allFiltered.vcf.gz
-# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools_VC/samtools_invariant_chr11_Filtered_dupsRemoved.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix fastq/invariants/filtered_combined/VCMA_GATK_chr11_secondFilter.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/final_combined/freebayes_invariant_chr11_allFiltered.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools_VC/samtools_invariant_chr11_Filtered_dupsRemoved.vcf.gz
 
 bcftools isec -n +2 -O z -p bcftools_isec/chr11_2 fastq/invariants/filtered_combined/VCMA_GATK_chr11_secondFilter.vcf.gz \
 samtools_VC/samtools_invariant_chr11_Filtered_dupsRemoved.vcf.gz \
