720d49b6eb53af9b220418900ee2bcd9b84ab388
melisakman <makman@berkeley.edu>
2018-07-11 06:56:31 -0700

    VSQR

--- shells/variant_calling/combine_variantCallers/VSQR/filtered/VSQR_chr01.sh
+++ shells/variant_calling/combine_variantCallers/VSQR/filtered/VSQR_chr01.sh
@@ -20,7 +20,7 @@ java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar
    -T VariantAnnotator \
    -V chr01_intersect.vcf.gz \
    -o chr01_intersect_annot.vcf.gz \
-   -A Coverage -A QualByDepth -A FisherStrand -A StrandOddsRatio -A MappingQualityRankSumTest -A ReadPosRankSum -A RMSMappingQuality -A InbreedingCoeff
+   -A Coverage -A QualByDepth -A FisherStrand -A StrandOddsRatio -A MappingQualityRankSumTest -A ReadPosRankSumTest -A RMSMappingQuality -A InbreedingCoeff
 
 # java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
 # 	-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
