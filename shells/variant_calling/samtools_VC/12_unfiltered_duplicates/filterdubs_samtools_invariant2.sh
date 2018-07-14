c2dff8fed3f3951b35608b3a75e772392c54f647
melisakman <makman@berkeley.edu>
2018-07-14 00:18:39 -0700

    chr02 ST dubsremoved

--- shells/variant_calling/samtools_VC/12_unfiltered_duplicates/filterdubs_samtools_invariant2.sh
+++ shells/variant_calling/samtools_VC/12_unfiltered_duplicates/filterdubs_samtools_invariant2.sh
@@ -30,7 +30,7 @@
 # gunzip samtools_invariant_chr17.vcf.gz 
 
 # python ~/git/Helianthus/shells/variant_calling/samtools_VC/12-filter/samtools_filter_by_qual.py samtools_invariant_chr01.vcf samtools_invariant_chr01_dupsRemoved.vcf
-python ~/git/Helianthus/shells/variant_calling/samtools_VC/12-filter/samtools_filter_by_qual.py samtools_invariant_chr02_fixed.vcf samtools_invariant_chr02_dupsRemoved.vcf
+python ~/git/Helianthus/shells/variant_calling/samtools_VC/13-filter/samtools_filter_by_qual.py samtools_invariant_chr02_fixed.vcf samtools_invariant_chr02_dupsRemoved.vcf
 # python ~/git/Helianthus/shells/variant_calling/samtools_VC/12-filter/samtools_filter_by_qual.py samtools_invariant_chr03.vcf samtools_invariant_chr03_dupsRemoved.vcf
 # python ~/git/Helianthus/shells/variant_calling/samtools_VC/12-filter/samtools_filter_by_qual.py samtools_invariant_chr04.vcf samtools_invariant_chr04_dupsRemoved.vcf
 # python ~/git/Helianthus/shells/variant_calling/samtools_VC/12-filter/samtools_filter_by_qual.py samtools_invariant_chr05.vcf samtools_invariant_chr05_dupsRemoved.vcf
