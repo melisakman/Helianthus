796f567b81dbbb7b714a0e9c105bdf607c2d5857
melisakman <makman@berkeley.edu>
2018-07-16 13:16:36 -0700

    GATK VSQR

--- shells/variant_calling/combine_variantCallers/VSQR/filtered/VSQR_filtered_chr01.sh
+++ shells/variant_calling/combine_variantCallers/VSQR/filtered/VSQR_filtered_chr01.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/chr01_2
-#SBATCH -J VSQchr01
+#SBATCH -J filchr01
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio
 #SBATCH --qos=rosalind_savio_normal
@@ -22,19 +22,15 @@ java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar
    -o chr01_intersect_annot.vcf.gz \
    -A Coverage -A QualByDepth -A FisherStrand -A StrandOddsRatio -A MappingQualityRankSumTest -A ReadPosRankSumTest -A RMSMappingQuality -A InbreedingCoeff
 
-# java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
-# 	-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
-# 	-V chr01_intersect.vcf.gz \
-# 	-selectType SNP \
-# 	-o chr01_intersect_SNP.vcf.gz 
-# 
-# java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
-# -T VariantRecalibrator \
-# -nt 6 \
-# -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
-# -input chr01_intersect_SNP.vcf.gz
-# -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an InbreedingCoeff \
-# -mode SNP \
-# -recalFile output.recal \
-# -tranchesFile output.tranches \
-# -rscriptFile output.plots.R
+java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
+-T VariantRecalibrator \
+-nt 6 \
+-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+-input chr01_intersect_annot.vcf.gz \
+-an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP -an InbreedingCoeff \
+-mode SNP \
+-resource:3callerscombined,known=false,training=true,truth=true,prior=10.0 ../truthSet/chr01_truth_GATK.vcf \
+-tranche 100.0 -tranche 99.9 -tranche 99.0 -tranche 90.0 \
+-recalFile filtered_chr01.recal \
+-tranchesFile filtered_chr01.tranches \
+-rscriptFile filtered_chr01.plots.R
