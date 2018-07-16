396234a002a66bbc2bef9b44e8baf9ab33c3bb62
melisakman <makman@berkeley.edu>
2018-07-16 15:31:52 -0700

    GATK VSQR

--- shells/variant_calling/combine_variantCallers/VSQR/filtered/VSQR_filtered_chr01.sh
+++ shells/variant_calling/combine_variantCallers/VSQR/filtered/VSQR_filtered_chr01.sh
@@ -14,14 +14,14 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load java
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_filtered_2plus_GATK.vcf > chr01_filtered_2plus_GATK.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf chr01_filtered_2plus_GATK.vcf.gz  
-java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
-   -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
-   -T VariantAnnotator \
-   -V chr01_filtered_2plus_GATK.vcf \
-   -o chr01_filtered_2plus_GATK_annot.vcf.gz \
-   -A Coverage -A QualByDepth -A FisherStrand -A StrandOddsRatio -A MappingQualityRankSumTest -A ReadPosRankSumTest -A RMSMappingQuality -A InbreedingCoeff
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_filtered_2plus_GATK.vcf > chr01_filtered_2plus_GATK.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf chr01_filtered_2plus_GATK.vcf.gz  
+# java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
+#    -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+#    -T VariantAnnotator \
+#    -V chr01_filtered_2plus_GATK.vcf \
+#    -o chr01_filtered_2plus_GATK_annot.vcf.gz \
+#    -A Coverage -A QualByDepth -A FisherStrand -A StrandOddsRatio -A MappingQualityRankSumTest -A ReadPosRankSumTest -A RMSMappingQuality -A InbreedingCoeff
 
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
 -T VariantRecalibrator \
@@ -30,7 +30,7 @@ java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clust
 -input chr01_filtered_2plus_GATK_annot.vcf.gz \
 -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP -an InbreedingCoeff \
 -mode SNP \
--resource:3callerscombined,known=false,training=true,truth=true,prior=10.0 ../truthSet/chr01_truth_GATK_variants.vcf \
+-resource:3callerscombined,known=false,training=true,truth=true,prior=10.0 truthSet/chr01_truth_GATK_variants.vcf \
 -tranche 100.0 -tranche 99.9 -tranche 99.0 -tranche 90.0 \
 -recalFile filtered_chr01.recal \
 -tranchesFile filtered_chr01.tranches \
