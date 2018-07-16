e4a637d0b8b0c1b4132350e4c2c99f3a2c8665f3
melisakman <makman@berkeley.edu>
2018-07-16 13:32:22 -0700

    GATK VSQR

--- shells/variant_calling/combine_variantCallers/VSQR/filtered/VSQR_filtered_chr01.sh
+++ shells/variant_calling/combine_variantCallers/VSQR/filtered/VSQR_filtered_chr01.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/chr01_2
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/
 #SBATCH -J filchr01
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio
@@ -14,19 +14,20 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load java
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix chr01_intersect.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_filtered_2plus_GATK.vcf > chr01_filtered_2plus_GATK.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf chr01_filtered_2plus_GATK.vcf.gz  
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
    -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
    -T VariantAnnotator \
-   -V chr01_intersect.vcf.gz \
-   -o chr01_intersect_annot.vcf.gz \
+   -V chr01_filtered_2plus_GATK.vcf.gz \
+   -o chr01_filtered_2plus_GATK_annot.vcf.gz \
    -A Coverage -A QualByDepth -A FisherStrand -A StrandOddsRatio -A MappingQualityRankSumTest -A ReadPosRankSumTest -A RMSMappingQuality -A InbreedingCoeff
 
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
 -T VariantRecalibrator \
 -nt 6 \
 -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
--input chr01_intersect_annot.vcf.gz \
+-input chr01_filtered_2plus_GATK_annot.vcf.gz \
 -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP -an InbreedingCoeff \
 -mode SNP \
 -resource:3callerscombined,known=false,training=true,truth=true,prior=10.0 ../truthSet/chr01_truth_GATK_variants.vcf \
