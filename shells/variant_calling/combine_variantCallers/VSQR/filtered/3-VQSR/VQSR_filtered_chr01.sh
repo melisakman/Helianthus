b86c0be2d83732d50d08db8ce2532e5f365cd61a
melisakman <makman@berkeley.edu>
2018-07-17 13:08:54 -0700

    GATK extract truth set

--- shells/variant_calling/combine_variantCallers/VSQR/filtered/3-VQSR/VQSR_filtered_chr01.sh
+++ shells/variant_calling/combine_variantCallers/VSQR/filtered/3-VQSR/VQSR_filtered_chr01.sh
@@ -2,9 +2,8 @@
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/
 #SBATCH -J filchr01
 #SBATCH --account=co_rosalind
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=rosalind_htc2_normal
-#SBATCH --cpus-per-task=4
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
 #SBATCH --time=80:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/VSQR_filtered_chr01.out
 #SBATCH -e /global/home/users/makman/GATK/outs/VSQR_filtered_chr01.err
