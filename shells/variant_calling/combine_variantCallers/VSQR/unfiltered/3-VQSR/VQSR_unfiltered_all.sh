b1b3cb31477bd8f9855c939223e8cb303751dc7e
melisakman <makman@berkeley.edu>
2018-07-18 23:57:13 -0700

    VQSR unfiltered all

--- shells/variant_calling/combine_variantCallers/VSQR/unfiltered/3-VQSR/VQSR_unfiltered_all.sh
+++ shells/variant_calling/combine_variantCallers/VSQR/unfiltered/3-VQSR/VQSR_unfiltered_all.sh
@@ -29,8 +29,21 @@ R_LIBS_USER=/global/home/users/makman/R/x86_64-pc-linux-gnu-library/3.4/
 -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
 -V unfiltered_2plus_GATK_annot.vcf.gz \
 -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP -an InbreedingCoeff \
+--max-gaussians 4 \
 -mode SNP \
 --resource 3callerscombined,known=false,training=true,truth=true,prior=10.0:../truthSet/truth_GATK_variants.vcf \
 -O unfiltered_all.recal \
 --tranches-file unfiltered_all.tranches \
 --rscript-file unfiltered_all.plots.R
+
+/clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk-4.0.1.2/gatk VariantRecalibrator \
+--java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" \
+-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+-V unfiltered_2plus_GATK_annot.vcf.gz \
+-an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP -an InbreedingCoeff \
+-mode SNP \
+--resource 3callerscombined,known=false,training=true,truth=true,prior=10.0:../truthSet/truth_GATK_variants.vcf \
+-O G8_unfiltered_all.recal \
+--tranches-file G8_unfiltered_all.tranches \
+--rscript-file G8_unfiltered_all.plots.R
+
