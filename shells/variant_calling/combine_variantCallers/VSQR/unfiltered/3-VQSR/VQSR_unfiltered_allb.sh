a5736afce07e6d88bf7e8bb064dee3a5ee76094c
melisakman <makman@berkeley.edu>
2018-07-19 09:54:52 -0700

    VQSR unfiltered all

--- shells/variant_calling/combine_variantCallers/VSQR/unfiltered/3-VQSR/VQSR_unfiltered_allb.sh
+++ shells/variant_calling/combine_variantCallers/VSQR/unfiltered/3-VQSR/VQSR_unfiltered_allb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/unfiltered
-#SBATCH -J unfil
+#SBATCH -J unfilb
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio2_htc
 #SBATCH --qos=rosalind_htc2_normal
 #SBATCH --cpus-per-task=12
 #SBATCH --time=80:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/VSQR_unfiltered_all.out
-#SBATCH -e /global/home/users/makman/GATK/outs/VSQR_unfiltered_all.err
+#SBATCH -o /global/home/users/makman/GATK/outs/VSQR_unfiltered_allb.out
+#SBATCH -e /global/home/users/makman/GATK/outs/VSQR_unfiltered_allb.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load java
@@ -19,19 +19,14 @@ module load Rcpp/0.12.14
 R_LIBS_USER=/global/home/users/makman/R/x86_64-pc-linux-gnu-library/3.4/
 
 
-/clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk-4.0.1.2/gatk IndexFeatureFile \
--F unfiltered_2plus_GATK_annot.vcf.gz
-
-
 /clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk-4.0.1.2/gatk VariantRecalibrator \
 --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" \
 -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
 -V unfiltered_2plus_GATK_annot.vcf.gz \
 -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP -an InbreedingCoeff \
---max-gaussians 4 \
 -mode SNP \
 --resource 3callerscombined,known=false,training=true,truth=true,prior=10.0:../truthSet/truth_GATK_variants.vcf \
--O unfiltered_all.recal \
---tranches-file unfiltered_all.tranches \
---rscript-file unfiltered_all.plots.R
+-O G8_unfiltered_all.recal \
+--tranches-file G8_unfiltered_all.tranches \
+--rscript-file G8_unfiltered_all.plots.R
 
