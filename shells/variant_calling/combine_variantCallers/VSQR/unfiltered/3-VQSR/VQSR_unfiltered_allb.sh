362bb0d866dc4dd65c5d354a7b3ebda649c362a1
melisakman <makman@berkeley.edu>
2018-07-22 15:55:23 -0700

    VQSR apply

--- shells/variant_calling/combine_variantCallers/VSQR/unfiltered/3-VQSR/VQSR_unfiltered_allb.sh
+++ shells/variant_calling/combine_variantCallers/VSQR/unfiltered/3-VQSR/VQSR_unfiltered_allb.sh
@@ -19,14 +19,35 @@ module load Rcpp/0.12.14
 R_LIBS_USER=/global/home/users/makman/R/x86_64-pc-linux-gnu-library/3.4/
 
 
-/clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk-4.0.1.2/gatk VariantRecalibrator \
---java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" \
+# /clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk-4.0.1.2/gatk VariantRecalibrator \
+# --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" \
+# -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+# -V unfiltered_2plus_GATK_annot.vcf.gz \
+# -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP -an InbreedingCoeff \
+# -mode SNP \
+# --resource 3callerscombined,known=false,training=true,truth=true,prior=10.0:../truthSet/truth_GATK_variants.vcf \
+# -O G8_unfiltered_all.recal \
+# --tranches-file G8_unfiltered_all.tranches \
+# --rscript-file G8_unfiltered_all.plots.R
+
+/clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk-4.0.1.2/gatk ApplyVQSR \
+--java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx128G" \
 -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
 -V unfiltered_2plus_GATK_annot.vcf.gz \
--an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP -an InbreedingCoeff \
--mode SNP \
---resource 3callerscombined,known=false,training=true,truth=true,prior=10.0:../truthSet/truth_GATK_variants.vcf \
--O G8_unfiltered_all.recal \
+-O unfiltered_2plus_GATK_annot_VSQR90.vcf.gz \
+--ts_filter_level 90.0 \
 --tranches-file G8_unfiltered_all.tranches \
---rscript-file G8_unfiltered_all.plots.R
-
+--recal-file G8_unfiltered_all.recal \
+-mode SNP
+  
+  
+/clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk-4.0.1.2/gatk ApplyVQSR \
+--java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx128G" \
+-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
+-V unfiltered_2plus_GATK_annot.vcf.gz \
+-O unfiltered_2plus_GATK_annot_VSQR99.vcf.gz \
+--ts_filter_level 99.0 \
+--tranches-file G8_unfiltered_all.tranches \
+--recal-file G8_unfiltered_all.recal \
+-mode SNP
+  
