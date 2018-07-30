1fb8b5dda734e64ec4050a0c6590b208366f2310
melisakman <makman@berkeley.edu>
2018-07-29 18:46:03 -0700

    gzip

--- shells/variant_calling/combine_variantCallers/combine_final/chr02_combine_final.sh
+++ shells/variant_calling/combine_variantCallers/combine_final/chr02_combine_final.sh
@@ -14,18 +14,19 @@ module load bcftools/1.6
 module load bio/vcftools
 module load java
 
-gunzip unfiltered_2plus_GATK_annot_VSQR90_G4.vcf.gz
-
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c unfiltered_2plus_GATK_annot_VSQR90_G4.vcf > unfiltered_2plus_GATK_annot_VSQR90_G4_2.vcf.gz
-
-zcat unfiltered_2plus_GATK_annot_VSQR90_G4_2.vcf.gz | bcftools filter -r HanXRQChr02 -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VQSRfiltered_90_G4_SNP_chr02.vcf.gz
-
-zcat ../chr02_filtered_2plus_GATK_annot.vcf.gz | bcftools filter -i 'TYPE = "indel" || TYPE = "ref"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../filtered_90_G4_indelRef_chr02.vcf.gz
-
-export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
-
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
-VQSRfiltered_90_G4_SNP_chr02.vcf.gz \
-../filtered_90_G4_indelRef_chr02.vcf.gz | vcf-sort -t /clusterfs/rosalind/users/makman/temp \
-| /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../final/chr02_final.vcf.gz
+zcat unfiltered_2plus_GATK_annot_VSQR90_G4_2.vcf.gz | tail
+# gunzip unfiltered_2plus_GATK_annot_VSQR90_G4.vcf.gz
+# 
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c unfiltered_2plus_GATK_annot_VSQR90_G4.vcf > unfiltered_2plus_GATK_annot_VSQR90_G4_2.vcf.gz
+# 
+# zcat unfiltered_2plus_GATK_annot_VSQR90_G4_2.vcf.gz | bcftools filter -r HanXRQChr02 -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VQSRfiltered_90_G4_SNP_chr02.vcf.gz
+# 
+# zcat ../chr02_filtered_2plus_GATK_annot.vcf.gz | bcftools filter -i 'TYPE = "indel" || TYPE = "ref"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../filtered_90_G4_indelRef_chr02.vcf.gz
+# 
+# export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
+# 
+# /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
+# VQSRfiltered_90_G4_SNP_chr02.vcf.gz \
+# ../filtered_90_G4_indelRef_chr02.vcf.gz | vcf-sort -t /clusterfs/rosalind/users/makman/temp \
+# | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../final/chr02_final.vcf.gz
 
