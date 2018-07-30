42128ee96377595fbc2d4d2d965fecef177e4f02
melisakman <makman@berkeley.edu>
2018-07-29 17:47:08 -0700

    gzip

--- shells/variant_calling/combine_variantCallers/combine_final/chr01_combine_final.sh
+++ shells/variant_calling/combine_variantCallers/combine_final/chr01_combine_final.sh
@@ -17,8 +17,8 @@ module load java
 # gunzip unfiltered_2plus_GATK_annot_VSQR90_G4.vcf.gz
 
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c unfiltered_2plus_GATK_annot_VSQR90_G4.vcf > unfiltered_2plus_GATK_annot_VSQR90_G4_2.vcf.gz
-
-zcat unfiltered_2plus_GATK_annot_VSQR90_G4_2.vcf.gz | bcftools filter -r HanXRQChr01 -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VQSRfiltered_90_G4_SNP_chr01.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix unfiltered_2plus_GATK_annot_VSQR90_G4_2.vcf.gz
+zcat unfiltered_2plus_GATK_annot_VSQR90_G4_2.vcf.gz | bcftools filter -r HanXRQChr01 -i 'TYPE = "snp"' > snp_test.vcf #| /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VQSRfiltered_90_G4_SNP_chr01.vcf.gz
 
 # zcat ../chr01_filtered_2plus_GATK_annot.vcf.gz | bcftools filter -i 'TYPE = "indel" || TYPE = "ref"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../filtered_90_G4_indelRef_chr01.vcf.gz
 # 
