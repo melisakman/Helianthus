959f53e7099e8ebb888ca1618e3f87aca03d3db0
melisakman <makman@berkeley.edu>
2018-07-31 17:51:54 -0700

    simple metrics

--- shells/variant_calling/tests/simple_metrics/VQSR_pi_site.sh
+++ shells/variant_calling/tests/simple_metrics/VQSR_pi_site.sh
@@ -11,38 +11,38 @@
 
 module load bio/vcftools/0.1.15
 
-vcftools --gzvcf chr01_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr01_10kb
-vcftools --gzvcf chr02_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr02_10kb
-vcftools --gzvcf chr03_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr03_10kb
-vcftools --gzvcf chr04_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr04_10kb
-vcftools --gzvcf chr05_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr05_10kb
-vcftools --gzvcf chr06_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr06_10kb
-vcftools --gzvcf chr07_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr07_10kb
-vcftools --gzvcf chr08_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr08_10kb
-vcftools --gzvcf chr09_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr09_10kb
-vcftools --gzvcf chr10_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr10_10kb
-vcftools --gzvcf chr11_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr11_10kb
-vcftools --gzvcf chr12_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr12_10kb
-vcftools --gzvcf chr13_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr13_10kb
-vcftools --gzvcf chr14_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr14_10kb
-vcftools --gzvcf chr15_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr15_10kb
-vcftools --gzvcf chr16_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr16_10kb
-vcftools --gzvcf chr17_final.vcf.gz --site-pi --keep lr_list.txt 10000 --out pi/lr_pi_chr17_10kb
+vcftools --gzvcf chr01_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr01_10kb
+vcftools --gzvcf chr02_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr02_10kb
+vcftools --gzvcf chr03_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr03_10kb
+vcftools --gzvcf chr04_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr04_10kb
+vcftools --gzvcf chr05_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr05_10kb
+vcftools --gzvcf chr06_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr06_10kb
+vcftools --gzvcf chr07_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr07_10kb
+vcftools --gzvcf chr08_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr08_10kb
+vcftools --gzvcf chr09_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr09_10kb
+vcftools --gzvcf chr10_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr10_10kb
+vcftools --gzvcf chr11_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr11_10kb
+vcftools --gzvcf chr12_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr12_10kb
+vcftools --gzvcf chr13_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr13_10kb
+vcftools --gzvcf chr14_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr14_10kb
+vcftools --gzvcf chr15_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr15_10kb
+vcftools --gzvcf chr16_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr16_10kb
+vcftools --gzvcf chr17_final.vcf.gz --site-pi --keep lr_list.txt --out pi/lr_pi_chr17_10kb
 
-vcftools --gzvcf chr01_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr01_10kb
-vcftools --gzvcf chr02_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr02_10kb
-vcftools --gzvcf chr03_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr03_10kb
-vcftools --gzvcf chr04_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr04_10kb
-vcftools --gzvcf chr05_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr05_10kb
-vcftools --gzvcf chr06_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr06_10kb
-vcftools --gzvcf chr07_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr07_10kb
-vcftools --gzvcf chr08_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr08_10kb
-vcftools --gzvcf chr09_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr09_10kb
-vcftools --gzvcf chr10_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr10_10kb
-vcftools --gzvcf chr11_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr11_10kb
-vcftools --gzvcf chr12_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr12_10kb
-vcftools --gzvcf chr13_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr13_10kb
-vcftools --gzvcf chr14_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr14_10kb
-vcftools --gzvcf chr15_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr15_10kb
-vcftools --gzvcf chr16_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr16_10kb
-vcftools --gzvcf chr17_final.vcf.gz --site-pi --keep wd_list.txt 10000 --out pi/wd_pi_chr17_10kb
+vcftools --gzvcf chr01_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr01_10kb
+vcftools --gzvcf chr02_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr02_10kb
+vcftools --gzvcf chr03_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr03_10kb
+vcftools --gzvcf chr04_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr04_10kb
+vcftools --gzvcf chr05_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr05_10kb
+vcftools --gzvcf chr06_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr06_10kb
+vcftools --gzvcf chr07_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr07_10kb
+vcftools --gzvcf chr08_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr08_10kb
+vcftools --gzvcf chr09_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr09_10kb
+vcftools --gzvcf chr10_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr10_10kb
+vcftools --gzvcf chr11_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr11_10kb
+vcftools --gzvcf chr12_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr12_10kb
+vcftools --gzvcf chr13_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr13_10kb
+vcftools --gzvcf chr14_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr14_10kb
+vcftools --gzvcf chr15_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr15_10kb
+vcftools --gzvcf chr16_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr16_10kb
+vcftools --gzvcf chr17_final.vcf.gz --site-pi --keep wd_list.txt --out pi/wd_pi_chr17_10kb
