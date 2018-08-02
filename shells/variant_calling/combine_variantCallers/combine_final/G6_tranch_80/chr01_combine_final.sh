3d0a58e9805182e7615cdf53503f681e9b4a27a5
melisakman <makman@berkeley.edu>
2018-08-02 09:44:04 -0700

    tranc 80 G6

---
+++ shells/variant_calling/combine_variantCallers/combine_final/G6_tranch_80/chr01_combine_final.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/unfiltered
+#SBATCH -J comchr01
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --time=24:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/VSQR_combine_sort_chr01.out
+#SBATCH -e /global/home/users/makman/GATK/outs/VSQR_combine_sort_chr01.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load bcftools/1.6
+module load bio/vcftools
+module load java
+
+# gunzip unfiltered_2plus_GATK_annot_VSQR80_G6.vcf.gz
+
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c unfiltered_2plus_GATK_annot_VSQR80_G6.vcf > unfiltered_2plus_GATK_annot_VSQR80_G6.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix unfiltered_2plus_GATK_annot_VSQR80_G6.vcf.gz
+
+bcftools filter -r HanXRQChr01 -i 'TYPE = "snp"' unfiltered_2plus_GATK_annot_VSQR80_G6.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VQSRfiltered_80_G6_SNP_chr01.vcf.gz
+
+export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
+
+/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
+VQSRfiltered_80_G6_SNP_chr01.vcf.gz \
+../filtered_90_G4_indelRef_chr01.vcf.gz | vcf-sort -t /clusterfs/rosalind/users/makman/temp \
+| /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../final/G6_80/chr01_final.vcf.gz
+
---
+++ shells/variant_calling/combine_variantCallers/combine_final/G6_tranch_80/chr17_combine_final.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/unfiltered
+#SBATCH -J comchr17
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --cpus-per-task=6
+#SBATCH --time=24:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/VSQR_combine_sort_chr17.out
+#SBATCH -e /global/home/users/makman/GATK/outs/VSQR_combine_sort_chr17.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load bcftools/1.6
+module load bio/vcftools
+module load java
+
+
+bcftools filter -r HanXRQChr17 -i 'TYPE = "snp"' unfiltered_2plus_GATK_annot_VSQR80_G6.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VQSRfiltered_80_G6_SNP_chr17.vcf.gz
+
+
+export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
+
+/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
+VQSRfiltered_80_G6_SNP_chr17.vcf.gz \
+../filtered_90_G4_indelRef_chr17.vcf.gz | vcf-sort -t /clusterfs/rosalind/users/makman/temp \
+| /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../final/G6_80/chr17_final.vcf.gz
+
