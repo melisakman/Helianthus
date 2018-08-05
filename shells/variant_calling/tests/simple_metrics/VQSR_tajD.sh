cfd91cda9418911423f3a273bae388ac83bf24b6
melisakman <makman@berkeley.edu>
2018-08-04 22:52:47 -0700

    G6_90 pi tajD fst

--- shells/variant_calling/tests/simple_metrics/VQSR_tajD.sh
+++ shells/variant_calling/tests/simple_metrics/VQSR_tajD.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/G6_90
 #SBATCH -J vcffst
 #SBATCH --partition=vector
 #SBATCH --qos=vector_batch
@@ -10,20 +10,20 @@
 #SBATCH --mail-type=All
 module load bio/vcftools/0.1.15
 
-vcftools --gzvcf chr01_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr01_10kb
-vcftools --gzvcf chr02_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr02_10kb
-vcftools --gzvcf chr03_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr03_10kb
-vcftools --gzvcf chr04_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr04_10kb
-vcftools --gzvcf chr05_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr05_10kb
-vcftools --gzvcf chr06_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr06_10kb
-vcftools --gzvcf chr07_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr07_10kb
-vcftools --gzvcf chr08_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr08_10kb
-vcftools --gzvcf chr09_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr09_10kb
-vcftools --gzvcf chr10_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr10_10kb
-vcftools --gzvcf chr11_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr11_10kb
-vcftools --gzvcf chr12_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr12_10kb
-vcftools --gzvcf chr13_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr13_10kb
-vcftools --gzvcf chr14_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr14_10kb
-vcftools --gzvcf chr15_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr15_10kb
-vcftools --gzvcf chr16_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr16_10kb
-vcftools --gzvcf chr17_final.vcf.gz --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr17_10kb
+vcftools --vcf chr01_final.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr01_10kb
+vcftools --gzvcf chr02_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr02_10kb
+vcftools --gzvcf chr03_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr03_10kb
+vcftools --gzvcf chr04_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr04_10kb
+vcftools --gzvcf chr05_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr05_10kb
+vcftools --gzvcf chr06_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr06_10kb
+vcftools --gzvcf chr07_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr07_10kb
+vcftools --gzvcf chr08_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr08_10kb
+vcftools --gzvcf chr09_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr09_10kb
+vcftools --gzvcf chr10_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr10_10kb
+vcftools --gzvcf chr11_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr11_10kb
+vcftools --gzvcf chr12_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr12_10kb
+vcftools --gzvcf chr13_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr13_10kb
+vcftools --gzvcf chr14_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr14_10kb
+vcftools --gzvcf chr15_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr15_10kb
+vcftools --gzvcf chr16_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr16_10kb
+vcftools --gzvcf chr17_final.vcf.gz --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr17_10kb
