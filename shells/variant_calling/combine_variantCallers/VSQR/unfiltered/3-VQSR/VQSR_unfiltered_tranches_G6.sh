98fcd17450a335c0d393f8656cb003fcd7fb09dc
melisakman <makman@berkeley.edu>
2018-08-08 23:28:47 -0700

    G4 and G6 tranches

--- shells/variant_calling/combine_variantCallers/VSQR/unfiltered/3-VQSR/VQSR_unfiltered_tranches_G6.sh
+++ shells/variant_calling/combine_variantCallers/VSQR/unfiltered/3-VQSR/VQSR_unfiltered_tranches_G6.sh
@@ -2,8 +2,9 @@
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/unfiltered
 #SBATCH -J G4
 #SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --mem=128000
 #SBATCH --time=80:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/VSQR_unfiltered_all_tranches_G4.out
 #SBATCH -e /global/home/users/makman/GATK/outs/VSQR_unfiltered_all_tranches_G4.err
