06cbbd3e69086e3e00f2544fe8f6605a109b1bbe
melisakman <makman@berkeley.edu>
2018-10-04 12:55:53 -0700

    vcf stats

---
+++ shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals.py
@@ -0,0 +1,48 @@
+file = open('chr03_final.vcf', 'r')
+
+out = open('lowerthan10SNPsregions.txt', 'w')
+
+chr_old = "HanXRQChr01"
+bin_start = 1
+bin_end = 10001
+invariant_no
+for line in file:
+	split = line.split("\t")
+	if split[0][0] == "#":
+		continue
+	else:
+		chr = split[0]
+		site = int(split[1])
+		ref = split[3]
+		alt = split[4]
+		if chr == chr_old:
+			if bin_end >= site >= bin_start:
+				if alt == ".":
+					invariant_no +=1
+				else:
+					continue
+			else:
+				out.write(chr + "\t" + (bin_start) + "\t" + str(bin_end) + "\t" + str(invariant_no) + "\n")	
+				invariant_no = 0
+				bin_start = bin_start + 10000
+				bin_end = bin_end + 10000
+				if alt == ".":
+					invariant_no +=1
+				else:
+					continue
+					
+		else:
+			out.write(chr + "\t" + (bin_start) + "\t" + str(bin_end) + "\t" + str(invariant_no) + "\n")	
+			invariant_no = 0
+			bin_start = bin_start + 10000
+			bin_end = bin_end + 10000
+			if alt == ".":
+				invariant_no +=1
+			else:
+				continue
+			
+				
+			
+
+
+
