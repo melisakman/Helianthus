d11c2e63fdd8bc94b922f468e815a682e50da75a
melisakman <makman@berkeley.edu>
2018-10-09 16:17:46 -0700

    count variant invariant

--- shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py
+++ shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py
@@ -29,8 +29,10 @@ for line in file:
 			out.write(chr + "\t" + str(bin_start) + "\t" + str(bin_end) + "\t" + str(invariant_no) + "\t" + str(variant_no) + "\n")	
 			invariant_no = 0
 			variant_no = 0
-			bin_start = bin_start + 500
-			bin_end = bin_end + 500
+			bin_diff = str(site - bin_start)
+			to_add = int(bin_diff[0:2]) * 100
+			bin_start = bin_start + to_add
+			bin_end = bin_end + to_add
 			if alt == ".":
 				invariant_no +=1
 			else:
@@ -41,3 +43,16 @@ out.write(chr + "\t" + str(bin_start) + "\t" + str(bin_end) + "\t" + str(invaria
 
 
 
+# HanXRQChr01     4220    .       TG      T
+# HanXRQChr01     6556    .       G       C
+# HanXRQChr01     6607    .       C       T
+# HanXRQChr01     6621    .       G       .
+# HanXRQChr01     6622    .       A       .
+# HanXRQChr01     6627    .       G       .
+# HanXRQChr01     6630    .       T       .
+# HanXRQChr01     6631    .       A       .
+# HanXRQChr01     6632    .       A       .
+# HanXRQChr01     6633    .       A       .
+# HanXRQChr01     6637    .       A       .
+# HanXRQChr01     6641    .       T       .
+# HanXRQChr01     6642    .       A       .
