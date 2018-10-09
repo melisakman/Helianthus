581d63e7be668c506574842af40991b3c0d2dfff
melisakman <makman@berkeley.edu>
2018-10-09 15:09:16 -0700

    count variant invariant

---
+++ shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py
@@ -0,0 +1,53 @@
+import sys
+
+InputFile = sys.argv[1]
+OutputFile = sys.argv[2]
+
+file = open(InputFile, 'r')
+out = open(OutputFile, 'w')
+
+bin_start = 1
+bin_end = 1001
+invariant_no = 0
+variant_no =0
+out.write("chr\tbin_start\tbin_end\tinvariant_no\tvariant_no")
+for line in file:
+	split = line.split("\t")
+	if split[0][0] == "#":
+		continue
+	else:
+		chr = split[0]
+		site = int(split[1])
+		ref = split[3]
+		alt = split[4]
+	if chr == chr_old:
+		if bin_end >= site >= bin_start:
+			if alt == ".":
+				invariant_no +=1
+			else:
+				variant_no +=1
+		else:
+			out.write(chr + "\t" + str(bin_start) + "\t" + str(bin_end) + "\t" + str(invariant_no) + "\t" + str(variant_no) + "\n")	
+			invariant_no = 0
+			bin_start = bin_start + 1000
+			bin_end = bin_end + 1000
+			if alt == ".":
+				invariant_no +=1
+			else:
+				variant_no +=1
+					
+	else:
+		out.write(chr + "\t" + str(bin_start) + "\t" + str(bin_end) + "\t" + str(invariant_no) + "\t" + str(variant_no) + "\n")	
+		invariant_no = 0
+		bin_start = bin_start + 1000
+		bin_end = bin_end + 1000
+		if alt == ".":
+			invariant_no +=1
+		else:
+			variant_no +=1
+			
+out.write(chr + "\t" + str(bin_start) + "\t" + str(bin_end) + "\t" + str(invariant_no) + "\t" + str(variant_no) + "\n")				
+			
+
+
+
