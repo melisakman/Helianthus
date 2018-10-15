6aa542799cc69bb2eee0ebe7e0c1592fa5b9f6ae
melisakman <makman@berkeley.edu>
2018-10-15 14:58:25 -0700

    references less stringent filtering GATK

---
+++ shells/variant_calling/GATK/16-second_filter/stringent_used/invariantSitesOnly/secondfilter_GATK_invariant4.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/invariants/filtered_combined/
+#SBATCH -J filGK4
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --time=24:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/bcftools_filter_GATK4.out
+#SBATCH -e /global/home/users/makman/GATK/outs/bcftools_filter_GATK4.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load bcftools/1.6
+module load bio/vcftools
+module load java
+
+# zcat VCMA_chr01_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr01_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr02_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr02_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr03_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr03_secondFilter_onlyInvariantSites.vcf.gz
+zcat VCMA_chr04_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr04_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr05_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr05_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr06_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr06_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr07_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr07_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr08_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr08_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr09_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr09_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr10_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr10_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr11_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr11_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr12_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr12_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr13_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr13_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr14_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr14_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr15_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr15_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr16_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr16_secondFilter_onlyInvariantSites.vcf.gz
+# zcat VCMA_chr17_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr17_secondFilter_onlyInvariantSites.vcf.gz
+
+
