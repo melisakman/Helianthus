2d8489b60951e5cdb66230739d563c53c8b66109
melisakman <makman@berkeley.edu>
2018-10-16 11:37:20 -0700

    check tail

--- shells/variant_calling/GATK/16-second_filter/stringent_used/invariantSitesOnly/secondfilter_GATK_invariant1.sh
+++ shells/variant_calling/GATK/16-second_filter/stringent_used/invariantSitesOnly/secondfilter_GATK_invariant1.sh
@@ -14,7 +14,7 @@ module load bcftools/1.6
 module load bio/vcftools
 module load java
 
-cat VCMA_chr01_NO_VARIATION_hardfiltered.vcf | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr01_secondFilter_onlyInvariantSites.vcf.gz
+# cat VCMA_chr01_NO_VARIATION_hardfiltered.vcf | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr01_secondFilter_onlyInvariantSites.vcf.gz
 # zcat VCMA_chr02_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr02_secondFilter_onlyInvariantSites.vcf.gz
 # zcat VCMA_chr03_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr03_secondFilter_onlyInvariantSites.vcf.gz
 # zcat VCMA_chr04_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr04_secondFilter_onlyInvariantSites.vcf.gz
@@ -33,3 +33,20 @@ cat VCMA_chr01_NO_VARIATION_hardfiltered.vcf | vcftools --vcf - --minDP 1 --max-
 # zcat VCMA_chr17_NO_VARIATION_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VCMA_GATK_chr17_secondFilter_onlyInvariantSites.vcf.gz
 
 
+zcat VCMA_GATK_chr02_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr02_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr03_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr04_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr05_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr06_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr07_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr08_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr09_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr10_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr11_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr12_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr13_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr14_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr15_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr16_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
+zcat VCMA_GATK_chr17_secondFilter_onlyInvariantSites.vcf.gz | tail -n 1
--- shells/variant_calling/samtools_VC/13-filter/invariantSitesOnly/hardfilter_samtools_invariant1.sh
+++ shells/variant_calling/samtools_VC/13-filter/invariantSitesOnly/hardfilter_samtools_invariant1.sh
@@ -16,10 +16,10 @@ module load bio/vcftools
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 module load java
 
-zcat old/samtools_invariant_chr01_dupsRemoved.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr01_Filtered_invariantSitesOnly.vcf.gz
-zcat old/samtools_invariant_chr02_dupsRemoved.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr02_Filtered_invariantSitesOnly.vcf.gz
-zcat old/samtools_invariant_chr03_dupsRemoved.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr03_Filtered_invariantSitesOnly.vcf.gz
-zcat old/samtools_invariant_chr04_dupsRemoved.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr04_Filtered_invariantSitesOnly.vcf.gz
+# zcat old/samtools_invariant_chr01_dupsRemoved.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr01_Filtered_invariantSitesOnly.vcf.gz
+# zcat old/samtools_invariant_chr02_dupsRemoved.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr02_Filtered_invariantSitesOnly.vcf.gz
+# zcat old/samtools_invariant_chr03_dupsRemoved.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr03_Filtered_invariantSitesOnly.vcf.gz
+# zcat old/samtools_invariant_chr04_dupsRemoved.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr04_Filtered_invariantSitesOnly.vcf.gz
 # zcat old/samtools_invariant_chr05.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr05_Filtered_invariantSitesOnly.vcf.gz
 # zcat old/samtools_invariant_chr06.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr06_Filtered_invariantSitesOnly.vcf.gz
 # zcat old/samtools_invariant_chr07.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr07_Filtered_invariantSitesOnly.vcf.gz
@@ -33,6 +33,27 @@ zcat old/samtools_invariant_chr04_dupsRemoved.vcf.gz | bcftools filter -g 5 -i '
 # zcat old/samtools_invariant_chr15.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr15_Filtered_invariantSitesOnly.vcf.gz
 # zcat old/samtools_invariant_chr16.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr16_Filtered_invariantSitesOnly.vcf.gz
 # zcat old/samtools_invariant_chr17.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools_invariant_chr17_Filtered_invariantSitesOnly.vcf.gz
+
+
+
+zcat samtools_invariant_chr01_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr02_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr03_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr04_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr05_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr06_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr07_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr08_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr09_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr10_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr11_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr12_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr13_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr14_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr15_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr16_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+zcat samtools_invariant_chr17_Filtered_invariantSitesOnly.vcf.gz | tail -n 1
+
 
 
 
