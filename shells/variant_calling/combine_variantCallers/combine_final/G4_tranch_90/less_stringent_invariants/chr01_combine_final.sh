5afe810964e509e8edaaf1edd8fbe784bc3534ef
melisakman <makman@berkeley.edu>
2018-10-19 23:45:20 -0700

    combine final

--- shells/variant_calling/combine_variantCallers/combine_final/G4_tranch_90/less_stringent_invariants/chr01_combine_final.sh
+++ shells/variant_calling/combine_variantCallers/combine_final/G4_tranch_90/less_stringent_invariants/chr01_combine_final.sh
@@ -15,8 +15,8 @@ module load bio/vcftools
 module load java
 
 
-bcftools filter -r HanXRQChr01 -i 'TYPE = "snp"' unfiltered_2plus_GATK_annot_VSQR90_G4.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VQSRfiltered_90_G4_SNP_chr01.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_unfiltered_2plus_GATK_invariantSitesOnly.vcf > chr01_unfiltered_2plus_GATK_invariantSitesOnly.vcf.gz
+# bcftools filter -r HanXRQChr01 -i 'TYPE = "snp"' unfiltered_2plus_GATK_annot_VSQR90_G4.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > VQSRfiltered_90_G4_SNP_chr01.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_unfiltered_2plus_GATK_invariantSitesOnly.vcf > chr01_unfiltered_2plus_GATK_invariantSitesOnly.vcf.gz
 
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
