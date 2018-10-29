ae889ef5f0e70f724d42d4e824d8936d04cb9d2c
melisakman <makman@berkeley.edu>
2018-10-29 08:24:57 -0700

    count all

--- shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/new/VQSR_fst.sh
+++ shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/new/VQSR_fst.sh
@@ -10,20 +10,20 @@
 #SBATCH --mail-type=All
 
 module load bio/vcftools/0.1.15
-vcftools --vcf chr01_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr01_10kb
-vcftools --vcf chr02_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr02_10kb
-vcftools --vcf chr03_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr03_10kb
-vcftools --vcf chr04_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr04_10kb
-vcftools --vcf chr05_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr05_10kb
-vcftools --vcf chr06_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr06_10kb
-vcftools --vcf chr07_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr07_10kb
-vcftools --vcf chr08_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr08_10kb
-vcftools --vcf chr09_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr09_10kb
-vcftools --vcf chr10_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr10_10kb
-vcftools --vcf chr11_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr11_10kb
-vcftools --vcf chr12_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr12_10kb
-vcftools --vcf chr13_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr13_10kb
-vcftools --vcf chr14_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr14_10kb
-vcftools --vcf chr15_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr15_10kb
-vcftools --vcf chr16_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr16_10kb
-vcftools --vcf chr17_final_lessStringentInvariants.vcf --weir-fst-pop ../wd_list.txt --weir-fst-pop ../lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr17_10kb
+vcftools --vcf chr01_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr01_10kb
+vcftools --vcf chr02_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr02_10kb
+vcftools --vcf chr03_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr03_10kb
+vcftools --vcf chr04_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr04_10kb
+vcftools --vcf chr05_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr05_10kb
+vcftools --vcf chr06_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr06_10kb
+vcftools --vcf chr07_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr07_10kb
+vcftools --vcf chr08_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr08_10kb
+vcftools --vcf chr09_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr09_10kb
+vcftools --vcf chr10_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr10_10kb
+vcftools --vcf chr11_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr11_10kb
+vcftools --vcf chr12_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr12_10kb
+vcftools --vcf chr13_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr13_10kb
+vcftools --vcf chr14_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr14_10kb
+vcftools --vcf chr15_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr15_10kb
+vcftools --vcf chr16_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr16_10kb
+vcftools --vcf chr17_final_lessStringentInvariants.vcf --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr17_10kb
--- shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/new/VQSR_tajD.sh
+++ shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/new/VQSR_tajD.sh
@@ -10,20 +10,20 @@
 #SBATCH --mail-type=All
 module load bio/vcftools/0.1.15
 
-vcftools --vcf chr01_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr01_10kb
-vcftools --vcf chr02_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr02_10kb
-vcftools --vcf chr03_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr03_10kb
-vcftools --vcf chr04_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr04_10kb
-vcftools --vcf chr05_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr05_10kb
-vcftools --vcf chr06_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr06_10kb
-vcftools --vcf chr07_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr07_10kb
-vcftools --vcf chr08_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr08_10kb
-vcftools --vcf chr09_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr09_10kb
-vcftools --vcf chr10_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr10_10kb
-vcftools --vcf chr11_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr11_10kb
-vcftools --vcf chr12_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr12_10kb
-vcftools --vcf chr13_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr13_10kb
-vcftools --vcf chr14_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr14_10kb
-vcftools --vcf chr15_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr15_10kb
-vcftools --vcf chr16_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr16_10kb
-vcftools --vcf chr17_final_lessStringentInvariants.vcf --keep ../lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr17_10kb
+vcftools --vcf chr01_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr01_10kb
+vcftools --vcf chr02_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr02_10kb
+vcftools --vcf chr03_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr03_10kb
+vcftools --vcf chr04_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr04_10kb
+vcftools --vcf chr05_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr05_10kb
+vcftools --vcf chr06_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr06_10kb
+vcftools --vcf chr07_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr07_10kb
+vcftools --vcf chr08_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr08_10kb
+vcftools --vcf chr09_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr09_10kb
+vcftools --vcf chr10_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr10_10kb
+vcftools --vcf chr11_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr11_10kb
+vcftools --vcf chr12_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr12_10kb
+vcftools --vcf chr13_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr13_10kb
+vcftools --vcf chr14_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr14_10kb
+vcftools --vcf chr15_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr15_10kb
+vcftools --vcf chr16_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr16_10kb
+vcftools --vcf chr17_final_lessStringentInvariants.vcf --keep lr_list.txt --TajimaD 10000 --out tajD/lr_TajD_chr17_10kb
