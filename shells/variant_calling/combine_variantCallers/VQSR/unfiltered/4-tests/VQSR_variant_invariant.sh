467922a8dc96d252296e4dcb50d1b9ea927b7489
melisakman <makman@berkeley.edu>
2018-10-28 23:23:06 -0700

    count all

--- shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/VQSR_variant_invariant.sh
+++ shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/VQSR_variant_invariant.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/G4_90/
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/
 #SBATCH -J var_invar
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio
@@ -10,23 +10,23 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr01_final.vcf chr01_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr02_final.vcf chr02_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr03_final.vcf chr03_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr04_final.vcf chr04_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr05_final.vcf chr05_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr06_final.vcf chr06_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr07_final.vcf chr07_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr08_final.vcf chr08_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr09_final.vcf chr09_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr10_final.vcf chr10_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr11_final.vcf chr11_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr12_final.vcf chr12_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr13_final.vcf chr13_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr14_final.vcf chr14_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr15_final.vcf chr15_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr16_final.vcf chr16_500bp_variant_invariant_counts.txt
-# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr17_final.vcf chr17_500bp_variant_invariant_counts.txt
+gunzip *.vcf.gz
 
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr01_final_lessStringentInvariants.vcf chr01_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr02_final_lessStringentInvariants.vcf chr02_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr03_final_lessStringentInvariants.vcf chr03_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr04_final_lessStringentInvariants.vcf chr04_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr05_final_lessStringentInvariants.vcf chr05_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr06_final_lessStringentInvariants.vcf chr06_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr07_final_lessStringentInvariants.vcf chr07_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr08_final_lessStringentInvariants.vcf chr08_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr09_final_lessStringentInvariants.vcf chr09_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr10_final_lessStringentInvariants.vcf chr10_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr11_final_lessStringentInvariants.vcf chr11_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr12_final_lessStringentInvariants.vcf chr12_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr13_final_lessStringentInvariants.vcf chr13_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr14_final_lessStringentInvariants.vcf chr14_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr15_final_lessStringentInvariants.vcf chr15_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr16_final_lessStringentInvariants.vcf chr16_10K_variant_invariant_counts.txt
+# python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr17_final_lessStringentInvariants.vcf chr17_10K_variant_invariant_counts.txt
 
-python ~/git/Helianthus/shells/variant_calling/combine_variantCallers/VQSR/unfiltered/4-tests/invariants_lowSNPintervals_chrSeparately.py chr01_final.vcf chr01_1K_variant_invariant_counts.txt
