8f7310544c534f4778acea85608eb0ea99f8df90
melisakman <makman@berkeley.edu>
2018-11-06 09:55:42 -0800

    G12

---
+++ shells/H12/VQSR/new/vcf2genotype_VQSR_A.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/
+#SBATCH -J G12-A
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=6:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/vcf2genotype_VQSR_A.out
+#SBATCH -e /global/home/users/makman/H12/outs/vcf2genotype_VQSR_A.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+
+module load python/2.7
+
+python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr01_final_lessStringentInvariants.vcf lr_cv_list.txt G12/chr01_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr02_final.vcf ../lr_cv_list.txt G12/VQSR_chr02_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr03_final.vcf ../lr_cv_list.txt G12/VQSR_chr03_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr04_final.vcf ../lr_cv_list.txt G12/VQSR_chr04_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr05_final.vcf /lr_cv_list.txt G12/VQSR_chr05_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr06_final.vcf /lr_cv_list.txt G12/VQSR_chr06_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr07_final.vcf /lr_cv_list.txt G12/VQSR_chr07_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr08_final.vcf /lr_cv_list.txt G12/VQSR_chr08_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr09_final.vcf /lr_cv_list.txt G12/VQSR_chr09_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr10_final.vcf /lr_cv_list.txt G12/VQSR_chr10_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr11_final.vcf /lr_cv_list.txt G12/VQSR_chr11_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr12_final.vcf /lr_cv_list.txt G12/VQSR_chr12_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr13_final.vcf /lr_cv_list.txt G12/VQSR_chr13_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr14_final.vcf /lr_cv_list.txt G12/VQSR_chr14_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr15_final.vcf /lr_cv_list.txt G12/VQSR_chr15_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr16_final.vcf /lr_cv_list.txt G12/VQSR_chr16_G12_input.txt
+# python /clusterfs/rosalind/users/makman/G12/vcf2genotype_Alex.py chr17_final.vcf /lr_cv_list.txt G12/VQSR_chr17_G12_input.txt
