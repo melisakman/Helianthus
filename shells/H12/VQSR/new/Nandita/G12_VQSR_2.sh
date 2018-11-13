e8704b2d9de15126af3724b23397bbe3cb6f0ebc
melisakman <makman@berkeley.edu>
2018-11-12 23:23:11 -0800

    G12

--- shells/H12/VQSR/new/Nandita/G12_VQSR_2.sh
+++ shells/H12/VQSR/new/Nandita/G12_VQSR_2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/G12/Nandita
-#SBATCH -J G12-N1
+#SBATCH -J G12-N2
 #SBATCH --account=fc_blackman
 #SBATCH --partition=savio2
 #SBATCH --qos=savio_normal
 #SBATCH --cpus-per-task=4
 #SBATCH --time=72:00:00
-#SBATCH -o /global/home/users/makman/H12/outs/G12_VQSR_Nandita_1.out
-#SBATCH -e /global/home/users/makman/H12/outs/G12_VQSR_Nandita_1.err
+#SBATCH -o /global/home/users/makman/H12/outs/G12_VQSR_Nandita_2.out
+#SBATCH -e /global/home/users/makman/H12/outs/G12_VQSR_Nandita_2.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
@@ -15,14 +15,14 @@
 module load python/2.7
 
 # python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr01_G12_input.txt 46 -o chr01_G12_output.txt 
-python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr02_G12_input.txt 46 -o chr02_G12_output.txt 
-python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr03_G12_input.txt 46 -o chr03_G12_output.txt 
-python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr04_G12_input.txt 46 -o chr04_G12_output.txt 
-# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr05_G12_input.txt 46 -o chr05_G12_output.txt 
-# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr06_G12_input.txt 46 -o chr06_G12_output.txt 
-# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr07_G12_input.txt 46 -o chr07_G12_output.txt 
-# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr08_G12_input.txt 46 -o chr08_G12_output.txt 
-# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr09_G12_input.txt 46 -o chr09_G12_output.txt 
+# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr02_G12_input.txt 46 -o chr02_G12_output.txt 
+# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr03_G12_input.txt 46 -o chr03_G12_output.txt 
+# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr04_G12_input.txt 46 -o chr04_G12_output.txt 
+python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr05_G12_input.txt 46 -o chr05_G12_output.txt 
+python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr06_G12_input.txt 46 -o chr06_G12_output.txt 
+python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr07_G12_input.txt 46 -o chr07_G12_output.txt 
+python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr08_G12_input.txt 46 -o chr08_G12_output.txt 
+python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr09_G12_input.txt 46 -o chr09_G12_output.txt 
 # python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr10_G12_input.txt 46 -o chr10_G12_output.txt 
 # python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr11_G12_input.txt 46 -o chr11_G12_output.txt 
 # python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr12_G12_input.txt 46 -o chr12_G12_output.txt 
--- shells/H12/VQSR/new/Nandita/G12_VQSR_4.sh
+++ shells/H12/VQSR/new/Nandita/G12_VQSR_4.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/G12/Nandita
-#SBATCH -J G12-N1
+#SBATCH -J G12-N4
 #SBATCH --account=fc_blackman
 #SBATCH --partition=savio2
 #SBATCH --qos=savio_normal
 #SBATCH --cpus-per-task=4
 #SBATCH --time=72:00:00
-#SBATCH -o /global/home/users/makman/H12/outs/G12_VQSR_Nandita_1.out
-#SBATCH -e /global/home/users/makman/H12/outs/G12_VQSR_Nandita_1.err
+#SBATCH -o /global/home/users/makman/H12/outs/G12_VQSR_Nandita_4.out
+#SBATCH -e /global/home/users/makman/H12/outs/G12_VQSR_Nandita_4.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
@@ -15,9 +15,9 @@
 module load python/2.7
 
 # python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr01_G12_input.txt 46 -o chr01_G12_output.txt 
-python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr02_G12_input.txt 46 -o chr02_G12_output.txt 
-python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr03_G12_input.txt 46 -o chr03_G12_output.txt 
-python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr04_G12_input.txt 46 -o chr04_G12_output.txt 
+# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr02_G12_input.txt 46 -o chr02_G12_output.txt 
+# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr03_G12_input.txt 46 -o chr03_G12_output.txt 
+# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr04_G12_input.txt 46 -o chr04_G12_output.txt 
 # python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr05_G12_input.txt 46 -o chr05_G12_output.txt 
 # python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr06_G12_input.txt 46 -o chr06_G12_output.txt 
 # python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr07_G12_input.txt 46 -o chr07_G12_output.txt 
@@ -27,8 +27,8 @@ python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr04_G12_input.
 # python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr11_G12_input.txt 46 -o chr11_G12_output.txt 
 # python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr12_G12_input.txt 46 -o chr12_G12_output.txt 
 # python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr13_G12_input.txt 46 -o chr13_G12_output.txt 
-# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr14_G12_input.txt 46 -o chr14_G12_output.txt 
-# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr15_G12_input.txt 46 -o chr15_G12_output.txt 
-# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr16_G12_input.txt 46 -o chr16_G12_output.txt 
-# python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr17_G12_input.txt 46 -o chr17_G12_output.txt 
+python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr14_G12_input.txt 46 -o chr14_G12_output.txt 
+python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr15_G12_input.txt 46 -o chr15_G12_output.txt 
+python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr16_G12_input.txt 46 -o chr16_G12_output.txt 
+python ~/git/Helianthus/shells/H12/VQSR/new/Nandita/H12_H2H1.py chr17_G12_input.txt 46 -o chr17_G12_output.txt 
 
