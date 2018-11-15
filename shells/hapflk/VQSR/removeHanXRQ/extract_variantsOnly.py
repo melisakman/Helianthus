5753c0769fced8d45129e92e855349bb69b784da
melisakman <makman@berkeley.edu>
2018-11-14 16:33:43 -0800

    G12

---
+++ shells/hapflk/VQSR/removeHanXRQ/extract_variantsOnly.py
@@ -0,0 +1,22 @@
+import sys
+
+InputFile = sys.argv[1] # vcf file
+
+OutputFile = sys.argv[2] # output file name group1
+
+file1 = open(InputFile, "r")
+file2 = open(OutputFile, 'w')
+
+for line in file1: 
+	splt = line.split("\t")
+	if splt[0][0:2] == "#":
+		file2.write(line)
+	else:
+		chr = splt[0]
+		pos = splt[1]
+		ref = splt[3]
+		alt = splt[4]
+		if alt == ".":
+			continue
+		else:
+			file2.write(line)
---
+++ shells/hapflk/VQSR/removeHanXRQ/hapflk_VQSR_1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/hapflk/VQSR/
+#SBATCH -J hpflk-1
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=600:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/hapflk_VQSR_1.out
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_1.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+
+python /global/home/users/makman/git/Helianthus/shells/hapflk/extracts_variantsOnly.py /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr01_final_lessStringentInvariants.vcf /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr01_final_lessStringentInvariants_onlyvariants.vcf
+
+sed 's/HanXRQChr//g' /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr01_final_lessStringentInvariants_onlyvariants.vcf > chr01_final_lessStringentInvariants_onlyvariants_noHanXRQ.vcf
+
+module load bio/vcftools/0.1.15
+
+vcftools --vcf chr01_final_lessStringentInvariants_onlyvariants_noHanXRQ.vcf --out chr01_final_plink  --plink
+
+python ../plink_ped_fixer.py ../samples_VQSR.txt chr01_final_plink.ped chr01_final_plink_fixed.ped
+
+module load hapflk/1.4
+
+hapflk --file chr01_final_plink_fixed --miss_pheno 0 --chr 01 --from 1 --to 20000000 -p chr01_1 --ncpu 16 -K 15
