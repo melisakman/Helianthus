3be34ce2188951455c2c865f679e5017dffc39dc
melisakman <makman@berkeley.edu>
2018-11-15 15:09:07 -0800

    hapflk

--- shells/hapflk/VQSR/hapflk_VQSR_1.sh
+++ shells/hapflk/VQSR/hapflk_VQSR_1.sh
@@ -12,15 +12,17 @@
 #SBATCH --mail-type=All
 
 
-python /global/home/users/makman/git/Helianthus/shells/hapflk/VQSR/extract_variantsOnly.py /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr01_final_lessStringentInvariants.vcf /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr01_final_lessStringentInvariants_onlyvariants.vcf
+# python /global/home/users/makman/git/Helianthus/shells/hapflk/VQSR/extract_variantsOnly.py /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr01_final_lessStringentInvariants.vcf /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr01_final_lessStringentInvariants_onlyvariants.vcf
+# 
+# sed 's/HanXRQChr//g' /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr01_final_lessStringentInvariants_onlyvariants.vcf > chr01_final_lessStringentInvariants_onlyvariants_noHanXRQ.vcf
+# 
+# module load bio/vcftools/0.1.15
+# 
+# vcftools --vcf chr01_final_lessStringentInvariants_onlyvariants_noHanXRQ.vcf --out chr01_final_plink  --plink
+# 
+# python ../plink_ped_fixer.py ../samples_VQSR.txt chr01_final_plink.ped chr01_final_plink_fixed.ped
 
-sed 's/HanXRQChr//g' /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr01_final_lessStringentInvariants_onlyvariants.vcf > chr01_final_lessStringentInvariants_onlyvariants_noHanXRQ.vcf
-
-module load bio/vcftools/0.1.15
-
-vcftools --vcf chr01_final_lessStringentInvariants_onlyvariants_noHanXRQ.vcf --out chr01_final_plink  --plink
-
-python ../plink_ped_fixer.py ../samples_VQSR.txt chr01_final_plink.ped chr01_final_plink_fixed.ped
+# cp chr01_final_plink.map chr01_final_plink_fixed.map
 
 module load hapflk/1.4
 
