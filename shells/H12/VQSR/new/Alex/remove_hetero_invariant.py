5ff48fcca6e064afae165ee4f756bc4b72d45ea8
melisakman <makman@berkeley.edu>
2018-11-19 10:36:57 -0800

    G12 remove hetero invariants

---
+++ shells/H12/VQSR/new/Alex/remove_hetero_invariant.py
@@ -0,0 +1,27 @@
+import sys
+
+InputFile = sys.argv[1]
+OutputFile = sys.argv[2]
+file = open(InputFile, "r")
+output = open(OutputFile, "w")
+
+list = []
+for line in file:
+	split = line.split(" ")
+	if split[0] == "Anzac_Pueblo":
+		output.write(line)
+	elif split[0] == "lr":
+		output.write(line)
+	else:
+		list.append(split[5:])
+		if "2" in list:
+			continue
+		elif "3" in list:
+			continue
+		elif "4" in list:
+			output.write(line)
+		else:
+			continue
+		list = []
+		
+
