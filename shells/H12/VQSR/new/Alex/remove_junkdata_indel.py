f8f90d66bd894c14253025d9c01aa636f75a91a8
melisakman <makman@berkeley.edu>
2018-12-03 10:40:24 -0800

    G12 run no indel Alex

---
+++ shells/H12/VQSR/new/Alex/remove_junkdata_indel.py
@@ -0,0 +1,23 @@
+import sys
+
+InputFile1 = sys.argv[1] # modified file
+
+OutputFile1 = sys.argv[2] # output file name group1
+
+file1 = open(InputFile1, "r")
+file2 = open(OutputFile1, "w")
+
+for line in file1:
+	dict = {}
+	split = line.split(" ")
+	if split[0] == "Anzac_Pueblo":
+		file2.write(line)
+	elif split[0] == "lr":
+		file2.write(line)
+	else:
+		ref = split[3]
+		alt = split[4]
+		if len(ref) == 1 and len(alt) == 1:
+			file2.wite(line)
+		else:
+			continue
