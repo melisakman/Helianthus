f37a2c7825d3f7f3b925b10e842d5bde62ce4a7a
melisakman <makman@berkeley.edu>
2018-12-07 18:57:07 -0800

    hapflk only domesticates

---
+++ shells/hapflk/VQSR/domesticates_only/hapflk/initial_tree/hapflk_VQSR_chr14_treestuff.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/rosalind/users/makman/hapflk/VQSR/
+#SBATCH -J hfchr14_1
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12 
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr14_kinship.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --time=600:00:00
+module load hapflk/1.4
+hapflk --file chr14_final_plink_domesticates_ethno --outgroup=Wild -p kinship/chr14_tree_kinship --ncpu 12 
