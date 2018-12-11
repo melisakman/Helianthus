53e279846e40eaad90a188f1f3f478bfdbf491e6
melisakman <makman@berkeley.edu>
2018-12-11 12:00:01 -0800

    hapflk only domesticates

---
+++ shells/hapflk/VQSR/count_characters.py
@@ -0,0 +1,13 @@
+import sys
+
+InputFile = sys.argv[1]
+
+file = open(InputFile, "r")
+
+for line in file:
+	count = 0
+	split_line = line.split(" ")
+	for character in split_line:
+		count +=1
+	print count
+		
