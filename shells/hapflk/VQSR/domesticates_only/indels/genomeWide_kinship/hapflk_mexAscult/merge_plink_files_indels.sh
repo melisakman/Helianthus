47a33f060d473d85402fc57c01e0a787e45a7ce3
melisakman <makman@berkeley.edu>
2018-12-12 12:22:09 -0800

    hapflk genome wide with indels mexcult 7 and 14 as mex cult

--- shells/hapflk/VQSR/domesticates_only/indels/genomeWide_kinship/hapflk_mexAscult/merge_plink_files_indels.sh
+++ shells/hapflk/VQSR/domesticates_only/indels/genomeWide_kinship/hapflk_mexAscult/merge_plink_files_indels.sh
@@ -17,21 +17,12 @@ module load bio/vcftools
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
 
-module load bio/vcftools/0.1.15
-
-# vcftools --vcf chr01_final_lessStringentInvariants_onlyvariants_noHanXRQ.vcf --out chr01_final_plink  --plink
-# 
-# python ../plink_ped_fixer.py ../samples_VQSR_domesticates_ethno.txt chr01_final_plink.ped chr01_final_plink_domesticates_ethno.ped
-
-# cp chr01_final_plink.map chr01_final_plink_domesticates_ethno.map
-
-#the files created above are merged with plink and then ran on hapflk
 
 module load plink
 
-# plink --file chr01_final_plink_domesticates_ethno --merge-list plink_merge_list.txt --recode --out all_final_plink_merged
+plink --file chr01_final_plink_domesticates_ethno_mexAscult --merge-list plink_merge_list_mexascult.txt --recode --out all_final_plink_domesticates_ethno_mexAscult
 
-hapflk --file all_final_plink_merged --outgroup=Wild --miss_pheno 0 -p kinship/all/all_tree_kinship --ncpu 20 
+hapflk --file all_final_plink_domesticates_ethno_mexAscult --outgroup=Wild --miss_pheno 0 -p kinship/indel_mexcult_as_cult/all_tree_kinship --ncpu 12 
 
 
-##!!!!! this one includes mexcult 7 and 14 as mex
+##!!!!! this one includes mexcult 7 and 14 as cult
