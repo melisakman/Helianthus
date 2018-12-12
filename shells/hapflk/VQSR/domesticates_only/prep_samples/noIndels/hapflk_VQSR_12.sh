c825f5d2b83697537ba2c603b73128c55cc453c9
melisakman <makman@berkeley.edu>
2018-12-11 16:09:04 -0800

    hapflk only domesticates

---
+++ shells/hapflk/VQSR/domesticates_only/prep_samples/noIndels/hapflk_VQSR_12.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/hapflk/VQSR/
+#SBATCH -J hfchr121
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --cpus-per-task=20
+#SBATCH --time=600:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/hapflk_VQSR_chr12.out
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr12.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+
+# python /global/home/users/makman/git/Helianthus/shells/hapflk/VQSR/extract_variantsOnly.py /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr12_final_lessStringentInvariants.vcf /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr12_final_lessStringentInvariants_onlyvariants.vcf
+# 
+# sed 's/HanXRQChr//g' /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr12_final_lessStringentInvariants_onlyvariants.vcf > chr12_final_lessStringentInvariants_onlyvariants_noHanXRQ.vcf
+
+module load bio/vcftools/0.1.15
+
+vcftools --vcf chr12_final_lessStringentInvariants_onlyvariants_noHanXRQ.vcf --remove-indels --out chr12_final_plink_noIndels --plink
+
+python ../plink_ped_fixer.py ../samples_VQSR_domesticates_ethno.txt chr12_final_plink_noIndels.ped chr12_final_plink_noIndels_domesticates_ethno.ped
+
+cp chr12_final_plink_noIndels.map chr12_final_plink_noIndels_domesticates_ethno.map
+
+# module load hapflk/1.4
+# 
+# hapflk --file chr12_final_plink_noIndels_domesticates_ethno --miss_pheno 0 --chr 01 --from 1 --to 20000000 -p chr12_1 --ncpu 12 -K 15
---
+++ shells/hapflk/VQSR/domesticates_only/prep_samples/noIndels/hapflk_VQSR_7.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/hapflk/VQSR/
+#SBATCH -J hfchr071
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --cpus-per-task=20
+#SBATCH --time=600:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/hapflk_VQSR_chr07.out
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr07.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+
+# python /global/home/users/makman/git/Helianthus/shells/hapflk/VQSR/extract_variantsOnly.py /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr07_final_lessStringentInvariants.vcf /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr07_final_lessStringentInvariants_onlyvariants.vcf
+# 
+# sed 's/HanXRQChr//g' /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr07_final_lessStringentInvariants_onlyvariants.vcf > chr07_final_lessStringentInvariants_onlyvariants_noHanXRQ.vcf
+# 
+module load bio/vcftools/0.1.15
+
+vcftools --vcf chr07_final_lessStringentInvariants_onlyvariants_noHanXRQ.vcf --remove-indels --out chr07_final_plink_noIndels --plink
+
+python ../plink_ped_fixer.py ../samples_VQSR_domesticates_ethno.txt chr07_final_plink_noIndels.ped chr07_final_plink_noIndels_domesticates_ethno.ped
+
+cp chr07_final_plink_noIndels.map chr07_final_plink_noIndels_domesticates_ethno.map
+
+# module load hapflk/1.4
+# 
+# hapflk --file chr07_final_plink_noIndels_domesticates_ethno --miss_pheno 0 --chr 01 --from 1 --to 20000000 -p chr07_1 --ncpu 12 -K 15
