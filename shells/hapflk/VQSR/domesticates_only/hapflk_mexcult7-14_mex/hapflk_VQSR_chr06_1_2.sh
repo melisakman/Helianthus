07a77557a34325671139ae3c89beac967a05d3ba
melisakman <makman@berkeley.edu>
2018-12-11 22:54:00 -0800

    hapflk only domesticates

--- shells/hapflk/VQSR/domesticates_only/hapflk_mexcult7-14_mex/hapflk_VQSR_chr06_1_2.sh
+++ shells/hapflk/VQSR/domesticates_only/hapflk_mexcult7-14_mex/hapflk_VQSR_chr06_1_2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH -D /clusterfs/rosalind/users/makman/hapflk/VQSR/
-#SBATCH -J hfchr14_8
+#SBATCH -J hfchr06_1
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio2_htc
 #SBATCH --qos=rosalind_htc2_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12 
-#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr14_8.err
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr06_1_2.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 #SBATCH --time=600:00:00
 module load hapflk/1.4
-hapflk --file chr14_final_plink_domesticates_ethno --outgroup=Wild --kinship kinship/chr14_tree_kinship_fij.txt --miss_pheno 0 --chr 14 --from 140000001 --to 160000000 -p hapflk_domesticates/chr14_8 --ncpu 12 -K 15
+hapflk --file chr06_final_plink_domesticates_ethno --outgroup=Wild --kinship kinship/chr06_tree_kinship_fij.txt --miss_pheno 0 --chr 06 --from 10000001 --to 20000000 -p hapflk_domesticates/chr06_1_2 --ncpu 12 -K 15
--- shells/hapflk/VQSR/domesticates_only/hapflk_mexcult7-14_mex/hapflk_VQSR_chr09_9_2.sh
+++ shells/hapflk/VQSR/domesticates_only/hapflk_mexcult7-14_mex/hapflk_VQSR_chr09_9_2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH -D /clusterfs/rosalind/users/makman/hapflk/VQSR/
-#SBATCH -J hfchr06_1
+#SBATCH -J hfchr09_9
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio2_htc
 #SBATCH --qos=rosalind_htc2_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12 
-#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr06_1.err
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr09_9_2.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 #SBATCH --time=600:00:00
 module load hapflk/1.4
-hapflk --file chr06_final_plink_domesticates_ethno --outgroup=Wild --kinship kinship/chr06_tree_kinship_fij.txt --miss_pheno 0 --chr 06 --from 1 --to 20000000 -p hapflk_domesticates/chr06_1 --ncpu 12 -K 15
+hapflk --file chr09_final_plink_domesticates_ethno --outgroup=Wild --kinship kinship/chr09_tree_kinship_fij.txt --miss_pheno 0 --chr 09 --from 170000001 --to 180000000 -p hapflk_domesticates/chr09_9_2 --ncpu 12 -K 15
