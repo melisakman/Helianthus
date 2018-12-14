abdfb92b2043927ab4bdebeddc0550130824f61f
melisakman <makman@berkeley.edu>
2018-12-13 22:04:59 -0800

    hapflk mex as cult

---
+++ shells/hapflk/VQSR/domesticates_only/indels/genomeWide_kinship/hapflk_mexAscult/hapflk_VQSR_chr10_7.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/rosalind/users/makman/hapflk/VQSR/
+#SBATCH -J hfchr10_7
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12 
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr10_7_Indel_mexascult.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --time=600:00:00
+module load hapflk/1.4
+hapflk --file chr10_final_plink_domesticates_ethno_mexAscult --outgroup=Wild --kinship kinship/indel_mexcult_as_cult/all_tree_kinship_fij.txt --miss_pheno 0 --chr 10 --from 120000001 --to 140000000 -p hapflk_domesticates/Indels_mexascult/chr10_7 --ncpu 12 -K 15
---
+++ shells/hapflk/VQSR/domesticates_only/indels/genomeWide_kinship/hapflk_mexAscult/hapflk_VQSR_chr13_6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/rosalind/users/makman/hapflk/VQSR/
+#SBATCH -J hfchr13_6
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12 
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr13_6_Indel_mexascult.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --time=600:00:00
+module load hapflk/1.4
+hapflk --file chr13_final_plink_domesticates_ethno_mexAscult --outgroup=Wild --kinship kinship/indel_mexcult_as_cult/all_tree_kinship_fij.txt --miss_pheno 0 --chr 13 --from 100000001 --to 120000000 -p hapflk_domesticates/Indels_mexascult/chr13_6 --ncpu 12 -K 15
