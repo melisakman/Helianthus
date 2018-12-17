c080c616bb21ef3f382f8ffba1684a23f7d4eed4
melisakman <makman@berkeley.edu>
2018-12-17 15:58:59 -0800

    hapflk failed ones

--- shells/hapflk/VQSR/domesticates_only/indels/genomeWide_kinship/hapflk_mexAscult/hapflk_VQSR_chr14_8_2.sh
+++ shells/hapflk/VQSR/domesticates_only/indels/genomeWide_kinship/hapflk_mexAscult/hapflk_VQSR_chr14_8_2.sh
@@ -7,9 +7,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12 
-#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr14_8_Indel_mexascult.err
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr14_8_2_Indel_mexascult.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 #SBATCH --time=600:00:00
 module load hapflk/1.4
-hapflk --file chr14_final_plink_domesticates_ethno_mexAscult --outgroup=Wild --kinship kinship/indel_mexcult_as_cult/all_tree_kinship_fij.txt --miss_pheno 0 --chr 14 --from 140000001 --to 150000000 -p hapflk_domesticates/Indels_mexascult/chr14_8_1 --ncpu 12 -K 15
+hapflk --file chr14_final_plink_domesticates_ethno_mexAscult --outgroup=Wild --kinship kinship/indel_mexcult_as_cult/all_tree_kinship_fij.txt --miss_pheno 0 --chr 14 --from 150000001 --to 160000000 -p hapflk_domesticates/Indels_mexascult/chr14_8_2 --ncpu 12 -K 15
