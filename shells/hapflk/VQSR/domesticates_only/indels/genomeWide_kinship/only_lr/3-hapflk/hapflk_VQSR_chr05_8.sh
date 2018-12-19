a98a646fda958175e063253350e125086717a64b
melisakman <makman@berkeley.edu>
2018-12-19 11:44:21 -0800

    hapflk only lr

---
+++ shells/hapflk/VQSR/domesticates_only/indels/genomeWide_kinship/only_lr/3-hapflk/hapflk_VQSR_chr05_8.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/rosalind/users/makman/hapflk/VQSR/
+#SBATCH -J chr05_8
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12 
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr05_8_lrOnly.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --time=600:00:00
+module load hapflk/1.4
+hapflk --file chr05_final_plink_lrOnly --outgroup=Wild --kinship kinship/lrOnly/lrOnly_tree_kinship_fij.txt --miss_pheno 0 --chr 05 --from 140000001 --to 160000000 -p hapflk_domesticates/lrOnly/chr05_8 --ncpu 12 -K 15
---
+++ shells/hapflk/VQSR/domesticates_only/indels/genomeWide_kinship/only_lr/3-hapflk/hapflk_VQSR_chr09_11.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -D /clusterfs/rosalind/users/makman/hapflk/VQSR/
+#SBATCH -J chr09_11
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12 
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr09_11_lrOnly.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --time=600:00:00
+module load hapflk/1.4
+hapflk --file chr09_final_plink_lrOnly --outgroup=Wild --kinship kinship/lrOnly/lrOnly_tree_kinship_fij.txt --miss_pheno 0 --chr 09 --from 200000001 --to 220000000 -p hapflk_domesticates/lrOnly/chr09_11 --ncpu 12 -K 15
