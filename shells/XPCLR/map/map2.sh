292ca4ac604270d926d2e077e385c6c4b0676b35
melisakman <makman@berkeley.edu>
2019-01-03 15:07:35 -0800

    sweep finder 2

--- shells/XPCLR/map/map2.sh
+++ shells/XPCLR/map/map2.sh
@@ -9,6 +9,7 @@
 #SBATCH -e /global/home/users/makman/H12/outs/map_chr02.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-
+ssh dtn
 cp /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final/chr02_final_lessStringentInvariants.vcf ./
+exit
 cat chr02_final_lessStringentInvariants.vcf | perl vcf2cmpositions.pl HanXRQr1.0-20151230.bp_to_cM.280x801.extradivisions_columnsFixed.txt > chr02_formap.vcf
