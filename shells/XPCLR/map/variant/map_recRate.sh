4ec11422bf41163b59c48be4c5e511c12204071f
melisakman <makman@berkeley.edu>
2019-01-06 13:56:36 -0800

    map

--- shells/XPCLR/map/variant/map_recRate.sh
+++ shells/XPCLR/map/variant/map_recRate.sh
@@ -14,20 +14,20 @@
 #SBATCH --mail-type=All
 
 
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr01_variants_map.txt chr01_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr02_variants_map.txt chr02_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr03_variants_map.txt chr03_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr04_variants_map.txt chr04_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr05_variants_map.txt chr05_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr06_variants_map.txt chr06_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr07_variants_map.txt chr07_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr08_variants_map.txt chr08_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr09_variants_map.txt chr09_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr10_variants_map.txt chr10_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr11_variants_map.txt chr11_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr12_variants_map.txt chr12_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr13_variants_map.txt chr13_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr14_variants_map.txt chr14_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr15_variants_map.txt chr15_variants_recRate.txt
-python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr16_variants_map.txt chr16_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr01_variants_map.txt chr01_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr02_variants_map.txt chr02_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr03_variants_map.txt chr03_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr04_variants_map.txt chr04_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr05_variants_map.txt chr05_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr06_variants_map.txt chr06_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr07_variants_map.txt chr07_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr08_variants_map.txt chr08_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr09_variants_map.txt chr09_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr10_variants_map.txt chr10_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr11_variants_map.txt chr11_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr12_variants_map.txt chr12_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr13_variants_map.txt chr13_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr14_variants_map.txt chr14_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr15_variants_map.txt chr15_variants_recRate.txt
+# python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr16_variants_map.txt chr16_variants_recRate.txt
 python ~/git/Helianthus/shells/XPCLR/map/map_recRate.py chr17_variants_map.txt chr17_variants_recRate.txt
