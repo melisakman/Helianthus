99af13b781679b3680ce03669c52bdc72cf6081f
melisakman <makman@berkeley.edu>
2019-01-05 16:34:21 -0800

    map

--- shells/XPCLR/map/subset_map.py
+++ shells/XPCLR/map/subset_map.py
@@ -9,14 +9,13 @@ input2 = open(InputFile2, "r")
 
 output = open(OutputFile1, 'w')
 output.write("position\trate")
-list = []
-for line2 in input2:
-	split2 = line2.split("\t")
-	position = split2[0]
-	list.append(position)
-
+dict = {}
 for line1 in input1:
 	split1 = line1.split("\t")
+	dict[split1[0]] = split1[1]
+
+for line2 in input2:
+	split1 = line1.split("\t")
 	pos = split1[0]
-	if pos in list:
-		output.write(line1) 
+	if pos in dict:
+		output.write(str(pos) + "\t" + dict[pos]) 
