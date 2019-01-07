d7af63c0bbfa2b11dc6d5cc7c2c48dd1006c5a24
melisakman <makman@berkeley.edu>
2019-01-07 12:03:49 -0800

    map

---
+++ shells/variant_calling/new/1-preprocessing/1_fastq_dump/fastq_dump4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/sra/
+#SBATCH -J fastqdump
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --cpus-per-task=4
+#SBATCH --time=720:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/fastqdump4.out
+#SBATCH -e /global/home/users/makman/GATK/outs/fastqdump4.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load sra-tools/2.8.1 
+
+echo '/global/home/users/makman/ncbi/public/root = "/clusterfs/rosalind/users/makman/tmp"' > $HOME/.ncbi/user-settings.mkfg
+# fastq-dump -I --split-files SRR5907869
+# fastq-dump -I --split-files SRR5907847
+# fastq-dump -I --split-files SRR5907848
+fastq-dump -I --split-files SRR5907845
+# fastq-dump -I --split-files SRR5907851
+# fastq-dump -I --split-files SRR5907826
+# fastq-dump -I --split-files SRR5907825
+# fastq-dump -I --split-files SRR5929273
+
+
--- shells/variant_calling/new/1-preprocessing/1_fastq_dump/fastq_dump7.sh
+++ shells/variant_calling/new/1-preprocessing/1_fastq_dump/fastq_dump7.sh
@@ -6,21 +6,20 @@
 #SBATCH --qos=rosalind_htc2_normal
 #SBATCH --cpus-per-task=4
 #SBATCH --time=720:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/fastqdump.out
-#SBATCH -e /global/home/users/makman/GATK/outs/fastqdump.err
+#SBATCH -o /global/home/users/makman/GATK/outs/fastqdump7.out
+#SBATCH -e /global/home/users/makman/GATK/outs/fastqdump7.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-#SBATCH --array=1-4
 module load sra-tools/2.8.1 
 
 echo '/global/home/users/makman/ncbi/public/root = "/clusterfs/rosalind/users/makman/tmp"' > $HOME/.ncbi/user-settings.mkfg
-fastq-dump -I --split-files SRR5907869
-fastq-dump -I --split-files SRR5907847
-fastq-dump -I --split-files SRR5907848
-fastq-dump -I --split-files SRR5907845
-fastq-dump -I --split-files SRR5907851
-fastq-dump -I --split-files SRR5907826
+# fastq-dump -I --split-files SRR5907869
+# fastq-dump -I --split-files SRR5907847
+# fastq-dump -I --split-files SRR5907848
+# fastq-dump -I --split-files SRR5907845
+# fastq-dump -I --split-files SRR5907851
+# fastq-dump -I --split-files SRR5907826
 fastq-dump -I --split-files SRR5907825
-fastq-dump -I --split-files SRR5929273
+# fastq-dump -I --split-files SRR5929273
 
 
