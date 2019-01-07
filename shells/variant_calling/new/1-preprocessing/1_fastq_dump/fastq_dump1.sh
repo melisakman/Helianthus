a0072132a440b6013310db8bf2ddd1952e2649ad
melisakman <makman@berkeley.edu>
2019-01-07 12:21:10 -0800

    fastq dump

--- shells/variant_calling/new/1-preprocessing/1_fastq_dump/fastq_dump1.sh
+++ shells/variant_calling/new/1-preprocessing/1_fastq_dump/fastq_dump1.sh
@@ -15,7 +15,7 @@
 #SBATCH --mail-type=All
 
 echo '/global/home/users/makman/ncbi/public/root = "/clusterfs/rosalind/users/makman/tmp"' > $HOME/.ncbi/user-settings.mkfg
-clusterfs/rosalind/users/makman/sratoolkit.2.9.2-centos_linux64/bin/fastq-dump -I --split-files SRR5907869
+/clusterfs/rosalind/users/makman/sratoolkit.2.9.2-centos_linux64/bin/fastq-dump -I --split-files SRR5907869
 # clusterfs/rosalind/users/makman/sratoolkit.2.9.2-centos_linux64/bin/fastq-dump -I --split-files SRR5907847
 # clusterfs/rosalind/users/makman/sratoolkit.2.9.2-centos_linux64/bin/fastq-dump -I --split-files SRR5907848
 # clusterfs/rosalind/users/makman/sratoolkit.2.9.2-centos_linux64/bin/fastq-dump -I --split-files SRR5907845
