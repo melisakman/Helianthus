bf382d414cf3a2847bf4d7ea741ba9d38f076804
melisakman <makman@berkeley.edu>
2019-01-08 13:50:05 -0800

    blast VQSR 2+ genes

---
+++ shells/variant_calling/old/gzip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/final
+#SBATCH -J comchr01
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --time=80:00:00
+#SBATCH --mem=30000
+#SBATCH -o /global/home/users/makman/GATK/outs/gzip.out
+#SBATCH -e /global/home/users/makman/GATK/outs/gzip.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+gzip *.vcf
