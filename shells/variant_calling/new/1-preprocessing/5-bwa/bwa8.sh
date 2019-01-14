036c0d82d18ebcfbfd1efa09fe11d7b0f0aaf580
melisakman <makman@berkeley.edu>
2019-01-13 16:28:31 -0800

    bwa

--- shells/variant_calling/new/1-preprocessing/5-bwa/bwa8.sh
+++ shells/variant_calling/new/1-preprocessing/5-bwa/bwa8.sh
@@ -1,24 +1,24 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/sra/ready/
-#SBATCH -J bwa1
+#SBATCH -J bwa8
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio
 #SBATCH --qos=rosalind_savio_normal
 #SBATCH --mem=54000
 #SBATCH --time=72:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/bwa1.out
-#SBATCH -e /global/home/users/makman/GATK/outs/bwa1.err
+#SBATCH -o /global/home/users/makman/GATK/outs/bwa8.out
+#SBATCH -e /global/home/users/makman/GATK/outs/bwa8.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
 module load bwa/0.7.17-r1188
 
-bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_index/HanXRQr1.0-20151230.fa ann01-cwIA_trimmed_R1.fastq.gz ann01-cwIA_trimmed_R2.fastq.gz > ann01-cwIA.sam
+# bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_index/HanXRQr1.0-20151230.fa ann01-cwIA_trimmed_R1.fastq.gz ann01-cwIA_trimmed_R2.fastq.gz > ann01-cwIA.sam
 # bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_index/HanXRQr1.0-20151230.fa ann04-nwAR_trimmed_R1.fastq.gz ann04-nwAR_trimmed_R2.fastq.gz > ann04-nwAR.sam
 # bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_index/HanXRQr1.0-20151230.fa ann05-ccNM_trimmed_R1.fastq.gz ann05-ccNM_trimmed_R2.fastq.gz > ann05-ccNM.sam
 # bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_index/HanXRQr1.0-20151230.fa ann10-ccIL_trimmed_R1.fastq.gz ann10-ccIL_trimmed_R2.fastq.gz > ann10-ccIL.sam
 # bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_index/HanXRQr1.0-20151230.fa ann20-seAZ_trimmed_R1.fastq.gz ann20-seAZ_trimmed_R2.fastq.gz > ann20-seAZ.sam
 # bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_index/HanXRQr1.0-20151230.fa ann28-swSK_trimmed_R1.fastq.gz ann28-swSK_trimmed_R2.fastq.gz > ann28-swSK.sam
 # bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_index/HanXRQr1.0-20151230.fa ann29-neSD_trimmed_R1.fastq.gz ann29-neSD_trimmed_R2.fastq.gz > ann29-neSD.sam
-# bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_index/HanXRQr1.0-20151230.fa ann44-ccCA_trimmed_R1.fastq.gz ann44-ccCA_trimmed_R2.fastq.gz > ann44-ccCA.sam
+bwa mem -t 16 -M /clusterfs/rosalind/users/makman/GATK/bwa_index/HanXRQr1.0-20151230.fa ann44-ccCA_trimmed_R1.fastq.gz ann44-ccCA_trimmed_R2.fastq.gz > ann44-ccCA.sam
 
