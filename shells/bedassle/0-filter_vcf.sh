f47ce0f9b256be52b5ddc7f0b09feda9b2a7f2dc
melisakman <makman@berkeley.edu>
2019-01-15 13:35:58 -0800

    cv cat

--- shells/bedassle/0-filter_vcf.sh
+++ shells/bedassle/0-filter_vcf.sh
@@ -13,7 +13,7 @@
 #SBATCH --mail-type=All
 
 module load bio/vcftools/0.1.15
-
+##tests below#
 # vcftools --vcf testy_recode_snpid_fixed.vcf --keep Accession_ForBEDASSLE.txt --max-missing-count 10 --012 --out testy_recode_snpid_fixed_bedassle2
 
 # vcftools --vcf testy_recode_snpid_fixed.vcf --keep Accession_ForBEDASSLE.txt --max-missing-count 30 --012 --out testy_recode_snpid_fixed_bedassle3
@@ -22,4 +22,14 @@ module load bio/vcftools/0.1.15
 # vcftools --vcf testy_recode_snpid_fixed.vcf --keep bedassle/accession_subset1.txt --max-missing-count 20 --012 --out bedassle/testy_recode_snpid_fixed_subset1_20
 # vcftools --vcf testy_recode_snpid_fixed.vcf --keep bedassle/accession_subset1.txt --max-missing-count 30 --012 --out bedassle/testy_recode_snpid_fixed_subset1_30
 # vcftools --vcf testy_recode_snpid_fixed.vcf --keep bedassle/accession_subset1.txt --max-missing-count 5 --012 --out bedassle/testy_recode_snpid_fixed_subset1_5
-vcftools --vcf testy_recode_snpid_fixed.vcf --keep bedassle/accession_subset1.txt --max-missing-count 0 --012 --out bedassle/testy_recode_snpid_fixed_subset1_0
+# vcftools --vcf testy_recode_snpid_fixed.vcf --keep bedassle/accession_subset1.txt --max-missing-count 0 --012 --out bedassle/testy_recode_snpid_fixed_subset1_0
+
+##tests above###
+
+
+vcftools --vcf testy_recode_snpid_fixed_ldprunned_2K.vcf --keep bedassle/accession_subset1.txt --max-missing-count 10 --012 --out bedassle/testy_recode_snpid_fixed_prunned_subset1_10
+vcftools --vcf testy_recode_snpid_fixed_ldprunned_2K.vcf --keep bedassle/accession_subset2.txt --max-missing-count 10 --012 --out bedassle/testy_recode_snpid_fixed_prunned_subset2_10
+vcftools --vcf testy_recode_snpid_fixed_ldprunned_2K.vcf --keep bedassle/accession_subset3.txt --max-missing-count 10 --012 --out bedassle/testy_recode_snpid_fixed_prunned_subset3_10
+vcftools --vcf testy_recode_snpid_fixed_ldprunned_2K.vcf --keep bedassle/accession_subset4.txt --max-missing-count 10 --012 --out bedassle/testy_recode_snpid_fixed_prunned_subset4_10
+vcftools --vcf testy_recode_snpid_fixed_ldprunned_2K.vcf --keep bedassle/accession_subset5.txt --max-missing-count 10 --012 --out bedassle/testy_recode_snpid_fixed_prunned_subset5_10
+
