ba463f55f0895ec6688c6376be305fd828dea177
melisakman <makman@berkeley.edu>
2019-01-15 14:56:50 -0800

    lr cat

--- shells/bedassle/ld-prun.sh
+++ shells/bedassle/ld-prun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/Arabidopsis
-#SBATCH -J filtvcf
+#SBATCH -J ldprun
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio2_htc
 #SBATCH --qos=rosalind_htc2_normal
