88a4cadc2b0761e565c389d5a0ad2df278cceb99
melisakman <makman@berkeley.edu>
2019-01-14 17:02:37 -0800

    cat wilds

--- shells/variant_calling/new/1-Preprocessing/1_cat/cat_fastq_wd1.sh
+++ shells/variant_calling/new/1-Preprocessing/1_cat/cat_fastq_wd1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/raw
 #SBATCH -J catfastqwd
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio2_htc
--- shells/variant_calling/new/1-Preprocessing/1_cat/cat_fastq_wd3.sh
+++ shells/variant_calling/new/1-Preprocessing/1_cat/cat_fastq_wd3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/raw
 #SBATCH -J catfastqwd
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio2_htc
@@ -21,6 +21,18 @@ cp HI.1565.008.BioO_21.ann34-cwKS_R1.fastq.gz ready/ann34-cwKS_R1.fastq.gz
 cp HI.1565.008.BioO_21.ann34-cwKS_R2.fastq.gz ready/ann34-cwKS_R2.fastq.gz
 cp HI.1565.008.BioO_24.ann39-ncNE_R1.fastq.gz ready/ann39-ncNE_R1.fastq.gz
 cp HI.1565.008.BioO_24.ann39-ncNE_R2.fastq.gz ready/ann39-ncNE_R2.fastq.gz
+
+cp HI.4520.005.NEBNext_Index_30.Mex_A1516_R1.fastq.gz ready/Mex_A1516_R1.fastq.gz 
+cp HI.4520.005.NEBNext_Index_37.Mex_A1517_R1.fastq.gz ready/Mex_A1517_R1.fastq.gz 
+cp HI.4520.005.NEBNext_Index_38.Mex_A1572_R1.fastq.gz ready/Mex_A1572_R1.fastq.gz 
+cp HI.4520.005.NEBNext_Index_39.Mex_A1574_R1.fastq.gz ready/Mex_A1574_R1.fastq.gz 
+cp HI.4520.005.NEBNext_Index_40.Mex_Ann261_R1.fastq.gz ready/Mex_Ann261_R1.fastq.gz
+
+cp HI.4520.005.NEBNext_Index_30.Mex_A1516_R2.fastq.gz ready/Mex_A1516_R2.fastq.gz 
+cp HI.4520.005.NEBNext_Index_37.Mex_A1517_R2.fastq.gz ready/Mex_A1517_R2.fastq.gz 
+cp HI.4520.005.NEBNext_Index_38.Mex_A1572_R2.fastq.gz ready/Mex_A1572_R2.fastq.gz 
+cp HI.4520.005.NEBNext_Index_39.Mex_A1574_R2.fastq.gz ready/Mex_A1574_R2.fastq.gz 
+cp HI.4520.005.NEBNext_Index_40.Mex_Ann261_R2.fastq.gz ready/Mex_Ann261_R2.fastq.gz
 
 
 
