9d842b06cf5e9e9a1f514b9945362b3fe834cb6d
melisakman <makman@berkeley.edu>
2019-01-17 08:18:44 -0800

    cv cp

--- shells/variant_calling/new/1-Preprocessing/2_cat/cat_fastq_cv3.sh
+++ shells/variant_calling/new/1-Preprocessing/2_cat/cat_fastq_cv3.sh
@@ -11,30 +11,30 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
-gzip SRR5912191_1.fastq
-gzip SRR5912193_1.fastq
-gzip SRR5912198_1.fastq
-# gzip SRR5912262_1.fastq
-# gzip SRR5912197_1.fastq
-# gzip SRR5912257_1.fastq
+# gzip SRR5912191_1.fastq
+# gzip SRR5912193_1.fastq
+# gzip SRR5912198_1.fastq
+gzip SRR5912262_1.fastq
+gzip SRR5912197_1.fastq
+gzip SRR5912257_1.fastq
 
-gzip SRR5912191_2.fastq
-gzip SRR5912193_2.fastq
-gzip SRR5912198_2.fastq
-# gzip SRR5912262_2.fastq
-# gzip SRR5912197_2.fastq
-# gzip SRR5912257_2.fastq
+# gzip SRR5912191_2.fastq
+# gzip SRR5912193_2.fastq
+# gzip SRR5912198_2.fastq
+gzip SRR5912262_2.fastq
+gzip SRR5912197_2.fastq
+gzip SRR5912257_2.fastq
 
-cp SRR5912191_1.fastq.gz ../raw/ready/tek/PPN255_Manchurian_R1.fastq.gz
-cp SRR5912193_1.fastq.gz ../raw/ready/tek/HA821_R1.fastq.gz
-cp SRR5912198_1.fastq.gz ../raw/ready/tek/RHA311_R1.fastq.gz
-# cp SRR5912262_1.fastq.gz ../raw/ready/tek/RHA358_R1.fastq.gz
-# cp SRR5912197_1.fastq.gz ../raw/ready/tek/PPN262_R1.fastq.gz
-# cp SRR5912257_1.fastq.gz ../raw/ready/tek/SF33_R1.fastq.gz
+# cp SRR5912191_1.fastq.gz ../raw/ready/tek/PPN255_Manchurian_R1.fastq.gz
+# cp SRR5912193_1.fastq.gz ../raw/ready/tek/HA821_R1.fastq.gz
+# cp SRR5912198_1.fastq.gz ../raw/ready/tek/RHA311_R1.fastq.gz
+cp SRR5912262_1.fastq.gz ../raw/ready/tek/RHA358_R1.fastq.gz
+cp SRR5912197_1.fastq.gz ../raw/ready/tek/PPN262_R1.fastq.gz
+cp SRR5912257_1.fastq.gz ../raw/ready/tek/SF33_R1.fastq.gz
 
-cp SRR5912191_2.fastq.gz ../raw/ready/tek/PPN255_Manchurian_R2.fastq.gz
-cp SRR5912193_2.fastq.gz ../raw/ready/tek/HA821_R2.fastq.gz
-cp SRR5912198_2.fastq.gz ../raw/ready/tek/RHA311_R2.fastq.gz
-# cp SRR5912262_2.fastq.gz ../raw/ready/tek/RHA358_R2.fastq.gz
-# cp SRR5912197_2.fastq.gz ../raw/ready/tek/PPN262_R2.fastq.gz
-# cp SRR5912257_2.fastq.gz ../raw/ready/tek/SF33_R2.fastq.gz
+# cp SRR5912191_2.fastq.gz ../raw/ready/tek/PPN255_Manchurian_R2.fastq.gz
+# cp SRR5912193_2.fastq.gz ../raw/ready/tek/HA821_R2.fastq.gz
+# cp SRR5912198_2.fastq.gz ../raw/ready/tek/RHA311_R2.fastq.gz
+cp SRR5912262_2.fastq.gz ../raw/ready/tek/RHA358_R2.fastq.gz
+cp SRR5912197_2.fastq.gz ../raw/ready/tek/PPN262_R2.fastq.gz
+cp SRR5912257_2.fastq.gz ../raw/ready/tek/SF33_R2.fastq.gz
