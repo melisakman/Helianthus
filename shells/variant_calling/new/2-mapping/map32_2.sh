6600e3ab486f9d74d63397bf5517b7b4a99c0dc3
melisakman <makman@berkeley.edu>
2019-02-06 17:25:54 -0800

    ann1238 small map

---
+++ shells/variant_calling/new/2-mapping/map32_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+#SBATCH -D /clusterfs/rosalind/users/makman/GATK/sams/
+#SBATCH -J map_32
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --time=172:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/map32_2.out
+#SBATCH -e /global/home/users/makman/GATK/outs/map32_2.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+/clusterfs/rosalind/users/makman/NextGenMap-0.5.2/bin/ngm-0.5.2/ngm -r /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta -p -1 ../fastq/raw/ready/ann1238_lane2_R1_trimmed.fastq.gz -2 ../fastq/raw/ready/ann1238_lane2_R2_trimmed.fastq.gz -o ann1238_lane2.sam
