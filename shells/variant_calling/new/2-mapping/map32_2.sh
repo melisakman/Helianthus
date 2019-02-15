d288b252df8a21509a7f7c8f4efb7bb123a0a98c
melisakman <makman@berkeley.edu>
2019-02-14 17:04:08 -0800

    ann1238 sort bam

--- shells/variant_calling/new/2-mapping/map32_2.sh
+++ shells/variant_calling/new/2-mapping/map32_2.sh
@@ -2,12 +2,14 @@
 #SBATCH -D /clusterfs/rosalind/users/makman/GATK/sams/
 #SBATCH -J map_32
 #SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --time=100:00:00
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=120:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/map32_2.out
 #SBATCH -e /global/home/users/makman/GATK/outs/map32_2.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
-/clusterfs/rosalind/users/makman/NextGenMap-0.5.2/bin/ngm-0.5.2/ngm -r /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta -p -1 ../fastq/raw/ready/ann1238_lane2_R1_trimmed.fastq.gz -2 ../fastq/raw/ready/ann1238_lane2_R2_trimmed.fastq.gz -o ann1238_lane2.sam
+/clusterfs/rosalind/users/makman/NextGenMap-0.5.2/bin/ngm-0.5.2/ngm -r /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta -p -1 ../fastq/raw/ready/ann1238_lane2_R1_trimmed.fastq.gz -2 ../fastq/raw/ready/ann1238_lane2_R2_trimmed.fastq.gz -o /global/scratch/makmen/GATK/sams/ann1238_lane2_v2.sam
