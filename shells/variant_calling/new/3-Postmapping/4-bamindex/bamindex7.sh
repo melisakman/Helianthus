ea475eac15c6622d784db6ef02420686d76684b2
melisakman <makman@berkeley.edu>
2019-02-17 11:41:14 -0800

    read groups ann1238

---
+++ shells/variant_calling/new/3-Postmapping/4-bamindex/bamindex7.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/GATK/sams/
+#SBATCH -J markdup6
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=180:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/bamindex7.out
+#SBATCH -e /global/home/users/makman/GATK/outs/bamindex7.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load samtools/1.8
+
+samtools index ann1238_lane2_62_5M_markdup_readGroup.bam ann1238_lane2_62_5M_markdup_readGroup.bai
