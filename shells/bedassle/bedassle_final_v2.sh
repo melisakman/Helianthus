b98e6b87562320ece2ca79fe3e0f464c843032a9
melisakman <makman@berkeley.edu>
2019-02-28 13:53:54 -0800

    bedassle

--- shells/bedassle/bedassle_final_v2.sh
+++ shells/bedassle/bedassle_final_v2.sh
@@ -26,5 +26,24 @@ module load r/3.5.1
 # python ~/git/Helianthus/shells/bedassle/fix_genotypeFile.py testy_recode_snpid_fixed_ldprunned_5K.012 testy_recode_snpid_fixed_ldprunned_5K.012.pos testy_recode_snpid_fixed_ldprunned_5K.012.indv accession_subset3.txt subset3_5K_allele_counts.txt subset3_5K_sample_size.txt
 # python ~/git/Helianthus/shells/bedassle/fix_genotypeFile.py testy_recode_snpid_fixed_ldprunned_5K.012 testy_recode_snpid_fixed_ldprunned_5K.012.pos testy_recode_snpid_fixed_ldprunned_5K.012.indv accession_subset4.txt subset4_5K_allele_counts.txt subset4_5K_sample_size.txt
 # python ~/git/Helianthus/shells/bedassle/fix_genotypeFile.py testy_recode_snpid_fixed_ldprunned_5K.012 testy_recode_snpid_fixed_ldprunned_5K.012.pos testy_recode_snpid_fixed_ldprunned_5K.012.indv accession_subset5.txt subset5_5K_allele_counts.txt subset5_5K_sample_size.txt
+R
+install.packages("BEDASSLE")
+library(BEDASSLE)
+setwd("/clusterfs/rosalind/users/makman/Arabidopsis/bedassle/final")
 
-Rscript ~/git/Helianthus/shells/bedassle/bedassle.R
+count = read.table("subset1_5K_allele_counts.txt", h=F)
+dim(count)
+count = as.matrix(count[1:2081])
+dim(count)
+sample = read.table("subset1_5K_sample_size.txt", h=F)
+sample = as.matrix(sample[1:2081])
+dim(sample)
+climate = load("subset1_envi.RData")
+
+
+
+MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
+     aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.2, mu_stp = 0.25, ngen = 1e6, 
+     printfreq=100000, savefreq=1e5, samplefreq=250, prefix = "subset1_",
+     continue = FALSE, continuing.params = NULL)
+# Rscript ~/git/Helianthus/shells/bedassle/bedassle.R
