e141b829f5bd0636fe4a09c4fa35c9ff7ac9b7fe
melisakman <makman@berkeley.edu>
2019-03-03 23:26:53 -0800

    bedass

---
+++ shells/bedassle/bedassle2_v4.R
@@ -0,0 +1,32 @@
+library(BEDASSLE, lib = "/global/home/users/makman/R/")
+
+count = read.table("subset2_5K_allele_counts.txt", h=F)
+dim(count)
+count = as.matrix(count[1:2081])
+dim(count)
+sample = read.table("subset2_5K_sample_size.txt", h=F)
+sample = as.matrix(sample[1:2081])
+dim(sample)
+climate = load("subset2_envi.RData")
+
+
+
+// MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
+//      aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.2, mu_stp = 0.25, ngen = 1e6, 
+//      printfreq=100000, savefreq=1e5, samplefreq=250, prefix = "subset2_",
+//      continue = FALSE, continuing.params = NULL)
+     
+// MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
+//      aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.2, mu_stp = 0.25, ngen = 1e6, 
+//      printfreq=100000, savefreq=1e5, samplefreq=250, prefix = "subset2_v3_",
+//      continue = FALSE, continuing.params = NULL)
+//      
+// MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
+//      aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.2, mu_stp = 0.25, ngen = 1000, 
+//      printfreq=10000, savefreq=1000, samplefreq=250, prefix = "subset2_v4_",
+//      continue = FALSE, continuing.params = NULL)
+
+MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
+     aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.2, mu_stp = 0.35, ngen = 1e6, 
+     printfreq=100000, savefreq=1e5, samplefreq=250, prefix = "subset2_v4_",
+     continue = FALSE, continuing.params = NULL)
