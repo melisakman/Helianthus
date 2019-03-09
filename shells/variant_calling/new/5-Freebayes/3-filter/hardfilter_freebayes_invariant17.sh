e1b5e9ea62d8c282db4bd2080280a136d6a9bc6d
melisakman <makman@berkeley.edu>
2019-03-08 23:06:05 -0800

    Freebayes filter

---
+++ shells/variant_calling/new/5-Freebayes/3-filter/hardfilter_freebayes_invariant17.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/GATK/sams/freebayes/
+#SBATCH -J FB_chr17
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=660:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/bcftools_filter_freebayes_chr17.out
+#SBATCH -e /global/home/users/makman/GATK/outs/bcftools_filter_freebayes_chr17.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load bcftools/1.6
+module load bio/vcftools
+module load java
+
+zcat freebayes_invariant_chr17_combined.vcf.gz | bcftools filter -g 5 -i 'TYPE = "snp" && QUAL > 20 && MQM > 40 || TYPE = "indel" && QUAL > 20 || TYPE = "mnp" && QUAL > 20 && MQM > 40 || TYPE = "ref" && QUAL < 2.723e-15 && MQMR > 40' | vcftools --vcf - --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_variants_chr17_filtered.vcf.gz
+zcat freebayes_invariant_chr17_combined.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL < 2.75e-15 && MQMR > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes_invariants_chr17_filtered.vcf.gz
+
