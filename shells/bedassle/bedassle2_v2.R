03949d359b2ed0eecf1dd6aea48ad8706982892d
melisakman <makman@berkeley.edu>
2019-03-10 22:01:13 -0700

    bedass

--- shells/bedassle/bedassle2_v2.R
+++ shells/bedassle/bedassle2_v2.R
@@ -9,11 +9,10 @@ sample = as.matrix(sample[1:2081])
 dim(sample)
 climate = load("subset2_envi.RData")
 
-
-
+# decreased aD and aE step from 0.1
      
 MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
-     aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.2, mu_stp = 0.25, ngen = 100000, 
-     printfreq=100000, savefreq=1e5, samplefreq=250, prefix = "subset2_v2_",
+     aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.2, mu_stp = 0.35, ngen = 1000, 
+     printfreq=100000, savefreq=1e5, samplefreq=5, prefix = "subset2_v2_",
      continue = FALSE, continuing.params = NULL)
      
--- shells/bedassle/bedassle2_v4.R
+++ shells/bedassle/bedassle2_v4.R
@@ -9,9 +9,9 @@ sample = as.matrix(sample[1:2081])
 dim(sample)
 climate = load("subset2_envi.RData")
 
-
+# phi_stp increased from 0.2 to 0.25
 
 MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
-     aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.2, mu_stp = 0.35, ngen = 10000, 
-     printfreq=100000, savefreq=1e5, samplefreq=25, prefix = "subset2_v4_",
+     aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.02, phi_stp = 0.25, thetas_stp = 0.2, mu_stp = 0.35, ngen = 1000, 
+     printfreq=100000, savefreq=1e5, samplefreq=5, prefix = "subset2_v4_",
      continue = FALSE, continuing.params = NULL)
