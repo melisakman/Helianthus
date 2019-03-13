9b7f7021e59eb1876751278f09ca610f5efe86c6
melisakman <makman@berkeley.edu>
2019-03-12 17:51:57 -0700

    bedass

--- shells/bedassle/bedassle2_v5.R
+++ shells/bedassle/bedassle2_v5.R
@@ -11,7 +11,12 @@ climate = load("subset2_envi.RData")
 
 # thetas increased from 0.2 to 0.25
 
+#MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
+#     aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.25, mu_stp = 0.35, ngen = 1000, 
+#     printfreq=1000, savefreq=1e3, samplefreq=5, prefix = "subset2_v5_",
+#     continue = FALSE, continuing.params = NULL)
+
 MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
-     aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.25, mu_stp = 0.35, ngen = 1000, 
-     printfreq=1000, savefreq=1e3, samplefreq=5, prefix = "subset2_v5_",
+     aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.1, mu_stp = 0.35, ngen = 1000, 
+     printfreq=1000, savefreq=1e3, samplefreq=5, prefix = "subset2_v5_2",
      continue = FALSE, continuing.params = NULL)
--- shells/bedassle/bedassle2_v7.R
+++ shells/bedassle/bedassle2_v7.R
@@ -11,7 +11,12 @@ climate = load("subset2_envi.RData")
 
 #mu_stp decreased from 0.35 to 0.3
 
+#MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
+#     aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.2, mu_stp = 0.3, ngen = 1000, 
+#     printfreq=1000, savefreq=1000, samplefreq=5, prefix = "subset2_v7_",
+#     continue = FALSE, continuing.params = NULL)
+
 MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
-     aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.2, mu_stp = 0.3, ngen = 1000, 
-     printfreq=1000, savefreq=1000, samplefreq=5, prefix = "subset2_v7_",
+     aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.02, phi_stp = 0.2, thetas_stp = 0.2, mu_stp = 0.5, ngen = 1000, 
+     printfreq=1000, savefreq=1000, samplefreq=5, prefix = "subset2_v7_2_",
      continue = FALSE, continuing.params = NULL)
