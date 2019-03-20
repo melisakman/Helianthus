ef91813751f31e32220171d5d912f774d093151d
melisakman <makman@berkeley.edu>
2019-03-20 12:08:54 -0700

    bedass

--- shells/bedassle/subsets/all.R
+++ shells/bedassle/subsets/all.R
@@ -9,9 +9,13 @@ sample = as.matrix(sample[1:2081])
 dim(sample)
 climate = load("all_envi_small.RData")
 
-MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
-     aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.01, phi_stp = 0.01, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1000, 
-     printfreq=1000, savefreq=100, samplefreq=5, prefix = "all_final_",
-     continue = FALSE, continuing.params = NULL)
+#MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
+#     aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.01, phi_stp = 0.01, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1000, 
+#     printfreq=1000, savefreq=100, samplefreq=5, prefix = "all_final_",
+#     continue = FALSE, continuing.params = NULL)
 
+MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
+     aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.01, phi_stp = 0.01, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1000000, 
+     printfreq=100000, savefreq=100000, samplefreq=5, prefix = "all_final_v2_",
+     continue = FALSE, continuing.params = NULL)
 
