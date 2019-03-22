d71ee4d832c4bc2ec5d7a23985b84b361130eb6d
melisakman <makman@berkeley.edu>
2019-03-21 21:48:19 -0700

    bedass

--- shells/bedassle/subsets/all.R
+++ shells/bedassle/subsets/all.R
@@ -34,15 +34,15 @@ climate = load("all_envi_small.RData")
 #   printfreq=100, savefreq=100, samplefreq=5, prefix = "all_final_v5_",
 #   continue = FALSE, continuing.params = NULL)
 
-MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
-   aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.01, phi_stp = 0.01, thetas_stp = 0.001, mu_stp = 1.5, ngen = 5000, 
-   printfreq=100, savefreq=100, samplefreq=5, prefix = "all_final_v6_",
-   continue = FALSE, continuing.params = NULL)
-
 #MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
-#   aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.01, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 5000, 
-#   printfreq=100, savefreq=100, samplefreq=5, prefix = "all_final_v7_",
+#   aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.01, phi_stp = 0.01, thetas_stp = 0.001, mu_stp = 1.5, ngen = 5000, 
+#   printfreq=100, savefreq=100, samplefreq=5, prefix = "all_final_v6_",
 #   continue = FALSE, continuing.params = NULL)
+#
+MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
+   aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.01, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 5000, 
+   printfreq=100, savefreq=100, samplefreq=5, prefix = "all_final_v7_",
+   continue = FALSE, continuing.params = NULL)
 #MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0001,
 #   aD_stp = 0.1, aE_stp = 0.1, a2_stp = 0.001, phi_stp = 0.01, thetas_stp = 0.01, mu_stp = 1.5, ngen = 5000, 
 #   printfreq=100, savefreq=100, samplefreq=5, prefix = "all_final_v8_",
