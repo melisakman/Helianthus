8016dcb9ad2438f5940159c2445d8083e822337a
melisakman <makman@berkeley.edu>
2019-03-26 21:34:08 +0100

    GATK filter

--- shells/variant_calling/new/4-GATK/4-filtering/SNPs/hardfilter_SNP_invariant_chr06.sh
+++ shells/variant_calling/new/4-GATK/4-filtering/SNPs/hardfilter_SNP_invariant_chr06.sh
@@ -2,13 +2,11 @@
 #SBATCH -D /global/scratch/makman/GATK/sams/gvcfs/genotyping
 #SBATCH -J filchr06
 #SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=400:00:00
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=800:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/hardfilter_SNP_chr06.out
 #SBATCH -e /global/home/users/makman/GATK/outs/hardfilter_SNP_chr06.err
 #SBATCH --mail-user=makman@berkeley.edu
