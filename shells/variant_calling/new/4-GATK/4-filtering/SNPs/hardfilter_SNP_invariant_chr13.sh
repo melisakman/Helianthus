20a785e61cd53d47c6a42d3d2acfed4544e9eae1
melisakman <makman@berkeley.edu>
2019-03-26 21:29:25 +0100

    GATK filter

--- shells/variant_calling/new/4-GATK/4-filtering/SNPs/hardfilter_SNP_invariant_chr13.sh
+++ shells/variant_calling/new/4-GATK/4-filtering/SNPs/hardfilter_SNP_invariant_chr13.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #SBATCH -D /global/scratch/makman/GATK/sams/gvcfs/genotyping
-#SBATCH -J filchr01
+#SBATCH -J filchr13
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio
 #SBATCH --qos=rosalind_savio_normal
@@ -9,8 +9,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
 #SBATCH --time=400:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/hardfilter_SNP_chr01.out
-#SBATCH -e /global/home/users/makman/GATK/outs/hardfilter_SNP_chr01.err
+#SBATCH -o /global/home/users/makman/GATK/outs/hardfilter_SNP_chr13.out
+#SBATCH -e /global/home/users/makman/GATK/outs/hardfilter_SNP_chr13.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load gcc/4.8.5 
@@ -18,30 +18,30 @@ module load java
 module load gatk/4.0.1.2
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_GATK.vcf > chr01_GATK.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr13_GATK.vcf > chr13_GATK.vcf.gz
 
 /global/scratch/makman/gatk-4.1.0.0/gatk IndexFeatureFile \
--F chr01_GATK.vcf.gz
+-F chr13_GATK.vcf.gz
 
 /global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \
 -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
--V chr01_GATK.vcf.gz \
+-V chr13_GATK.vcf.gz \
 --select-type-to-include SNP \
--O chr01_GATK_SNP.vcf.gz 
+-O chr13_GATK_SNP.vcf.gz 
 
-cp chr01_GATK_SNP.vcf.gz /clusterfs/vector/instrumentData/blackmanlab/Helianthus/GATK_4Peter
+cp chr13_GATK_SNP.vcf.gz /clusterfs/vector/instrumentData/blackmanlab/Helianthus/GATK_4Peter
 
 /global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" VariantFiltration \
 -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
--V chr01_GATK_SNP.vcf.gz \
+-V chr13_GATK_SNP.vcf.gz \
 --filter-expression "QD < 2.0 || MQ < 40.0 || FS > 60.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
 --filter-name "my_SNP_filter" \
--O chr01_GATK_SNP_filterInfo.vcf.gz  
+-O chr13_GATK_SNP_filterInfo.vcf.gz  
 	
 /global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \	
 -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
--V chr01_GATK_SNP_filterInfo.vcf.gz \
+-V chr13_GATK_SNP_filterInfo.vcf.gz \
 --exclude-filtered \
--O chr01_GATK_SNP_hardfiltered.vcf.gz 
+-O chr13_GATK_SNP_hardfiltered.vcf.gz 
 	
-cp chr01_GATK_SNP_hardfiltered.vcf.gz /clusterfs/vector/instrumentData/blackmanlab/Helianthus/GATK_4Peter
+cp chr13_GATK_SNP_hardfiltered.vcf.gz /clusterfs/vector/instrumentData/blackmanlab/Helianthus/GATK_4Peter
