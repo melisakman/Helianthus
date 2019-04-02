8db492ae5ee95c5b5c4c443c17f39ca97609af48
melisakman <makman@berkeley.edu>
2019-04-03 00:29:28 +0200

    bcftools 2+

--- shells/variant_calling/new/8-2+Set/bcftools/bcftools-isec_chr04.sh
+++ shells/variant_calling/new/8-2+Set/bcftools/bcftools-isec_chr04.sh
@@ -5,20 +5,27 @@
 #SBATCH --partition=savio
 #SBATCH --qos=rosalind_savio_normal
 #SBATCH --time=8:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/bcf-isec_chr04_3.out
-#SBATCH -e /global/home/users/makman/GATK/outs/bcf-isec_chr04_3.err
+#SBATCH -o /global/home/users/makman/GATK/outs/bcf-isec_chr04_2.out
+#SBATCH -e /global/home/users/makman/GATK/outs/bcf-isec_chr04_2.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
 module load bcftools/1.6
 module load bio/vcftools
 
-zcat gvcfs/genotyping/chr04_GATK_SNP_hardfiltered.vcf.gz | vcftools --vcf - --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > gvcfs/genotyping/chr04_GATK_SNP_hardfiltered_secondfilter.vcf.gz
+zcat samtools/samtools_invariant_chr04_dupsRemoved.vcf | bcftools filter -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools/samtools_variants_chr04.vcf.gz
+zcat freebayes/freebayes_invariant_chr04_combined.vcf.gz | bcftools filter -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes/freebayes_variants_chr04.vcf.gz
 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix gvcfs/genotyping/chr04_GATK_SNP_hardfiltered_secondfilter.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/freebayes_variants_chr04_filtered.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools/samtools_variants_chr04_filtered.vcf.gz
 
-bcftools isec -n =3 -O z -p ../bcftools_isec/chr04_3 gvcfs/genotyping/chr04_GATK_SNP_hardfiltered_secondfilter.vcf.gz \
-freebayes/freebayes_variants_chr04_filtered.vcf.gz \
-samtools/samtools_variants_chr04_filtered.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix gvcfs/genotyping/chr04_GATK_SNP.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/freebayes_variants_chr04.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools/samtools_variants_chr04.vcf.gz
+
+bcftools isec -n +2 -O z -p ../bcftools_isec/chr04_2 gvcfs/genotyping/chr04_GATK_SNP.vcf.gz \
+freebayes/freebayes_variants_chr04.vcf.gz \
+samtools/samtools_variants_chr04.vcf.gz
+
+
+
+
+
--- shells/variant_calling/new/8-2+Set/bcftools/bcftools-isec_chr09.sh
+++ shells/variant_calling/new/8-2+Set/bcftools/bcftools-isec_chr09.sh
@@ -5,20 +5,27 @@
 #SBATCH --partition=savio
 #SBATCH --qos=rosalind_savio_normal
 #SBATCH --time=8:00:00
-#SBATCH -o /global/home/users/makman/GATK/outs/bcf-isec_chr09_3.out
-#SBATCH -e /global/home/users/makman/GATK/outs/bcf-isec_chr09_3.err
+#SBATCH -o /global/home/users/makman/GATK/outs/bcf-isec_chr09_2.out
+#SBATCH -e /global/home/users/makman/GATK/outs/bcf-isec_chr09_2.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
 module load bcftools/1.6
 module load bio/vcftools
 
-zcat gvcfs/genotyping/chr09_GATK_SNP_hardfiltered.vcf.gz | vcftools --vcf - --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > gvcfs/genotyping/chr09_GATK_SNP_hardfiltered_secondfilter.vcf.gz
+zcat samtools/samtools_invariant_chr09_dupsRemoved.vcf | bcftools filter -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools/samtools_variants_chr09.vcf.gz
+zcat freebayes/freebayes_invariant_chr09_combined.vcf.gz | bcftools filter -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes/freebayes_variants_chr09.vcf.gz
 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix gvcfs/genotyping/chr09_GATK_SNP_hardfiltered_secondfilter.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/freebayes_variants_chr09_filtered.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools/samtools_variants_chr09_filtered.vcf.gz
 
-bcftools isec -n =3 -O z -p ../bcftools_isec/chr09_3 gvcfs/genotyping/chr09_GATK_SNP_hardfiltered_secondfilter.vcf.gz \
-freebayes/freebayes_variants_chr09_filtered.vcf.gz \
-samtools/samtools_variants_chr09_filtered.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix gvcfs/genotyping/chr09_GATK_SNP.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/freebayes_variants_chr09.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools/samtools_variants_chr09.vcf.gz
+
+bcftools isec -n +2 -O z -p ../bcftools_isec/chr09_2 gvcfs/genotyping/chr09_GATK_SNP.vcf.gz \
+freebayes/freebayes_variants_chr09.vcf.gz \
+samtools/samtools_variants_chr09.vcf.gz
+
+
+
+
+
