e6ffe8ae2a6efa2579e3ccb8d19c130b2207551f
melisakman <makman@berkeley.edu>
2019-04-03 16:43:16 +0200

    bcftools 2+

--- shells/variant_calling/new/8-2+Set/2-extractSTFBSites/extract_sites_chr01.sh
+++ shells/variant_calling/new/8-2+Set/2-extractSTFBSites/extract_sites_chr01.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH -D /global/scratch/makman/GATK/bcftools/
+#SBATCH -D /global/scratch/makman/GATK/bcftools_isec/
 #SBATCH -J 3chr01
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio
