e40580cdafb78804275f5e1818f10b95fbd98a5d
melisakman <makman@berkeley.edu>
2019-04-04 16:29:07 +0200

    annotate

--- shells/variant_calling/new/7-TruthSet/2-vcf_concat/vcf_concat.sh
+++ shells/variant_calling/new/7-TruthSet/2-vcf_concat/vcf_concat.sh
@@ -14,22 +14,22 @@ TMPDIR=/clusterfs/rosalind/users/makman/temp
 module load bcftools/1.6
 
 /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
-chr01_truth_GATK.vcf \
-chr02_truth_GATK.vcf \
-chr03_truth_GATK.vcf \
-chr04_truth_GATK.vcf \
-chr05_truth_GATK.vcf \
-chr06_truth_GATK.vcf \
-chr07_truth_GATK.vcf \
-chr08_truth_GATK.vcf \
-chr09_truth_GATK.vcf \
-chr10_truth_GATK.vcf \
-chr11_truth_GATK.vcf \
-chr12_truth_GATK.vcf \
-chr13_truth_GATK.vcf \
-chr14_truth_GATK.vcf \
-chr15_truth_GATK.vcf \
-chr16_truth_GATK.vcf \
-chr17_truth_GATK.vcf > truth_GATK.vcf.gz
+../chr01_3/0000.vcf.gz \
+../chr02_3/0000.vcf.gz \
+../chr03_3/0000.vcf.gz \
+../chr04_3/0000.vcf.gz \
+../chr05_3/0000.vcf.gz \
+../chr06_3/0000.vcf.gz \
+../chr07_3/0000.vcf.gz \
+../chr08_3/0000.vcf.gz \
+../chr09_3/0000.vcf.gz \
+../chr10_3/0000.vcf.gz \
+../chr11_3/0000.vcf.gz \
+../chr12_3/0000.vcf.gz \
+../chr13_3/0000.vcf.gz \
+../chr14_3/0000.vcf.gz \
+../chr15_3/0000.vcf.gz \
+../chr16_3/0000.vcf.gz \
+../chr17_3/0000.vcf.gz > truth_GATK.vcf
 
 
