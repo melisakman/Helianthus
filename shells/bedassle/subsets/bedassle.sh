840986b46055bd30d4bced52682fe34b76f2a7bb
melisakman <makman@berkeley.edu>
2019-04-13 15:03:48 -0700

    bedassle

--- shells/bedassle/subsets/bedassle.sh
+++ shells/bedassle/subsets/bedassle.sh
@@ -6,8 +6,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=800:00:00
-#SBATCH -o /global/home/users/makman/Arabidopsis/bedassle_final6.out
-#SBATCH -e /global/home/users/makman/Arabidopsis/bedassle_final6.err
+#SBATCH -o /global/home/users/makman/Arabidopsis/bedassle_final7.out
+#SBATCH -e /global/home/users/makman/Arabidopsis/bedassle_final7.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
