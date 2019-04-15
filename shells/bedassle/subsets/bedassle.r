341c4939a3c997cd08882a05c42613922dd5a991
melisakman <makman@berkeley.edu>
2019-04-15 11:29:10 -0700

    bedassle

--- shells/bedassle/subsets/bedassle.r
+++ shells/bedassle/subsets/bedassle.r
@@ -74,17 +74,17 @@ climate = load("all_envi_small.RData")
 #  printfreq=15, savefreq=15, samplefreq=15, prefix = "all_final_v22_temp_ssd_3",
 #  continue = FALSE, continuing.params = NULL)
 
-MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate$Precip_summer, k=nrow(count), loci=ncol(count), delta = 0.0005,
+MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate$Precip_Summer, k=nrow(count), loci=ncol(count), delta = 0.0005,
   aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 3000, 
   printfreq=15, savefreq=15, samplefreq=15, prefix = "all_final_v22_summer_precip_1",
   continue = FALSE, continuing.params = NULL)
 
-MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate$Precip_summer, k=nrow(count), loci=ncol(count), delta = 0.0005,
+MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate$Precip_Summer, k=nrow(count), loci=ncol(count), delta = 0.0005,
   aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 3000, 
   printfreq=15, savefreq=15, samplefreq=15, prefix = "all_final_v22_summer_precip_2",
   continue = FALSE, continuing.params = NULL)
 
-MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate$Precip_summer, k=nrow(count), loci=ncol(count), delta = 0.0005,
+MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate$Precip_Summer, k=nrow(count), loci=ncol(count), delta = 0.0005,
   aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 3000, 
   printfreq=15, savefreq=15, samplefreq=15, prefix = "all_final_v22_summer_precip_3",
   continue = FALSE, continuing.params = NULL)
