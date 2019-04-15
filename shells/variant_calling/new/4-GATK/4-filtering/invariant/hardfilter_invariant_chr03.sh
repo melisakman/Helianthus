a20dac6be21a288ab419e969b5acc45dcb2f4a3c
melisakman <makman@berkeley.edu>
2019-04-15 16:41:29 -0700

    noVar filter

---
+++ shells/variant_calling/new/4-GATK/4-filtering/invariant/hardfilter_invariant_chr03.sh
@@ -0,0 +1,45 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/GATK/sams/gvcfs/genotyping
+#SBATCH -J nochr03
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/hardfilter_SNP_chr03.out
+#SBATCH -e /global/scratch/users/makman/GATK/outs/hardfilter_SNP_chr03.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load gcc/4.8.5 
+module load java
+module load gatk/4.0.1.2
+export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
+module load bcftools/1.6
+module load bio/vcftools
+
+/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \
+-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+-V chr03_GATK.vcf.gz \
+--select-type-to-include NO_VARIATION \
+-O chr03_GATK_NoVar.vcf.gz
+
+/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" VariantFiltration \
+-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+-V chr03_GATK_Novar.vcf.gz \
+--filter-expression "QD < 2.0 || MQ < 40.0 || FS > 60.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
+--filter-name "novar_filter" \
+-O chr03_GATK_NoVar_filterInfo.vcf.gz
+	
+/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \
+-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+-V chr03_GATK_NoVar_filterInfo.vcf.gz \
+--exclude-filtered \
+-O chr03_GATK_NoVar_hardfiltered.vcf.gz
+	
+
+zcat chr03_GATK_NoVar_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > chr03_GATK_NoVar_hardfiltered_secondFilter.vcf.gz
+
+
