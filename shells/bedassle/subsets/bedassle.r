efb9260203a11725d438f4b2655246fdd73ed8c1
melisakman <makman@berkeley.edu>
2019-04-15 11:56:05 -0700

    bedassle

--- shells/bedassle/subsets/bedassle.r
+++ shells/bedassle/subsets/bedassle.r
@@ -8,6 +8,7 @@ sample = read.table("allsamples_5K_sample_size.txt", h=F)
 sample = as.matrix(sample[1:2081])
 dim(sample)
 climate = load("all_envi_small.RData")
+head(Climate$Temp_Mean)
 
 #MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.0005,
 #   aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 3000, 
