e7aec02f6782d69c0abb03c2a48733941a589311
melisakman <makman@berkeley.edu>
2019-04-17 11:16:03 -0700

    bedassle

--- shells/bedassle/subsets/bedassle.r
+++ shells/bedassle/subsets/bedassle.r
@@ -79,7 +79,7 @@ head(Climate$Temp_Mean)
 #  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 3000, 
 #  printfreq=15, savefreq=15, samplefreq=15, prefix = "all_final_v22_summer_precip_1",
 #  continue = FALSE, continuing.params = NULL)
-#
+
 MCMC_BB(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate$Precip_Summer, k=nrow(count), loci=ncol(count), delta = 0.0005,
   aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 3000, 
   printfreq=15, savefreq=15, samplefreq=15, prefix = "all_final_v22_summer_precip_2",
