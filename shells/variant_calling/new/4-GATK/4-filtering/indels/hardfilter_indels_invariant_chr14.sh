9a1267b74b88b0d396f3d069daf645463ec54030
melisakman <makman@berkeley.edu>
2019-04-16 21:52:39 -0700

    indel filter

---
+++ shells/variant_calling/new/4-GATK/4-filtering/indels/hardfilter_indels_invariant_chr14.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/GATK/sams/gvcfs/genotyping
+#SBATCH -J filchr14
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/hardfilter_indel_chr14.out
+#SBATCH -e /global/home/users/makman/GATK/outs/hardfilter_indel_chr14.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load gcc/4.8.5 
+module load java
+module load gatk/4.0.1.2
+export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
+
+
+/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \
+-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+-V chr14_GATK.vcf.gz \
+--select-type-to-include INDEL \
+-O chr14_GATK_indel.vcf.gz
+
+/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" VariantFiltration \
+-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+-V chr14_GATK_indel.vcf.gz \
+--filter-expression "QD < 2.0 || FS > 200.0 || SOR > 10.0 || ReadPosRankSum < -20.0" \
+--filter-name "my_indel_filter" \
+-O chr14_GATK_indel_filterInfo.vcf.gz
+	
+/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \
+-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+-V chr14_GATK_indel_filterInfo.vcf.gz \
+--exclude-filtered \
+-O chr14_GATK_indel_hardfiltered.vcf.gz
+	
+zcat chr14_GATK_indel_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > chr14_GATK_indel_secondFilter.vcf.gz
+
