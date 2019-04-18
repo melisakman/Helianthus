f8057d65be48738d47e963b7fbf8081a1a434b94
melisakman <makman@berkeley.edu>
2019-04-18 16:47:06 -0700

    GATK invariants

--- shells/variant_calling/new/10-combine/1-invariants/invariants_chr03.sh
+++ shells/variant_calling/new/10-combine/1-invariants/invariants_chr03.sh
@@ -16,11 +16,11 @@ module load bcftools/1.6
 module load bio/vcftools
 
 
-zcat freebayes/freebayes_invariant_chr03_combined.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL < 2.723e-15 && MQMR > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes/freebayes_invariant_chr03_combined_ref_filtered.vcf.gz
-zcat samtools/samtools_invariant_chr03_dupsRemoved.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools/samtools_invariant_chr03_dupsRemoved_ref_filtered.vcf.gz
-
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/freebayes_invariant_chr03_combined_ref_filtered.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools/samtools_invariant_chr03_dupsRemoved_ref_filtered.vcf.gz
+# zcat freebayes/freebayes_invariant_chr03_combined.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL < 2.723e-15 && MQMR > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes/freebayes_invariant_chr03_combined_ref_filtered.vcf.gz
+# zcat samtools/samtools_invariant_chr03_dupsRemoved.vcf.gz | bcftools filter -g 5 -i 'TYPE = "ref" && QUAL/DP > 2.0 && QUAL > 20 && MQ > 40' | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | vcf-sort -t /clusterfs/rosalind/users/makman/temp | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools/samtools_invariant_chr03_dupsRemoved_ref_filtered.vcf.gz
+# 
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/freebayes_invariant_chr03_combined_ref_filtered.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools/samtools_invariant_chr03_dupsRemoved_ref_filtered.vcf.gz
 
 bcftools isec -n +2 -O z -p ../bcftools_isec/chr03_2_ref gvcfs/genotyping/chr03_GATK_NoVar_hardfiltered_secondFilter.vcf.gz \
 freebayes/freebayes_invariant_chr03_combined_ref_filtered.vcf.gz \
--- shells/variant_calling/new/4-GATK/4-filtering/invariant/hardfilter_invariant_chr06.sh
+++ shells/variant_calling/new/4-GATK/4-filtering/invariant/hardfilter_invariant_chr06.sh
@@ -1,14 +1,12 @@
 #!/bin/bash 
 #SBATCH -D /global/scratch/makman/GATK/sams/gvcfs/genotyping
 #SBATCH -J nochr06
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=400:00:00
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=savio_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/hardfilter_SNP_chr06.out
 #SBATCH -e /global/scratch/users/makman/GATK/outs/hardfilter_SNP_chr06.err
 #SBATCH --mail-user=makman@berkeley.edu
