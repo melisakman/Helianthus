2ac57d181d50c4bbe8fce177727dff54db78b4aa
melisakman <makman@berkeley.edu>
2019-05-10 13:29:41 -0700

    bedassle

--- shells/bedassle/subsets/bedassle.sh
+++ shells/bedassle/subsets/bedassle.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
 #SBATCH -D /clusterfs/rosalind/users/makman/Arabidopsis/bedassle/final
-#SBATCH -J badass13
+#SBATCH -J badass14
 #SBATCH --account=co_rosalind
 #SBATCH --partition=savio2_htc
 #SBATCH --qos=rosalind_htc2_normal
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
 #SBATCH --time=880:00:00
-#SBATCH -o /global/home/users/makman/Arabidopsis/bedassle_v22_13.out
-#SBATCH -e /global/home/users/makman/Arabidopsis/bedassle_v22_13.err
+#SBATCH -o /global/home/users/makman/Arabidopsis/bedassle_v22_14.out
+#SBATCH -e /global/home/users/makman/Arabidopsis/bedassle_v22_14.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
