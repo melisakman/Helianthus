e4a508bde90c8d9e48ac4bab79247d831ab5fa19
melisakman <makman@berkeley.edu>
2019-05-10 13:28:55 -0700

    bedassle

--- shells/bedassle/subsets/bedassle.r
+++ shells/bedassle/subsets/bedassle.r
@@ -110,16 +110,16 @@ head(Climate$Temp_Mean)
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_11",
 #  continue = FALSE, continuing.params = NULL)
 #
-MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-  aD_stp = 0.01, aE_stp = 0.001, a2_stp = 0.001, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
-  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_12",
-  continue = FALSE, continuing.params = NULL)
-
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.1, a2_stp = 0.001, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
-#  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_13",
+#  aD_stp = 0.01, aE_stp = 0.001, a2_stp = 0.001, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
+#  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_12",
 #  continue = FALSE, continuing.params = NULL)
 #
+MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
+  aD_stp = 0.01, aE_stp = 0.1, a2_stp = 0.001, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
+  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_13",
+  continue = FALSE, continuing.params = NULL)
+
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
 #  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.0001, phi_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_14",
