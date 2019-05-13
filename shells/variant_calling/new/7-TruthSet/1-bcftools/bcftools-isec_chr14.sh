055c1517568be41c43019b4d27b13579b3925a81
melisakman <makman@berkeley.edu>
2019-05-13 12:47:35 -0700

    freebayes

--- shells/variant_calling/new/7-TruthSet/1-bcftools/bcftools-isec_chr14.sh
+++ shells/variant_calling/new/7-TruthSet/1-bcftools/bcftools-isec_chr14.sh
@@ -13,12 +13,12 @@
 module load bcftools/1.6
 module load bio/vcftools
 
-zcat gvcfs/genotyping/chr14_GATK_SNP_hardfiltered.vcf.gz | vcftools --vcf - --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > gvcfs/genotyping/chr14_GATK_SNP_hardfiltered_secondfilter.vcf.gz
+# zcat gvcfs/genotyping/chr14_GATK_SNP_hardfiltered.vcf.gz | vcftools --vcf - --minDP 3 --max-missing 0.8 --maxDP 25 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > gvcfs/genotyping/chr14_GATK_SNP_hardfiltered_secondfilter.vcf.gz
 
 /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix gvcfs/genotyping/chr14_GATK_SNP_hardfiltered_secondfilter.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/freebayes_variants_chr14_filtered.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/no_mnp/freebayes_variants_chr14_filtered.vcf.gz
 /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools/samtools_variants_chr14_filtered.vcf.gz
 
 bcftools isec -n =3 -O z -p ../bcftools_isec/chr14_3 gvcfs/genotyping/chr14_GATK_SNP_hardfiltered_secondfilter.vcf.gz \
-freebayes/freebayes_variants_chr14_filtered.vcf.gz \
+freebayes/no_mnp/freebayes_variants_chr14_filtered.vcf.gz \
 samtools/samtools_variants_chr14_filtered.vcf.gz
