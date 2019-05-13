50455b7de66bb7b5bd40e7507ceae7f332e56f34
melisakman <makman@berkeley.edu>
2019-05-13 13:02:28 -0700

    freebayes

--- shells/variant_calling/new/8-2+Set/1-bcftools/bcftools-isec_chr06.sh
+++ shells/variant_calling/new/8-2+Set/1-bcftools/bcftools-isec_chr06.sh
@@ -13,16 +13,16 @@
 module load bcftools/1.6
 module load bio/vcftools
 
-zcat samtools/samtools_invariant_chr06_dupsRemoved.vcf | bcftools filter -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools/samtools_variants_chr06.vcf.gz
-zcat freebayes/freebayes_invariant_chr06_combined.vcf.gz | bcftools filter -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes/freebayes_variants_chr06.vcf.gz
+# zcat samtools/samtools_invariant_chr06_dupsRemoved.vcf | bcftools filter -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > samtools/samtools_variants_chr06.vcf.gz
+zcat freebayes/no_mnp/freebayes_invariant_chr06_combined.vcf.gz | bcftools filter -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > freebayes/no_mnp/freebayes_variants_chr06.vcf.gz
 
 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix gvcfs/genotyping/chr06_GATK_SNP.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/freebayes_variants_chr06.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools/samtools_variants_chr06.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix gvcfs/genotyping/chr06_GATK_SNP.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix freebayes/no_mnp/freebayes_variants_chr06.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix samtools/samtools_variants_chr06.vcf.gz
 
 bcftools isec -n +2 -O z -p ../bcftools_isec/chr06_2 gvcfs/genotyping/chr06_GATK_SNP.vcf.gz \
-freebayes/freebayes_variants_chr06.vcf.gz \
+freebayes/no_mnp/freebayes_variants_chr06.vcf.gz \
 samtools/samtools_variants_chr06.vcf.gz
 
 
