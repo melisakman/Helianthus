646426d720fe70dc10637b1abe1c7bdf3a05bb74
melisakman <makman@berkeley.edu>
2019-05-14 12:22:37 -0700

    freebayes

--- shells/bedassle/subsets/bedassle.r
+++ shells/bedassle/subsets/bedassle.r
@@ -90,78 +90,78 @@ head(Climate$Temp_Mean)
 #  printfreq=15, savefreq=15, samplefreq=15, prefix = "all_final_v22_summer_precip_3",
 #  continue = FALSE, continuing.params = NULL)
 
-#MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
-#  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_8",
-#  continue = FALSE, continuing.params = NULL)
-#
+MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
+  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e7, 
+  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_8",
+  continue = FALSE, continuing.params = FALSE)
+
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.005,
-#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_9",
 #  continue = FALSE, continuing.params = NULL)
 #
-MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-  aD_stp = 0.001, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
-  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_10",
-  continue = FALSE, continuing.params = NULL)
+#MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
+#  aD_stp = 0.001, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e7, 
+#  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_10",
+#  continue = FALSE, continuing.params = NULL)
 
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.1, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
+#  aD_stp = 0.1, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_11",
 #  continue = FALSE, continuing.params = NULL)
 #
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.001, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.001, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_12",
 #  continue = FALSE, continuing.params = NULL)
 #
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.1, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.1, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_13",
 #  continue = FALSE, continuing.params = NULL)
 #
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.0001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.0001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_14",
 #  continue = FALSE, continuing.params = NULL)
 
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.01, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.01, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_15",
 #  continue = FALSE, continuing.params = NULL)
 #
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, phi_stp = 0.0001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, phi_stp = 0.0001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_16",
 #  continue = FALSE, continuing.params = NULL)
 #
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, phi_stp = 0.01, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, phi_stp = 0.01, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_17",
 #  continue = FALSE, continuing.params = NULL)
 #
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.001, mu_stp = 1.5, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.001, mu_stp = 1.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_18",
 #  continue = FALSE, continuing.params = NULL)
 #
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.1, mu_stp = 1.5, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.1, mu_stp = 1.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_19",
 #  continue = FALSE, continuing.params = NULL)
 #
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 0.15, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 0.15, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_20",
 #  continue = FALSE, continuing.params = NULL)
 #
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 10.5, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 10.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_21",
 #  continue = FALSE, continuing.params = NULL)
 #
 #MCMC(counts=count, sample_sizes = sample, D=Euc_dist, E=Climate, k=nrow(count), loci=ncol(count), delta = 0.00005,
-#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e6, 
+#  aD_stp = 0.01, aE_stp = 0.01, a2_stp = 0.001, thetas_stp = 0.01, mu_stp = 1.5, ngen = 1e7, 
 #  printfreq=10000, savefreq=1e5, samplefreq=10000, prefix = "all_final_v22_8",
 #  continue = FALSE, continuing.params = NULL)
 #
