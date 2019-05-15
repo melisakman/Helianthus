4440e00d18d79ab5932e5a6d199105918a54ed8b
melisakman <makman@berkeley.edu>
2019-05-15 16:41:26 -0700

    freebayes

--- shells/variant_calling/new/8-2+Set/2-extractSTFBSites/extract_sites_chr01.sh
+++ shells/variant_calling/new/8-2+Set/2-extractSTFBSites/extract_sites_chr01.sh
@@ -17,19 +17,30 @@ module load bio/vcftools
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 TMPDIR=/clusterfs/rosalind/users/makman/temp
 module load bcftools/1.6
-
-gunzip chr01_2/0001.vcf.gz
-
-python ~/git/Helianthus/shells/variant_calling/new/8-2+Set/2-extractSTFBSites/extract_GATK_variants.py chr01_2/0001.vcf chr01_2/sites.txt chr01_2/FB_ST_sites_chr01.vcf
-
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_2/FB_ST_sites_chr01.vcf > chr01_2/FB_ST_sites_chr01.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_2/0001.vcf > chr01_2/0001.vcf.gz
-
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-shuffle-cols -t chr01_2/0000.vcf.gz chr01_2/FB_ST_sites_chr01.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > chr01_2/FB_ST_sites_chr01_sorted.vcf.gz
+module load java
 
 
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
-chr01_2/0000.vcf.gz chr01_2/FB_ST_sites_chr01_sorted.vcf.gz | /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-sort > chr01_2plus_update.vcf.gz
+# gunzip chr01_2/0001.vcf.gz
+# 
+# python ~/git/Helianthus/shells/variant_calling/new/8-2+Set/2-extractSTFBSites/extract_GATK_variants.py chr01_2/0001.vcf chr01_2/sites.txt chr01_2/FB_ST_sites_chr01.vcf
+# 
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_2/FB_ST_sites_chr01.vcf > chr01_2/FB_ST_sites_chr01.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_2/0001.vcf > chr01_2/0001.vcf.gz
+# 
+# /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-shuffle-cols -t chr01_2/0000.vcf.gz chr01_2/FB_ST_sites_chr01.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > chr01_2/FB_ST_sites_chr01_sorted.vcf.gz
+# 
+java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
+   -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+   -T CombineVariants \
+   --variant chr01_2/0000.vcf.gz \
+   --variant chr01_2/FB_ST_sites_chr01_sorted.vcf.gz \
+   -o chr01_2plus.vcf \
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_2plus.vcf > chr01_2plus.vcf.gz
+
+##not used anymore
+# /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
+# chr01_2/0000.vcf.gz chr01_2/FB_ST_sites_chr01_sorted.vcf.gz | /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-sort > chr01_2plus_update.vcf.gz
 
 
 
