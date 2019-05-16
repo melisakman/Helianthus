0294982866e86a34efa31bf70c23db64974c3c6e
melisakman <makman@berkeley.edu>
2019-05-15 23:14:55 -0700

    VQSR

--- shells/variant_calling/new/9-VQSR/2-VQSR/G4.sh
+++ shells/variant_calling/new/9-VQSR/2-VQSR/G4.sh
@@ -1,14 +1,12 @@
 #!/bin/bash 
 #SBATCH -D /global/scratch/makman/GATK/bcftools_isec/
 #SBATCH -J G4
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=80:00:00
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=savio_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/VSQR_annot_unfilt_chr01.out
 #SBATCH -e /global/home/users/makman/GATK/outs/VSQR_annot_unfilt_chr01.err
 #SBATCH --mail-user=makman@berkeley.edu
