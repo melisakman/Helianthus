af31a017cdba1c5d397821e87e90f9e1df651bf9
melisakman <makman@berkeley.edu>
2019-05-15 23:13:38 -0700

    VQSR

--- shells/variant_calling/new/9-VQSR/2-VQSR/G4.sh
+++ shells/variant_calling/new/9-VQSR/2-VQSR/G4.sh
@@ -26,48 +26,36 @@ R_LIBS_USER=/global/home/users/makman/R/x86_64-pc-linux-gnu-library/3.4/
 
 
 
-# /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
-# chr01_2plus_annot.vcf.gz \
-# chr02_2plus_annot.vcf.gz \
-# chr03_2plus_annot.vcf.gz \
-# chr04_2plus_annot.vcf.gz \
-# chr05_2plus_annot.vcf.gz \
-# chr06_2plus_annot.vcf.gz \
-# chr07_2plus_annot.vcf.gz \
-# chr08_2plus_annot.vcf.gz \
-# chr09_2plus_annot.vcf.gz \
-# chr10_2plus_annot.vcf.gz \
-# chr11_2plus_annot.vcf.gz \
-# chr12_2plus_annot.vcf.gz \
-# chr13_2plus_annot.vcf.gz \
-# chr14_2plus_annot.vcf.gz \
-# chr15_2plus_annot.vcf.gz \
-# chr16_2plus_annot.vcf.gz \
-# chr17_2plus_annot.vcf.gz > allChr_2plus_annot.vcf
-# 
-# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c allChr_2plus_annot.vcf > allChr_2plus_annot.vcf.gz
-# 
-# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf allChr_2plus_annot.vcf.gz
+/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
+chr01_2plus_annot.vcf.gz \
+chr02_2plus_annot.vcf.gz \
+chr03_2plus_annot.vcf.gz \
+chr04_2plus_annot.vcf.gz \
+chr05_2plus_annot.vcf.gz \
+chr06_2plus_annot.vcf.gz \
+chr07_2plus_annot.vcf.gz \
+chr08_2plus_annot.vcf.gz \
+chr09_2plus_annot.vcf.gz \
+chr10_2plus_annot.vcf.gz \
+chr11_2plus_annot.vcf.gz \
+chr12_2plus_annot.vcf.gz \
+chr13_2plus_annot.vcf.gz \
+chr14_2plus_annot.vcf.gz \
+chr15_2plus_annot.vcf.gz \
+chr16_2plus_annot.vcf.gz \
+chr17_2plus_annot.vcf.gz > allChr_2plus_annot.vcf
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c allChr_2plus_annot.vcf > allChr_2plus_annot.vcf.gz
+
+rm allChr_2plus_annot.vcf
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf allChr_2plus_annot.vcf.gz
 
 
 
-# /clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk-4.0.1.2/gatk IndexFeatureFile \
-# --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" \
-# -F truthSet/truth_GATK.vcf
-# 
-# /clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk-4.0.1.2/gatk VariantRecalibrator \
-# --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" \
-# -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
-# -V allChr_2plus_annot.vcf.gz \
-# -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP -an InbreedingCoeff \
-# --max-gaussians 4 \
-# -mode SNP \
-# --resource 3callerscombined,known=false,training=true,truth=true,prior=10.0:truthSet/truth_GATK.vcf \
-# -O G4_unfiltered_all_tranches.recal \
-# -tranche 90 -tranche 80 -tranche 70 \
-# --tranches-file G4_unfiltered_all_tranches.tranches \
-# --rscript-file G4_unfiltered_all_tranches.plots.R
- 
+/clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk-4.0.1.2/gatk IndexFeatureFile \
+--java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" \
+-F truthSet/truth_GATK.vcf
+
 /clusterfs/vector/home/groups/software/sl-7.x86_64/modules/gatk-4.0.1.2/gatk VariantRecalibrator \
 --java-options "-Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files/ -Xmx64G" \
 -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
@@ -76,7 +64,8 @@ R_LIBS_USER=/global/home/users/makman/R/x86_64-pc-linux-gnu-library/3.4/
 --max-gaussians 4 \
 -mode SNP \
 --resource 3callerscombined,known=false,training=true,truth=true,prior=10.0:truthSet/truth_GATK.vcf \
--O G4_unfiltered_90plus_tranches.recal \
--tranche 99 -tranche 95 -tranche 92 -tranche 90 \
---tranches-file G4_unfiltered_90plus_tranches.tranches \
---rscript-file G4_unfiltered_90plus_tranches.plots.R
+-O G4_unfiltered_all_tranches.recal \
+-tranche 99 -tranche 95 -tranche 92 -tranche 90 -tranche 80 -tranche 70 \
+--tranches-file G4_unfiltered_all_tranches.tranches \
+--rscript-file G4_unfiltered_all_tranches.plots.R
+ 
