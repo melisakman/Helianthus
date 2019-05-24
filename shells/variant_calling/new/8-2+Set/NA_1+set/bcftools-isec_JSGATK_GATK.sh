20ce1c952e4f2e76b320721290cd60487659cd21
melisakman <makman@berkeley.edu>
2019-05-24 11:40:38 -0700

    JS GATK comp

--- shells/variant_calling/new/8-2+Set/NA_1+set/bcftools-isec_JSGATK_GATK.sh
+++ shells/variant_calling/new/8-2+Set/NA_1+set/bcftools-isec_JSGATK_GATK.sh
@@ -31,9 +31,9 @@ module load bio/vcftools
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ../bcftools_isec/chr01_2plus_annot_G8_VQSR99.vcf.gz 
 # 
 
-zcat ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01.vcf.gz | bcftools filter -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_SNP.vcf.gz 
+# zcat ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01.vcf.gz | bcftools filter -i 'TYPE = "snp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_SNP.vcf.gz 
 
-
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_SNP.vcf.gz
 bcftools isec -n +1 -O z -p ../bcftools_isec/chr01_1_JS_GATK gvcfs/genotyping/chr01_GATK_SNP.vcf.gz \
 ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_SNP.vcf.gz 
 
