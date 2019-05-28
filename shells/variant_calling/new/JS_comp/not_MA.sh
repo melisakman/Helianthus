6ba6fd485597cb4989a7d8ad1ea412d3b26bcf25
melisakman <makman@berkeley.edu>
2019-05-28 14:51:42 -0700

    JS GATK comp

---
+++ shells/variant_calling/new/JS_comp/not_MA.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/GATK/sams/
+#SBATCH -J G6
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=savio_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=72:00:00
+#SBATCH -o /global/home/users/makman/GATK/outs/JS_MA_comp.out
+#SBATCH -e /global/home/users/makman/GATK/outs/JS_MA_comp.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+gunzip gvcfs/genotyping/chr01_GATK.vcf.gz
+
+python /global/home/users/makman/git/Helianthus/shells/variant_calling/new/JS_comp/not_MA.py /global/scratch/makman/GATK/bcftools_isec/chr01_1_JS_GATK/sites.txt gvcfs/genotyping/chr01_GATK.vcf /global/scratch/makman/GATK/bcftools_isec/chr01_1_JS_GATK/JS_MA_comp.txt
