89f757fe6ef07c084c3125be7ebbbac00f82f1f4
melisakman <makman@berkeley.edu>
2019-05-29 14:29:16 -0700

    JS VQSR filter

--- shells/variant_calling/new/JS_comp/not_MA.py
+++ shells/variant_calling/new/JS_comp/not_MA.py
@@ -21,41 +21,36 @@ for line in file1:
 	if category == "01":
 		positions[pos] = [ref, alt]
 ref_count = 0
+other_count = 0
 del_count = 0
 for line2 in file2:
 	split2 = line2.split("\t")
 	if split2[0][0] == "#":
 		continue
 	else:
-		pos2 = split2[1]
+		pos2 = int(split2[1])
 		ref2 = split2[3]
 		alt2 = split2[4]	
-		list = positions[int(pos2)]
-		JS_ref = list[0]
-		JS_alt = list[1]
-		file_out.write(str(pos2) + "\t" + ref2 + "\t" + alt2 + "\tJS: " + JS_ref + "/" + JS_alt + "\n")
 		if pos2 in positions:
 			if alt2 == ".":
 				ref_count +=1
 			else:
-				if len(JS_alt) == 3:
-					if JS_alt[0] =="*" or JS_alt[2] =="*":
+				if len(alt2) == 3:
+					other_count +=1
+					if alt2[0] =="*" or alt2[2] =="*":
 						del_count +=1
-					else:
-						other_count +=1
-				elif len(JS_alt) == 5:
-					
-					if JS_alt[0] =="*" or JS_alt[2] =="*" or JS_alt[4] =="*":
+				elif len(alt2) == 5:
+					other_count +=1
+					if alt2[0] =="*" or alt2[2] =="*" or alt2[4] =="*":
 						del_count +=1
-					else:
-						other_count +=1
-				elif len(JS_alt) == 7:
-
-					if JS_alt[0] =="*" or JS_alt[2] =="*" or JS_alt[4] =="*" or JS_alt[6] =="*":
+				elif len(alt2) == 7:
+					other_count +=1
+					if alt2[0] =="*" or alt2[2] =="*" or alt2[4] =="*" or alt2[6] =="*":
 						del_count +=1
-					else:
-						other_count +=1
-			
+			list = positions[pos2]
+			JS_ref = list[0]
+			JS_alt = list[1]
+			file_out.write(str(pos2) + "\t" + ref2 + "\t" + alt2 + "\tJS: " + JS_ref + "/" + JS_alt + "\n")
 print ref_count
 print other_count
 print del_count
