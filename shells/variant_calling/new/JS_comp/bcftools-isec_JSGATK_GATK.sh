9aa67dc776199a30326003b63e9be99c4d2fadc8
melisakman <makman@berkeley.edu>
2019-05-29 10:27:47 -0700

    JS GATK comp

--- shells/variant_calling/new/JS_comp/bcftools-isec_JSGATK_GATK.sh
+++ shells/variant_calling/new/JS_comp/bcftools-isec_JSGATK_GATK.sh
@@ -38,15 +38,15 @@ zcat gvcfs/genotyping/chr01_GATK.vcf.gz | bcftools filter -i 'TYPE = "other"' | 
 # ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_SNP.vcf.gz 
 
 
-zcat ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01.vcf.gz | bcftools filter -i 'TYPE = "mnp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_MNP.vcf.gz 
-zcat ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01.vcf.gz | bcftools filter -i 'TYPE = "indel"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_INDEL.vcf.gz 
-zcat ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01.vcf.gz | bcftools filter -i 'TYPE = "bnd"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_BND.vcf.gz 
-zcat ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01.vcf.gz | bcftools filter -i 'TYPE = "other"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_other.vcf.gz 
-
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_MNP.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_INDEL.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_BND.vcf.gz
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_other.vcf.gz
+# zcat ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01.vcf.gz | bcftools filter -i 'TYPE = "mnp"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_MNP.vcf.gz 
+# zcat ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01.vcf.gz | bcftools filter -i 'TYPE = "indel"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_INDEL.vcf.gz 
+# zcat ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01.vcf.gz | bcftools filter -i 'TYPE = "bnd"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_BND.vcf.gz 
+# zcat ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01.vcf.gz | bcftools filter -i 'TYPE = "other"' | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_other.vcf.gz 
+# 
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_MNP.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_INDEL.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_BND.vcf.gz
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_other.vcf.gz
 
 bcftools isec -n +1 -O z -p ../bcftools_isec/chr01_1_JS_GATK_MNP gvcfs/genotyping/chr01_GATK_MNP.vcf.gz \
 ../JS/vqsr_50.00-70.00-90.00-99.00-100.00_HanXRQChr01_MNP.vcf.gz 
