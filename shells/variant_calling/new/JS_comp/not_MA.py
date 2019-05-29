864a2cd501c98fb94c85bc3c5ecb75ee6c25e53f
melisakman <makman@berkeley.edu>
2019-05-28 23:06:33 -0700

    JS GATK comp

--- shells/variant_calling/new/JS_comp/not_MA.py
+++ shells/variant_calling/new/JS_comp/not_MA.py
@@ -20,7 +20,9 @@ for line in file1:
 	category = split1[4][:-1]
 	if category == "01":
 		positions[pos] = [ref, alt]
-
+ref_count = 0
+other_count = 0
+del_count = 0
 for line2 in file2:
 	split2 = line2.split("\t")
 	if split2[0][0] == "#":
@@ -30,7 +32,24 @@ for line2 in file2:
 		ref2 = split2[3]
 		alt2 = split2[4]	
 		if pos2 in positions:
+			if alt2 == ".":
+				ref_count +=1
+			elif len(alt2) == 3:
+				other_count +=1
+				if alt2[0] =="*" or alt[2] =="*":
+					del_count +=1
+			elif len(alt2) == 5:
+				other_count +=1
+				if alt2[0] =="*" or alt[2] =="*" or alt[4] =="*":
+					del_count +=1
+			elif len(alt2) == 7:
+				other_count +=1
+				if alt2[0] =="*" or alt[2] =="*" or alt[4] =="*" or alt[6] =="*":
+					del_count +=1
 			list = positions[pos2]
 			JS_ref = list[0]
 			JS_alt = list[1]
 			file_out.write(str(pos2) + "\t" + ref2 + "\t" + alt2 + "\tJS: " + JS_ref + "/" + JS_alt + "\n")
+print ref_count
+print other_count
+print del_count
