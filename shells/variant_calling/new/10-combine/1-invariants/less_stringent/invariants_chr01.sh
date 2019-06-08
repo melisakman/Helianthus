b3a1308675137d6dbf52c2dbc3d3af790b1049b2
melisakman <makman@berkeley.edu>
2019-06-07 23:05:57 -0700

    ref

--- shells/variant_calling/new/10-combine/1-invariants/less_stringent/invariants_chr01.sh
+++ shells/variant_calling/new/10-combine/1-invariants/less_stringent/invariants_chr01.sh
@@ -43,22 +43,22 @@ module load java
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01.vcf > ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01.vcf.gz
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c ../bcftools_isec/chr01_1_ref/0001.vcf > ../bcftools_isec/chr01_1_ref/0001.vcf.gz
 
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-shuffle-cols -t ../bcftools_isec/chr01_1_ref/0000.vcf.gz ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01_sorted.vcf.gz
+# /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-shuffle-cols -t ../bcftools_isec/chr01_1_ref/0000.vcf.gz ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01.vcf.gz | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01_sorted.vcf.gz
+# 
+# 
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01_sorted.vcf.gz
+# 
+# java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
+#    -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+#    -T CombineVariants \
+#    --variant ../bcftools_isec/chr01_1_ref/0000.vcf.gz \
+#    --variant ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01_sorted.vcf.gz \
+#    -o ../bcftools_isec/chr01_1_ref/chr01_2plus_ref.vcf \
+#    --assumeIdenticalSamples
 
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c ../bcftools_isec/chr01_1_ref/chr01_2plus_ref.vcf > ../bcftools_isec/chr01_1_ref/chr01_2plus_ref.vcf.gz
 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01_sorted.vcf.gz
-
-java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
-   -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
-   -T CombineVariants \
-   --variant ../bcftools_isec/chr01_1_ref/0000.vcf.gz \
-   --variant ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01_sorted.vcf.gz \
-   -o ../bcftools_isec/chr01_1_ref/chr01_2plus_ref.vcf \
-   --assumeIdenticalSamples
-
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c ../bcftools_isec/chr01_1_ref/chr01_2plus_ref.vcf > chr01_2plus.vcf.gz
-
-# rm ../bcftools_isec/chr01_1_ref/chr01_2plus_ref.vcf
-# rm ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01.vcf.gz
-# rm ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01.vcf
-# rm ../bcftools_isec/chr01_1_ref/0001.vcf.gz
+rm ../bcftools_isec/chr01_1_ref/chr01_2plus_ref.vcf
+rm ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01.vcf.gz
+rm ../bcftools_isec/chr01_1_ref/FB_ST_sites_chr01.vcf
+rm ../bcftools_isec/chr01_1_ref/0001.vcf
