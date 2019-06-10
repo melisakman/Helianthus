224b8ba1d589b7abffd7bd8d86e14ea930822e75
melisakman <makman@berkeley.edu>
2019-06-10 16:51:25 -0700

    indel filter

--- shells/variant_calling/new/10-combine/2-indels/hardfilter/hardfilter_indel_invariant_chr01.sh
+++ shells/variant_calling/new/10-combine/2-indels/hardfilter/hardfilter_indel_invariant_chr01.sh
@@ -1,14 +1,12 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/fastq/invariants/
-#SBATCH -J filchr01
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=400:00:00
+#SBATCH -D /global/scratch/makman/GATK/sams/gvcfs/genotyping
+#SBATCH -J chr01
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=savio_normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/hardfilter_VCMA_indel_chr01_invariant.out
 #SBATCH -e /global/home/users/makman/GATK/outs/hardfilter_VCMA_indel_chr01_invariant.err
 #SBATCH --mail-user=makman@berkeley.edu
@@ -18,11 +16,17 @@ module load java
 module load gatk/4.0.1.2
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
-# java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
-# 	-R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
-# 	-V VCMA_chr01.vcf.gz \
-# 	-selectType INDEL \
-# 	-o VCMA_chr01_indel.vcf.gz 
+java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
+	-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+	-V chr01_GATK.vcf.gz \
+	-selectType INDEL \
+	-o chr01_GATK_indel.vcf.gz 
+
+/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" VariantAnnotator \
+   -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+   -V chr01_GATK_indel.vcf.gz \
+   -O chr01_GATK_indel_annot.vcf.gz \
+   -A Coverage -A QualByDepth -A FisherStrand -A StrandOddsRatio -A MappingQualityRankSumTest -A ReadPosRankSumTest -A RMSMappingQuality
 
 
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T VariantFiltration \
@@ -30,11 +34,12 @@ java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar
 -V VCMA_chr01_indel.vcf.gz \
 --filterExpression "QD < 2.0 || FS > 200.0 || SOR > 10.0 || ReadPosRankSum < -20.0" \
 --filterName "my_indel_filter" \
--o VCMA_chr01_indel_filterInfo.vcf.gz  
+-o chr01_GATK_indel_annot_filterInfo.vcf.gz  
 	
 java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
 -R /clusterfs/rosalind/users/makman/HanXRQr1.0-20151230.fa \
--V VCMA_chr01_indel_filterInfo.vcf.gz \
+-V chr01_GATK_indel_annot_filterInfo.vcf.gz \
 --excludeFiltered \
--o VCMA_chr01_indel_hardfiltered.vcf.gz 
+-o chr01_GATK_indel_annot_hardfiltered.vcf.gz
 
+zcat chr01_GATK_indel_annot_hardfiltered.vcf.gz | vcftools --vcf - --minDP 1 --max-missing 0.8 --maxDP 30 --recode --stdout | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > chr01_GATK_indel_annot_hardfiltered_secondFilter.vcf.gz
