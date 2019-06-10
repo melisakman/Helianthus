4caa08d21dbedc4be1c9b24f74e3ede43cc3890a
melisakman <makman@berkeley.edu>
2019-06-10 15:24:16 -0700

    trth deneme QD

--- shells/variant_calling/new/7-TruthSet/1-bcftools/bcftools-isec_chr01_QDdeneme.sh
+++ shells/variant_calling/new/7-TruthSet/1-bcftools/bcftools-isec_chr01_QDdeneme.sh
@@ -21,28 +21,28 @@ module load gatk/4.0.1.2
 
 
 
-/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" VariantAnnotator \
-   -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
-   -V gvcfs/genotyping/chr01_GATK_SNP.vcf.gz \
-   -O gvcfs/genotyping/chr01_GATK_SNP_annot.vcf.gz \
-   -A Coverage -A QualByDepth -A FisherStrand -A StrandOddsRatio -A MappingQualityRankSumTest -A ReadPosRankSumTest -A RMSMappingQuality -A InbreedingCoeff
-
-/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" VariantFiltration \
--R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
--V gvcfs/genotyping/chr01_GATK_SNP_annot.vcf.gz \
---filter-expression "QD < 2.0 || MQ < 40.0 || FS > 60.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
---filter-name "my_SNP_filter" \
--O chr01_GATK_SNP_annot_filterInfo.vcf.gz
+# /global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" VariantAnnotator \
+#    -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+#    -V gvcfs/genotyping/chr01_GATK_SNP.vcf.gz \
+#    -O gvcfs/genotyping/chr01_GATK_SNP_annot.vcf.gz \
+#    -A Coverage -A QualByDepth -A FisherStrand -A StrandOddsRatio -A MappingQualityRankSumTest -A ReadPosRankSumTest -A RMSMappingQuality -A InbreedingCoeff
+# 
+# /global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" VariantFiltration \
+# -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+# -V gvcfs/genotyping/chr01_GATK_SNP_annot.vcf.gz \
+# --filter-expression "QD < 2.0 || MQ < 40.0 || FS > 60.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
+# --filter-name "my_SNP_filter" \
+# -O chr01_GATK_SNP_annot_filterInfo.vcf.gz
 	
-/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \
--R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
--V chr01_GATK_SNP_annot_filterInfo.vcf.gz \
---exclude-filtered \
--O chr01_GATK_SNP_annot_filterInfo_hardfiltered.vcf.gz
+# /global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \
+# -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+# -V chr01_GATK_SNP_annot_filterInfo.vcf.gz \
+# --exclude-filtered \
+# -O chr01_GATK_SNP_annot_filterInfo_hardfiltered.vcf.gz
+# 
 
 
-
-bcftools isec -n =3 -O z -p ../bcftools_isec/chr01_3_QD gvcfs/genotyping/chr01_GATK_SNP_annot_filterInfo_hardfiltered.vcf.gz \
+bcftools isec -n =3 -O z -p ../bcftools_isec/chr01_3_QD chr01_GATK_SNP_annot_filterInfo_hardfiltered.vcf.gz \
 freebayes/no_mnp/freebayes_variants_chr01_filtered.vcf.gz \
 samtools/samtools_variants_chr01_filtered.vcf.gz
 
