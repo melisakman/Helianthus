4e44109f9c63421fa201199ec9bdb753272072dd
melisakman <makman@berkeley.edu>
2019-06-11 10:16:26 -0700

    indel filter

--- shells/variant_calling/new/10-combine/1-invariants/less_stringent/invariants_chr01.sh
+++ shells/variant_calling/new/10-combine/1-invariants/less_stringent/invariants_chr01.sh
@@ -61,30 +61,33 @@ module load java
 # 
 # /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf ../bcftools_isec/chr01_2_ref/chr01_2plus_ref.vcf.gz
 
-rm ../bcftools_isec/chr01_2_ref/chr01_2plus_ref.vcf.gz.tbi
+# rm ../bcftools_isec/chr01_2_ref/chr01_2plus_ref.vcf.gz.tbi
+# 
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf ../bcftools_isec/chr01_2_ref/chr01_2plus_ref.vcf.gz
+# 
+# /global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" VariantAnnotator \
+#    -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+#    -V ../bcftools_isec/chr01_2_ref/chr01_2plus_ref.vcf.gz \
+#    -O ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_annot.vcf.gz \
+#    -A Coverage -A QualByDepth -A FisherStrand -A StrandOddsRatio -A MappingQualityRankSumTest -A ReadPosRankSumTest -A RMSMappingQuality
+# 
+# 
+# 
+# java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T VariantFiltration \
+# 	-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+# 	-V ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_annot.vcf.gz \
+# 	--filterExpression "QD < 2.0 || MQ < 40.0 || FS > 60.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
+# 	--filterName "my_NO_VARIATION_filter" \
+# 	-o ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_filterInfo_QDdeneme.vcf.gz  
+# 	
+# java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
+# 	-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+# 	-V ../bcftools_isec/chr01_2_ref/chr01_2plus_ref.vcf.gz \
+# 	--excludeFiltered \
+# 	-o ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_hardfiltered_QDdeneme.vcf.gz 
 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf ../bcftools_isec/chr01_2_ref/chr01_2plus_ref.vcf.gz
-
-/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" VariantAnnotator \
-   -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
-   -V ../bcftools_isec/chr01_2_ref/chr01_2plus_ref.vcf.gz \
-   -O ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_annot.vcf.gz \
-   -A Coverage -A QualByDepth -A FisherStrand -A StrandOddsRatio -A MappingQualityRankSumTest -A ReadPosRankSumTest -A RMSMappingQuality
-
-
-
-java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T VariantFiltration \
-	-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
-	-V ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_annot.vcf.gz \
-	--filterExpression "QD < 2.0 || MQ < 40.0 || FS > 60.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
-	--filterName "my_NO_VARIATION_filter" \
-	-o ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_filterInfo_QDdeneme.vcf.gz  
-	
-java -Djava.io.tmpdir=/clusterfs/rosalind/users/makman/temp_files2/ -Xmx60G -jar /clusterfs/rosalind/users/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar -T SelectVariants \
-	-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
-	-V ../bcftools_isec/chr01_2_ref/chr01_2plus_ref.vcf.gz \
-	--excludeFiltered \
-	-o ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_hardfiltered_QDdeneme.vcf.gz 
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_hardfiltered_QDdeneme.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_hardfiltered.vcf.gz
 
 bcftools isec -n =3 -O z -p ../bcftools_isec/chr01_2_ref_QDdeneme ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_hardfiltered_QDdeneme.vcf.gz \
 ../bcftools_isec/chr01_2_ref/chr01_2plus_ref_hardfiltered.vcf.gz  \	
