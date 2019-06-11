427c0ca12e5e049a7a4d4252a07dc491d6798d94
melisakman <makman@berkeley.edu>
2019-06-11 10:54:48 -0700

    combine

--- shells/variant_calling/new/10-combine/3-combine/combine_chr02.sh
+++ shells/variant_calling/new/10-combine/3-combine/combine_chr02.sh
@@ -21,5 +21,5 @@ module load java
 /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-concat \
  chr02_2plus_annot_G8_VQSR99.vcf.gz \
 ../sams/gvcfs/genotyping/ chr02_GATK_indel_annot_hardfiltered_secondFilter.vcf.gz \
- chr02_2_ref/ chr02_2plus_ref_hardfiltered_secondfilter.vcf.gz | vcf-sort -t /clusterfs/rosalind/users/makman/temp \
+ chr02_2_ref/ chr02_2plus_ref_hardfiltered_secondfilter.vcf.gz | /clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/bin/vcf-sort -t /clusterfs/rosalind/users/makman/temp \
 | /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c > ../final/ chr02_final.vcf.gz
