056fda97446d959be88379227a1f5b941f194077
melisakman <makman@berkeley.edu>
2019-06-18 21:35:03 -0700

    pi average

--- shells/variant_calling/new/11-tests/vcftools_VCMA_pi_persite_avarage.sh
+++ shells/variant_calling/new/11-tests/vcftools_VCMA_pi_persite_avarage.sh
@@ -1,46 +1,51 @@
 #!/bin/bash 
-#SBATCH -D /clusterfs/rosalind/users/makman/GATK/bcftools_isec/secondFilter/
+#SBATCH -D /global/scratch/makman/GATK/final
 #SBATCH -J vcfpi1
-#SBATCH --partition=vector
-#SBATCH --qos=vector_batch
-#SBATCH --time=4:00:00
-#SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_VCMA_pi_average.out
-#SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_VCMA_pi_average.err
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_MA_pi_average.out
+#SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_MA_pi_average.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
 module load python/2.7
-python window-pi.py pi/wd_pi_chr01_10kb.sites.pi 10000 pi/wd_pi_chr01_sites_average.pi pi/wd_pi_chr01_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr02_10kb.sites.pi 10000 pi/wd_pi_chr02_sites_average.pi pi/wd_pi_chr02_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr03_10kb.sites.pi 10000 pi/wd_pi_chr03_sites_average.pi pi/wd_pi_chr03_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr04_10kb.sites.pi 10000 pi/wd_pi_chr04_sites_average.pi pi/wd_pi_chr04_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr05_10kb.sites.pi 10000 pi/wd_pi_chr05_sites_average.pi pi/wd_pi_chr05_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr06_10kb.sites.pi 10000 pi/wd_pi_chr06_sites_average.pi pi/wd_pi_chr06_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr07_10kb.sites.pi 10000 pi/wd_pi_chr07_sites_average.pi pi/wd_pi_chr07_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr08_10kb.sites.pi 10000 pi/wd_pi_chr08_sites_average.pi pi/wd_pi_chr08_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr09_10kb.sites.pi 10000 pi/wd_pi_chr09_sites_average.pi pi/wd_pi_chr09_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr10_10kb.sites.pi 10000 pi/wd_pi_chr10_sites_average.pi pi/wd_pi_chr10_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr11_10kb.sites.pi 10000 pi/wd_pi_chr11_sites_average.pi pi/wd_pi_chr11_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr12_10kb.sites.pi 10000 pi/wd_pi_chr12_sites_average.pi pi/wd_pi_chr12_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr13_10kb.sites.pi 10000 pi/wd_pi_chr13_sites_average.pi pi/wd_pi_chr13_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr14_10kb.sites.pi 10000 pi/wd_pi_chr14_sites_average.pi pi/wd_pi_chr14_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr15_10kb.sites.pi 10000 pi/wd_pi_chr15_sites_average.pi pi/wd_pi_chr15_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr16_10kb.sites.pi 10000 pi/wd_pi_chr16_sites_average.pi pi/wd_pi_chr16_sites_averageNA.pi
-python window-pi.py pi/wd_pi_chr17_10kb.sites.pi 10000 pi/wd_pi_chr17_sites_average.pi pi/wd_pi_chr17_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr01_10kb.sites.pi 10000 pi/lr_pi_chr01_sites_average.pi pi/lr_pi_chr01_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr02_10kb.sites.pi 10000 pi/lr_pi_chr02_sites_average.pi pi/lr_pi_chr02_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr03_10kb.sites.pi 10000 pi/lr_pi_chr03_sites_average.pi pi/lr_pi_chr03_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr04_10kb.sites.pi 10000 pi/lr_pi_chr04_sites_average.pi pi/lr_pi_chr04_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr05_10kb.sites.pi 10000 pi/lr_pi_chr05_sites_average.pi pi/lr_pi_chr05_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr06_10kb.sites.pi 10000 pi/lr_pi_chr06_sites_average.pi pi/lr_pi_chr06_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr07_10kb.sites.pi 10000 pi/lr_pi_chr07_sites_average.pi pi/lr_pi_chr07_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr08_10kb.sites.pi 10000 pi/lr_pi_chr08_sites_average.pi pi/lr_pi_chr08_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr09_10kb.sites.pi 10000 pi/lr_pi_chr09_sites_average.pi pi/lr_pi_chr09_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr10_10kb.sites.pi 10000 pi/lr_pi_chr10_sites_average.pi pi/lr_pi_chr10_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr11_10kb.sites.pi 10000 pi/lr_pi_chr11_sites_average.pi pi/lr_pi_chr11_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr12_10kb.sites.pi 10000 pi/lr_pi_chr12_sites_average.pi pi/lr_pi_chr12_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr13_10kb.sites.pi 10000 pi/lr_pi_chr13_sites_average.pi pi/lr_pi_chr13_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr14_10kb.sites.pi 10000 pi/lr_pi_chr14_sites_average.pi pi/lr_pi_chr14_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr15_10kb.sites.pi 10000 pi/lr_pi_chr15_sites_average.pi pi/lr_pi_chr15_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr16_10kb.sites.pi 10000 pi/lr_pi_chr16_sites_average.pi pi/lr_pi_chr16_sites_averageNA.pi
-python window-pi.py pi/lr_pi_chr17_10kb.sites.pi 10000 pi/lr_pi_chr17_sites_average.pi pi/lr_pi_chr17_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr01_10kb.sites.pi 10000 pi/wd_pi_chr01_sites_average.pi pi/wd_pi_chr01_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr02_10kb.sites.pi 10000 pi/wd_pi_chr02_sites_average.pi pi/wd_pi_chr02_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr03_10kb.sites.pi 10000 pi/wd_pi_chr03_sites_average.pi pi/wd_pi_chr03_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr04_10kb.sites.pi 10000 pi/wd_pi_chr04_sites_average.pi pi/wd_pi_chr04_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr05_10kb.sites.pi 10000 pi/wd_pi_chr05_sites_average.pi pi/wd_pi_chr05_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr06_10kb.sites.pi 10000 pi/wd_pi_chr06_sites_average.pi pi/wd_pi_chr06_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr07_10kb.sites.pi 10000 pi/wd_pi_chr07_sites_average.pi pi/wd_pi_chr07_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr08_10kb.sites.pi 10000 pi/wd_pi_chr08_sites_average.pi pi/wd_pi_chr08_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr09_10kb.sites.pi 10000 pi/wd_pi_chr09_sites_average.pi pi/wd_pi_chr09_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr10_10kb.sites.pi 10000 pi/wd_pi_chr10_sites_average.pi pi/wd_pi_chr10_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr11_10kb.sites.pi 10000 pi/wd_pi_chr11_sites_average.pi pi/wd_pi_chr11_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr12_10kb.sites.pi 10000 pi/wd_pi_chr12_sites_average.pi pi/wd_pi_chr12_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr13_10kb.sites.pi 10000 pi/wd_pi_chr13_sites_average.pi pi/wd_pi_chr13_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr14_10kb.sites.pi 10000 pi/wd_pi_chr14_sites_average.pi pi/wd_pi_chr14_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr15_10kb.sites.pi 10000 pi/wd_pi_chr15_sites_average.pi pi/wd_pi_chr15_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr16_10kb.sites.pi 10000 pi/wd_pi_chr16_sites_average.pi pi/wd_pi_chr16_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/wd_pi_chr17_10kb.sites.pi 10000 pi/wd_pi_chr17_sites_average.pi pi/wd_pi_chr17_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr01_10kb.sites.pi 10000 pi/lr_pi_chr01_sites_average.pi pi/lr_pi_chr01_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr02_10kb.sites.pi 10000 pi/lr_pi_chr02_sites_average.pi pi/lr_pi_chr02_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr03_10kb.sites.pi 10000 pi/lr_pi_chr03_sites_average.pi pi/lr_pi_chr03_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr04_10kb.sites.pi 10000 pi/lr_pi_chr04_sites_average.pi pi/lr_pi_chr04_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr05_10kb.sites.pi 10000 pi/lr_pi_chr05_sites_average.pi pi/lr_pi_chr05_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr06_10kb.sites.pi 10000 pi/lr_pi_chr06_sites_average.pi pi/lr_pi_chr06_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr07_10kb.sites.pi 10000 pi/lr_pi_chr07_sites_average.pi pi/lr_pi_chr07_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr08_10kb.sites.pi 10000 pi/lr_pi_chr08_sites_average.pi pi/lr_pi_chr08_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr09_10kb.sites.pi 10000 pi/lr_pi_chr09_sites_average.pi pi/lr_pi_chr09_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr10_10kb.sites.pi 10000 pi/lr_pi_chr10_sites_average.pi pi/lr_pi_chr10_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr11_10kb.sites.pi 10000 pi/lr_pi_chr11_sites_average.pi pi/lr_pi_chr11_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr12_10kb.sites.pi 10000 pi/lr_pi_chr12_sites_average.pi pi/lr_pi_chr12_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr13_10kb.sites.pi 10000 pi/lr_pi_chr13_sites_average.pi pi/lr_pi_chr13_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr14_10kb.sites.pi 10000 pi/lr_pi_chr14_sites_average.pi pi/lr_pi_chr14_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr15_10kb.sites.pi 10000 pi/lr_pi_chr15_sites_average.pi pi/lr_pi_chr15_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr16_10kb.sites.pi 10000 pi/lr_pi_chr16_sites_average.pi pi/lr_pi_chr16_sites_averageNA.pi
+python git/Helianthus/shells/variant_calling/new/11-tests/window-pi.py pi/lr_pi_chr17_10kb.sites.pi 10000 pi/lr_pi_chr17_sites_average.pi pi/lr_pi_chr17_sites_averageNA.pi
---
+++ shells/variant_calling/new/11-tests/window-pi.py
@@ -0,0 +1,54 @@
+# This script is created for calculating pi including invariant sites a window defined by user. 
+# The input is the pi as computed by vcftools site-pi.
+import sys
+inputFile = sys.argv[1] # pi file as created by vcftools
+bin = sys.argv[2] # bin length
+outputFile1 = sys.argv[3] # output file name w/o NA
+outputFile2 = sys.argv[4] # output file name w NA
+
+file_in = open(inputFile, "r")
+file_out1= open(outputFile1, "w")
+file_out2= open(outputFile2, "w")
+
+file_out1.write("chr\tbin-end\tno_sites\tno_snp\tpi\n")
+file_out2.write("chr\tbin-end\tno_sites\tno_snp\tpi\n")
+
+pi_sum = 0
+bin_start = 0
+current_bin = int(bin)
+counter = 0;
+first_line = True;
+snp_no = 0
+for line in file_in:
+	split = line.split("\t")
+	if split[0] == "CHROM":
+		continue
+	else:
+		pos_start = int(split[1])
+		chr = split[0]		
+		pi = float(split[2])
+		if first_line == True:
+			first_line = False
+			current_bin = pos_start + int(bin)
+		
+		if pos_start <= current_bin:
+			pi_sum += pi
+			counter += 1
+			if pi != 0:
+				snp_no += 1
+			
+		else:
+			if counter != 0:
+				pi_avg = float(pi_sum) / counter
+				file_out1.write(str(chr) + "\t" + str(current_bin) + "\t" + str(counter) + "\t" + str(snp_no) + "\t" + str(pi_avg) + "\n")
+			else:
+				pi_avg = "NA"
+				file_out2.write(str(chr) + "\t" + str(current_bin) + "\t" + str(counter) + "\t" + str(snp_no) + "\t" + str(pi_avg) + "\n")
+
+			counter = 0
+			pi_sum = 0
+			current_bin += int(bin)
+			snp_no = 0
+if counter >= 0:
+	pi_avg = float(pi_sum) / counter		
+	file_out1.write(str(chr) + "\t" + str(current_bin) + "\t" + str(counter) + "\t" + str(snp_no) + "\t" + str(pi_avg) + "\n")
