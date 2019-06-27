6b0f90d87002ce3476c65761177272067540bea4
melisakman <makman@berkeley.edu>
2019-06-27 14:20:48 -0700

    stats

--- shells/variant_calling/new/11-tests/vcftools_MA_stats.sh
+++ shells/variant_calling/new/11-tests/vcftools_MA_stats.sh
@@ -1,12 +1,14 @@
 #!/bin/bash 
 #SBATCH -D /global/scratch/makman/GATK/final/
 #SBATCH -J stats
-#SBATCH --account=fc_blackman
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=savio_normal
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --time=72:00:00
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_MA_stats.out
 #SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_MA_stats.err
 #SBATCH --mail-user=makman@berkeley.edu
