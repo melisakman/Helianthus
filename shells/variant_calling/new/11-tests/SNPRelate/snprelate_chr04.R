1b8ba9ca59ac2ee474028f68b928c62f88951ed1
melisakman <makman@berkeley.edu>
2019-06-28 16:00:18 -0700

    snpRelate

---
+++ shells/variant_calling/new/11-tests/SNPRelate/snprelate_chr04.R
@@ -0,0 +1,41 @@
+
+# 
+# install.packages("gdsfmt")
+# source("https://bioconductor.org/biocLite.R")
+# biocLite("SNPRelate")
+
+library(gdsfmt)
+library(SNPRelate)
+setwd("/global/scratch/makman/GATK/final/")
+
+vcf_chr04.fn = "../bcftools_isec/chr04_2plus_annot_G8_VQSR99.vcf.gz"
+snpgdsVCF2GDS(vcf_chr04.fn, "MA_chr04.gds", method="biallelic.only")
+snpgdsSummary("MA_chr04.gds")
+genofile <- snpgdsOpen("MA_chr04.gds")
+
+
+set.seed(1000)
+# Try different LD thresholds for sensitivity analysis
+snpset <- snpgdsLDpruning(genofile, ld.threshold=0.2)
+names(snpset)
+snpset.id <- unlist(snpset)
+
+
+
+pca <- snpgdsPCA(genofile, snp.id=snpset.id, num.thread=2)
+pc.percent <- pca$varprop*100
+tab <- data.frame(sample.id = pca$sample.id, EV1 = pca$eigenvect[,1],    # the first eigenvector
+	EV2 = pca$eigenvect[,2],    # the second eigenvector
+	EV3 = pca$eigenvect[,3],
+	EV4 = pca$eigenvect[,4],
+	EV5 = pca$eigenvect[,5],
+	EV6 = pca$eigenvect[,6],
+	EV7 = pca$eigenvect[,7],
+	stringsAsFactors = FALSE)
+head(tab)
+
+write.table(tab, file = "PCA_chr04.csv")
+
+
+## Sariel merged
+
---
+++ shells/variant_calling/new/11-tests/SNPRelate/snprelate_chr11.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/GATK/final/
+#SBATCH -J SNPRelate
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/vcftools/outs/MA_snpRelate_chr11.out
+#SBATCH -e /global/home/users/makman/vcftools/outs/MA_snpRelate_chr11.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load r/3.4.2
+
+Rscript /global/home/users/makman/git/Helianthus/shells/variant_calling/new/11-tests/snprelate_chr11.R
