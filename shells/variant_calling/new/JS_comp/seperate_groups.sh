edd199838a959e804c16f571b69d32e3871ab48b
melisakman <makman@berkeley.edu>
2019-06-30 23:28:49 -0700

    seperate groups stats

--- shells/variant_calling/new/JS_comp/seperate_groups.sh
+++ shells/variant_calling/new/JS_comp/seperate_groups.sh
@@ -2,11 +2,13 @@
 #SBATCH -D /global/scratch/makman/GATK/bcftools_isec/
 #SBATCH -J group
 #SBATCH --account=co_rosalind
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=rosalind_htc2_normal
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=160:00:00
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/GATK/outs/seperate_groups_JS_GATK.out
 #SBATCH -e /global/home/users/makman/GATK/outs/seperate_groups_JS_GATK.err
 #SBATCH --mail-user=makman@berkeley.edu
@@ -24,18 +26,18 @@ module load python
 # chr01_1_JS_GATK_VQSR_noVQSR/0002.vcf.gz \
 # chr01_1_JS_GATK_VQSR_noVQSR/0003.vcf.gz > chr01_1_JS_GATK_VQSR_noVQSR/all.vcf
 
-python /global/home/users/makman/git/Helianthus/shells/variant_calling/new/JS_comp/seperate_groups.py chr01_1_JS_GATK_VQSR_noVQSR/sites.txt chr01_1_JS_GATK_VQSR_noVQSR/all.vcf
+# python /global/home/users/makman/git/Helianthus/shells/variant_calling/new/JS_comp/seperate_groups.py chr01_1_JS_GATK_VQSR_noVQSR/sites.txt chr01_1_JS_GATK_VQSR_noVQSR/all.vcf
 
-bcftools stats chr01_1_JS_GATK_VQSR_noVQSR/JS_VQSR.vcf > chr01_1_JS_GATK_VQSR_noVQSR/JS_VQSR.vchk
-bcftools stats chr01_1_JS_GATK_VQSR_noVQSR/JS_GATK_VQSR.vcf > chr01_1_JS_GATK_VQSR_noVQSR/JS_GATK_VQSR.vchk
-bcftools stats chr01_1_JS_GATK_VQSR_noVQSR/MA_VQSR.vcf > chr01_1_JS_GATK_VQSR_noVQSR/MA_VQSR.vchk
-bcftools stats chr01_1_JS_GATK_VQSR_noVQSR/JS_noVQSR.vcf > chr01_1_JS_GATK_VQSR_noVQSR/JS_noVQSR.vchk
-bcftools stats chr01_1_JS_GATK_VQSR_noVQSR/MA_noVQSR.vcf > chr01_1_JS_GATK_VQSR_noVQSR/MA_noVQSR.vchk
+bcftools stats JS_VQSR.vcf > chr01_1_JS_GATK_VQSR_noVQSR/JS_VQSR.vchk
+bcftools stats JS_GATK_VQSR.vcf > chr01_1_JS_GATK_VQSR_noVQSR/JS_GATK_VQSR.vchk
+bcftools stats MA_VQSR.vcf > chr01_1_JS_GATK_VQSR_noVQSR/MA_VQSR.vchk
+bcftools stats JS_noVQSR.vcf > chr01_1_JS_GATK_VQSR_noVQSR/JS_noVQSR.vchk
+bcftools stats MA_noVQSR.vcf > chr01_1_JS_GATK_VQSR_noVQSR/MA_noVQSR.vchk
 
-plot-vcfstats JS_VQSR.vchk -p chr01_1_JS_GATK_VQSR_noVQSR/plot_JS_VQSR
-plot-vcfstats JS_GATK_VQSR.vchk -p chr01_1_JS_GATK_VQSR_noVQSR/plot_JS_GATK_VQSR
-plot-vcfstats MA_VQSR.vchk -p chr01_1_JS_GATK_VQSR_noVQSR/plot_MA_VQSR
-plot-vcfstats JS_noVQSR.vchk -p chr01_1_JS_GATK_VQSR_noVQSR/plot_JS_noVQSR
-plot-vcfstats MA_noVQSR.vchk -p chr01_1_JS_GATK_VQSR_noVQSR/plot_MA_noVQSR
+plot-vcfstats chr01_1_JS_GATK_VQSR_noVQSR/JS_VQSR.vchk -p chr01_1_JS_GATK_VQSR_noVQSR/plot_JS_VQSR
+plot-vcfstats chr01_1_JS_GATK_VQSR_noVQSR/JS_GATK_VQSR.vchk -p chr01_1_JS_GATK_VQSR_noVQSR/plot_JS_GATK_VQSR
+plot-vcfstats chr01_1_JS_GATK_VQSR_noVQSR/MA_VQSR.vchk -p chr01_1_JS_GATK_VQSR_noVQSR/plot_MA_VQSR
+plot-vcfstats chr01_1_JS_GATK_VQSR_noVQSR/JS_noVQSR.vchk -p chr01_1_JS_GATK_VQSR_noVQSR/plot_JS_noVQSR
+plot-vcfstats chr01_1_JS_GATK_VQSR_noVQSR/MA_noVQSR.vchk -p chr01_1_JS_GATK_VQSR_noVQSR/plot_MA_noVQSR
 
 
