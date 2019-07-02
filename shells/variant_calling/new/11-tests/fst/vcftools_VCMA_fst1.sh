87016d013be215871e88d28d278d5a9ddd29b536
melisakman <makman@berkeley.edu>
2019-07-02 14:51:20 -0700

    tests

--- shells/variant_calling/new/11-tests/fst/vcftools_VCMA_fst1.sh
+++ shells/variant_calling/new/11-tests/fst/vcftools_VCMA_fst1.sh
@@ -1,16 +1,13 @@
 #!/bin/bash 
 #SBATCH -D /global/scratch/makman/GATK/final
 #SBATCH -J vcffst
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=400:00:00
-#SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_MA_fst.out
-#SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_MA_fst.err
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --cpus-per-task=4
+#SBATCH --time=72:00:00
+#SBATCH -o /global/home/users/makman/vcftools/outs/vcftools_MA_fst_chr4tests.out
+#SBATCH -e /global/home/users/makman/vcftools/outs/vcftools_MA_fst_chr4tests.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
@@ -24,14 +21,15 @@ module load bio/vcftools/0.1.15
 # vcftools --gzvcf chr06_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr06.bed --out fst/lr_wd_chr06_10kb
 # vcftools --gzvcf chr07_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr07.bed --out fst/lr_wd_chr07_10kb
 # vcftools --gzvcf chr08_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr08.bed --out fst/lr_wd_chr08_10kb
-vcftools --gzvcf chr09_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr09.bed --out fst/lr_wd_chr09_10kb
-vcftools --gzvcf chr10_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr10.bed --out fst/lr_wd_chr10_10kb
-vcftools --gzvcf chr11_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr11.bed --out fst/lr_wd_chr11_10kb
-vcftools --gzvcf chr12_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr12.bed --out fst/lr_wd_chr12_10kb
+# vcftools --gzvcf chr09_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr09.bed --out fst/lr_wd_chr09_10kb
+# vcftools --gzvcf chr10_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr10.bed --out fst/lr_wd_chr10_10kb
+# vcftools --gzvcf chr11_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr11.bed --out fst/lr_wd_chr11_10kb
+# vcftools --gzvcf chr12_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr12.bed --out fst/lr_wd_chr12_10kb
 # vcftools --gzvcf chr13_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr13.bed --out fst/lr_wd_chr13_10kb
 # vcftools --gzvcf chr14_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr14.bed --out fst/lr_wd_chr14_10kb
 # vcftools --gzvcf chr15_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr15.bed --out fst/lr_wd_chr15_10kb
 # vcftools --gzvcf chr16_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr16.bed --out fst/lr_wd_chr16_10kb
 # vcftools --gzvcf chr17_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --exclude-bed Repeats_chr17.bed --out fst/lr_wd_chr17_10kb
 
-# vcftools --gzvcf chr04_final.vcf.gz --weir-fst-pop wd_list.txt --weir-fst-pop lr_list_noHopi.txt --fst-window-size 10000 --out fst/lr_wd_chr04_10kb_noHopi
+vcftools --gzvcf chr04_final.vcf.gz --weir-fst-pop wd_list_noNM.txt --weir-fst-pop lr_list_noHopi.txt --fst-window-size 10000 --out fst/lr_wd_chr04_10kb_noHopinoNM
+vcftools --gzvcf chr04_final.vcf.gz --weir-fst-pop wd_list_noNM.txt --weir-fst-pop lr_list.txt --fst-window-size 10000 --out fst/lr_wd_chr04_10kb_noNM
