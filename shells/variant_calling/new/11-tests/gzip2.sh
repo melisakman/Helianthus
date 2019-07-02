f9dd66fdceb6f116475c27877943a5a6dd313ff9
melisakman <makman@berkeley.edu>
2019-07-02 13:28:40 -0700

    G12

--- shells/variant_calling/new/11-tests/gzip2.sh
+++ shells/variant_calling/new/11-tests/gzip2.sh
@@ -13,15 +13,15 @@
 #SBATCH -e /global/home/users/makman/vcftools/outs/gzip1.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-gzip chr04_final.vcf
-gzip chr05_final.vcf
-gzip chr06_final.vcf
-gzip chr07_final.vcf
-gzip chr08_final.vcf
-# gzip chr09_final.vcf
-# gzip chr10_final.vcf
-# gzip chr11_final.vcf
-# gzip chr12_final.vcf
+# gzip chr04_final.vcf
+# gzip chr05_final.vcf
+# gzip chr06_final.vcf
+# gzip chr07_final.vcf
+# gzip chr08_final.vcf
+gzip chr09_final.vcf
+gzip chr10_final.vcf
+gzip chr11_final.vcf
+gzip chr12_final.vcf
 # gzip chr13_final.vcf
 # gzip chr14_final.vcf
 # gzip chr15_final.vcf
--- shells/variant_calling/new/11-tests/gzip3.sh
+++ shells/variant_calling/new/11-tests/gzip3.sh
@@ -13,17 +13,17 @@
 #SBATCH -e /global/home/users/makman/vcftools/outs/gzip1.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-gzip chr04_final.vcf
-gzip chr05_final.vcf
-gzip chr06_final.vcf
-gzip chr07_final.vcf
-gzip chr08_final.vcf
+# gzip chr04_final.vcf
+# gzip chr05_final.vcf
+# gzip chr06_final.vcf
+# gzip chr07_final.vcf
+# gzip chr08_final.vcf
 # gzip chr09_final.vcf
 # gzip chr10_final.vcf
 # gzip chr11_final.vcf
 # gzip chr12_final.vcf
-# gzip chr13_final.vcf
-# gzip chr14_final.vcf
-# gzip chr15_final.vcf
-# gzip chr16_final.vcf
-# gzip chr17_final.vcf
+gzip chr13_final.vcf
+gzip chr14_final.vcf
+gzip chr15_final.vcf
+gzip chr16_final.vcf
+gzip chr17_final.vcf
