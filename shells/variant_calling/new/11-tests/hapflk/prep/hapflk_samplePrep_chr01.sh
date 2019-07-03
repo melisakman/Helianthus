50dd4b73692e03fb2e2a1e43b750538095c654c4
melisakman <makman@berkeley.edu>
2019-07-03 15:03:05 -0700

    hapflk

--- shells/variant_calling/new/11-tests/hapflk/prep/hapflk_samplePrep_chr01.sh
+++ shells/variant_calling/new/11-tests/hapflk/prep/hapflk_samplePrep_chr01.sh
@@ -20,26 +20,31 @@ module load gatk/4.0.1.2
 module load bio/vcftools/0.1.15
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 
-gunzip chr01_final.vcf.gz
+# gunzip chr01_final.vcf.gz
+# 
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_final.vcf > chr01_final.vcf.gz
+# 
+# /clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf chr01_final.vcf.gz
+# 
+# /global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \
+# -R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+# -V chr01_final.vcf.gz \
+# --select-type-to-include SNP \
+# --select-type-to-include INDEL \
+# -O chr01_SNP_INDEL.vcf.gz
+# 
+# gunzip chr01_SNP_INDEL.vcf.gz
+# 
+# sed 's/HanXRQChr//g' chr01_SNP_INDEL.vcf > chr01_SNP_INDEL_noHanXRQ.vcf
+# 
+# vcftools --vcf chr01_SNP_INDEL_noHanXRQ.vcf --out hapflk/chr01_final_plink  --plink
+# 
+# python /global/home/users/makman/hapflk-1.4/scripts_Hannah/plink_ped_fixer.py all_samples_list.txt hapflk/chr01_final_plink.ped hapflk/chr01_final_plink_fixed.ped
+# 
+# cp hapflk/chr01_final_plink.map chr01_final_plink_fixed.map
 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr01_final.vcf > chr01_final.vcf.gz
+mv chr01_final_plink_fixed.map hapflk
 
-/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf chr01_final.vcf.gz
+cd hapflk
 
-/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \
--R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
--V chr01_final.vcf.gz \
---select-type-to-include SNP \
---select-type-to-include INDEL \
--O chr01_SNP_INDEL.vcf.gz
-
-gunzip chr01_SNP_INDEL.vcf.gz
-
-sed 's/HanXRQChr//g' chr01_SNP_INDEL.vcf > chr01_SNP_INDEL_noHanXRQ.vcf
-
-vcftools --vcf chr01_SNP_INDEL_noHanXRQ.vcf --out hapflk/chr01_final_plink  --plink
-
-python /global/home/users/makman/hapflk-1.4/scripts_Hannah/plink_ped_fixer.py all_samples_list.txt hapflk/chr01_final_plink.ped hapflk/chr01_final_plink_fixed.ped
-
-cp hapflk/chr01_final_plink.map chr01_final_plink_fixed.map
-
+hapflk --file chr01_final_plink_fixed --outgroup=Wild --miss_pheno 0 -p kinship/chr01_tree_kinship --ncpu 20
