9a094e7ba5a45f786c0fd047e86163326a0e3138
melisakman <makman@berkeley.edu>
2019-07-03 17:32:07 -0700

    hapflk

---
+++ shells/variant_calling/new/11-tests/hapflk/prep/hapflk_samplePrep_chr07.sh
@@ -0,0 +1,50 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/GATK/final
+#SBATCH -J hfchr07
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/hapflk_chr07.out
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_chr07.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load gcc/4.8.5 
+module load java
+module load gatk/4.0.1.2
+module load bio/vcftools/0.1.15
+module load hapflk/1.4
+export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
+
+# gunzip chr07_final.vcf.gz
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr07_final.vcf > chr07_final.vcf.gz
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf chr07_final.vcf.gz
+
+/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \
+-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+-V chr07_final.vcf.gz \
+--select-type-to-include SNP \
+--select-type-to-include INDEL \
+-O chr07_SNP_INDEL.vcf.gz
+
+gunzip chr07_SNP_INDEL.vcf.gz
+
+sed 's/HanXRQChr//g' chr07_SNP_INDEL.vcf > chr07_SNP_INDEL_noHanXRQ.vcf
+
+vcftools --vcf chr07_SNP_INDEL_noHanXRQ.vcf --out hapflk/chr07_final_plink  --plink
+
+python /global/home/users/makman/hapflk-1.4/scripts_Hannah/plink_ped_fixer.py hapflk/lr_mex_ethno_list.txt hapflk/chr07_final_plink.ped hapflk/chr07_final_plink_lr_mex_ethno_fixed.ped
+
+cp hapflk/chr07_final_plink.map hapflk/chr07_final_plink_lr_mex_ethno_fixed.map
+
+
+cd hapflk
+
+hapflk --file chr07_final_plink_lr_mex_ethno_fixed --outgroup=Wild --miss_pheno 0 -p kinship/chr07_tree_lr_mex_ethno_kinship --ncpu 20
---
+++ shells/variant_calling/new/11-tests/hapflk/prep/hapflk_samplePrep_chr12.sh
@@ -0,0 +1,50 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/GATK/final
+#SBATCH -J hfchr12
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/hapflk_chr12.out
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_chr12.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load gcc/4.8.5 
+module load java
+module load gatk/4.0.1.2
+module load bio/vcftools/0.1.15
+module load hapflk/1.4
+export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
+
+# gunzip chr12_final.vcf.gz
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr12_final.vcf > chr12_final.vcf.gz
+
+/clusterfs/rosalind/users/makman/tabix-0.2.6/tabix -p vcf chr12_final.vcf.gz
+
+/global/scratch/makman/gatk-4.1.0.0/gatk --java-options "-Xmx64g -Djava.io.tmpdir=/global/scratch/makman/temp_files/" SelectVariants \
+-R /clusterfs/rosalind/users/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+-V chr12_final.vcf.gz \
+--select-type-to-include SNP \
+--select-type-to-include INDEL \
+-O chr12_SNP_INDEL.vcf.gz
+
+gunzip chr12_SNP_INDEL.vcf.gz
+
+sed 's/HanXRQChr//g' chr12_SNP_INDEL.vcf > chr12_SNP_INDEL_noHanXRQ.vcf
+
+vcftools --vcf chr12_SNP_INDEL_noHanXRQ.vcf --out hapflk/chr12_final_plink  --plink
+
+python /global/home/users/makman/hapflk-1.4/scripts_Hannah/plink_ped_fixer.py hapflk/lr_mex_ethno_list.txt hapflk/chr12_final_plink.ped hapflk/chr12_final_plink_lr_mex_ethno_fixed.ped
+
+cp hapflk/chr12_final_plink.map hapflk/chr12_final_plink_lr_mex_ethno_fixed.map
+
+
+cd hapflk
+
+hapflk --file chr12_final_plink_lr_mex_ethno_fixed --outgroup=Wild --miss_pheno 0 -p kinship/chr12_tree_lr_mex_ethno_kinship --ncpu 20
