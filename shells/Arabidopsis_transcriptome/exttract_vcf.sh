2006a3f10838bd868bd7ffaed04504d2631da678
melisakman <makman@berkeley.edu>
2019-07-09 12:44:26 -0700

    At vcf extract

--- shells/Arabidopsis_transcriptome/exttract_vcf.sh
+++ shells/Arabidopsis_transcriptome/exttract_vcf.sh
@@ -15,4 +15,4 @@
 
 module load bio/vcftools/0.1.15
 
-vcftools --vcf testy.recode.vcf --bed topMM_genes_5KupDownStream.bed --out testy_topMM_5KupDownStream.recode.vcf 
+vcftools --vcf testy.recode.vcf --bed topMM_genes_5KupDownStream.bed --recode --out testy_topMM_5KupDownStream.recode.vcf 
