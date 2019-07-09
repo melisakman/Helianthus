d055d7b9d9d84a7b7fb748129ed8399edc3ed5a8
melisakman <makman@berkeley.edu>
2019-07-09 00:31:31 -0700

    hapflk

--- shells/variant_calling/new/11-tests/hapflk/hapflk/iterate_htc.py
+++ shells/variant_calling/new/11-tests/hapflk/hapflk/iterate_htc.py
@@ -2,12 +2,12 @@ import sys
 
 chr = sys.argv[1]
 chr_length = int(sys.argv[2])
-bins = int(chr_length/8000000) + 2
+bins = int(chr_length/4000000) + 2
 
 iterations = range(1,bins)
 
 start = 1
-end = 8000000
+end = 4000000
 
 for i in iterations:
 	file_name = "hapflk_VQSR_chr" + str(chr) + "_" + str(i) + ".sh"
@@ -27,7 +27,7 @@ for i in iterations:
 #SBATCH --time=600:00:00
 module load hapflk/1.4
 hapflk --file chr""" + str(chr) + "_final_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr " + str(chr) + " --from " + str(start) + " --to " + str(end) + " -p chr" + str(chr) + "_" + str(i) + " --ncpu 12 -K 15")
-	start += 8000000
-	end += 8000000
+	start += 4000000
+	end += 4000000
 
 
