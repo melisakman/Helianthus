e0cecb00f4a1246898b7c306c85a4c621045f552
melisakman <makman@berkeley.edu>
2019-07-10 12:38:06 -0700

    hapflk only SNP

--- shells/variant_calling/new/11-tests/hapflk/hapflk/hapflk_VQSR_chr01_8.sh
+++ shells/variant_calling/new/11-tests/hapflk/hapflk/hapflk_VQSR_chr01_8.sh
@@ -2,14 +2,13 @@
 #SBATCH -D /global/scratch/makman/GATK/final/hapflk/
 #SBATCH -J hfchr01_8
 #SBATCH --account=co_rosalind
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=12 
+#SBATCH --mem=64000
 #SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr01_8.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 #SBATCH --time=600:00:00
 module load hapflk/1.4
-hapflk --file chr01_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr 01 --from 70000001 --to 80000000 -p chr01_8 --ncpu 12 -K 15
+hapflk --file chr01_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr 01 --from 140000001 --to 160000000 -p chr01_8 --ncpu 20 -K 15
