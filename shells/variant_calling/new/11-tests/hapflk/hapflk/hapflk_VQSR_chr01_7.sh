f33673775374dfd54f4fd416df46799031ad71f5
melisakman <makman@berkeley.edu>
2019-07-10 15:38:16 -0700

    hapflk only SNP

--- shells/variant_calling/new/11-tests/hapflk/hapflk/hapflk_VQSR_chr01_7.sh
+++ shells/variant_calling/new/11-tests/hapflk/hapflk/hapflk_VQSR_chr01_7.sh
@@ -12,4 +12,4 @@
 #SBATCH --mail-type=All
 #SBATCH --time=72:00:00
 module load hapflk/1.4
-hapflk --file chr01_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr 01 --from 60000001 --to 70000000 -p chr01_7 --ncpu 18 -K 15
+hapflk --file chr01_SNP_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr 01 --from 60000001 --to 70000000 -p chr01_7 --ncpu 18 -K 15
