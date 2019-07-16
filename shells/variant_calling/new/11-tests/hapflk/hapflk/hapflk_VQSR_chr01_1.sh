aaeb9e8bb0531242228006915ec4aecdfb907328
melisakman <makman@berkeley.edu>
2019-07-16 11:39:06 -0700

    hapflk temp dir

--- shells/variant_calling/new/11-tests/hapflk/hapflk/hapflk_VQSR_chr01_1.sh
+++ shells/variant_calling/new/11-tests/hapflk/hapflk/hapflk_VQSR_chr01_1.sh
@@ -6,10 +6,12 @@
 #SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=1
 #SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
 #SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr01_1.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-#SBATCH --time=600:00:00
 module load hapflk/1.4
 export TMPDIR=/global/scratch/makman/temp
 
