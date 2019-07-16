2c06f0d7c6f67dcc66c35ebd56826f05b312ad39
melisakman <makman@berkeley.edu>
2019-07-16 11:17:13 -0700

    CLR wilds

---
+++ shells/variant_calling/new/11-tests/sweep_finder2/variant/wd/chr05.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/GATK/final/
+#SBATCH -J sf_chr05
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/sf2chr05.out
+#SBATCH -e /global/home/users/makman/H12/outs/sf2chr05.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+
+/clusterfs/rosalind/users/makman/SF2/SweepFinder2 -lg 10000 sweep_finder/chr05_SNP.SF sweep_finder/all_spectFile.txt sweep_finder/chr05_SNP_output.txt
