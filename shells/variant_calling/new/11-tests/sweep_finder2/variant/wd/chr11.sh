5505df8685683553828aa445e460f1534733e45c
melisakman <makman@berkeley.edu>
2019-07-16 16:21:03 -0700

    clr wd

--- shells/variant_calling/new/11-tests/sweep_finder2/variant/wd/chr11.sh
+++ shells/variant_calling/new/11-tests/sweep_finder2/variant/wd/chr11.sh
@@ -1,14 +1,15 @@
 #!/bin/bash 
 #SBATCH -D /global/scratch/makman/GATK/final/
 #SBATCH -J sf_chr11
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
 #SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=400:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/H12/outs/sf2chr11.out
 #SBATCH -e /global/home/users/makman/H12/outs/sf2chr11.err
 #SBATCH --mail-user=makman@berkeley.edu
