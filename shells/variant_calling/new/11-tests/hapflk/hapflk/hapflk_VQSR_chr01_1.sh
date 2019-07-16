96a5ae5e50227d6f46e7a1c347f856a97a6b98dc
melisakman <makman@berkeley.edu>
2019-07-16 11:33:11 -0700

    hapflk temp dir

--- shells/variant_calling/new/11-tests/hapflk/hapflk/hapflk_VQSR_chr01_1.sh
+++ shells/variant_calling/new/11-tests/hapflk/hapflk/hapflk_VQSR_chr01_1.sh
@@ -6,11 +6,11 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24 
+#SBATCH --cpus-per-task=12 
 #SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr01_1.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 #SBATCH --time=72:00:00
 module load hapflk/1.4
 export TMPDIR=/global/scratch/makman/temp
-hapflk --file chr01_SNP_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr 01 --from 1 --to 1000000 -p chr01_1 --ncpu 24 -K 15
+hapflk --file chr01_SNP_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr 01 --from 1 --to 1000000 -p chr01_1 --ncpu 12 -K 15
