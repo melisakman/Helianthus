bd42b69cbe717fa2c9903401fc9d30ec21e3644b
melisakman <makman@berkeley.edu>
2019-07-17 11:52:37 -0700

    hapflk

--- shells/variant_calling/new/11-tests/hapflk/hapflk/hapflk_VQSR_chr01_1.sh
+++ shells/variant_calling/new/11-tests/hapflk/hapflk/hapflk_VQSR_chr01_1.sh
@@ -9,6 +9,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
 #SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/hapflk_VQSR_chr01_1.out
 #SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr01_1.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
