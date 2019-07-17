bd64eecba3ab074223362eced35e2ea0f8f3970c
melisakman <makman@berkeley.edu>
2019-07-17 15:38:57 -0700

    pi

--- shells/variant_calling/new/11-tests/pi/fix_missing_pi.py
+++ shells/variant_calling/new/11-tests/pi/fix_missing_pi.py
@@ -1,7 +1,7 @@
 import sys
-inputFile = sys.argv[1] # pi file as created by vcftools
+inputFile = sys.argv[1] # vcf file
 
-outputFile = sys.argv[2] # output file name w/o NA
+outputFile = sys.argv[2] # fixed vcf file
 
 
 file_in = open(inputFile, "r")
@@ -11,8 +11,10 @@ for line in file_in:
 	split = line.split("\t")
 	if split[0][0] == "#":
 		file_out.write(line)
+	
 	else:
-		for x in range(0,93):
+		file_out.write(split[0] + "\t" + split[1] + "\t" + split[2] + "\t" + split[3] + "\t" + split[4] + "\t" + split[5] + "\t" + split[6] + "\t" + split[7] + "\t" + split[8] + "\t")
+		for x in range(9,93):
 			item = split[x]
 			coding = item.split(":")
 			if x != 92:
