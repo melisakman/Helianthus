9c9ce41400a9ea0ddec3e521d13dc3015fc15c27
melisakman <makman@berkeley.edu>
2019-07-18 16:25:22 -0700

    lassi vcf

---
+++ shells/variant_calling/new/11-tests/lassi/lr_cv/prep_chr16.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -D /global/scratch/makman/GATK/final/lassi
+#SBATCH -J laschr16
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/lassi_prep_chr16_lrcv.out
+#SBATCH -e /global/home/users/makman/H12/outs/lassi_prep_chr16_lrcv.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+python /global/home/users/makman/git/shells/Helianthus/variant_calling/new/11-tests/lassi/vcf2genotype_lassi.py ../chr16_SNP.vcf ../cv_lr_list.txt chr16_SNP_cv_lr_lassi.txt  
