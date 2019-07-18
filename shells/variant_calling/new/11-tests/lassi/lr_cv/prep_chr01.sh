208fca23c6ea61e4dbca42c52df1e7d3f0254285
melisakman <makman@berkeley.edu>
2019-07-18 16:28:52 -0700

    lassi vcf

--- shells/variant_calling/new/11-tests/lassi/lr_cv/prep_chr01.sh
+++ shells/variant_calling/new/11-tests/lassi/lr_cv/prep_chr01.sh
@@ -5,8 +5,8 @@
 #SBATCH --partition=savio
 #SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=1
-#SBATCH --mem=64000
 #SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
 #SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/H12/outs/lassi_prep_chr01_lrcv.out
 #SBATCH -e /global/home/users/makman/H12/outs/lassi_prep_chr01_lrcv.err
