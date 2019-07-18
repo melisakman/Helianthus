9227ea90ac8e43bd442f41cbb166b4b62510dc44
melisakman <makman@berkeley.edu>
2019-07-17 23:36:11 -0700

    xp-ehh

---
+++ shells/variant_calling/new/11-tests/xp-ehh/1-prep/chr17.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -D /global/scratch/makman/GATK/final/xp-ehh
+#SBATCH -J beagle
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --cpus-per-task=12
+#SBATCH --time=72:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/xpehh_prep_chr17.out
+#SBATCH -e /global/home/users/makman/H12/outs/xpehh_prep_chr17.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+module load java
+java -Xmx64G -jar /global/scratch/makman/beagle/beagle.12Jul19.0df.jar gt=../chr17_final_fixed_forPi.vcf out=chr17_phased
