ec45fd7267fd5ec369dd0c477f1528fe75b33d35
melisakman <makman@berkeley.edu>
2019-07-23 21:02:28 -0700

    lassi

---
+++ shells/variant_calling/new/11-tests/lassi/lr_cv/lassi/lassi_chr09.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -D /global/scratch/makman/GATK/final/lassi
+#SBATCH -J laschr09
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --cpus-per-task=4
+#SBATCH --time=72:00:00
+#SBATCH -o /global/home/users/makman/H12/outs/lassi_prep_chr09.out
+#SBATCH -e /global/home/users/makman/H12/outs/lassi_prep_chr09.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load python/2.7
+module load numpy/1.13.3
+
+python LASSI_iterator.py initial 09 15 lassi_output_cv_lr_chr09 chr,_SNP_cv_lr_lassi_mlg.txt cv_lr cv_lr_headfile.txt 117 12 yes
+
+python LASSI_iterator.py rescan 09 20 lassi_output_cv_lr_chr09 chr,_SNP_cv_lr_lassi_mlg.txt cv_lr cv_lr_headfile.txt 117 12 yes
+
+
+# python LASSI_iterator.py neutavg 09 20 lassi_output_cv_lr_chr09 chr,_SNP_cv_lr_lassi_mlg.txt cv_lr 135414
+# 
+# python LASSI_iterator.py MLcalc 09 20 lassi_output_cv_lr_chr09 chr,_SNP_cv_lr_lassi_mlg.txt cv_lr 3
