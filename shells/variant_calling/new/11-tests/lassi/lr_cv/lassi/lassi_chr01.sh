be7b77424a3ac31ade16e08d0486046e6fedf9e7
melisakman <makman@berkeley.edu>
2019-07-23 22:06:19 -0700

    lassi

--- shells/variant_calling/new/11-tests/lassi/lr_cv/lassi/lassi_chr01.sh
+++ shells/variant_calling/new/11-tests/lassi/lr_cv/lassi/lassi_chr01.sh
@@ -6,8 +6,8 @@
 #SBATCH --qos=savio_normal
 #SBATCH --cpus-per-task=4
 #SBATCH --time=72:00:00
-#SBATCH -o /global/home/users/makman/H12/outs/lassi_prep_chr01.out
-#SBATCH -e /global/home/users/makman/H12/outs/lassi_prep_chr01.err
+#SBATCH -o /global/home/users/makman/H12/outs/lassi_prep_chr01_cv_lr.out
+#SBATCH -e /global/home/users/makman/H12/outs/lassi_prep_chr01_cv_lr.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
