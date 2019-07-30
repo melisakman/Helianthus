154dfd63b5d4d6d59291b9ec7dd5ff2941061a6f
melisakman <makman@berkeley.edu>
2019-07-29 17:22:53 -0700

    xp-ehh

--- shells/variant_calling/new/11-tests/xp-ehh/xp-ehh/chr01.R
+++ shells/variant_calling/new/11-tests/xp-ehh/xp-ehh/chr01.R
@@ -2,14 +2,14 @@
 library(rehh)
 
 setwd("/global/scratch/makman/GATK/final")
-lr <- data2haplohh("chr01_SNP_lr.vcf", min_perc_geno.mrk = 30)
-wd <- data2haplohh("chr01_SNP_wd.vcf", min_perc_geno.mrk = 30)
+lr <- data2haplohh("chr01_SNP_lr.vcf.recode.vcf", min_perc_geno.mrk = 30)
+wd <- data2haplohh("chr01_SNP_wd.vcf.recode.vcf", min_perc_geno.mrk = 30)
 
 analyses = ies2xpehh(lr, wd, popname1 = "landrace", popname2 = "wild")
 
 write.table(analyses, file = "chr01_lr_xpehh.txt", sep = "\t")
 
-cvlr <- data2haplohh("chr01_SNP_cv_lr.vcf", min_perc_geno.mrk = 30)
+cvlr <- data2haplohh("chr01_SNP_cv_lr.recode.vcf.vcf", min_perc_geno.mrk = 30)
 
 analyses = ies2xpehh(cvlr, wd, popname1 = "domesticates", popname2 = "wild")
 
