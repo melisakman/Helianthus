ca80f39bbccf26416798ba84fa3225d1865ebe9a
melisakman <makman@berkeley.edu>
2019-08-01 11:46:37 -0700

    hapflk

---
+++ shells/variant_calling/new/11-tests/hapflk/hapflk/failed_ones/hapflk_VQSR_chr11_15_2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -D /global/scratch/makman/GATK/final/hapflk/
+#SBATCH -J hfchr11_15
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12 
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr11_15.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --time=48:00:00
+module load hapflk/1.4
+hapflk --file chr11_final_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr 11 --from 70000001 --to 75000000 -p chr11_15 --ncpu 11 -K 15
---
+++ shells/variant_calling/new/11-tests/hapflk/hapflk/failed_ones/hapflk_VQSR_chr17_37_2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -D /global/scratch/makman/GATK/final/hapflk/
+#SBATCH -J hfchr17_37
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12 
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr17_37.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --time=48:00:00
+module load hapflk/1.4
+hapflk --file chr17_final_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr 17 --from 180000001 --to 185000000 -p chr17_37 --ncpu 11 -K 15
