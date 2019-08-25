edc0f61ee158b4d86b3a4d204d5ecdc50c205d8d
melisakman <makman@berkeley.edu>
2019-08-25 16:18:50 -0700

    hapflk chr01 failed

---
+++ shells/variant_calling/new/11-tests/hapflk/hapflk/failed_ones/create_failed/hapflk_VQSR_chr01_28_1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -D /global/scratch/makman/GATK/final/hapflk/
+#SBATCH -J hfchr01_28_1
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6 
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr01_28_1.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --time=6:00:00
+module load hapflk/1.4
+hapflk --file chr01_final_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr 01 --from 136000001 --to 137000000 -p chr01_28_1 --ncpu 6 -K 15
