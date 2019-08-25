31d55cab7beef7ee2ce775d30d249aa3f1d909fb
melisakman <makman@berkeley.edu>
2019-08-25 00:00:50 -0700

    failed

---
+++ shells/variant_calling/new/11-tests/hapflk/hapflk/failed_ones/create_failed/failed/failed/failed/create_failed/hapflk_VQSR_chr04_41_4_1_1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -D /global/scratch/makman/GATK/final/hapflk/
+#SBATCH -J hfchr04_41_4_1_1
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4 
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr04_41_4_1_1.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --time=6:00:00
+module load hapflk/1.4
+hapflk --file chr04_final_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr 04 --from 204750001 --to 205000000 -p chr04_41_4_1_1 --ncpu 4 -K 15
---
+++ shells/variant_calling/new/11-tests/hapflk/hapflk/failed_ones/create_failed/failed/failed/failed/create_failed/hapflk_VQSR_chr10_9_2_0_1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -D /global/scratch/makman/GATK/final/hapflk/
+#SBATCH -J hfchr10_9_2_0_1
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4 
+#SBATCH -e /global/home/users/makman/H12/outs/hapflk_VQSR_chr10_9_2_0_1.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+#SBATCH --time=6:00:00
+module load hapflk/1.4
+hapflk --file chr10_final_plink_lr_mex_ethno_fixed --miss_pheno 0 --chr 10 --from 42250001 --to 42500000 -p chr10_9_2_0_1 --ncpu 4 -K 15
