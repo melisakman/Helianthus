b2ee73545d972d1a224162049918cc1d973eb6c8
melisakman <makman@berkeley.edu>
2019-08-27 07:23:27 -0700

    xp-ehh

--- shells/variant_calling/new/11-tests/xp-ehh/xp-ehh/chr08.R
+++ shells/variant_calling/new/11-tests/xp-ehh/xp-ehh/chr08.R
@@ -10,22 +10,26 @@ phased = FALSE, polarized = FALSE, scalegap = NA, maxgap = NA,
 discard_integration_at_border = TRUE, lower_ehh_y_bound = 0.05,
 lower_ehhs_y_bound = 0.05, threads = 12)
 
+write.table(lr_scan, file = "chr08_lr_scanhh.txt", sep = "\t")
+
 wd_scan = scan_hh(wd, limhaplo = 2, limehh = 0.05, limehhs = 0.05,
 phased = FALSE, polarized = FALSE, scalegap = NA, maxgap = NA,
 discard_integration_at_border = TRUE, lower_ehh_y_bound = 0.05,
 lower_ehhs_y_bound = 0.05, threads = 12)
 
-analyses = ies2xpehh(lr_scan, wd_scan, popname1 = "landrace", popname2 = "wild", include_freq =TRUE)
+write.table(wd_scan, file = "chr08_wd_scanhh.txt", sep = "\t")
+
+#analyses = ies2xpehh(lr_scan, wd_scan, popname1 = "landrace", popname2 = "wild", include_freq =TRUE)
 
 write.table(analyses, file = "chr08_lr_xpehh.txt", sep = "\t")
 
-cvlr <- data2haplohh("chr08_SNP_cv_lr.vcf.recode.vcf", min_perc_geno.mrk = 30, polarize_vcf = FALSE)
- 
-cvlr_scan = scan_hh(cvlr, limhaplo = 2, limehh = 0.05, limehhs = 0.05,
-phased = FALSE, polarized = FALSE, scalegap = NA, maxgap = NA,
-discard_integration_at_border = TRUE, lower_ehh_y_bound = 0.05,
-lower_ehhs_y_bound = 0.05, threads = 12)
-
-analyses = ies2xpehh(cvlr_scan, wd_scan, popname1 = "domesticates", popname2 = "wild", include_freq =TRUE)
- 
-write.table(analyses, file = "chr08_cvlr_xpehh.txt", sep = "\t")
+# cvlr <- data2haplohh("chr08_SNP_cv_lr.vcf.recode.vcf", min_perc_geno.mrk = 30, polarize_vcf = FALSE)
+#  
+# cvlr_scan = scan_hh(cvlr, limhaplo = 2, limehh = 0.05, limehhs = 0.05,
+# phased = FALSE, polarized = FALSE, scalegap = NA, maxgap = NA,
+# discard_integration_at_border = TRUE, lower_ehh_y_bound = 0.05,
+# lower_ehhs_y_bound = 0.05, threads = 12)
+# 
+# analyses = ies2xpehh(cvlr_scan, wd_scan, popname1 = "domesticates", popname2 = "wild", include_freq =TRUE)
+#  
+# write.table(analyses, file = "chr08_cvlr_xpehh.txt", sep = "\t")
