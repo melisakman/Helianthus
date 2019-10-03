8e99a12f9121288cc592099eb502b1dc22912941
melisakman <makman@berkeley.edu>
2019-10-02 21:28:57 -0700

    ancestry

--- shells/ancestry/final/ancestry_final_dataStructure.sh
+++ shells/ancestry/final/ancestry_final_dataStructure.sh
@@ -13,21 +13,21 @@
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 module load python/2.7 
-python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr01_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr01_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr02_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr02_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr03_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr03_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr04_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr04_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr05_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr05_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr06_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr06_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr07_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr07_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr08_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr08_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr09_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr09_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr10_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr10_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr11_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr11_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr12_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr12_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr13_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr13_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr14_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr14_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr15_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr15_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr16_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr16_ancestry_dataStructure.txt
-# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr17_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr17_ancestry_dataStructure.txt
+# python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr01_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr01_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr02_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr02_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr03_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr03_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr04_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr04_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr05_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr05_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr06_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr06_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr07_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr07_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr08_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr08_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr09_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr09_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr10_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr10_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr11_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr11_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr12_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr12_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr13_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr13_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr14_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr14_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr15_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr15_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr16_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr16_ancestry_dataStructure.txt
+python /global/scratch/makman/ancestry/ancestry_informative_from_vcf_dataStructure.py chr17_SNP.vcf /global/scratch/makman/GATK/final/ancestry/chr17_ancestry_dataStructure.txt
 # 
