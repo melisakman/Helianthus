868a703af4998bfa0e139614042e43222bc7621f
melisakman <makman@berkeley.edu>
2019-10-13 21:51:34 -0700

    neutral extended blast

---
+++ shells/variant_calling/new/11-tests/blast/blast_neutralGenes_extended.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -D /global/scratch/makman/GATK/final/blast
+#SBATCH -J blast
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=10:00:00
+#SBATCH -o /global/home/users/makman/vcftools/outs/neutral_blast_extended.out
+#SBATCH -e /global/home/users/makman/vcftools/outs/neutral_blast_extended.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load blast
+# srun blastx -query /global/scratch/makman/GATK/final/neutral/100gene_CDs.txt -db /global/scratch/makman/GATK/final/blast/TAIR10_pep_20101214 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out neutral_genes2AT.txt
+
+# srun blastx -query /global/scratch/makman/GATK/final/neutral/all4plus_genes_CDs.txt -db /global/scratch/makman/GATK/final/blast/TAIR10_pep_20101214 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out all4plus_neutralgenes_2AT.txt
+
+srun blastx -query /global/scratch/makman/GATK/final/neutral/6plus_neutral_extended.txt -db /global/scratch/makman/GATK/final/blast/TAIR10_pep_20101214 -outfmt "7 qseqid qlen sseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps" -out 6plus_neutral_extended_2AT.txt
