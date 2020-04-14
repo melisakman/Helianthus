bfca06a0a1173ca9eaf18cf4df39e5dc74028b81
melisakman <makman@berkeley.edu>
2020-04-13 23:36:50 -0700

    paleo test

--- shells/iceplant/paleomix_test.sh
+++ shells/iceplant/paleomix_test.sh
@@ -2,12 +2,13 @@
 #SBATCH -D /global/scratch/makman/iceplant
 #SBATCH -J paleo
 #SBATCH --account=co_rosalind
-#SBATCH --partition=savio2_htc
-#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --partition=savio
+#SBATCH --qos=rosalind_savio_normal
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --time=40:00:00
+#SBATCH --mem=64000
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=400:00:00
 #SBATCH -o /global/home/users/makman/paloemix_outs/paleomix_test.out
 #SBATCH -e /global/home/users/makman/paloemix_outs/paleomix_test.err
 #SBATCH --mail-user=makman@berkeley.edu
