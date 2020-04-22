9839e242f3568f7664bd1df4b9dab938c6fbda41
melisakman <makman@berkeley.edu>
2020-04-22 16:58:17 -0700

    iceplant blast angsd

--- shells/iceplant/pcangsd.sh
+++ shells/iceplant/pcangsd.sh
@@ -6,7 +6,7 @@
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=28
+#SBATCH --cpus-per-task=24
 #SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/paloemix_outs/iceplant_angsd.out
 #SBATCH -e /global/home/users/makman/paloemix_outs/iceplant_angsd.err
@@ -14,7 +14,7 @@
 #SBATCH --mail-type=All
 
 
-/global/home/users/makman/angsd/angsd -bam 11good_samples_list.txt -out 11good_samples -nThreads 28 \
+/global/home/users/makman/angsd/angsd -bam 11good_samples_list.txt -out 11good_samples -nThreads 24 \
 -GL 1 -doGlf 2 \
 -doMajorMinor 1 -SNP_pval 1e-6 \
 -doMaf 1 -minMaf 0.05 \
