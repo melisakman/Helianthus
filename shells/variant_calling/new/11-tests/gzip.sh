1a4896f1fcdb9916e76a0d6b85bda7dca5d459d0
melisakman <makman@berkeley.edu>
2020-04-29 09:54:57 -0700

    sunflower zip

--- shells/variant_calling/new/11-tests/gzip.sh
+++ shells/variant_calling/new/11-tests/gzip.sh
@@ -1,29 +1,32 @@
 #!/bin/bash 
 #SBATCH -D /global/scratch/makman/GATK/final
-#SBATCH -J vcfTajD
-#SBATCH --account=co_rosalind
-#SBATCH --partition=savio
-#SBATCH --qos=rosalind_savio_normal
+#SBATCH -J zip
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2_htc
+#SBATCH --qos=rosalind_htc2_normal
 #SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=400:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/vcftools/outs/gzip1.out
 #SBATCH -e /global/home/users/makman/vcftools/outs/gzip1.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
-gzip chr04_final.vcf
-gzip chr05_final.vcf
-gzip chr06_final.vcf
-gzip chr07_final.vcf
-gzip chr08_final.vcf
-# gzip chr09_final.vcf
-# gzip chr10_final.vcf
-# gzip chr11_final.vcf
-# gzip chr12_final.vcf
-# gzip chr13_final.vcf
-# gzip chr14_final.vcf
-# gzip chr15_final.vcf
-# gzip chr16_final.vcf
-# gzip chr17_final.vcf
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr02_final.vcf > chr02_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr03_final.vcf > chr03_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr04_final.vcf > chr04_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr05_final.vcf > chr05_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr06_final.vcf > chr06_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr07_final.vcf > chr07_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr08_final.vcf > chr08_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr09_final.vcf > chr09_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr10_final.vcf > chr10_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr11_final.vcf > chr11_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr12_final.vcf > chr12_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr13_final.vcf > chr13_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr14_final.vcf > chr14_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr15_final.vcf > chr15_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr16_final.vcf > chr16_final.vcf.gz
+/clusterfs/rosalind/users/makman/tabix-0.2.6/bgzip -c chr17_final.vcf > chr17_final.vcf.gz
+
+cp *_final.vcf.gz /clusterfs/vector/instrumentData/blackmanlab/Helianthus/final_vcf/
