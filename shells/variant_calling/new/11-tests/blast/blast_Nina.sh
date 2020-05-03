8840d0a885f25d0128185d026657f0ee762ca3b2
melisakman <makman@berkeley.edu>
2020-05-03 16:17:38 -0700

    Nina blast

--- shells/variant_calling/new/11-tests/blast/blast_Nina.sh
+++ shells/variant_calling/new/11-tests/blast/blast_Nina.sh
@@ -2,13 +2,12 @@
 #SBATCH -D /global/scratch/makman/GATK/final/blast
 #SBATCH -J blast
 #SBATCH --account=fc_blackman
-#SBATCH --partition=savio2_htc
+#SBATCH --partition=savio2_bigmem
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
-#SBATCH --mem=64000
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --time=10:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --time=72:00:00
 #SBATCH -o /global/home/users/makman/vcftools/outs/Nina_blast.out
 #SBATCH -e /global/home/users/makman/vcftools/outs/Nina_blast.err
 #SBATCH --mail-user=makman@berkeley.edu
