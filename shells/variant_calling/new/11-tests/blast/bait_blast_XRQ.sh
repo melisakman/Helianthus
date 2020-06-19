32c252e7927e7a9d51ffd63450363b21f2ef0d7c
melisakman <makman@berkeley.edu>
2020-06-19 12:48:03 -0700

    bait blast

--- shells/variant_calling/new/11-tests/blast/bait_blast_XRQ.sh
+++ shells/variant_calling/new/11-tests/blast/bait_blast_XRQ.sh
@@ -6,8 +6,8 @@
 #SBATCH --qos=savio_normal
 #SBATCH --cpus-per-task=8
 #SBATCH --time=72:00:00
-#SBATCH -o /global/home/users/makman/vcftools/outs/neutral_5_blast.out
-#SBATCH -e /global/home/users/makman/vcftools/outs/neutral_5_blast.err
+#SBATCH -o /global/home/users/makman/vcftools/outs/bait_blast.out
+#SBATCH -e /global/home/users/makman/vcftools/outs/bait_blast.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
