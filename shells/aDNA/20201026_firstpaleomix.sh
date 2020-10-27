93c2084cff016aaf4483a69df3fb537d025d2d02
melisakman <makman@berkeley.edu>
2020-10-27 15:26:06 -0700

    aDNA sunflower

--- shells/aDNA/20201026_firstpaleomix.sh
+++ shells/aDNA/20201026_firstpaleomix.sh
@@ -19,13 +19,13 @@ module load samtools/1.6
 module load bwa/0.7.17-r1188
 module unload r/3.2.5
 module load r/3.4.2
-module unload gsl
-module load gsl/2.3-gcc
+# module unload gsl
+# module load gsl/2.3-gcc
 
-module load hdf5
-module load netcdf
-module load udunits
-module load openmpi
+# module load hdf5
+# module load netcdf
+# module load udunits
+# module load openmpi
 
 
 export PATH=/global/home/users/makman/install/jar_root/adapterremoval-2.3.1/build/:$PATH
