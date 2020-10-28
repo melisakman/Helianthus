b968bdcd47e6fc37f8da028ffc83ddf25ce9a380
melisakman <makman@berkeley.edu>
2020-10-27 21:41:41 -0700

    aDNA sunflower

---
+++ shells/aDNA/20201026_firstpaleomix8.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/aDNA/raw_data/
+#SBATCH -J paleo8
+#SBATCH --account=co_rosalind
+#SBATCH --partition=savio1
+#SBATCH --qos=rosalind_savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=72:00:00
+#SBATCH -o /global/home/users/makman/paloemix_outs/paleomix_run_aDNA8.out
+#SBATCH -e /global/home/users/makman/paloemix_outs/paleomix_run_aDNA8.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load paleomix/1.2.12
+module load mapDamage/2.08
+module load samtools/1.6
+module load bwa/0.7.17-r1188
+
+
+export PATH=/global/home/users/makman/install/jar_root/adapterremoval-2.3.1/build/:$PATH
+
+paleomix bam_pipeline run /global/home/users/makman/git/Helianthus/shells/aDNA/20201026_firstpaleomix8.yml --max-threads 20 --bwa-max-threads 5 --jre-option -Xmx16g --temp-root=/global/scratch/makman/temp/ --destination=/global/scratch/makman/Sunflower_paleomix/analyses
+
+
+
+
