9a83c76899d6d550ef6aba07af031348fba89dc3
Melis Akman <chrisbaysdorfer@A131522.local>
2021-01-20 12:12:09 -0800

    rescale

--- shells/aDNA/paleomix/20201026_firstpaleomix5.sh
+++ shells/aDNA/paleomix/20201026_firstpaleomix5.sh
@@ -25,7 +25,7 @@ paleomix bam_pipeline run /global/home/users/makman/git/Helianthus/shells/aDNA/p
 
 
 # the following line is for addition of the extra data.
-paleomix bam_pipeline run /global/home/users/makman/git/Helianthus/shells/aDNA/paleomix/20201026_firstpaleomix5_1.yml --max-threads 24 --bwa-max-threads 6 --jre-option -Xmx16g --temp-root=/global/scratch/makman/temp/ --destination=/global/scratch/makman/Sunflower_paleomix/analyses/additional_data
+# paleomix bam_pipeline run /global/home/users/makman/git/Helianthus/shells/aDNA/paleomix/20201026_firstpaleomix5_1.yml --max-threads 24 --bwa-max-threads 6 --jre-option -Xmx16g --temp-root=/global/scratch/makman/temp/ --destination=/global/scratch/makman/Sunflower_paleomix/analyses/additional_data
 
 
 
