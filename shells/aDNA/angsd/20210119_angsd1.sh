0521bd6a88522873082b5423217937793d3deffc
Melis Akman <makman@Meliss-MacBook-Pro.local>
2021-01-27 21:00:33 -0800

    seperately

--- shells/aDNA/angsd/20210119_angsd1.sh
+++ shells/aDNA/angsd/20210119_angsd1.sh
@@ -40,16 +40,41 @@ module load angsd/0.919-50-gc558037
 # doPOst 2 using uniform prior; 1 using frequency as prior; 3 using SFS as prior
 
 angsd -bam /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/bams_file.txt -nThreads ${SLURM_CPUS_PER_TASK} \
--out /global/scratch/makman/Sunflower_angsd/ \
--GL 2 -doGlf 2 \
+-out /global/scratch/makman/Sunflower_angsd/all_angsd \
+-GL 2 -doGlf 2 -SNP_pval 1e-6 \
 -checkBamHeaders 0 \
 -rf /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/regions_file.txt \
 -doMajorMinor 1 \
 -doMaf 1 -minMaf 0.01 \
 -minMapQ 30 -minQ 20 -doDepth 1 \
 -doCounts 1 -dumpCounts 2 -rmTrans 1 \
--doIBS 1 -doCov 1 \
+-doIBS 1 -makeMatrix 1 -doCov 1 \
 -doHaploCall 2 \
 -doPlink 2 -doGeno 55 -geno_minDepth -1 -postCutoff 0.95 -doPost 1 -doVcf 1
 
-#-SNP_pval 1e-6 \
+
+angsd -bam /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/bams_file.txt -nThreads ${SLURM_CPUS_PER_TASK} \
+-out /global/scratch/makman/Sunflower_angsd/dom_angsd \
+-GL 2 -doGlf 2 -SNP_pval 1e-6 \
+-checkBamHeaders 0 \
+-rf /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/dom_regions_file.txt \
+-doMajorMinor 1 \
+-doMaf 1 -minMaf 0.01 \
+-minMapQ 30 -minQ 20 -doDepth 1 \
+-doCounts 1 -dumpCounts 2 -rmTrans 1 \
+-doIBS 1 -makeMatrix 1 -doCov 1 \
+-doHaploCall 2 \
+-doPlink 2 -doGeno 55 -geno_minDepth -1 -postCutoff 0.95 -doPost 1 -doVcf 1
+
+angsd -bam /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/bams_file.txt -nThreads ${SLURM_CPUS_PER_TASK} \
+-out /global/scratch/makman/Sunflower_angsd/neut_angsd \
+-GL 2 -doGlf 2 -SNP_pval 1e-6 \
+-checkBamHeaders 0 \
+-rf /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/neut_regions_file.txt \
+-doMajorMinor 1 \
+-doMaf 1 -minMaf 0.01 \
+-minMapQ 30 -minQ 20 -doDepth 1 \
+-doCounts 1 -dumpCounts 2 -rmTrans 1 \
+-doIBS 1 -makeMatrix 1 -doCov 1 \
+-doHaploCall 2 \
+-doPlink 2 -doGeno 55 -geno_minDepth -1 -postCutoff 0.95 -doPost 1 -doVcf 1
