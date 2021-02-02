2fd1351334705f1e62cddc4373aa382922141a2f
Melis Akman <makman@Meliss-MacBook-Pro.local>
2021-02-01 23:17:26 -0800

    mandan1 removed

--- shells/aDNA/angsd/20210119_angsd1.sh
+++ shells/aDNA/angsd/20210119_angsd1.sh
@@ -54,18 +54,18 @@ module load angsd/0.919-50-gc558037
 
 
 
-# angsd -bam /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/bams_file.txt -nThreads ${SLURM_CPUS_PER_TASK} \
-# -out /global/scratch/makman/Sunflower_angsd/all_angsd \
-# -GL 2 -doGlf 2 -SNP_pval 1e-6 \
-# -checkBamHeaders 0 \
-# -rf /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/regions_file.txt \
-# -doMajorMinor 1 \
-# -doMaf 1 -minMaf 0.01 \
-# -minMapQ 30 -minQ 20 -doDepth 1 \
-# -doCounts 1 -dumpCounts 2 -rmTrans 1 \
-# -doIBS 1 -makeMatrix 1 -doCov 1 \
-# -doHaploCall 2 \
-# -doPlink 2 -doGeno 55 -geno_minDepth -1 -postCutoff 0.95 -doPost 1 -doVcf 1
+angsd -bam /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/bams_file.txt -nThreads ${SLURM_CPUS_PER_TASK} \
+-out /global/scratch/makman/Sunflower_angsd/all_angsd \
+-GL 2 -doGlf 2 -SNP_pval 1e-6 \
+-checkBamHeaders 0 \
+-rf /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/regions_file.txt \
+-doMajorMinor 1 \
+-doMaf 1 -minMaf 0.01 \
+-minMapQ 30 -minQ 20 -doDepth 1 \
+-doCounts 1 -dumpCounts 2 -rmTrans 1 \
+-doIBS 1 -makeMatrix 1 -doCov 1 \
+-doHaploCall 2 \
+-doPlink 2 -doGeno 55 -geno_minDepth -1 -postCutoff 0.95 -doPost 1 -doVcf 1
 
 
 angsd -bam /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/bams_file.txt -nThreads ${SLURM_CPUS_PER_TASK} \
