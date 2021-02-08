1dc052fc4916a1b90fc1418ef82f00fad9269c55
Melis Akman <makman@Meliss-MacBook-Pro.local>
2021-02-08 14:33:58 -0800

    pegas

--- shells/aDNA/pegas/pegas_haplotype_network.sh
+++ shells/aDNA/pegas/pegas_haplotype_network.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH -D /global/scratch/makman/Sunflower_paleomix/analyses/
+#SBATCH -D /global/scratch/makman/aDNA/pegas/
 #SBATCH -J haplonet
 #SBATCH --account=fc_blackman
 #SBATCH --partition=savio2
@@ -8,8 +8,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --time=72:00:00
-#SBATCH -o /global/home/users/makman/paloemix_outs/angsd_1.out
-#SBATCH -e /global/home/users/makman/paloemix_outs/angsd_1.err
+#SBATCH -o /global/home/users/makman/paloemix_outs/pegas_1.out
+#SBATCH -e /global/home/users/makman/paloemix_outs/pegas_1.err
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH --mail-type=All
 
@@ -19,15 +19,14 @@
 
 export PERL5LIB=/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/vcftools/0.1.13/perl/
 export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/global/home/groups/allhands/bin:/global/home/users/makman/bin:/global/home/users/makman/angsd/liblzma/:/global/home/users/makman/angsd/liblzma/include/lzma/:/clusterfs/vector/scratch/makman/tabix-0.2.6/
-module load samtools/1.3.1
-module load vcftools/0.1.13
+module load samtools
+module load bio/vcftools/0.1.15
 
 module load java
 module load r/3.2.5
-module load ghostscript/9.04
 
 # Count number of genes in bed file and add 1 for loop  ***** CHANGE BED FILE IN NEXT LINE *******
-Total_lines=`grep -c "^" regions_bed.txt`
+Total_lines=`grep -c "^" /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/regions_bed.txt`
 Total_lines=`expr $Total_lines + 1`
 
 # Loops through each line of the bed file and does phasing for the gene
@@ -35,7 +34,7 @@ Line_to_read=1
 while [ $Line_to_read -lt $Total_lines ]
 do
    # ********* CHANGE BED FILE IN NEXT LINE (do not modify "Current_gene_tested.bed") ********
-  sed "${Line_to_read}q;d" regions_bed.txt > Current_gene_tested.bed;
+  sed "${Line_to_read}q;d" /global/home/users/makman/git/Helianthus/shells/aDNA/angsd/regions_bed.txt > Current_gene_tested.bed;
   Line_to_read=`expr $Line_to_read + 1`
   
 	var_current_gene_chr=`cut -f 1 Current_gene_tested.bed` 
@@ -47,10 +46,10 @@ do
 	
 	# Make VCF file for gene to speed things up 
 	srun vcftools --vcf /global/scratch/makman/Sunflower_angsd/all_angsd.vcf.gz --chr $var_current_gene_chr --from-bp $var_current_gene_start --to-bp $var_current_gene_end --recode --out ${var_current_gene_name}
- 	mv ${var_current_gene_name}.recode.vcf /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_per_gene/${var_current_gene_name}.vcf
+ 	mv ${var_current_gene_name}.recode.vcf /global/scratch/makman/aDNA/pegas/Haplo_by_gene/VCF_per_gene/${var_current_gene_name}.vcf
 
 	# Make a loop to process each BAM file individually
-	for i in $( ls /clusterfs/vector/scratch/makman/haplotype_networks/bams/bam_recal/*2W*.bam ); do
+	for i in $( ls /global/scratch/makman/aDNA/all_bams/*.bam ); do
 	Var_current_path=${i}
 	Var_current_file=`basename $Var_current_path`
 	Var_current_sample=${Var_current_file/_dedup_recal.bam/}
@@ -58,22 +57,22 @@ do
 	srun samtools view -hb -@ 19 $Var_current_path ${var_current_gene_chr}:${var_lower_flanking}-${var_upper_flanking} -o ../Haplo_by_gene/BAMs_for_gene/Reads_${var_current_gene_name}_${Var_current_sample}.bam
 	srun samtools index ../Haplo_by_gene/BAMs_for_gene/Reads_${var_current_gene_name}_${Var_current_sample}.bam
 # 
-java -Djava.io.tmpdir=/clusterfs/vector/scratch/makman/temp1 -Xmx32G -jar /clusterfs/vector/scratch/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
+java -Djava.io.tmpdir=/global/scratch/makman/temp -Xmx32G -jar /global/scratch/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
      -T HaplotypeCaller \
      -nct 20 \
-     -R /clusterfs/vector/scratch/makman/haplotype_networks/HanXRQr1.0-20151230_no_Chr00.fasta \
-     -I /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/BAMs_for_gene/Reads_${var_current_gene_name}_${Var_current_sample}.bam \
+     -R /global/scratch/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
+     -I /global/scratch/makman/aDNA/pegas/Haplo_by_gene/BAMs_for_gene/Reads_${var_current_gene_name}_${Var_current_sample}.bam \
      -L Current_gene_tested.bed \
      --emitRefConfidence GVCF \
-     --dbsnp /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/VCF_per_gene/${var_current_gene_name}_original.vcf \
-     -o /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/gVCF_samples/raw.snps.indels_${var_current_gene_name}_${Var_current_sample}.g.vcf
+     --dbsnp /global/scratch/makman/aDNA/pegas/Haplo_by_gene/VCF_per_gene/${var_current_gene_name}_original.vcf \
+     -o /global/scratch/makman/aDNA/pegas/Haplo_by_gene/gVCFs/raw.snps.indels_${var_current_gene_name}_${Var_current_sample}.g.vcf
 
 	done
 
 # Join the gVCF for the samples.  
 # java -Djava.io.tmpdir=/clusterfs/vector/scratch/makman/temp2 -Xmx32G -jar /clusterfs/vector/scratch/makman/GenomeAnalysisTK-3.7-0/GenomeAnalysisTK.jar \
 #    -T GenotypeGVCFs \
-#    -R /clusterfs/vector/scratch/makman/haplotype_networks/HanXRQr1.0-20151230_no_Chr00.fasta \
+#    -R /global/scratch/makman/HanXRQr2/HanXRQr2.0-SUNRISE-2.1.genome.fasta \
 #    --variant /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/gVCF_samples/raw.snps.indels_${var_current_gene_name}_Anzac_Pueblo.g.vcf \
 #    --variant /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/gVCF_samples/raw.snps.indels_${var_current_gene_name}_Arikara.g.vcf \
 #    --variant /clusterfs/vector/scratch/makman/haplotype_networks/Haplo_by_gene/gVCF_samples/raw.snps.indels_${var_current_gene_name}_Havasupai.g.vcf \
