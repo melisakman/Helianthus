a915e89b8bee5a0fae39895b36074c7302370896
Melis Akman <makman@Meliss-MacBook-Pro.local>
2021-02-23 20:42:26 -0800

    capture shotgun ngsadmix k1

--- shells/aDNA/angsd/ngsadmix.sh
+++ shells/aDNA/angsd/ngsadmix.sh
@@ -15,39 +15,50 @@
 
 module load angsd/0.919-50-gc558037
 
+NGSadmix -likes dom_angsd.beagle.gz -K 1 -P ${SLURM_CPUS_PER_TASK} -o dom_K1_ngsadmix -minMaf 0.01 -printInfo 1
+
 # NGSadmix -likes dom_angsd.beagle.gz -K 2 -P ${SLURM_CPUS_PER_TASK} -o dom_K2_ngsadmix -minMaf 0.01 -printInfo 1
 # NGSadmix -likes dom_angsd.beagle.gz -K 3 -P ${SLURM_CPUS_PER_TASK} -o dom_K3_ngsadmix -minMaf 0.01 -printInfo 1
 # NGSadmix -likes dom_angsd.beagle.gz -K 4 -P ${SLURM_CPUS_PER_TASK} -o dom_K4_ngsadmix -minMaf 0.01 -printInfo 1
 # NGSadmix -likes dom_angsd.beagle.gz -K 5 -P ${SLURM_CPUS_PER_TASK} -o dom_K5_ngsadmix -minMaf 0.01 -printInfo 1
 # NGSadmix -likes dom_angsd.beagle.gz -K 6 -P ${SLURM_CPUS_PER_TASK} -o dom_K6_ngsadmix -minMaf 0.01 -printInfo 1
 # 
+NGSadmix -likes neut_angsd.beagle.gz -K 1 -P ${SLURM_CPUS_PER_TASK} -o neut_K1_ngsadmix -minMaf 0.01 -printInfo 1
+
 # NGSadmix -likes neut_angsd.beagle.gz -K 2 -P ${SLURM_CPUS_PER_TASK} -o neut_K2_ngsadmix -minMaf 0.01 -printInfo 1
 # NGSadmix -likes neut_angsd.beagle.gz -K 3 -P ${SLURM_CPUS_PER_TASK} -o neut_K3_ngsadmix -minMaf 0.01 -printInfo 1
 # NGSadmix -likes neut_angsd.beagle.gz -K 4 -P ${SLURM_CPUS_PER_TASK} -o neut_K4_ngsadmix -minMaf 0.01 -printInfo 1
 # NGSadmix -likes neut_angsd.beagle.gz -K 5 -P ${SLURM_CPUS_PER_TASK} -o neut_K5_ngsadmix -minMaf 0.01 -printInfo 1
 # NGSadmix -likes neut_angsd.beagle.gz -K 6 -P ${SLURM_CPUS_PER_TASK} -o neut_K6_ngsadmix -minMaf 0.01 -printInfo 1
 
+NGSadmix -likes dom_angsd_capture.beagle.gz -K 1 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_K1_ngsadmix -minMaf 0.01 -printInfo 1
 
-NGSadmix -likes dom_angsd_capture.beagle.gz -K 2 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_K2_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes dom_angsd_capture.beagle.gz -K 3 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_K3_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes dom_angsd_capture.beagle.gz -K 4 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_K4_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes dom_angsd_capture.beagle.gz -K 5 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_K5_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes dom_angsd_capture.beagle.gz -K 6 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_K6_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes dom_angsd_capture.beagle.gz -K 2 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_K2_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes dom_angsd_capture.beagle.gz -K 3 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_K3_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes dom_angsd_capture.beagle.gz -K 4 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_K4_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes dom_angsd_capture.beagle.gz -K 5 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_K5_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes dom_angsd_capture.beagle.gz -K 6 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_K6_ngsadmix -minMaf 0.01 -printInfo 1
+# 
+NGSadmix -likes neut_angsd_capture.beagle.gz -K 1 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_K1_ngsadmix -minMaf 0.01 -printInfo 1
 
-NGSadmix -likes neut_angsd_capture.beagle.gz -K 2 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_K2_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes neut_angsd_capture.beagle.gz -K 3 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_K3_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes neut_angsd_capture.beagle.gz -K 4 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_K4_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes neut_angsd_capture.beagle.gz -K 5 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_K5_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes neut_angsd_capture.beagle.gz -K 6 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_K6_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes neut_angsd_capture.beagle.gz -K 2 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_K2_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes neut_angsd_capture.beagle.gz -K 3 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_K3_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes neut_angsd_capture.beagle.gz -K 4 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_K4_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes neut_angsd_capture.beagle.gz -K 5 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_K5_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes neut_angsd_capture.beagle.gz -K 6 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_K6_ngsadmix -minMaf 0.01 -printInfo 1
+# 
+NGSadmix -likes dom_angsd_capture_shotgun_ethno.beagle.gz -K 1 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_shotgun_ethno_K1_ngsadmix -minMaf 0.01 -printInfo 1
 
-NGSadmix -likes dom_angsd_capture_shotgun_ethno.beagle.gz -K 2 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_shotgun_ethno_K2_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes dom_angsd_capture_shotgun_ethno.beagle.gz -K 3 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_shotgun_ethno_K3_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes dom_angsd_capture_shotgun_ethno.beagle.gz -K 4 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_shotgun_ethno_K4_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes dom_angsd_capture_shotgun_ethno.beagle.gz -K 5 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_shotgun_ethno_K5_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes dom_angsd_capture_shotgun_ethno.beagle.gz -K 6 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_shotgun_ethno_K6_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes dom_angsd_capture_shotgun_ethno.beagle.gz -K 2 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_shotgun_ethno_K2_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes dom_angsd_capture_shotgun_ethno.beagle.gz -K 3 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_shotgun_ethno_K3_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes dom_angsd_capture_shotgun_ethno.beagle.gz -K 4 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_shotgun_ethno_K4_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes dom_angsd_capture_shotgun_ethno.beagle.gz -K 5 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_shotgun_ethno_K5_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes dom_angsd_capture_shotgun_ethno.beagle.gz -K 6 -P ${SLURM_CPUS_PER_TASK} -o dom_capture_shotgun_ethno_K6_ngsadmix -minMaf 0.01 -printInfo 1
+# 
+NGSadmix -likes neut_angsd_capture.beagle_shotgun_ethno.gz -K 1 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_shotgun_ethno_K1_ngsadmix -minMaf 0.01 -printInfo 1
 
-NGSadmix -likes neut_angsd_capture.beagle_shotgun_ethno.gz -K 2 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_shotgun_ethno_K2_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes neut_angsd_capture.beagle_shotgun_ethno.gz -K 3 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_shotgun_ethno_K3_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes neut_angsd_capture.beagle_shotgun_ethno.gz -K 4 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_shotgun_ethno_K4_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes neut_angsd_capture.beagle_shotgun_ethno.gz -K 5 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_shotgun_ethno_K5_ngsadmix -minMaf 0.01 -printInfo 1
-NGSadmix -likes neut_angsd_capture.beagle_shotgun_ethno.gz -K 6 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_shotgun_ethno_K6_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes neut_angsd_capture.beagle_shotgun_ethno.gz -K 2 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_shotgun_ethno_K2_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes neut_angsd_capture.beagle_shotgun_ethno.gz -K 3 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_shotgun_ethno_K3_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes neut_angsd_capture.beagle_shotgun_ethno.gz -K 4 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_shotgun_ethno_K4_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes neut_angsd_capture.beagle_shotgun_ethno.gz -K 5 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_shotgun_ethno_K5_ngsadmix -minMaf 0.01 -printInfo 1
+# NGSadmix -likes neut_angsd_capture.beagle_shotgun_ethno.gz -K 6 -P ${SLURM_CPUS_PER_TASK} -o neut_capture_shotgun_ethno_K6_ngsadmix -minMaf 0.01 -printInfo 1
