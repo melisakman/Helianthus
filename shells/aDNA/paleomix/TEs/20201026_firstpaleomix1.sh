7116d1e8afc10ff67514e8546e7d6ec0b93d437d
Melis Akman <makman@Meliss-MacBook-Pro.local>
2021-04-07 22:34:11 -0700

    CKX

---
+++ shells/aDNA/paleomix/TEs/20201026_firstpaleomix1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/aDNA/raw_data/
+#SBATCH -J paleo1
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --time=72:00:00
+#SBATCH -o /global/home/users/makman/paloemix_outs/paleomix_run_aDNA1b_TE.out
+#SBATCH -e /global/home/users/makman/paloemix_outs/paleomix_run_aDNA1b_TE.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load paleomix/1.2.12
+module load mapDamage/2.08
+module load samtools/1.6
+module load bwa/0.7.17-r1188
+
+
+export PATH=/global/home/users/makman/install/jar_root/adapterremoval-2.3.1/build/:$PATH
+
+paleomix bam_pipeline run /global/home/users/makman/git/Helianthus/shells/aDNA/paleomix/20201026_firstpaleomix1.yml --max-threads 24 --bwa-max-threads 6 --jre-option -Xmx16g --temp-root=/global/scratch/makman/temp/ --destination=/global/scratch/makman/Sunflower_paleomix/analyses/TEs
+
+
+
+
---
+++ shells/aDNA/paleomix/TEs/20201026_firstpaleomix1_4.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+#SBATCH -D /global/scratch/makman/aDNA/raw_data/
+#SBATCH -J paleo1_4
+#SBATCH --account=fc_blackman
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --time=72:00:00
+#SBATCH -o /global/home/users/makman/paloemix_outs/paleomix_run_aDNA1_4b_TE.out
+#SBATCH -e /global/home/users/makman/paloemix_outs/paleomix_run_aDNA1_4b_TE.err
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH --mail-type=All
+
+module load paleomix/1.2.12
+module load mapDamage/2.08
+module load samtools/1.6
+module load bwa/0.7.17-r1188
+
+
+export PATH=/global/home/users/makman/install/jar_root/adapterremoval-2.3.1/build/:$PATH
+
+paleomix bam_pipeline run /global/home/users/makman/git/Helianthus/shells/aDNA/paleomix/20201026_firstpaleomix1_4.yml --max-threads 24 --bwa-max-threads 24 --jre-option -Xmx64g --temp-root=/global/scratch/makman/temp/ --destination=/global/scratch/makman/Sunflower_paleomix/analyses/TEs
+
+
+
+
